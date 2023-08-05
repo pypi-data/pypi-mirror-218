b11433d9d0a239f56ff19746998878089f7e612a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:32 +0000

    Add package letsgo/5.2.0/letsgo-5.2.0.tar.gz

---
+++ packages/letsgo/letsgo-5.2.0.tar.gz/letsgo-5.2.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="letsgo",
+    version="5.2.0",
+    author="zlge",
+    author_email="andysoftwareexp.co@qq.com",
+    description="",
+    long_description="test Module",
+    long_description_content_type="text",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
