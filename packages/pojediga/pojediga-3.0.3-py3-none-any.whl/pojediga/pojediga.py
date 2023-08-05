b965cf3a35eb3ab969b1f1a3b717f311cd3a234c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package pojediga/3.0.3/pojediga-3.0.3-py3-none-any.whl

---
+++ packages/pojediga/pojediga-3.0.3-py3-none-any.whl/pojediga/pojediga.py
@@ -0,0 +1,7 @@
+class PojediGa:
+    def pojediga(self, name: str) -> str:
+        return f'Pojeo sam ga {name} sine.'
+    def Masala(self) -> str:
+        return 'مصلحت'
+    def Noga(self, name: str):
+        return f'The foot has penetrated deeply in {name}.'
