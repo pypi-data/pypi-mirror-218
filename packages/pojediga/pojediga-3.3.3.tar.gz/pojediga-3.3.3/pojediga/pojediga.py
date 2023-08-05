e7e031274c9f116fb53ee2c7b261f84a9918a28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:57 +0000

    Add package pojediga/3.3.3/pojediga-3.3.3.tar.gz

---
+++ packages/pojediga/pojediga-3.3.3.tar.gz/pojediga-3.3.3/pojediga/pojediga.py
@@ -0,0 +1,7 @@
+class PojediGa:
+    def pojediga(name: str) -> str:
+        return f'Pojeo sam ga {name} sine.'
+    def Masala() -> str:
+        return 'مصلحت'
+    def Noga(name: str):
+        return f'The foot has penetrated deeply in {name}.'
