c7a0d7452c162f11cd7e43b8a12e2d706b954bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package pytelibs/0.1.2/pytelibs-0.1.2.tar.gz

---
+++ packages/pytelibs/pytelibs-0.1.2.tar.gz/pytelibs-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pytelibs import __version__
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as rd:
+    long_description = rd.read()
+
+setup(
+    name="pytelibs",
+    version=__version__,
+    author="Unknown",
+    author_email="unknownkz@outlook.co.id",
+    description="Client library for Pytel",
+    url="https://github.com/kastaid/pytelibs",
+    license="AGPL",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pyrogram', 'tgcrypto'],
+    keywords=['pypi', 'pytelibs', 'python', 'pyrogram', 'telebot'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
+
