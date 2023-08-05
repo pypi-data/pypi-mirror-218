9d745fad95c05bd687f0634abd785a567179f808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:43 +0000

    Add package django-hint/0.3.2/django_hint-0.3.2-py3-none-any.whl

---
+++ packages/django-hint/django_hint-0.3.2-py3-none-any.whl/django_hint/__init__.py
@@ -0,0 +1,27 @@
+from .typehint import QueryType, QueryFilter, RequestType, DRFTokenRequestType, StandardModelType, List, Optional, Union, Deque, Dict, DefaultDict, FrozenSet, ChainMap, Counter, Set
+from .typehint import Generic, Callable, Tuple, TypeVar, Type, ClassVar
+__all__ = [
+    "QueryType",
+    "RequestType",
+    "DRFTokenRequestType",
+    "QueryFilter",
+    "StandardModelType",
+    "List",
+    "Optional",
+    "Union",
+    "Dict",
+    "DefaultDict",
+    "Set",
+    "FrozenSet",
+    "Counter",
+    "Deque",
+    "ChainMap",
+    'Generic',
+    'Callable',
+    'Tuple',
+    'TypeVar',
+    'Type',
+    'ClassVar'
+]
+
+
