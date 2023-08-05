00fd9df9692bb4b509b307cb51af832a8db34f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:55 +0000

    Add package aider-chat/0.8.0/aider-chat-0.8.0.tar.gz

---
+++ packages/aider-chat/aider-chat-0.8.0.tar.gz/aider-chat-0.8.0/aider/utils.py
@@ -0,0 +1,19 @@
+from .dump import dump  # noqa: F401
+
+
+def show_messages(messages, title=None, functions=None):
+    if title:
+        print(title.upper(), "*" * 50)
+
+    for msg in messages:
+        role = msg["role"].upper()
+        content = msg.get("content")
+        if content:
+            for line in content.splitlines():
+                print(role, line)
+        content = msg.get("function_call")
+        if content:
+            print(role, content)
+
+    if functions:
+        dump(functions)
---
+++ packages/aider-chat/aider-chat-0.8.0.tar.gz/aider-chat-0.8.0/tests/test_commands.py
@@ -0,0 +1,79 @@
+import codecs
+import os
+import shutil
+import sys
+import tempfile
+from io import StringIO
+from unittest import TestCase
+
+from aider import models
+from aider.coders import Coder
+from aider.commands import Commands
+from aider.dump import dump  # noqa: F401
+from aider.io import InputOutput
+
+
+class TestCommands(TestCase):
+    def setUp(self):
+        self.original_cwd = os.getcwd()
+        self.tempdir = tempfile.mkdtemp()
+        os.chdir(self.tempdir)
+
+    def tearDown(self):
+        os.chdir(self.original_cwd)
+        shutil.rmtree(self.tempdir)
+
+    def test_cmd_add(self):
+        # Initialize the Commands and InputOutput objects
+        io = InputOutput(pretty=False, yes=True)
+        from aider.coders import Coder
+
+        coder = Coder.create(models.GPT35, None, io, openai_api_key="deadbeef")
+        commands = Commands(io, coder)
+
+        # Call the cmd_add method with 'foo.txt' and 'bar.txt' as a single string
+        commands.cmd_add("foo.txt bar.txt")
+
+        # Check if both files have been created in the temporary directory
+        self.assertTrue(os.path.exists("foo.txt"))
+        self.assertTrue(os.path.exists("bar.txt"))
+
+    def test_cmd_add_bad_encoding(self):
+        # Initialize the Commands and InputOutput objects
+        io = InputOutput(pretty=False, yes=True)
+        from aider.coders import Coder
+
+        coder = Coder.create(models.GPT35, None, io, openai_api_key="deadbeef")
+        commands = Commands(io, coder)
+
+        # Create a new file foo.bad which will fail to decode as utf-8
+        with codecs.open("foo.bad", "w", encoding="iso-8859-15") as f:
+            f.write("ÆØÅ")  # Characters not present in utf-8
+
+        commands.cmd_add("foo.bad")
+
+        self.assertEqual(coder.abs_fnames, set())
+
+    def test_cmd_tokens(self):
+        # Initialize the Commands and InputOutput objects
+        io = InputOutput(pretty=False, yes=True)
+
+        coder = Coder.create(models.GPT35, None, io, openai_api_key="deadbeef")
+        commands = Commands(io, coder)
+
+        commands.cmd_add("foo.txt bar.txt")
+
+        # Redirect the standard output to an instance of io.StringIO
+        stdout = StringIO()
+        sys.stdout = stdout
+
+        commands.cmd_tokens("")
+
+        # Reset the standard output
+        sys.stdout = sys.__stdout__
+
+        # Get the console output
+        console_output = stdout.getvalue()
+
+        self.assertIn("foo.txt", console_output)
+        self.assertIn("bar.txt", console_output)
