00edd89be627ef27aadffb2f2177f8b30905c3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:33 +0000

    Add package tmmc-lnpy/0.5.0/tmmc-lnpy-0.5.0.tar.gz

---
+++ packages/tmmc-lnpy/tmmc-lnpy-0.5.0.tar.gz/tmmc-lnpy-0.5.0/src/lnpy/examples.py
@@ -0,0 +1,209 @@
+"""
+Examples (:mod:`~lnpy.examples`)
+================================
+"""
+
+try:
+    import importlib_resources as resources
+except ImportError:
+    import importlib.resources as resources
+
+import json
+from collections.abc import Callable
+from dataclasses import asdict, dataclass
+
+from ._lazy_imports import np, xr
+
+# from .lnpiseries import lnPiCollection
+from .lnpidata import lnPiMasked
+from .segment import PhaseCreator
+from .utils import dataset_to_lnpimasked
+
+
+def json_to_dict(basename):
+    """
+    Load a json file into a dict.
+
+    All files names are relative to 'lnpy/data/'
+
+    Parameters
+    ----------
+    basename : string
+        Name of file to load in 'lnpy/data' directory.
+
+    Returns
+    -------
+    output : dict
+
+    """
+
+    if basename.endswith(".gz"):
+        import gzip
+
+        fopen = gzip.open
+    else:
+        fopen = open
+
+    with fopen(resources.files("lnpy.data").joinpath(basename), "r") as f:
+        out = json.load(f)
+
+    return out
+
+
+def load_example_dict(name):
+    """
+    Load a dictionary of data
+
+    Parameters
+    ----------
+    name : {'lj_sub', 'lj_sup', 'ljmix_sup', 'hsmix', 'watermof}
+    """
+
+    ref = load_example_lnpimasked(name)
+
+    return {
+        "lnPi_data": ref.data,
+        "lnPi_mask": ref.mask,
+        "state_kws": ref.state_kws,
+        "extra_kws": ref.extra_kws,
+        "lnz": ref.lnz,
+    }
+
+
+def load_example_lnpimasked(name):
+    """
+    Load an example file
+
+    Parameters
+    ----------
+    name : {'lj_sub', 'lj_sup', 'ljmix_sup', 'hsmix', 'watermof}
+    """
+
+    extensions = {
+        "lj_sub": "json",
+        "lj_sup": "json",
+        "ljmix_sup": "json.gz",
+        "hsmix": "json.gz",
+        "watermof": "json",
+    }
+
+    assert name in extensions
+
+    basename = f"{name}_example.{extensions[name]}"
+
+    d = json_to_dict(basename)
+    ds = xr.Dataset.from_dict(d)
+
+    ref = dataset_to_lnpimasked(ds)
+    return ref
+
+
+@dataclass
+class Example:
+    """Dataclass to hold example data."""
+
+    #: Reference state :class:`~lnpy.lnpidata.lnPiMasked`.
+    ref: lnPiMasked
+    #: :class:`~lnpy.segment.PhaseCreator` instance
+    phase_creator: PhaseCreator
+    #: Callable to build phases.
+    build_phases: Callable
+
+    def to_dict(self):
+        """Transform class to dictionary."""
+        return asdict(self)
+
+    def unpack(self, keys=None):
+        """Unpack keys."""
+        if keys is None:
+            keys = ["ref", "phase_creator", "build_phases"]
+        return (getattr(self, k) for k in keys)
+
+
+def lj_sup_example():
+    """Create an :class:`Example` instance for a Lennard-Jones fluid (subcritical)"""
+    ref = load_example_lnpimasked("lj_sup")
+
+    phase_creator = PhaseCreator(
+        nmax=1,
+        nmax_peak=1,
+        ref=ref,
+        merge_kws={"efac": 0.8},
+        tag_phases=None,
+    )
+
+    build_phases = phase_creator.build_phases_mu([None])
+
+    return Example(ref=ref, phase_creator=phase_creator, build_phases=build_phases)
+
+
+def tag_phases_single_comp_simple(x):
+    if len(x) > 2:
+        raise ValueError("bad tag function")
+    argmax0 = np.array([xx.local_argmax()[0] for xx in x])
+    return np.where(argmax0 <= x[0].shape[0] / 2, 0, 1)
+
+
+def lj_sub_example():
+    """Create an :class:`Example` instance for a Lennard-Jones fluid (subcritical)"""
+    ref = load_example_lnpimasked("lj_sub")
+
+    phase_creator = PhaseCreator(
+        nmax=2,
+        nmax_peak=4,
+        ref=ref,
+        merge_kws={"efac": 0.8},
+        tag_phases=tag_phases_single_comp_simple,
+    )
+
+    build_phases = phase_creator.build_phases_mu([None])
+
+    return Example(ref=ref, phase_creator=phase_creator, build_phases=build_phases)
+
+
+def ljmix_sup_example():
+    """Create an :class:`Example` instance for a Lennard-Jones mixture (supercritical)."""
+    ref = load_example_lnpimasked("ljmix_sup")
+
+    phase_creator = PhaseCreator(
+        nmax=1,
+        ref=ref,
+    )
+
+    build_phases = phase_creator.build_phases
+    return Example(ref=ref, phase_creator=phase_creator, build_phases=build_phases)
+
+
+def hsmix_example():
+    """Create an :class:`Example` instance for a hard-sphere mixture."""
+
+    def tag_phases(x):
+        if len(x) > 2:
+            raise ValueError("bad tag function")
+        argmax0 = np.array([xx.local_argmax()[0] for xx in x])
+        return np.where(argmax0 <= x[0].shape[0] / 2, 0, 1)
+
+    ref = load_example_lnpimasked("hsmix")
+
+    phase_creator = PhaseCreator(
+        nmax=2, nmax_peak=4, ref=ref, tag_phases=tag_phases, merge_kws={"efac": 0.8}
+    )
+
+    return Example(ref=ref, phase_creator=phase_creator, build_phases=None)
+
+
+# def watermof_example():
+#     def tag_phases(x):
+#         if len(x) > 2:
+#             raise ValueError("bad tag function")
+#         argmax0 = np.array([xx.local_argmax()[0] for xx in x])
+#         return np.where(argmax0 <= x[0].shape[0] / 2, 0, 1)
+
+#     ref = load_example_lnpimasked('watermof')
+
+#     phase_creator = PhaseCreator(
+#         nmax=2,
+#         nmax_peak=10,
+#         merge_kws={'efac': 0.8}
+
+#     )
---
+++ packages/tmmc-lnpy/tmmc-lnpy-0.5.0.tar.gz/tmmc-lnpy-0.5.0/src/lnpy/utils.py
@@ -0,0 +1,577 @@
+"""
+Utility functions (:mod:`~lnpy.utils`)
+======================================
+"""
+
+from functools import lru_cache, partial
+
+from ._lazy_imports import np
+from .options import OPTIONS
+
+
+# --- TQDM setup -----------------------------------------------------------------------
+@lru_cache
+def _get_tqdm():
+    try:
+        import tqdm
+    except ImportError:
+        tqdm = None
+    return tqdm
+
+
+@lru_cache
+def _get_tqdm_default():
+    _tqdm = _get_tqdm()
+    if _tqdm:
+        try:
+            from IPython import get_ipython
+
+            p = get_ipython()
+            if p is not None and p.has_trait("kernel"):
+                from tqdm.notebook import tqdm as tqdm_default
+            else:
+                tqdm_default = _tqdm.tqdm
+        except ImportError:
+            tqdm_default = _tqdm.tqdm
+    else:
+        tqdm_default = None
+
+    return tqdm_default
+
+
+def tqdm(*args, **kwargs):
+    opt = OPTIONS["tqdm_bar"]
+    if opt == "text":
+        func = _get_tqdm().tqdm
+    elif opt == "notebook":
+        func = _get_tqdm().tqdm_notebook
+    else:
+        func = _get_tqdm_default()
+    return func(*args, **kwargs)
+
+
+def get_tqdm(seq, len_min, leave=None, **kwargs):
+    n = kwargs.get("total", None)
+    _tqdm = _get_tqdm()
+
+    if isinstance(len_min, str):
+        len_min = OPTIONS[len_min]
+
+    if n is None:
+        n = len(seq)
+    if _tqdm and OPTIONS["tqdm_use"] and n >= len_min:
+        if leave is None:
+            leave = OPTIONS["tqdm_leave"]
+        seq = tqdm(seq, leave=leave, **kwargs)
+    return seq
+
+
+get_tqdm_calc = partial(get_tqdm, len_min="tqdm_len_calc")
+get_tqdm_build = partial(get_tqdm, len_min="tqdm_len_build")
+
+# def get_tqdm_calc(seq, len_min=None, leave=None, **kwargs):
+#     if len_min is None:
+#         len_min = OPTIONS['tqdm_len_calc']
+#     return _get_tqdm(seq, len_min=len_min, leave=leave, **kwargs)
+
+
+# def get_tqdm_build(seq, len_min=None, leave=None, **kwargs):
+#     if len_min is None:
+#         len_min = OPTIONS['tqdm_len_build']
+#     return _get_tqdm(seq, len_min=len_min, leave=leave, **kwargs)
+
+
+# --------------------------------------------------
+# JOBLIB stuff
+@lru_cache
+def _get_joblib():
+    try:
+        import joblib
+    except ImportError:
+        joblib = None
+    return joblib
+
+
+def parallel_map_build(func, items, *args, **kwargs):
+    joblib = _get_joblib()
+    if joblib and OPTIONS["joblib_use"] and len(items) >= OPTIONS["joblib_len_build"]:
+        return joblib.Parallel(
+            n_jobs=OPTIONS["joblib_n_jobs"],
+            backend=OPTIONS["joblib_backend"],
+            **OPTIONS["joblib_kws"],
+        )(joblib.delayed(func)(x, *args, **kwargs) for x in items)
+    else:
+        return [func(x, *args, **kwargs) for x in items]
+
+
+def _func_call(x, *args, **kwargs):
+    return x(*args, **kwargs)
+
+
+def parallel_map_call(items, use_joblib, *args, **kwargs):
+    joblib = _get_joblib()
+    if (
+        use_joblib
+        and joblib
+        and OPTIONS["joblib_use"]
+        and len(items) >= OPTIONS["joblib_len_calc"]
+    ):
+        return joblib.Parallel(
+            n_jobs=OPTIONS["joblib_n_jobs"],
+            backend=OPTIONS["joblib_backend"],
+            **OPTIONS["joblib_kws"],
+        )(joblib.delayed(_func_call)(x, *args, **kwargs) for x in items)
+    else:
+        return [x(*args, **kwargs) for x in items]
+
+
+def parallel_map_attr(attr, use_joblib, items):
+    from operator import attrgetter
+
+    joblib = _get_joblib()
+    func = attrgetter(attr)
+
+    if (
+        use_joblib
+        and joblib
+        and OPTIONS["joblib_use"]
+        and len(items) >= OPTIONS["joblib_len_calc"]
+    ):
+        return joblib.Parallel(
+            n_jobs=OPTIONS["joblib_n_jobs"],
+            backend=OPTIONS["joblib_backend"],
+            **OPTIONS["joblib_kws"],
+        )(joblib.delayed(func)(x) for x in items)
+    else:
+        return [func(x) for x in items]
+
+
+def parallel_map_func_starargs(func, use_joblib, items, total=None):
+    if total is None:
+        total = len(items)
+
+    joblib = _get_joblib()
+
+    if (
+        use_joblib
+        and joblib
+        and OPTIONS["joblib_use"]
+        and total >= OPTIONS["joblib_len_calc"]
+    ):
+        return joblib.Parallel(
+            n_jobs=OPTIONS["joblib_n_jobs"],
+            backend=OPTIONS["joblib_backend"],
+            **OPTIONS["joblib_kws"],
+        )(joblib.delayed(func)(*x) for x in items)
+    else:
+        return [func(*x) for x in items]
+
+
+# ----------------------------------------
+# pandas stuff
+def allbut(levels, *names):
+    names = set(names)
+    return [item for item in levels if item not in names]
+
+
+# ----------------------------------------
+# xarray utils
+def dim_to_suffix_dataarray(da, dim, join="_"):
+    if dim in da.dims:
+        return da.assign_coords(
+            **{dim: lambda x: [f"{x.name}{join}{c}" for c in x[dim].values]}
+        ).to_dataset(dim=dim)
+    else:
+        return da.to_dataset()
+
+
+def dim_to_suffix_dataset(table, dim, join="_"):
+    out = table
+    for k in out:
+        if dim in out[k].dims:
+            out = out.drop(k).update(table[k].pipe(dim_to_suffix_dataarray, dim, join))
+    return out
+
+
+def dim_to_suffix(ds, dim="component", join="_"):
+    from xarray import DataArray, Dataset
+
+    if isinstance(ds, DataArray):
+        f = dim_to_suffix_dataarray
+    elif isinstance(ds, Dataset):
+        f = dim_to_suffix_dataset
+    else:
+        raise ValueError("`ds` must be `DataArray` or `Dataset`")
+    return f(ds, dim=dim, join=join)
+
+
+def _convention_to_bool(convention):
+    if convention == "image":
+        convention = True
+    elif convention == "masked":
+        convention = False
+    else:
+        assert convention in [True, False]
+    return convention
+
+
+def mask_change_convention(mask, convention_in="image", convention_out="masked"):
+    """
+    Convert an array from one 'mask' convention to another.
+
+    Parameters
+    ----------
+    mask : array-like
+        Masking array.
+    convention_in, convention_out : string or bool or None
+        Convention for input and output.
+        Convention for mask.  Allowable values are:
+
+        * ``'image' ``or ``True`` : `True` values included, `False` values excluded.
+          This is the normal convention in :mod:`scipy.ndimage`.
+        * ``'masked'`` or ``False``:  `False` values are included, `True` values are excluded.
+          This is the convention in :mod:`numpy.ma`
+
+        If ``None``, then pass return input `mask`
+
+    Returns
+    -------
+    new_mask : ndarray
+        New 'mask' array with specified convention.
+    """
+
+    if mask is None:
+        return mask
+
+    convention_in = _convention_to_bool(convention_in)
+    convention_out = _convention_to_bool(convention_out)
+
+    if convention_in != convention_out:
+        mask = ~mask
+    return mask
+
+
+def masks_change_convention(masks, convention_in="image", convention_out="masked"):
+    """
+    Perform convention change of sequence of masks
+
+    Parameters
+    ----------
+    masks : sequence of array-like
+        masks[i] is the 'ith' mask
+    convention_in, convention_out : string or bool or None
+        Convention for input and output.
+        Convention for mask.  Allowable values are:
+
+        * 'image' or True : `True` values included, `False` values excluded.
+          This is the normal convention in :mod:`scipy.ndimage`.
+        * 'masked' or False:  `False` values are included, `True` values are excluded.
+          This is the convention in :mod:`numpy.ma`
+
+        If `None`, then pass return input `mask`
+
+    Returns
+    -------
+    new_masks : list of array
+        New 'masks' array with specified convention.
+    """
+
+    convention_in = _convention_to_bool(convention_in)
+    convention_out = _convention_to_bool(convention_out)
+
+    if convention_in != convention_out:
+        masks = [m if m is None else ~m for m in masks]
+    return masks
+
+
+##################################################
+# labels/masks utilities
+##################################################
+def labels_to_masks(
+    labels,
+    features=None,
+    include_boundary=False,
+    convention="image",
+    check_features=True,
+    **kwargs,
+):
+    """
+    Convert labels array to list of masks
+
+    Parameters
+    ----------
+    labels : array-like of int
+        Each unique value `i` in `labels` indicates a mask.
+        That is ``labels == i``.
+    features : array-like, optional
+        list of features to extract from labels.  Note that returned
+        mask[i] corresponds to labels == feature[i].
+    include_boundary : bool, default=False
+        if True, include boundary regions in output mask
+    convention : {'image','masked'} or bool
+        convention for output masks
+    check_features : bool, default=True
+        if True, and supply features, then make sure each feature is in labels
+
+    **kwargs
+        arguments to find_boundary if include_boundary is True
+        default to mode='outer', connectivity=labels.ndim
+
+    Returns
+    -------
+    output : list of array of bool
+        List of mask arrays, each with same shape as ``labels``.
+        Mask for each feature.
+    features : list
+        features
+
+    See Also
+    --------
+    masks_to_labels
+    :func:`skimage.segmentation.find_boundaries`
+
+    """
+    from skimage import segmentation
+
+    if include_boundary:
+        kwargs = dict(dict(mode="outer", connectivity=labels.ndim), **kwargs)
+    if features is None:
+        features = [i for i in np.unique(labels) if i > 0]
+    elif check_features:
+        vals = np.unique(labels)
+        assert np.all([x in vals for x in features])
+
+    convention = _convention_to_bool(convention)
+
+    output = []
+    for i in features:
+        m = labels == i
+        if include_boundary:
+            b = segmentation.find_boundaries(m.astype(int), **kwargs)
+            m = m + b
+        if not convention:
+            m = ~m
+        output.append(m)
+    return output, features
+
+
+def masks_to_labels(masks, features=None, convention="image", dtype=int, **kwargs):
+    """
+    Convert list of masks to labels
+
+    Parameters
+    ----------
+    masks : list of array-like of bool
+        List of mask arrays.
+    features : array-like of int, optional
+        Value for each feature.
+        labels[mask[i]] = features[i] + feature_offset
+        Default = range(1, len(masks) + 1)
+
+    convention : {'image','masked'} or bool
+        convention of masks
+
+    Returns
+    -------
+    ndarray
+        Label array.
+
+
+    See Also
+    --------
+    labels_to_masks
+    """
+
+    if features is None:
+        features = range(1, len(masks) + 1)
+    else:
+        assert len(features) == len(masks)
+
+    labels = np.full(masks[0].shape, fill_value=0, dtype=dtype)
+
+    masks = masks_change_convention(masks, convention, True)
+
+    for i, m in zip(features, masks):
+        labels[m] = i
+    return labels
+
+
+def ffill(arr, axis=-1, limit=None):
+    import bottleneck
+
+    _limit = limit if limit is not None else arr.shape[axis]
+    return bottleneck.push(arr, n=_limit, axis=axis)
+
+
+def bfill(arr, axis=-1, limit=None):
+    """Inverse of ffill"""
+    import bottleneck
+
+    # work around for bottleneck 178
+    _limit = limit if limit is not None else arr.shape[axis]
+
+    arr = np.flip(arr, axis=axis)
+    # fill
+    arr = bottleneck.push(arr, axis=axis, n=_limit)
+    # reverse back to original
+    return np.flip(arr, axis=axis)
+
+
+##################################################
+# calculations
+##################################################
+
+
+def get_lnz_iter(lnz, x):
+    """
+    Create a lnz_iter object for varying a single lnz
+
+    Parameters
+    ----------
+    lnz : list
+        list with one element equal to None.  This is the component which will be varied
+        For example, lnz=[lnz0,None,lnz2] implies use values of lnz0,lnz2 for components 0 and 2, and
+        vary component 1
+
+    x : array
+        values to insert for variable component
+
+    Returns
+    -------
+    ndarray
+        Shape ``(len(x),len(lnz))``.
+        array with rows [lnz0,lnz1,lnz2]
+    """
+
+    z = np.zeros_like(x)
+
+    x = np.asarray(x)
+
+    L = []
+    for m in lnz:
+        if m is None:
+            L.append(x)
+        else:
+            L.append(z + m)
+
+    return np.array(L).T
+
+
+##################################################
+# utilities
+##################################################
+
+
+def sort_lnPis(input, comp=0):
+    """
+    Sort list of lnPi  that component `comp` mole fraction increases
+
+    Parameters
+    ----------
+    input : list of lnPiMasked
+
+
+    comp : int, default=0
+     component to sort along
+
+    Returns
+    -------
+    output : list of lnPiMasked
+        Objects in sorted order.
+    """
+
+    molfrac_comp = np.array([x.molfrac[comp] for x in input])
+
+    order = np.argsort(molfrac_comp)
+
+    output = [input[i] for i in order]
+
+    return output
+
+
+def distance_matrix(mask, convention="image"):
+    """
+    Create matrix of distances from elements of mask
+    to nearest background point
+
+    Parameters
+    ----------
+    mask : array-like
+        image mask
+    convention : str or bool, default='image'
+        mask convention
+
+    Returns
+    -------
+    distance : ndarray
+        Same shape as mask.
+        Distance from possible feature elements to background
+
+
+    See Also
+    --------
+    ~scipy.ndimage.distance_transform_edt
+    """
+    from scipy.ndimage import distance_transform_edt
+
+    mask = np.asarray(mask, dtype=bool)
+    mask = masks_change_convention(mask, convention_in=convention, convention_out=True)
+
+    # pad mask
+    # add padding to end of matrix in each dimension
+    ndim = mask.ndim
+    pad_width = ((0, 1),) * ndim
+    mask = np.pad(mask, pad_width=pad_width, mode="constant", constant_values=False)
+
+    # distance filter
+    dist = distance_transform_edt(mask)
+
+    # remove padding
+    s = (slice(None, -1),) * ndim
+    return dist[s]
+
+
+def lnpimasked_to_dataset(data, keys=("lnpi", "PE")):
+    """
+    Convert a :class:`~lnpy.lnpidata.lnPiMasked` object into as :class:`~xarray.Dataset`.
+
+    Parameters
+    ----------
+    data : lnPiMasked
+
+    Returns
+    -------
+    output : Dataset
+    """
+
+    return data.xce.table(keys=keys, default_keys=None)
+
+
+def dataset_to_lnpimasked(ds, lnpi_name="lnpi", pe_name="PE", extra_kws=None, **kwargs):
+    """
+    Convert a :class:`~xarray.Dataset` to a :class:`~lnpy.lnpidata.lnPiMasked` object.
+
+    Parameters
+    ----------
+    ds : :class:`~xarray.Dataset`
+    lnpi_name, pe_name : str
+        Names of 'lnPi' and 'PE' parameters
+    extra_kws : mapping, optional
+        parameter `extra_kws`.  Note that if `pe_name` found in `ds`, then will add it to `extra_kws`.
+
+    Returns
+    -------
+    lnpi : lnPiMasked
+    """
+
+    from .lnpidata import lnPiMasked
+
+    data = ds[lnpi_name]
+
+    if extra_kws is None:
+        extra_kws = {}
+
+    if pe_name in ds:
+        extra_kws[pe_name] = ds[pe_name].values
+
+    return lnPiMasked.from_dataarray(da=data, extra_kws=extra_kws, **kwargs)
