d17c581467d1c7264d97bb80c44b9402decd00ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package django-material-admin-plus/1.8.9/django-material-admin-plus-1.8.9.tar.gz

---
+++ packages/django-material-admin-plus/django-material-admin-plus-1.8.9.tar.gz/django-material-admin-plus-1.8.9/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name="django-material-admin-plus",
+    version="1.8.9",
+    license='MIT License',
+    packages=find_packages(),
+    author="Anton Maistrenko",
+    include_package_data=True,
+    author_email="it2015maistrenko@gmail.com",
+    description="Material Design For Django Administration",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaistrenkoAnton/django-material-admin",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
