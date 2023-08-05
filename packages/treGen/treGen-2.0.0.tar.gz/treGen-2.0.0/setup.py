abf8d4a507411f0b4c2199cd63b1b173f0a6e5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:39 +0000

    Add package treGen/2.0.0/treGen-2.0.0.tar.gz

---
+++ packages/treGen/treGen-2.0.0.tar.gz/treGen-2.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  "Operating System :: OS Independent",
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='treGen',
+  version='2.0.0',
+  description='A simple Directory Tree Generator',
+  readme='README.MD',
+  url='https://github.com/JRS296/Directory-Tree',  
+  author='Jonathan Rufus Samuel',
+  author_email='jrsstudios@skiff.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='tree-generator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
