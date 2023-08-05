8bc2f66f6be4824376b4fe3777e75b1ceff53bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:13 +0000

    Add package bullhorn/0.0.3/bullhorn-0.0.3-py3-none-any.whl

---
+++ packages/bullhorn/bullhorn-0.0.3-py3-none-any.whl/examples/get_placements_and_candidates.py
@@ -0,0 +1,30 @@
+import os
+
+from bullhorn.client import BullhornClient
+
+
+def get_placements_and_candidates():
+    # Set credentials
+    token = os.environ.get("BULLHORN_SESSION_TOKEN")
+    rest_url = os.environ.get("BULLHORN_REST_URL")
+
+    # Initialise client
+    bc = BullhornClient(
+        token=token,
+        rest_url=rest_url,
+    )
+    bc.login()
+
+    # Get placements result
+    placements = bc.get_placements(
+        query="dateLastModified:{2023/01/01 TO *}",
+        fields="id,candidate,comments,dateAdded,dateBegin,dateEnd,dateLastModified,referralFee,salary,status",
+    )
+    print("Success: %r" % (placements))
+
+    # Get candidates result
+    candidates = bc.get_candidates(
+        query="dateLastModified:{2023/01/01 TO *}",
+        fields="id,firstName,lastName",
+    )
+    print("Success: %r" % (candidates))
