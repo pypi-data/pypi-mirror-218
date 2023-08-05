26640569efe8801e716a1051b57ec7179cccaa1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package graphix/0.2.4/graphix-0.2.4.tar.gz

---
+++ packages/graphix/graphix-0.2.4.tar.gz/graphix-0.2.4/README.md
@@ -0,0 +1,89 @@
+<img src="https://github.com/TeamGraphix/graphix/raw/master/docs/logo/black_with_name.png" alt="logo" width="550">
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/graphix)
+![PyPI](https://img.shields.io/pypi/v/graphix)
+[![Unitary Fund](https://img.shields.io/badge/Supported%20By-UNITARY%20FUND-brightgreen.svg)](https://unitary.fund/)
+[![DOI](https://zenodo.org/badge/573466585.svg)](https://zenodo.org/badge/latestdoi/573466585)
+[![Documentation Status](https://readthedocs.org/projects/graphix/badge/?version=latest)](https://graphix.readthedocs.io/en/latest/?badge=latest)
+![GitHub](https://img.shields.io/github/license/TeamGraphix/graphix)
+[![Downloads](https://static.pepy.tech/badge/graphix)](https://pepy.tech/project/graphix)
+
+**Graphix** is a measurement-based quantum computing (MBQC) compiler, which makes it easier to generate, optimize and simulate MBQC *measurement patterns*.
+
+## Feature
+
+- We integrate an efficient [graph state simulator](https://graphix.readthedocs.io/en/latest/lc-mbqc.html) as an optimization routine of MBQC *measurement pattern*, with which we can classically [preprocess all Pauli measurements](https://graphix.readthedocs.io/en/latest/tutorial.html#performing-pauli-measurements) (corresponding to the elimination of all Clifford gates in the gate network - c.f. [Gottesman-Knill theorem](https://en.wikipedia.org/wiki/Gottesman–Knill_theorem)), significantly reducing the required size of graph state to run the computation.
+- We implement tensor-network simulation of MBQC with which thousands of qubits (graph nodes) can be simulated with modest computing resources (e.g. laptop), without approximation.
+- Our pattern-based construction and optimization routines are suitable for high-level optimization to run quantum algorithms on MBQC quantum hardware with minimal resource state size requirements. We plan to add quantum hardware emulators (and quantum hardware) as pattern execution backends.
+
+## Installation
+Install `graphix` with `pip`:
+
+```bash
+$ pip install graphix
+```
+
+Install together with device interface:
+```bash
+$ pip install graphix[extra]
+```
+this will install `graphix` and [IBMQ interface](https://github.com/TeamGraphix/graphix-ibmq) to run MBQC patterns on IBM devices and Aer simulator.
+
+We are currently adding more quantum device interfaces.
+Please suggest in [issues](https://github.com/TeamGraphix/graphix/issues) if you have any particular device in mind!
+
+
+## Next Steps
+
+- We have a few [demos](https://graphix.readthedocs.io/en/latest/gallery/index.html) showing basic usages of `Graphix`.
+- You can run demos on your browser:
+  - Preprocessing Clifford gates: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/TeamGraphix/graphix-examples/HEAD?labpath=deutsch-jozsa.ipynb)
+  - Using tensor-network simulator backend: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/TeamGraphix/graphix-examples/HEAD?labpath=qft_with_tn.ipynb)
+  - QAOA circuit: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/TeamGraphix/graphix-examples/HEAD?labpath=qaoa.ipynb)
+
+- Read the [tutorial](https://graphix.readthedocs.io/en/latest/tutorial.html) for more comprehensive guide.
+
+- For theoretical background, read our quick introduction into [MBQC](https://graphix.readthedocs.io/en/latest/intro.html) and [LC-MBQC](https://graphix.readthedocs.io/en/latest/lc-mbqc.html).
+
+## Citing
+
+> S. Sunami and M. Fukushima. "Graphix: optimizing and simulating measurement-based quantum computation on local-Clifford decorated graph", 
+> [arXiv:2212.11975](https://arxiv.org/abs/2212.11975) (2022).
+
+Update on the paper: [^1]
+
+[^1]: Following the release of this arXiv preprint, we were made aware of a previous work by [Backens et al.](https://quantum-journal.org/papers/q-2021-03-25-421/) where Pauli measurement elimination method for MBQC was developed in the context of circuit optimization. 
+Many thanks for letting us know about this work, we will properly mention this work in the next version of our paper.
+
+## Contributing
+
+We use [GitHub issues](https://github.com/TeamGraphix/graphix/issues) for tracking requests and bugs. 
+
+## Discord Server
+
+Please visit [Unitary Fund's Discord server](https://discord.com/servers/unitary-fund-764231928676089909), where you can find a channel for `graphix` to ask questions.
+
+## Core Contributors
+
+Dr. Shinichi Sunami (University of Oxford)
+
+Masato Fukushima (University of Tokyo, Fixstars Amplify)
+
+## Acknowledgements
+
+We are proud to be supported by [unitary fund microgrant program](https://unitary.fund/grants.html). 
+
+<p><a href="https://unitary.fund/grants.html">
+<img src="https://user-images.githubusercontent.com/33350509/233384863-654485cf-b7d0-449e-8868-265c6fea2ced.png" alt="unitary-fund" width="150"/>
+</a></p>
+
+Special thanks to Fixstars Amplify:
+
+<p><a href="https://amplify.fixstars.com/en/">
+<img src="https://github.com/TeamGraphix/graphix/raw/master/docs/imgs/fam_logo.png" alt="amplify" width="200"/>
+</a></p>
+
+
+## License
+
+[Apache License 2.0](LICENSE)
---
+++ packages/graphix/graphix-0.2.4.tar.gz/graphix-0.2.4/graphix/sim/statevec.py
@@ -0,0 +1,349 @@
+import numpy as np
+from graphix.ops import Ops
+from graphix.clifford import CLIFFORD_CONJ, CLIFFORD, CLIFFORD_MUL
+from copy import deepcopy
+from scipy.linalg import norm
+
+
+class StatevectorBackend:
+    """MBQC simulator with statevector method."""
+
+    def __init__(self, pattern, max_qubit_num=20):
+        """
+        Parameteres
+        -----------
+        pattern: :class:`graphix.pattern.Pattern` object
+            MBQC pattern to be simulated.
+        backend: str, 'statevector'
+            optional argument for simulation.
+        max_qubit_num : int
+            optional argument specifying the maximum number of qubits
+            to be stored in the statevector at a time.
+        """
+        # check that pattern has output nodes configured
+        assert len(pattern.output_nodes) > 0
+        self.pattern = pattern
+        self.results = deepcopy(pattern.results)
+        self.state = None
+        self.node_index = []
+        self.Nqubit = 0
+        self.to_trace = []
+        self.to_trace_loc = []
+        self.max_qubit_num = max_qubit_num
+        if pattern.max_space() > max_qubit_num:
+            raise ValueError("Pattern.max_space is larger than max_qubit_num. Increase max_qubit_num and try again")
+
+    def qubit_dim(self):
+        """Returns the qubit number in the internal statevector
+        Returns
+        -------
+        n_qubit : int
+        """
+        return len(self.state.dims())
+
+    def add_nodes(self, nodes):
+        """add new qubit to internal statevector
+        and assign the corresponding node number
+        to list self.node_index.
+
+        Parameters
+        ----------
+        nodes : list of node indices
+        """
+        if not self.state:
+            self.state = Statevec(nqubit=0)
+        n = len(nodes)
+        sv_to_add = Statevec(nqubit=n)
+        self.state.tensor(sv_to_add)
+        self.node_index.extend(nodes)
+        self.Nqubit += 1
+        if self.Nqubit == self.max_qubit_num:
+            self.trace_out()
+
+    def entangle_nodes(self, edge):
+        """Apply CZ gate to two connected nodes
+
+        Parameters
+        ----------
+        edge : tuple (i, j)
+            a pair of node indices
+        """
+        target = self.node_index.index(edge[0])
+        control = self.node_index.index(edge[1])
+        self.state.entangle((target, control))
+
+    def measure(self, cmd):
+        """Perform measurement of a node in the internal statevector and trace out the qubit
+        Parameters
+        ----------
+        cmd : list
+            measurement command : ['M', node, plane angle, s_domain, t_domain]
+        """
+        # choose the measurement result randomly
+        result = np.random.choice([0, 1])
+        self.results[cmd[1]] = result
+
+        # extract signals for adaptive angle
+        s_signal = np.sum([self.results[j] for j in cmd[4]])
+        t_signal = np.sum([self.results[j] for j in cmd[5]])
+        angle = cmd[3] * np.pi
+        if len(cmd) == 7:
+            vop = cmd[6]
+        else:
+            vop = 0
+        if int(s_signal % 2) == 1:
+            vop = CLIFFORD_MUL[1, vop]
+        if int(t_signal % 2) == 1:
+            vop = CLIFFORD_MUL[3, vop]
+        m_op = meas_op(angle, vop=vop, plane=cmd[2], choice=result)
+        loc = self.node_index.index(cmd[1])
+        self.state.evolve_single(m_op, loc)
+
+        self.to_trace.append(cmd[1])
+        self.to_trace_loc.append(loc)
+
+    def correct_byproduct(self, cmd):
+        """Byproduct correction
+        correct for the X or Z byproduct operators,
+        by applying the X or Z gate.
+        """
+        if np.mod(np.sum([self.results[j] for j in cmd[2]]), 2) == 1:
+            loc = self.node_index.index(cmd[1])
+            if cmd[0] == "X":
+                op = Ops.x
+            elif cmd[0] == "Z":
+                op = Ops.z
+            self.state.evolve_single(op, loc)
+
+    def apply_clifford(self, cmd):
+        """Apply single-qubit Clifford gate,
+        specified by vop index specified in graphix.clifford.CLIFFORD
+        """
+        loc = self.node_index.index(cmd[1])
+        self.state.evolve_single(CLIFFORD[cmd[2]], loc)
+
+    def finalize(self):
+        """to be run at the end of pattern simulation."""
+        self.trace_out()
+        self.sort_qubits()
+        self.state.normalize()
+
+    def trace_out(self):
+        """trace out the qubits buffered in self.to_trace from self.state"""
+        self.state.normalize()
+        self.state.ptrace(self.to_trace_loc)
+        for node in self.to_trace:
+            self.node_index.remove(node)
+        self.Nqubit -= len(self.to_trace)
+        self.to_trace = []
+        self.to_trace_loc = []
+
+    def sort_qubits(self):
+        """sort the qubit order in internal statevector"""
+        for i, ind in enumerate(self.pattern.output_nodes):
+            if not self.node_index[i] == ind:
+                move_from = self.node_index.index(ind)
+                self.state.swap((i, move_from))
+                self.node_index[i], self.node_index[move_from] = (
+                    self.node_index[move_from],
+                    self.node_index[i],
+                )
+
+
+def meas_op(angle, vop=0, plane="XY", choice=0):
+    """Returns the projection operator for given measurement angle and local Clifford op (VOP).
+
+    .. seealso:: :mod:`graphix.clifford`
+
+    Parameters
+    ----------
+    angle: float
+        original measurement angle in radian
+    vop : int
+        index of local Clifford (vop), see graphq.clifford.CLIFFORD
+    plane : 'XY', 'YZ' or 'ZX'
+        measurement plane on which angle shall be defined
+    choice : 0 or 1
+        choice of measurement outcome. measured eigenvalue would be (-1)**choice.
+
+    Returns
+    -------
+    op : numpy array
+        projection operator
+
+    """
+    assert vop in np.arange(24)
+    assert choice in [0, 1]
+    assert plane in ["XY", "YZ", "XZ"]
+    if plane == "XY":
+        vec = (np.cos(angle), np.sin(angle), 0)
+    elif plane == "YZ":
+        vec = (0, np.cos(angle), np.sin(angle))
+    elif plane == "XZ":
+        vec = (np.cos(angle), 0, np.sin(angle))
+    op_mat = np.eye(2, dtype=np.complex128) / 2
+    for i in range(3):
+        op_mat += (-1) ** (choice) * vec[i] * CLIFFORD[i + 1] / 2
+    op_mat = CLIFFORD[CLIFFORD_CONJ[vop]] @ op_mat @ CLIFFORD[vop]
+    return op_mat
+
+
+CZ_TENSOR = np.array(
+    [[[[1, 0], [0, 0]], [[0, 1], [0, 0]]], [[[0, 0], [1, 0]], [[0, 0], [0, -1]]]],
+    dtype=np.complex128,
+)
+CNOT_TENSOR = np.array(
+    [[[[1, 0], [0, 0]], [[0, 1], [0, 0]]], [[[0, 0], [0, 1]], [[0, 0], [1, 0]]]],
+    dtype=np.complex128,
+)
+SWAP_TENSOR = np.array(
+    [[[[1, 0], [0, 0]], [[0, 0], [1, 0]]], [[[0, 1], [0, 0]], [[0, 0], [0, 1]]]],
+    dtype=np.complex128,
+)
+
+
+class Statevec:
+    """Simple statevector simulator"""
+
+    def __init__(self, plus_states=True, nqubit=1):
+        """Initialize statevector
+
+        Args:
+            plus_states (bool, optional): whether or not to start all qubits in + state or 0 state. Defaults to +
+            nqubit (int, optional): number of qubits. Defaults to 1.
+        """
+        if plus_states:
+            self.psi = np.ones((2,) * nqubit) / np.sqrt(2**nqubit)
+        else:
+            self.psi = np.zeros((2,) * nqubit)
+            self.psi[(0,) * nqubit] = 1
+
+    def __repr__(self):
+        return f"Statevec, data={self.psi}, shape={self.dims()}"
+
+    def evolve_single(self, op, i):
+        """Single-qubit operation
+
+        Args:
+            op (np.array): 2*2 matrix
+            i (int): qubit index
+        """
+        self.psi = np.tensordot(op, self.psi, (1, i))
+        self.psi = np.moveaxis(self.psi, 0, i)
+
+    def evolve(self, op, qargs):
+        """Multi-qubit operation
+
+        Args:
+            op (np.array): 2^n*2^n matrix
+            qargs (list of ints): target qubits' indexes
+        """
+        op_dim = int(np.log2(len(op)))
+        shape = [2 for _ in range(2 * op_dim)]
+        op_tensor = op.reshape(shape)
+        self.psi = np.tensordot(
+            op_tensor,
+            self.psi,
+            (tuple(op_dim + i for i in range(len(qargs))), tuple(qargs)),
+        )
+        self.psi = np.moveaxis(self.psi, [i for i in range(len(qargs))], qargs)
+
+    def dims(self):
+        return self.psi.shape
+
+    def ptrace(self, qargs):
+        """partial trace
+
+        Args:
+            qargs (list of ints): qubit inices to trace over
+        """
+        nqubit_after = len(self.psi.shape) - len(qargs)
+        psi = self.psi
+        rho = np.tensordot(psi, psi.conj(), axes=(qargs, qargs))  # density matrix
+        rho = np.reshape(rho, (2**nqubit_after, 2**nqubit_after))
+        evals, evecs = np.linalg.eig(rho)  # back to statevector
+        self.psi = np.reshape(evecs[:, np.argmax(evals)], (2,) * nqubit_after)
+
+    def entangle(self, edge):
+        """connect graph nodes
+
+        Args:
+            edge (tuple of ints): (control, target) qubit indices
+        """
+        # contraction: 2nd index - control index, and 3rd index - target index.
+        self.psi = np.tensordot(CZ_TENSOR, self.psi, ((2, 3), edge))
+        # sort back axes
+        self.psi = np.moveaxis(self.psi, (0, 1), edge)
+
+    def tensor(self, other):
+        r"""Tensor product state with other qubits.
+        Results in self :math:`\otimes` other.
+
+        Args:
+            other (_type_): graphix.sim.statevec.Statevec instance
+        """
+        psi_self = self.psi.flatten()
+        psi_other = other.psi.flatten()
+        total_num = len(self.dims()) + len(other.dims())
+        self.psi = np.kron(psi_self, psi_other).reshape((2,) * total_num)
+
+    def CNOT(self, qubits):
+        """apply CNOT
+
+        Args:
+            qubits (tuple of ints): (control, target) qubit indices
+        """
+        # contraction: 2nd index - control index, and 3rd index - target index.
+        self.psi = np.tensordot(CNOT_TENSOR, self.psi, ((2, 3), qubits))
+        # sort back axes
+        self.psi = np.moveaxis(self.psi, (0, 1), qubits)
+
+    def swap(self, qubits):
+        """swap qubits
+
+        Args:
+            qubits (tuple of ints): (control, target) qubit indices
+        """
+        # contraction: 2nd index - control index, and 3rd index - target index.
+        self.psi = np.tensordot(SWAP_TENSOR, self.psi, ((2, 3), qubits))
+        # sort back axes
+        self.psi = np.moveaxis(self.psi, (0, 1), qubits)
+
+    def normalize(self):
+        """normalize the state"""
+        self.psi = self.psi / norm(self.psi)
+
+    def flatten(self):
+        """returns flattened statevector"""
+        return self.psi.flatten()
+
+    def expectation_single(self, op, loc):
+        """Expectation value of single-qubit operator.
+
+        Args:
+            op (np.array): 2*2 Hermite operator
+            loc (int): target qubit
+        Returns:
+            complex: expectation value.
+        """
+        st1 = deepcopy(self)
+        st1.normalize()
+        st2 = st1.deepcopy(st1)
+        st1.evolve_single(op, loc)
+        return np.dot(st2.psi.flatten().conjugate(), st1.psi.flatten())
+
+    def expectation_value(self, op, qargs):
+        """Expectation value of multi-qubit operator.
+
+        Args:
+            op (np.array): 2^n*2^n operator
+            qargs (list of ints): target qubit
+
+        Returns:
+            complex: expectation value
+        """
+        st1 = deepcopy(self)
+        st1.normalize()
+        st2 = deepcopy(st1)
+        st1.evolve(op, qargs)
+        return np.dot(st2.psi.flatten().conjugate(), st1.psi.flatten())
