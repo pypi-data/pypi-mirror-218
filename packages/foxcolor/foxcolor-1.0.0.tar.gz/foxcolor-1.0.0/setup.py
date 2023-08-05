3c75778a72c28fb5afdad030d9c847c0eb00b2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:19 +0000

    Add package foxcolor/1.0.0/foxcolor-1.0.0.tar.gz

---
+++ packages/foxcolor/foxcolor-1.0.0.tar.gz/foxcolor-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='foxcolor',
+    version='1.0.0',
+    description='Biblioteca de cores para o python, com uma vasta variedades de cores para serem usadas.',
+    author='@FoxWaynne',
+    author_email='foxys.fow@gmail.com',
+    url='https://github.com/FoxWaynne/FoxColor/',
+    packages=['foxcolor'],
+    install_requires=[
+        'argparse',
+        'colorama'
+    ],
+)
