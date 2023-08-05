dda277ef512518ac91fb2810a696decb24307ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:21 +0000

    Add package langchain-ray/0.0.1/langchain_ray-0.0.1.tar.gz

---
+++ packages/langchain-ray/langchain_ray-0.0.1.tar.gz/langchain_ray-0.0.1/langchain_ray/imports.py
@@ -0,0 +1,23 @@
+from dreamai.core import *
+
+import ray
+from ray import serve
+from ray import data as rd
+from transformers import pipeline
+from langchain.schema import Document
+from langchain.llms import HuggingFacePipeline
+from langchain.vectorstores import Chroma, FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders.pdf import BasePDFLoader
+from langchain.document_loaders.blob_loaders import Blob
+from langchain.document_loaders.base import BaseBlobParser
+from langchain.prompts.pipeline import PipelinePromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import TransformChain, SimpleSequentialChain, SequentialChain
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain import (
+    PromptTemplate,
+    FewShotPromptTemplate,
+    HuggingFaceHub,
+    LLMChain,
+)
