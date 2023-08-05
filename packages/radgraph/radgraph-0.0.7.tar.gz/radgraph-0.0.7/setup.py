be7ce48dcc6298814d5044bf5c08e12715afc0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:55 +0000

    Add package radgraph/0.0.7/radgraph-0.0.7.tar.gz

---
+++ packages/radgraph/radgraph-0.0.7.tar.gz/radgraph-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='radgraph',
+    version='0.0.7',
+    author='Jean-Benoit Delbrouck',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    install_requires=['torch>=1.8.1',
+                      'transformers>=4.23.1',
+                      "appdirs",
+                      'jsonpickle',
+                      'filelock',
+                      'h5py',
+                      'spacy',
+                      'nltk',
+                      ],
+    packages=find_packages(),
+    zip_safe=False)
