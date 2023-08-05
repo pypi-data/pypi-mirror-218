ead08018a64a2da5f0b854e631b2c428b97b8bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package dash-auth-external/1.1.0/dash-auth-external-1.1.0.tar.gz

---
+++ packages/dash-auth-external/dash-auth-external-1.1.0.tar.gz/dash-auth-external-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+NAME = "dash-auth-external"
+
+this_directory = Path(__file__).parent
+long_description = (
+    "Integrate your dashboards with 3rd party APIs and external OAuth providers."
+)
+requires = ["dash >= 2.0.0", "requests >= 1.0.0", "requests-oauthlib >= 0.3.0"]
+
+setup(
+    name=NAME,
+    version="1.1.0",
+    description=long_description,
+    python_requires=">=3.7",
+    author_email="jholcombe@hotmail.co.uk",
+    url="https://github.com/jamesholcombe/dash-auth-external",
+    keywords=["Dash", "Plotly", "Authentication", "Auth", "External"],
+    install_requires=requires,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+)
