26728a58f3a11c7e92466c87f3ad598df791842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:13 +0000

    Add package bullhorn/0.0.3/bullhorn-0.0.3.tar.gz

---
+++ packages/bullhorn/bullhorn-0.0.3.tar.gz/bullhorn-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="bullhorn",
+    version="0.0.3",
+    author="lloydtao (Lewis Lloyd)",
+    author_email="lewis@recruit-hub.com",
+    description="Python wrapper for the Bullhorn REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/recruithub/bullhorn",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp==3.8.4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
