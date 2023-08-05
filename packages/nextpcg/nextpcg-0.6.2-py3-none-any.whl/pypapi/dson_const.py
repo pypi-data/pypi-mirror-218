2404b82272c1f6b9d44fdeaae37b74fef664e371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package nextpcg/0.6.2/nextpcg-0.6.2-py3-none-any.whl

---
+++ packages/nextpcg/nextpcg-0.6.2-py3-none-any.whl/pypapi/dson_const.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""define some const terms
+Author  : NextPCG
+"""
+
+'''Json Tag'''
+
+dson_inputs_tag = "Inputs"
+dson_input_tag = "input"
+dson_params_tag = "Params"
+dson_outputs_tag = "Outputs"
+dson_output_tag = "output"
+dson_doc_tag = "Doc"
+dson_meta_field_tag = "DsonMeta"
+dson_meta_clsname_tag = "clsName"
+dson_meta_if_in_server_tag = "ifInServer"
+dson_meta_plugin_name = "pluginName"
+
+
+'''common'''
+
+dson_plugin_file_folder = '.dson'
+dson_plugin_execute_folder = '.nextpcg'
