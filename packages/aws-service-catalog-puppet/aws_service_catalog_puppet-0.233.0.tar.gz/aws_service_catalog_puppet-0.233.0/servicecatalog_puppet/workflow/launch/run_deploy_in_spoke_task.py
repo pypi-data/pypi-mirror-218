d1e3f38519637ac0d250338c22c0b6821003f4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:00 +0000

    Add package aws-service-catalog-puppet/0.233.0/aws_service_catalog_puppet-0.233.0.tar.gz

---
+++ packages/aws-service-catalog-puppet/aws_service_catalog_puppet-0.233.0.tar.gz/aws_service_catalog_puppet-0.233.0/servicecatalog_puppet/workflow/launch/run_deploy_in_spoke_task.py
@@ -0,0 +1,40 @@
+#  Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+
+import luigi
+
+from servicecatalog_puppet import constants
+from servicecatalog_puppet.workflow.dependencies import tasks
+
+
+class RunDeployInSpokeTask(tasks.TaskWithReference):
+    account_id = luigi.Parameter()
+    generate_manifest_ref = luigi.Parameter()
+    cachable_level = constants.CACHE_LEVEL_NO_CACHE
+
+    def params_for_results_display(self):
+        return {
+            "task_reference": self.task_reference,
+            "puppet_account_id": self.puppet_account_id,
+            "account_id": self.account_id,
+        }
+
+    def run(self):
+        generated_manifest = self.get_output_from_reference_dependency(
+            self.generate_manifest_ref
+        )
+        build_spec = generated_manifest.get("build_spec")
+        spoke_execution_mode_deploy_env = generated_manifest.get(
+            "spoke_execution_mode_deploy_env"
+        )
+        vars = generated_manifest.get("vars")
+
+        with self.spoke_client("codebuild") as codebuild:
+            response = codebuild.start_build(
+                projectName=constants.EXECUTION_SPOKE_CODEBUILD_PROJECT_NAME,
+                environmentVariablesOverride=vars,
+                computeTypeOverride=spoke_execution_mode_deploy_env,
+                buildspecOverride=build_spec,
+                imageOverride=constants.CODEBUILD_DEFAULT_IMAGE,
+            )
+        self.write_output(dict(account_id=self.account_id, **response))
