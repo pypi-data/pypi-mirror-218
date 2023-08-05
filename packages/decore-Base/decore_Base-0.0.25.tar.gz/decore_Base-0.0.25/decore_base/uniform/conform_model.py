b6caaf8f07d00b04e653b704fdb1361412649b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package decore-Base/0.0.25/decore_Base-0.0.25.tar.gz

---
+++ packages/decore-Base/decore_Base-0.0.25.tar.gz/decore_Base-0.0.25/decore_base/uniform/conform_model.py
@@ -0,0 +1,17 @@
+from ..classes.decore_model import *
+
+class Conform_model(Decore_model):
+    
+    def __init__(self, *args, **kwargs):
+        Decore_model.__init__(self, *args, **kwargs)
+
+    class Meta:
+        # TODO - diese Zeile wieder einsetzen wenn alle Relationen zwischen conform und perform passen
+        # database = SqliteDatabase('state/database.db', pragmas=(('cache_size', -1024 * 64),('journal_mode', 'wal')))
+        database = SqliteDatabase('state/database.db')
+
+    @classmethod
+    def register(cls):
+        super(Conform_model, cls).register()
+        cls.migrate_database()
+        return cls
