b8822a309e6b9bb675bee13a7b07c42579c74559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package llm-chain/0.0.19/llm-chain-0.0.19.tar.gz

---
+++ packages/llm-chain/llm-chain-0.0.19.tar.gz/llm-chain-0.0.19/llm_chain/utilities.py
@@ -0,0 +1,143 @@
+
+"""Helper functions and classes."""
+import inspect
+import datetime
+import hashlib
+from functools import cache
+from collections.abc import Callable
+import tenacity
+import tiktoken
+from tiktoken import Encoding
+
+
+
+class Timer:
+    """Provides way to time the duration of code within the context manager."""
+
+    def __enter__(self):
+        self._start = datetime.datetime.now()
+        return self
+
+    def __exit__(self, *args):  # noqa
+        self._end = datetime.datetime.now()
+        self.interval = self._end - self._start
+
+    def __str__(self):
+        return self.formatted(units='seconds', decimal_places=2)
+
+    def formatted(self, units: str = 'seconds', decimal_places: int = 2) -> str:
+        """
+        Returns a string with the number of seconds that elapsed on the timer. Displays out to
+        `decimal_places`.
+
+        Args:
+            units:
+                format the elapsed time in terms of seconds, minutes, hours
+                (currently only supports seconds)
+            decimal_places:
+                the number of decimal places to display
+        """
+        if units == 'seconds':
+            return f"{self.interval.total_seconds():.{decimal_places}f} seconds"
+
+        raise ValueError("Only suppports seconds.")
+
+
+def create_hash(value: str) -> str:
+    """Based on `value`, returns a hash."""
+    # Create a new SHA-256 hash object
+    hash_object = hashlib.sha256()
+    # Convert the string value to bytes and update the hash object
+    hash_object.update(value.encode('utf-8'))
+    # Get the hexadecimal representation of the hash
+    return hash_object.hexdigest()
+
+
+@cache
+def _get_encoding_for_model(model_name: str) -> Encoding:
+    """Gets the encoding for a given model so that we can calculate the number of tokens."""
+    return tiktoken.encoding_for_model(model_name)
+
+
+def num_tokens(model_name: str, value: str) -> int:
+    """For a given model, returns the number of tokens based on the str `value`."""
+    return len(_get_encoding_for_model(model_name=model_name).encode(value))
+
+
+def num_tokens_from_messages(model_name: str, messages: list[dict]) -> int:
+    """
+    Copied from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+    Returns the number of tokens used by a list of messages.
+    """
+    if model_name in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model_name == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model_name:
+        # Warning: gpt-3.5-turbo may update over time.
+        # Returning num tokens assuming gpt-3.5-turbo-0613
+        return num_tokens_from_messages(model_name="gpt-3.5-turbo-0613", messages=messages)
+    elif "gpt-4" in model_name:
+        # Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.
+        return num_tokens_from_messages(model_name="gpt-4-0613", messages=messages)
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model_name}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")  # noqa
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(_get_encoding_for_model(model_name=model_name).encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+def retry_handler(num_retries: int = 3, wait_fixed: int = 1) -> Callable:
+    """
+    Returns a tenacity callable object that can be used for retrying a function call.
+
+    ```
+    r = retry_handler()
+    r(
+        openai.Completion.create,
+        model="text-davinci-003",
+        prompt="Once upon a time,"
+    )
+    ```
+    """
+    return tenacity.Retrying(
+        stop=tenacity.stop_after_attempt(num_retries),
+        wait=tenacity.wait_fixed(wait_fixed),
+        reraise=True,
+    )
+
+
+def has_property(obj: object, property_name: str) -> bool:
+    """
+    Returns True if the object has a property (or instance variable) with the name
+    `property_name`.
+    """
+    # if `obj` is itself a function, it will not have any properties
+    if inspect.isfunction(obj):
+        return False
+
+    return hasattr(obj, property_name) and \
+        not callable(getattr(obj.__class__, property_name, None))
+
+
+def has_method(obj: object, method_name: str) -> bool:
+    """Returns True if the object has a method with the name `property_name`."""
+    # if `obj` is itself a function, it will not have any properties
+    if inspect.isfunction(obj):
+        return False
+    return hasattr(obj, method_name) and callable(getattr(obj.__class__, method_name, None))
