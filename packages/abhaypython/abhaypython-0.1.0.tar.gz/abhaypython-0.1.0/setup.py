a7e50c48a7555a96d6e8d0e74ffbfe5bbdf4e1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:58 +0000

    Add package abhaypython/0.1.0/abhaypython-0.1.0.tar.gz

---
+++ packages/abhaypython/abhaypython-0.1.0.tar.gz/abhaypython-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='abhaypython',
+    version='0.1.0',
+    description='A library for finding exponentials',
+    author='ABHAY',
+    author_email='abhaysrivastva13@gmail.com',
+    packages=['abhaypython'],
+    install_requires=[],
+)
