9d31817f9853d8a10b7590f41a76954efd9000d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:41 +0000

    Add package illini-snake/0.2.0/illini_snake-0.2.0.tar.gz

---
+++ packages/illini-snake/illini_snake-0.2.0.tar.gz/illini_snake-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        package_data={'illini_snake': 
+        [ 
+            'static/opponent_logos/*.png',
+            'static/opponent_logos/*.svg',
+            'static/Illinois.png',
+            'static/nom nom nom.wav',
+            'settings.json'
+        ]
+        }
+    )
