97d26efb3747f9cbf8ed40527ff41b4c8e86f1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:52 +0000

    Add package my-shell-script/0.1/my_shell_script-0.1.tar.gz

---
+++ packages/my-shell-script/my_shell_script-0.1.tar.gz/my_shell_script-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='my_shell_script',
+    version='0.1',
+    description='A Python package that contains a shell script.',
+    py_modules=['my_shell_script'],
+)
