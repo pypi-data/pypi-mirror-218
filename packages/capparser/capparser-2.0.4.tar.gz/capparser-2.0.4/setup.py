257ff37cac9a98c86375bfe02f53e41277f35769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:21 +0000

    Add package capparser/2.0.4/capparser-2.0.4.tar.gz

---
+++ packages/capparser/capparser-2.0.4.tar.gz/capparser-2.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="capparser",
+    version="2.0.4",
+    author="David Araújo",
+    author_email="david2araujo5@gmail.com",
+    description="Common Alerting Protocol (CAP) parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["capparser"],
+    package_dir={'': '.'},
+)
