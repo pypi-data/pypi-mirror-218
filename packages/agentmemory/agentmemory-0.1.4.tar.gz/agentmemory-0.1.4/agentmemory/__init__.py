ec8ba859782ad3d495f0544c4f8a267205b4267a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:40 +0000

    Add package agentmemory/0.1.4/agentmemory-0.1.4.tar.gz

---
+++ packages/agentmemory/agentmemory-0.1.4.tar.gz/agentmemory-0.1.4/agentmemory/__init__.py
@@ -0,0 +1,9 @@
+"""
+agentmemory
+
+Simple agent memory, powered by chromadb
+"""
+
+__version__ = "0.1.4"
+__author__ = 'Moon (https://github.com/lalalune)'
+__credits__ = 'https://github.com/lalalune/agentmemory and https://www.trychroma.com/'
