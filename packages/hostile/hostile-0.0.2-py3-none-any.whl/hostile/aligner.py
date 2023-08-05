b0f920dc09c4ebe2790df596663ab1a642367fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:44 +0000

    Add package hostile/0.0.2/hostile-0.0.2-py3-none-any.whl

---
+++ packages/hostile/hostile-0.0.2-py3-none-any.whl/hostile/aligner.py
@@ -0,0 +1,143 @@
+import logging
+import subprocess
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from hostile import util
+
+
+@dataclass
+class Aligner:
+    name: str
+    short_name: str
+    bin_path: Path
+    cdn_base_url: str
+    working_dir: Path
+    cmd: str
+    paired_cmd: str
+    idx_archive_fn: str = ""
+    ref_archive_fn: str = ""
+    idx_name: str = ""
+    idx_paths: tuple[Path] = tuple()
+
+    def __post_init__(self):
+        self.ref_archive_url = f"{self.cdn_base_url}/{self.ref_archive_fn}"
+        self.idx_archive_url = f"{self.cdn_base_url}/{self.idx_archive_fn}"
+        self.ref_archive_path = self.working_dir / self.ref_archive_fn
+        self.idx_archive_path = self.working_dir / self.idx_archive_fn
+        self.idx_path = self.working_dir / self.idx_name
+        Path(self.working_dir).mkdir(exist_ok=True, parents=True)
+
+    def check(self):
+        logging.info(f"Found {self.name}")
+        if self.name == "Bowtie2":
+            if not all(path.exists() for path in self.idx_paths):
+                self.working_dir.mkdir(exist_ok=True, parents=True)
+                logging.info(f"Fetching human index")
+                util.download(self.idx_archive_url, self.idx_archive_path)
+                util.untar_file(self.idx_archive_path, self.working_dir)
+                self.idx_archive_path.unlink()
+                logging.info(f"Saved human index ({self.idx_path})")
+            else:
+                logging.info(f"Found cached human index ({self.idx_path})")
+        elif self.name == "Minimap2":
+            if not self.ref_archive_path.exists():
+                util.download(self.ref_archive_url, self.ref_archive_path)
+                logging.info(f"Saved human reference ({self.ref_archive_path})")
+            else:
+                logging.info(f"Found cached human reference ({self.ref_archive_path})")
+        try:
+            util.run(f"{self.bin_path} --help", cwd=self.working_dir)
+        except subprocess.CalledProcessError:
+            raise RuntimeError(f"Failed to execute {self.bin_path}")
+
+    def gen_clean_cmd(
+        self,
+        fastq: Path,
+        out_dir: Path,
+        custom_index: Path | None = None,
+        threads: int = 2,
+    ) -> str:
+        fastq, out_dir = Path(fastq), Path(out_dir)
+        out_dir.mkdir(exist_ok=True, parents=True)
+        fastq_stem = util.fastq_path_to_stem(fastq)
+        fastq_out_path = out_dir / f"{fastq_stem}.clean.fastq.gz"
+        count_before_path = out_dir / f"{fastq_stem}.reads_in.txt"
+        count_after_path = out_dir / f"{fastq_stem}.reads_out.txt"
+        if custom_index:
+            self.idx_path = Path(custom_index)
+            self.ref_archive_path = Path(custom_index)
+            logging.info(f"Using custom index {custom_index}")
+        cmd_template = {  # Templating for Aligner.cmd
+            "{BIN_PATH}": str(self.bin_path),
+            "{REF_ARCHIVE_PATH}": str(self.ref_archive_path),
+            "{INDEX_PATH}": str(self.idx_path),
+            "{FASTQ}": str(fastq),
+            "{THREADS}": str(threads),
+        }
+        for k in cmd_template.keys():
+            self.cmd = self.cmd.replace(k, cmd_template[k])
+        cmd = (
+            # Align, stream reads to stdout in SAM format
+            f"{self.cmd}"
+            # Count reads in stream before filtering (2048 + 256 = 2304)
+            f" | tee >(samtools view -F 2304 -c - > '{count_before_path}')"
+            # Discard mapped reads
+            f" | samtools view --threads {int(threads/2)} -f 4 -"
+            # Count reads in stream after filtering
+            f" | tee >(samtools view -F 256 -c - > '{count_after_path}')"
+            # Replace paired read headers with integers
+            f' | awk \'BEGIN{{FS=OFS="\\t"}} {{$1=int((NR+1)/2)" "; print $0}}\''
+            # Stream remaining records into fastq files
+            f" | samtools fastq --threads {int(threads/2)} -c 6 -0 '{fastq_out_path}'"
+        )
+        logging.debug(f"{cmd}")
+        return cmd
+
+    def gen_paired_clean_cmd(
+        self,
+        fastq1: Path,
+        fastq2: Path,
+        out_dir: Path,
+        custom_index: Path | None = None,
+        threads: int = 2,
+    ) -> str:
+        fastq1, fastq2, out_dir = Path(fastq1), Path(fastq2), Path(out_dir)
+        out_dir.mkdir(exist_ok=True, parents=True)
+        fastq1_stem = util.fastq_path_to_stem(fastq1)
+        fastq2_stem = util.fastq_path_to_stem(fastq2)
+        fastq1_out_path = out_dir / f"{fastq1_stem}.clean_1.fastq.gz"
+        fastq2_out_path = out_dir / f"{fastq2_stem}.clean_2.fastq.gz"
+        count_before_path = out_dir / f"{fastq1_stem}.reads_in.txt"
+        count_after_path = out_dir / f"{fastq1_stem}.reads_out.txt"
+        if custom_index:
+            self.idx_path = Path(custom_index)
+            self.ref_archive_path = Path(custom_index)
+            logging.info(f"Using custom index ({custom_index})")
+        cmd_template = {  # Templating for Aligner.cmd
+            "{BIN_PATH}": str(self.bin_path),
+            "{REF_ARCHIVE_PATH}": str(self.ref_archive_path),
+            "{INDEX_PATH}": str(self.idx_path),
+            "{FASTQ1}": str(fastq1),
+            "{FASTQ2}": str(fastq2),
+            "{THREADS}": str(threads),
+        }
+        for k in cmd_template.keys():
+            self.paired_cmd = self.paired_cmd.replace(k, cmd_template[k])
+        cmd = (
+            # Align, stream reads to stdout in SAM format
+            f"{self.paired_cmd}"
+            # Count reads in stream before filtering (2048 + 256 = 2304)
+            f" | tee >(samtools view -F 2304 -c - > '{count_before_path}')"
+            # Discard mapped reads and reads with mapped mates
+            f" | samtools view -f 12 -"
+            # Count reads in stream after filtering
+            f" | tee >(samtools view -F 256 -c - > '{count_after_path}')"
+            # Replace paired read headers with integers
+            f' | awk \'BEGIN{{FS=OFS="\\t"}} {{$1=int((NR+1)/2)" "; print $0}}\''
+            # Stream remaining records into fastq files
+            f" | samtools fastq --threads 2 -c 6 -N -1 '{fastq1_out_path}' -2 '{fastq2_out_path}'"
+        )
+        logging.debug(f"{cmd}")
+        return cmd
---
+++ packages/hostile/hostile-0.0.2-py3-none-any.whl/hostile/cli.py
@@ -0,0 +1,93 @@
+import json
+from pathlib import Path
+
+import defopt
+
+from hostile import lib
+
+
+def clean(
+    *,
+    fastq1: Path,
+    fastq2: Path | None = None,
+    aligner: lib.ALIGNERS = lib.ALIGNERS.bowtie2,
+    custom_index: Path | None = None,
+    out_dir: Path = lib.CWD,
+    threads: int = lib.THREADS,
+    debug: bool = False,
+) -> None:
+    """
+    Remove human reads from paired fastq(.gz) files
+
+    :arg fastq1: path to forward fastq(.gz) file
+    :arg fastq2: optional path to reverse fastq(.gz) file
+    :arg aligner: alignment algorithm
+    :arg custom_index: path to custom index
+    :arg out_dir: output directory for decontaminated fastq.gz files
+    :arg threads: number of CPU threads to use
+    :arg debug: show debug messages
+    """
+    if fastq2:
+        stats = lib.clean_paired_fastqs(
+            [(fastq1, fastq2)],
+            out_dir=out_dir,
+            threads=threads,
+            aligner=aligner,
+            custom_index=custom_index,
+        )
+    else:
+        stats = lib.clean_fastqs(
+            [fastq1],
+            out_dir=out_dir,
+            threads=threads,
+            aligner=aligner,
+            custom_index=custom_index,
+        )
+    print(json.dumps(stats, indent=4))
+
+
+def clean_many(
+    *fastqs: str,
+    aligner: lib.ALIGNERS = lib.ALIGNERS.bowtie2,
+    custom_index: Path | None = None,
+    out_dir: Path = lib.CWD,
+    threads: int = lib.THREADS,
+    debug: bool = False,
+) -> None:
+    """
+    Remove human reads from comma-separated pairs of fastq(.gz) files
+
+    :arg fastqs: path to fastq(.gz) or bam file(s). Paired fastq paths should be comma-separated, e.g. reads_1.fastq.gz,reads_2.fastq.gz
+    :arg aligner: alignment algorithm
+    :arg custom_index: path to custom index
+    :arg out_dir: output directory for decontaminated fastq.gz files
+    :arg threads: number of threads to use
+    :arg debug: show debug messages
+    """
+    if "," in fastqs[0]:  # Paired fastq
+        paired_fastqs = [tuple(pair.split(",")) for pair in fastqs]
+        paired_fastqs = [tuple([Path(fq1), Path(fq2)]) for fq1, fq2 in paired_fastqs]
+        stats = lib.clean_paired_fastqs(
+            paired_fastqs,
+            out_dir=out_dir,
+            threads=threads,
+            aligner=aligner,
+            custom_index=custom_index,
+        )
+        print(json.dumps(stats, indent=4))
+    else:
+        raise NotImplementedError(
+            "Forward and reverse fastq(.gz) paths should be separated with a comma"
+        )
+
+
+def main():
+    defopt.run(
+        {
+            "clean": clean,
+            "clean-many": clean_many,
+        },
+        no_negated_flags=True,
+        strict_kwonly=False,
+        short={},
+    )
