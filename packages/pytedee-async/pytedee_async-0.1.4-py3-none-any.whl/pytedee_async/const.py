790309e261cc484ee5e05464e7639d7f23db9b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:32 +0000

    Add package pytedee-async/0.1.4/pytedee_async-0.1.4-py3-none-any.whl

---
+++ packages/pytedee-async/pytedee_async-0.1.4-py3-none-any.whl/pytedee_async/const.py
@@ -0,0 +1,12 @@
+API_URL_BASE = "https://api.tedee.com/api/v1.30/"
+API_URL_DEVICE = API_URL_BASE + "my/device/"
+API_URL_LOCK = API_URL_BASE + "my/lock/"
+API_URL_SYNC = API_URL_LOCK + "sync"
+
+API_PATH_UNLOCK = "/operation/unlock"
+API_PATH_LOCK = "/operation/lock"
+API_PATH_PULL = "/operation/pull"
+
+TIMEOUT = 10
+UNLOCK_DELAY = 5
+LOCK_DELAY = 5
