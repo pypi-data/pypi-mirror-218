ef26a12e11449d65904b09d819cc6bf1d46063bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:04 +0000

    Add package AshenDB/0.0.7/AshenDB-0.0.7.tar.gz

---
+++ packages/AshenDB/AshenDB-0.0.7.tar.gz/AshenDB-0.0.7/ashendb/exception.py
@@ -0,0 +1,14 @@
+class NotFound(Exception):
+    pass
+
+
+class AlreadyExists(Exception):
+    pass
+
+
+class InvalidArgumentType(Exception):
+    pass
+
+
+class InvalidClusterName(Exception):
+    pass
