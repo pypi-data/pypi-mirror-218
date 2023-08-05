b91521fe8ca71f27369288f3c7c6b406e2e2ddb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:48 +0000

    Add package emailalert/1.4.6/emailalert-1.4.6.tar.gz

---
+++ packages/emailalert/emailalert-1.4.6.tar.gz/emailalert-1.4.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='emailalert',
+    version='1.4.6',
+    description='A simple library for sending alert email with attachmment',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['sck'],
+)
