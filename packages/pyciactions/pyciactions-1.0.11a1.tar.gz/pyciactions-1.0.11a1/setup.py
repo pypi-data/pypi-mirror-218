53ed178ae4307b66b36b6dff81e184ad833bf8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:46 +0000

    Add package pyciactions/1.0.11a1/pyciactions-1.0.11a1.tar.gz

---
+++ packages/pyciactions/pyciactions-1.0.11a1.tar.gz/pyciactions-1.0.11a1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyciactions",
+    version="1.0.11-alpha-1",
+    description="Declarative builder for Github Action Scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Julien Bongars",
+    packages=["pyciactions", "pyciactions.github"],
+    install_requires=["dataclasses==0.6", "PyYAML==6.0"],
+    url="https://github.com/JBongars/pyciactions",
+)
