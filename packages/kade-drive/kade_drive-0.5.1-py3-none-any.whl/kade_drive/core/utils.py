f61ec7d39a8f0e01fc8badbc8f069c808c3cf004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:49 +0000

    Add package kade-drive/0.5.1/kade_drive-0.5.1-py3-none-any.whl

---
+++ packages/kade-drive/kade_drive-0.5.1-py3-none-any.whl/kade_drive/core/utils.py
@@ -0,0 +1,62 @@
+"""
+General catchall for functions that don't make sense as methods.
+"""
+import hashlib
+import operator
+import netifaces as ni
+import logging
+
+
+def digest(string):
+    if not isinstance(string, bytes):
+        string = str(string).encode("utf8")
+    return hashlib.sha1(string).digest()
+
+
+def shared_prefix(args):
+    """
+    Find the shared prefix between the strings.
+
+    For instance:
+
+        sharedPrefix(['blahblah', 'blahwhat'])
+
+    returns 'blah'.
+    """
+    i = 0
+    while i < min(map(len, args)):
+        if len(set(map(operator.itemgetter(i), args))) != 1:
+            break
+        i += 1
+    return args[0][:i]
+
+
+def bytes_to_bit_string(bites):
+    bits = [bin(bite)[2:].rjust(8, "0") for bite in bites]
+    return "".join(bits)
+
+
+def get_ips():
+    # Get all network interfaces
+    interfaces = ni.interfaces()
+
+    # Sort the interfaces by preference: LAN, WLAN, and localhost
+    interfaces = sorted(
+        interfaces, key=lambda x: ("wl" in x, "eth" in x, "en" in x), reverse=True
+    )
+
+    ips = []
+    for interface in interfaces:
+        try:
+            # Get the IP address for the current interface
+
+            ip = ni.ifaddresses(interface)[ni.AF_INET][0]
+            if ip:
+                ips.append(ip)
+        except Exception as e:
+            # Investigate why this fails so much
+            # logger = logging.getLogger(__name__)
+            # logger.warning(f"The following exception was throwed in get_ips {e}")
+            pass
+
+    return ips
