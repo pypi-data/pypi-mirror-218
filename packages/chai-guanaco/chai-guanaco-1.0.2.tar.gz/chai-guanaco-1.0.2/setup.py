c9e7c8884cda028457f53223b25dcdbcea3f98b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package chai-guanaco/1.0.2/chai-guanaco-1.0.2.tar.gz

---
+++ packages/chai-guanaco/chai-guanaco-1.0.2.tar.gz/chai-guanaco-1.0.2/setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+from setuptools import find_packages, setup
+
+current_dir = Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+if os.environ.get('CI_COMMIT_TAG', None):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = "1.0.2"
+
+setup(
+    name='chai-guanaco',
+    version=version,
+    description='Chai Guanaco',
+    author='Chai Research Corp.',
+    author_email='hello@chai-research.com',
+    license='MIT',
+    packages=["chai_guanaco"],
+    package_dir={"chai_guanaco": "./src/chai_guanaco/"},
+    url='https://www.chai-research.com',
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': ['chai-guanaco = chai_guanaco.login_cli:cli'],
+    },
+)
