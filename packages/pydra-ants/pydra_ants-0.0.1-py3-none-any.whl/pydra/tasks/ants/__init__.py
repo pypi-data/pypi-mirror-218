18fedc19b9d077b23f4d4dc35a62957fdfc72729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package pydra-ants/0.0.1/pydra_ants-0.0.1-py3-none-any.whl

---
+++ packages/pydra-ants/pydra_ants-0.0.1-py3-none-any.whl/pydra/tasks/ants/__init__.py
@@ -0,0 +1,10 @@
+"""
+ANTs
+====
+
+>>> from pydra.tasks import ants
+"""
+
+from .apply_transforms import ApplyTransforms
+from .bias_correction import N4BiasFieldCorrection
+from .registration import RegistrationSyNQuick
