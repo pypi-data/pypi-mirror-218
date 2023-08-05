3f3cc6268ee7f4a72e1ae8430fd858c87394153f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:54 +0000

    Add package light-curve/0.7.3/light_curve-0.7.3-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/light-curve/light_curve-0.7.3-cp311-cp311-macosx_10_9_x86_64.whl/light_curve/light_curve_py/features/skew.py
@@ -0,0 +1,15 @@
+from scipy.stats import skew
+
+from ._base import BaseFeature
+
+
+class Skew(BaseFeature):
+    def _eval(self, t, m, sigma=None):
+        return skew(m, bias=False)
+
+    @property
+    def size(self):
+        return 1
+
+
+__all__ = ("Skew",)
