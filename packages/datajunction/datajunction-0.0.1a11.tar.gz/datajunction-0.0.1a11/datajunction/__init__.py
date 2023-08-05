a3523e444fe49f6febdde7c5d2460bb8fc6ccd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:50 +0000

    Add package datajunction/0.0.1a11/datajunction-0.0.1a11.tar.gz

---
+++ packages/datajunction/datajunction-0.0.1a11.tar.gz/datajunction-0.0.1a11/datajunction/__init__.py
@@ -0,0 +1,48 @@
+"""
+A DataJunction client for connecting to a DataJunction server
+"""
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+from datajunction.client import (
+    Cube,
+    Dimension,
+    DJClient,
+    Metric,
+    Namespace,
+    Node,
+    Source,
+    Transform,
+)
+from datajunction.models import (
+    AvailabilityState,
+    ColumnAttribute,
+    Engine,
+    MaterializationConfig,
+    NodeMode,
+)
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    DIST_NAME = __name__
+    __version__ = version(DIST_NAME)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+
+__all__ = [
+    "AvailabilityState",
+    "ColumnAttribute",
+    "DJClient",
+    "Source",
+    "Dimension",
+    "Transform",
+    "MaterializationConfig",
+    "Metric",
+    "Cube",
+    "Node",
+    "NodeMode",
+    "Namespace",
+    "Engine",
+]
