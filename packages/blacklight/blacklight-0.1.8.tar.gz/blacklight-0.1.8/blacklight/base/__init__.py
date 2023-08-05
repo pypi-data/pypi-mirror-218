2e950397f825a3981544b67fa16302e990f64d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:43 +0000

    Add package blacklight/0.1.8/blacklight-0.1.8.tar.gz

---
+++ packages/blacklight/blacklight-0.1.8.tar.gz/blacklight-0.1.8/blacklight/base/__init__.py
@@ -0,0 +1,11 @@
+from .individual import Individual
+from .population import Population
+
+__all__ = [
+    "Individual",
+    "Population",
+    "individuals",
+    "populations",
+    "chromosomes",
+    "utils",
+]
