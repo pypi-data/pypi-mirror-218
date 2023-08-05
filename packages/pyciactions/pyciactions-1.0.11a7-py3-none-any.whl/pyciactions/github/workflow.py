f06fe96febdc80d5b1b89536de71fb9b8d6df0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:43 +0000

    Add package pyciactions/1.0.11a7/pyciactions-1.0.11a7-py3-none-any.whl

---
+++ packages/pyciactions/pyciactions-1.0.11a7-py3-none-any.whl/pyciactions/github/workflow.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional, List, Dict
+
+from pyciactions import IWorkflow
+from .on import On
+from .job import Job
+
+
+@dataclass
+class Workflow(IWorkflow):
+    name: str
+    on: On
+    jobs: List[Job]
+    permissions: Optional[Dict[str, str]] = None
+    env: Optional[Dict[str, str]] = None
+
+    def to_dict(self):
+        result = {
+            "name": self.name,
+            "on": self.on.to_dict(),
+            "jobs": {j.id: j.to_dict() for j in self.jobs},
+        }
+        additional_attrs = {
+            k: v for k, v in vars(self).items() if v is not None and k not in result
+        }
+        result.update(additional_attrs)
+        return result
+
+    def __getstate__(self):
+        return self.to_dict()
