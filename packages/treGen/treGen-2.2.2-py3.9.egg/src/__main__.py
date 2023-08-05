771fdf88680bfd73182bb7a2e157971ed43b7e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:39 +0000

    Add package treGen/2.2.2/treGen-2.2.2-py3.9.egg

---
+++ packages/treGen/treGen-2.2.2-py3.9.egg/src/__main__.py
@@ -0,0 +1,8 @@
+"""This module provides RP Tree entry point script."""
+
+from src import cli
+
+def main():
+    cli.main()
+
+#main()
---
+++ packages/treGen/treGen-2.2.2-py3.9.egg/src/tree.py
@@ -0,0 +1,6 @@
+"""This module provides RP Tree entry point script."""
+
+from src import cli
+
+if __name__ == "__main__":
+    cli.main()
