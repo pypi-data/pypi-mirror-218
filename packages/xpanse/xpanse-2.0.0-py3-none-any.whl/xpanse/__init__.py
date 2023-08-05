5229096d663fc447b11d6c1f15ab582279f4006a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:14 +0000

    Add package xpanse/2.0.0/xpanse-2.0.0-py3-none-any.whl

---
+++ packages/xpanse/xpanse-2.0.0-py3-none-any.whl/xpanse/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "2.0.0"
+__author__ = "Palo Alto Cortex Xpanse"
+__license__ = "ISC"
+__maintainer__ = "Palo Alto Cortex Xpanse"
+__email__ = "xpanse-integrations@paloaltonetworks.com"
+__status__ = "Production"
