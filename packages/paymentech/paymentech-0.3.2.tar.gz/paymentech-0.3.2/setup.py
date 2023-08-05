83c203c40b160cc84bb002072365ef70dafc019b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package paymentech/0.3.2/paymentech-0.3.2.tar.gz

---
+++ packages/paymentech/paymentech-0.3.2.tar.gz/paymentech-0.3.2/setup.py
@@ -0,0 +1,42 @@
+"""Python SDK for Chase Paymentech"""
+import os
+import setuptools
+
+
+base_path = os.path.split(__file__)[0]
+
+readme_path = os.path.join(base_path, "README.md")
+with open(readme_path, "r") as readme_file:
+    long_description = readme_file.read()
+
+version_path = os.path.join(base_path, "paymentech/version.py")
+with open(version_path, "r") as version_file:
+    version = ""
+    # Execute the code in version.py.
+    exec(compile(version_file.read(), version_path, 'exec'))
+
+requirements_path = os.path.join(base_path, "requirements.txt")
+with open(requirements_path, "r") as requirements_file:
+    install_requires = requirements_file.read().splitlines()
+
+setuptools.setup(
+    name="paymentech",
+    version=version,
+    author="Jordan Ambra",
+    author_email="jordan@serenity.software",
+    description="Python SDK for Chase Paymentech",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SerenitySoftware/paymentech",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: Freely Distributable"
+    ],
+    keywords=['chase', 'paymentech', 'e-commerce', 'payments'],
+    install_requires=install_requires,
+    package_data={'paymentech': ['templates/*.xml']}
+)
