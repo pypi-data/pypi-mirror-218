5ca6b5f54ffee4409490d05d1abb3223b711a7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:54 +0000

    Add package policyengine-us/0.357.1/policyengine_us-0.357.1-py3-none-any.whl

---
+++ packages/policyengine-us/policyengine_us-0.357.1-py3-none-any.whl/policyengine_us/tests/microsimulation/test_microsim.py
@@ -0,0 +1,6 @@
+def test_microsim_runs_cps():
+    from policyengine_us import Microsimulation
+
+    sim = Microsimulation()
+    hnet = sim.calc("household_net_income")
+    assert not hnet.isna().any(), "Some households have NaN net income."
