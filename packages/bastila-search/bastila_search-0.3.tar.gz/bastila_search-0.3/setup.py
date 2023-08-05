a04e1450d91de0f9079fb2b72075cc57228bf96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:22 +0000

    Add package bastila-search/0.3/bastila_search-0.3.tar.gz

---
+++ packages/bastila-search/bastila_search-0.3.tar.gz/bastila_search-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bastila_search',
+    version='0.3',
+    description='A python script that catches commits that introduce predefined deprecated patterns',
+    url='https://github.com/GetBastila/bastila-hook',
+    author='Bastila',
+    author_email='hello@bastila.app',
+    license='MIT',
+    packages=['bastila_search'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe=False
+)
