2aebfdd1ead43fd930930cd002af94008254238f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package hello-whmou/0.1/hello-whmou-0.1.tar.gz

---
+++ packages/hello-whmou/hello-whmou-0.1.tar.gz/hello-whmou-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hello-whmou',
+    version='0.1',
+    description='Hello World App',
+    py_modules=['hello'],
+    author='whmou',
+    author_email='whmou0@gmail.com',
+    url='https://github.com/yourusername/hello-world-app',
+)
