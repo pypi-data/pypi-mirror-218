a0dd5e4e592299550de03013beda50ea5f3c2b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:22 +0000

    Add package pojediga/1.0.1/pojediga-1.0.1.tar.gz

---
+++ packages/pojediga/pojediga-1.0.1.tar.gz/pojediga-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pojediga",
+    version="1.0.1",
+    author="🇷🇸",
+    author_email="ja_sam_lekar_za_sve@pojediga.rs",
+    description="A simple pojediga package to feel good every day, every night",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["pojediga"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
