b4f503c30c42c4edc44b65aceab482c4e63cf0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:16 +0000

    Add package scriptime/0.1.2/scriptime-0.1.2.tar.gz

---
+++ packages/scriptime/scriptime-0.1.2.tar.gz/scriptime-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="scriptime",
+    version="0.1.2",
+    author="Jake Strasler",
+    author_email="jstr36@gmail.com",
+    description="A Python library to notify when a script has finished running and providing insights such as run time, CPU and RAM usage, and more.",
+    packages=["scriptime"],
+    package_data={"scriptime": ["scriptime/alert.wav"]},
+    include_package_data=True,
+    install_requires=["psutil", "simpleaudio", "setuptools"],
+)
