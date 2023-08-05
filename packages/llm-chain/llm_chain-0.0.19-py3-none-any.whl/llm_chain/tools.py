820172612fa6b8386c99f8f40b9789a355a1de70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package llm-chain/0.0.19/llm_chain-0.0.19-py3-none-any.whl

---
+++ packages/llm-chain/llm_chain-0.0.19-py3-none-any.whl/llm_chain/tools.py
@@ -0,0 +1,248 @@
+"""Contains misc tools that can be used within a Chain."""
+import os
+from itertools import islice
+import re
+import requests
+from datetime import datetime
+from pydantic import BaseModel, Field, validator
+from llm_chain.utilities import retry_handler
+from llm_chain.base import Document, Link, Record
+from llm_chain.exceptions import RequestError
+
+
+def split_documents(
+        docs: list[Document],
+        max_chars: int = 500,
+        preserve_words: bool = True) -> list[Document]:
+    """
+    split_documents divides a list of documents into smaller segments based on the specified
+    maximum character limit for each individual Document object.
+
+    Args:
+        docs:
+            A list of Document objects to be segmented.
+        max_chars:
+            The maximum allowable character count for each Document object.
+        preserve_words:
+            If set to True, guarantees that the document segmentation does not occur in the middle
+            of a word, ensuring the integrity of the entire word.
+    """
+    new_docs = []
+    for doc in docs:
+        if doc.content:
+            content = doc.content.strip()
+            metadata = doc.metadata
+            while len(content) > max_chars:
+                # find the last space that is within the limit
+                if preserve_words:
+                    split_at = max_chars
+                    # find the last whitespace that is within the limit
+                    # if no whitespace found, take the whole chunk
+                    # we are going 1 beyond the limit in case it is whitespace so that
+                    # we can keep everything up until that point
+                    for match in re.finditer(r'\s', content[:max_chars + 1]):
+                        split_at = match.start()
+                else:
+                    split_at = max_chars
+                new_doc = Document(
+                    content=content[:split_at].strip(),
+                    metadata=metadata,
+                )
+                new_docs.append(new_doc)
+                content = content[split_at:].strip()  # remove the chunk we added
+            # check for remaining/leftover content
+            if content:
+                new_docs.append(Document(
+                    content=content,
+                    metadata=metadata,
+                ))
+    return new_docs
+
+
+def scrape_url(url: str) -> str:
+    """
+    Scrapes the content of a given URL and returns it as a string.
+
+    Args:
+        url: The URL to scrape.
+    """
+    from bs4 import BeautifulSoup
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RequestError(status_code=response.status_code, reason=response.reason)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup.get_text().strip()
+
+
+class SearchRecord(Record):
+    """Contains the query and requests from a search request (e.g. from a web-search)."""
+
+    query: str
+    results: list[dict]
+
+
+class DuckDuckGoSearch(Link):
+    """
+    A wrapper around DuckDuckGo web search. The object is called with a query and returns a list of
+    SearchRecord objects associated with the top search results.
+    """
+
+    def __init__(self, top_n: int = 3):
+        self.top_n = top_n
+        self._history = []
+
+    def __call__(self, query: str) -> list[dict]:
+        """
+        The object is called with a query and returns a list of SearchRecord objects associated
+        with the `top_n` search results. `top_n` is set during object initialization.
+        """
+        from duckduckgo_search import DDGS
+        with DDGS() as ddgs:
+            ddgs_generator = ddgs.text(query, region='wt-wt', safesearch='Off', timelimit='y')
+            results = list(islice(ddgs_generator, self.top_n))
+        self._history.append(SearchRecord(
+            query=query,
+            results=results,
+        ))
+        return results
+
+    @property
+    def history(self) -> list[SearchRecord]:
+        """A history of web-searches (list of SearchResult objects)."""
+        return self._history
+
+
+class StackAnswer(BaseModel):
+    """
+    An object that encapsulates the contents of a Stack Overflow answer.
+
+    The body property holds the original answer in HTML format. The text property provides the
+    same content with HTML tags removed, while the markdown property offers the content converted
+    to Markdown format.
+    """
+
+    answer_id: int
+    is_accepted: bool
+    score: int
+    body: str
+    text: str | None
+    markdown: str | None
+    creation_date: int
+
+    def __init__(self, **data):  # noqa
+        from bs4 import BeautifulSoup
+        from markdownify import markdownify as html_to_markdown
+        super().__init__(**data)
+        self.text = BeautifulSoup(self.body, 'html.parser').get_text(separator=' ')
+        self.markdown = html_to_markdown(self.body)
+
+
+    @validator('creation_date')
+    def convert_to_datetime(cls, value: str) -> datetime:  # noqa: N805
+        """Convert from string to datetime."""
+        return datetime.fromtimestamp(value)
+
+
+class StackQuestion(BaseModel):
+    """
+    An object that encapsulates the contents of a Stack Overflow question.
+
+    The body property holds the original question in HTML format. The text property provides the
+    same content with HTML tags removed, while the markdown property offers the content converted
+    to Markdown format.
+    """
+
+    question_id: int
+    score: int
+    creation_date: int
+    answer_count: int
+    title: str
+    link: str
+    body: str
+    text: str | None
+    markdown: str | None
+    content_license: str | None
+    answers: list[StackAnswer] = Field(default_factory=list)
+
+    def __init__(self, **data):  # noqa
+        from bs4 import BeautifulSoup
+        from markdownify import markdownify as html_to_markdown
+        super().__init__(**data)
+        self.text = BeautifulSoup(self.body, 'html.parser').get_text(separator=' ')
+        self.markdown = html_to_markdown(self.body)
+
+    @validator('creation_date')
+    def convert_to_datetime(cls, value: str) -> datetime:  # noqa: N805
+        """Convert from string to datetime."""
+        return datetime.fromtimestamp(value)
+
+
+def _get_stack_overflow_answers(question_id: int, max_answers: int = 2) -> list[StackAnswer]:
+    """For a given question_id on Stack Overflow, returns the top `num_answers`."""
+    params = {
+        'site': 'stackoverflow',
+        'key': os.getenv('STACK_OVERFLOW_KEY'),
+        'filter': 'withbody',  # Include the answer body in the response
+        'sort': 'votes',  # Sort answers by votes (highest first)
+        'order': 'desc',
+        'pagesize': max_answers,  # Fetch only the top answers
+    }
+    response = retry_handler()(
+        requests.get,
+        f'https://api.stackexchange.com/2.3/questions/{question_id}/answers',
+        params=params,
+    )
+    if response.status_code != 200:  # let client decide what to do
+        raise RequestError(status_code=response.status_code, reason=response.reason)
+    answers = response.json().get('items', [])
+    return [StackAnswer(**x) for x in answers]
+
+
+def search_stack_overflow(
+        query: str,
+        max_questions: int = 2,
+        max_answers: int = 2) -> list[StackQuestion]:
+    """
+    Retrieves the top relevant Stack Overflow questions based on a given search query.
+    The function assumes that the STACK_OVERFLOW_KEY environment variable is properly set and
+    contains a valid Stack Overflow API key.
+
+    To obtain an API key, please create an account and an app at [Stack Apps](https://stackapps.com/).
+    Use the `key` generated for your app (not the `secret`).
+
+    Args:
+        query:
+            The search query used to find relevant Stack Overflow questions.
+        max_questions:
+            The maximum number of questions to be returned. If the API doesn't find enough relevant
+            questions, fewer questions may be returned.
+        max_answers:
+            The maximum number of answers to be returned with each question. If the number of
+            answers is fewer than `max_answers`, fewer answers will be returned.
+    """
+    params = {
+        'site': 'stackoverflow',
+        'key': os.getenv('STACK_OVERFLOW_KEY'),
+        'q': query,
+        'sort': 'relevance',
+        'order': 'desc',
+        'filter': 'withbody',  # Include the question body in the response
+        'pagesize': max_questions,
+        'page': 1,
+    }
+    response = retry_handler()(
+        requests.get,
+        'https://api.stackexchange.com/2.3/search/advanced',
+        params=params,
+    )
+    assert response.status_code == 200
+    questions = response.json().get('items', [])
+    questions = [StackQuestion(**x) for x in questions]
+
+    for question in questions:
+        if question.answer_count > 0:
+            question.answers = _get_stack_overflow_answers(
+                question_id=question.question_id,
+                max_answers=max_answers,
+            )
+    return questions
---
+++ packages/llm-chain/llm_chain-0.0.19-py3-none-any.whl/tests/test_utilities.py
@@ -0,0 +1,110 @@
+"""Tests the utilities.py file."""
+from time import sleep
+import re
+import pytest
+import openai
+from llm_chain.utilities import Timer, create_hash, has_method, has_property, num_tokens, \
+    num_tokens_from_messages, retry_handler
+
+def test_timer_seconds():  # noqa
+    with Timer() as timer:
+        sleep(1.1)
+
+    assert timer.interval
+    assert re.match(pattern=r'1\.\d+ seconds', string=timer.formatted())
+    assert str(timer) == timer.formatted()
+
+    with pytest.raises(ValueError):  # noqa
+        timer.formatted(units='days')
+
+def test_create_hash():  # noqa
+    value_a = create_hash('Test value 1')
+    assert value_a
+    value_b = create_hash('Test value 2')
+    assert value_b
+    assert value_a != value_b
+    value_c = create_hash('Test value 1')
+    assert value_c == value_a
+
+def test_num_tokens():  # noqa
+    assert num_tokens(model_name='gpt-3.5-turbo', value="This should be six tokens.") == 6
+
+def test_num_tokens_from_messages():  # noqa
+    # copied from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+    example_messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful, pattern-following assistant that translates corporate jargon into plain English.",  # noqa
+        },
+        {
+            "role": "system",
+            "name": "example_user",
+            "content": "New synergies will help drive top-line growth.",
+        },
+        {
+            "role": "system",
+            "name": "example_assistant",
+            "content": "Things working well together will increase revenue.",
+        },
+        {
+            "role": "system",
+            "name": "example_user",
+            "content": "Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage.",  # noqa
+        },
+        {
+            "role": "system",
+            "name": "example_assistant",
+            "content": "Let's talk later when we're less busy about how to do better.",
+        },
+        {
+            "role": "user",
+            "content": "This late pivot means we don't have time to boil the ocean for the client deliverable.",  # noqa
+        },
+    ]
+    model_name = 'gpt-3.5-turbo'
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=example_messages,
+        temperature=0,
+        max_tokens=1,  # we're only counting input tokens here, so let's not waste tokens on output
+    )
+    expected_value = response["usage"]["prompt_tokens"]
+    actual_value = num_tokens_from_messages(model_name=model_name, messages=example_messages)
+    assert expected_value == actual_value
+
+    # above we checked that the numbers match exactly from what OpenAI returns;
+    # here, let's just check that the other models run and return >0 to avoid API calls
+    assert num_tokens_from_messages(model_name='gpt-3.5-turbo-0301', messages=example_messages) > 0
+    assert num_tokens_from_messages(model_name='gpt-4', messages=example_messages) > 0
+    assert num_tokens_from_messages(model_name='gpt-4-0314', messages=example_messages) > 0
+    with pytest.raises(NotImplementedError):
+        num_tokens_from_messages(model_name='<not implemented>', messages=example_messages)
+
+def test_retry_handler():  # noqa
+    r = retry_handler()
+    actual_value = r(
+        lambda x, y: (x, y),
+        x='A',
+        y='B',
+    )
+    assert actual_value == ('A', 'B')
+
+def test_has_method_has_property():  # noqa
+    class Fake:
+        def __init__(self) -> None:
+            self.variable_c = 'c'
+
+        def method_a(self) -> str:
+            return 'a'
+
+        @property
+        def property_b(self) -> str:
+            return 'b'
+
+    assert has_method(Fake(), 'method_a')
+    assert not has_method(Fake(), 'property_b')
+    assert not has_method(Fake(), 'variable_c')
+
+    assert not has_property(Fake(), 'method_a')
+    assert has_property(Fake(), 'property_b')
+    assert has_property(Fake(), 'variable_c')
