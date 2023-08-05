121e1934288de932ec850e64ec84e87bcf57594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:21 +0000

    Add package edrn.bmdb.testing/0.0.0/edrn.bmdb.testing-0.0.0.tar.gz

---
+++ packages/edrn.bmdb.testing/edrn.bmdb.testing-0.0.0.tar.gz/edrn.bmdb.testing-0.0.0/README.md
@@ -0,0 +1,3 @@
+# EDRN BMDB Testing
+
+This package tests the [Focus Biomarker Database](https://bmdb.jpl.nasa.gov/) of the [Early Detection Research Network](https://edrn.nci.nih.gov).
---
+++ packages/edrn.bmdb.testing/edrn.bmdb.testing-0.0.0.tar.gz/edrn.bmdb.testing-0.0.0/src/edrn/bmdb/testing/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+'''🧪 Testing of the EDRN Focus Biomarker Database.'''
+
+import importlib.resources
+
+
+PACKAGE_NAME = 'edrn.bmdb.testing'
+__version__ = VERSION =  importlib.resources.read_text(__name__, 'VERSION.txt')
