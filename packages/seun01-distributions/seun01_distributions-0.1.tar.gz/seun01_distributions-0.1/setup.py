71eb5e325c00211e1fdfc6db755e2112d9db61b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:28 +0000

    Add package seun01-distributions/0.1/seun01_distributions-0.1.tar.gz

---
+++ packages/seun01-distributions/seun01_distributions-0.1.tar.gz/seun01_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='seun01_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['seun01_distributions'],
+      author = 'Aboderin Samuel',
+      zip_safe=False)
