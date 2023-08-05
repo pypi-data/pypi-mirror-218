047b40e7ec37e6018e51754ba8ded2280961bd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package snowpark-session/0.1.1/snowpark_session-0.1.1-py3-none-any.whl

---
+++ packages/snowpark-session/snowpark_session-0.1.1-py3-none-any.whl/it.py
@@ -0,0 +1,28 @@
+# %%
+from snowpark_session import DatabricksSQLSession
+
+# %%
+spark_sql = DatabricksSQLSession()
+# %%
+pdf = spark_sql.query_lakehouse("select * from microchip_logs limit 10")
+print(pdf.head())
+# %%
+
+from snowpark_session import DatabricksSparkSession
+
+# %%
+spark = DatabricksSparkSession().get_session()
+
+# %%
+sdf = spark.read.table("microchip_logs")
+print(sdf.show())
+# %%
+
+from snowpark_session import DatabricksMLFlowSession
+
+# %%
+mlflow_session = DatabricksMLFlowSession().get_session()
+
+# %%
+print(mlflow_session.client.MlflowClient())
+# %%
