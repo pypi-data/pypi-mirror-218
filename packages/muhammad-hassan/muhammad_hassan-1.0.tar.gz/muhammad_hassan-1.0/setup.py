1ead33c8566a3f13de3e285019532f2e9ebbe67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:59 +0000

    Add package muhammad-hassan/1.0/muhammad_hassan-1.0.tar.gz

---
+++ packages/muhammad-hassan/muhammad_hassan-1.0.tar.gz/muhammad_hassan-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='muhammad_hassan',
+      version='1.0',
+      description='Gaussian distributions and Binomial distribution',
+      packages=['muhammad_hassan'],
+      author = "Muhammad Hassan",
+      zip_safe=False)
