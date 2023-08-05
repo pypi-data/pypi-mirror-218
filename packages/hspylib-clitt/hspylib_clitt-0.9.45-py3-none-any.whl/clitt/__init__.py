8faf284c384dd0a2af9035c589e2b7e0dfa01954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:31 +0000

    Add package hspylib-clitt/0.9.45/hspylib_clitt-0.9.45-py3-none-any.whl

---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.45-py3-none-any.whl/clitt/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.45
+#
+# Package: main.clitt
+"""Package initialization."""
+
+__all__ = [
+  'addons', 
+  'core'
+]
+__version__ = '0.9.45'
---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.45-py3-none-any.whl/clitt/core/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.45
+#
+# Package: main.clitt.core
+"""Package initialization."""
+
+__all__ = [
+  'icons', 
+  'tui'
+]
+__version__ = '0.9.45'
