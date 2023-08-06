2e9ff572ba8aa5e41c2524f7de1d7e478f0f62d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package aiochris/0.2.1/aiochris-0.2.1.tar.gz

---
+++ packages/aiochris/aiochris-0.2.1.tar.gz/aiochris-0.2.1/aiochris/models/public.py
@@ -0,0 +1,41 @@
+"""
+Read-only models for CUBE resources.
+"""
+from dataclasses import dataclass
+
+import serde
+from serde import deserialize
+
+from aiochris.link.linked import LinkedModel
+from aiochris.models.enums import PluginType
+from aiochris.models.types import *
+
+
+@deserialize
+@dataclass(frozen=True)
+class PublicPlugin(LinkedModel):
+    """
+    A ChRIS plugin.
+    """
+
+    url: PluginUrl
+    id: PluginId
+    name: PluginName
+    version: PluginVersion
+    dock_image: ImageTag
+    public_repo: str
+    plugin_type: PluginType = serde.field(rename="type")
+
+
+@deserialize
+@dataclass(frozen=True)
+class ComputeResource:
+    url: ApiUrl
+    id: ComputeResourceId
+    creation_date: str
+    modification_date: str
+    name: ComputeResourceName
+    compute_url: PfconUrl
+    compute_auth_url: str
+    description: str
+    max_job_exec_seconds: int
