00aa2618f28a12b02e6845341c1c93be0586bc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:23 +0000

    Add package bondzai.gateway-sdk/0.0.8/bondzai.gateway_sdk-0.0.8-py3-none-any.whl

---
+++ packages/bondzai.gateway-sdk/bondzai.gateway_sdk-0.0.8-py3-none-any.whl/bondzai/gateway_sdk/transcode.py
@@ -0,0 +1,43 @@
+from .enums import EventOperationID, AgentAIType
+
+
+def transcode(op: EventOperationID, data: dict):
+    transcode_data = data
+
+    if op == EventOperationID.EVT_EXT_ASL_RESULT:
+        if data["label_type"] != 0:  # Classification
+            transcode_data = {
+                "label_type": data["label_type"],
+                "confidence": data["confidence"],
+                "label": data["result"][0]
+            }
+        else:
+            transcode_data = {
+                "confidence": data["confidence"],
+                "regression": data["result"]
+            }
+
+    elif op == EventOperationID.EVT_EXT_VM_RESULT:
+        if data["ai_type"] == AgentAIType.APP_AI_TYPE_CLASSIFICATION.value:
+            results = data["result"]
+            res_nb = int(len(results) / 2)
+            confidences = {}
+            for k in range(res_nb):
+                confidences[results[2 * k + 1]] = results[2 * k]
+
+            transcode_data = {
+                "ai_type": data["ai_type"],
+                "label_type": data["label_type"],
+                "step": data["step"],
+                "confidences": confidences
+            }
+
+        else:
+            transcode_data = {
+                "ai_type": data["ai_type"],
+                "label_type": data["label_type"],
+                "step": data["step"],
+                "regression": data["result"]
+            }
+
+    return transcode_data
