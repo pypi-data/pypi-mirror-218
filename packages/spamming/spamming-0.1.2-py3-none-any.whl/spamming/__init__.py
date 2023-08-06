1c75d803d4c98afdbbc31b507b2047c4be590be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:57 +0000

    Add package spamming/0.1.2/spamming-0.1.2-py3-none-any.whl

---
+++ packages/spamming/spamming-0.1.2-py3-none-any.whl/spamming/__init__.py
@@ -0,0 +1,7 @@
+import spamming.nonsense as nonsense
+import spamming.spamclick as spamclick
+
+"""
+Spamming: a simple package for spamming people
+go to www.pypi.org/project/spamming/0.1.2/ to see its documentation
+"""
