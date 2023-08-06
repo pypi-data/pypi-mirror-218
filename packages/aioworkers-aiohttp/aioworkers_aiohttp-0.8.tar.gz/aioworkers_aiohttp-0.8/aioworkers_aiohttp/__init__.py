9522c40bd181718caae7dc0252ed421834f03f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:58 +0000

    Add package aioworkers-aiohttp/0.8/aioworkers_aiohttp-0.8.tar.gz

---
+++ packages/aioworkers-aiohttp/aioworkers_aiohttp-0.8.tar.gz/aioworkers_aiohttp-0.8/aioworkers_aiohttp/__init__.py
@@ -0,0 +1,38 @@
+import argparse
+from pathlib import Path
+from typing import Dict, Iterable, Tuple
+
+from aioworkers.core.plugin import Plugin
+
+from .server import WebServer
+
+
+__version__ = "0.8"
+
+BASE = Path(__file__).parent
+
+
+class plugin(Plugin):
+    configs = (BASE / "plugin.ini",)
+
+    def __init__(self):
+        super().__init__()
+        self._config = {}
+
+    def get_config(self) -> Dict:
+        return self._config
+
+    def add_arguments(self, parser: argparse.ArgumentParser):
+        try:
+            parser.add_argument('--port', type=int)
+        except argparse.ArgumentError:
+            pass
+
+    def parse_known_args(
+        self,
+        args: Iterable[str],
+        namespace: argparse.Namespace,
+    ) -> Tuple[argparse.Namespace, Iterable[str]]:
+        if namespace.port:
+            WebServer.set_port(namespace.port)
+        return namespace, args
