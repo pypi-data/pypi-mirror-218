23a64f8914a25aeaedc15667d671c34fcba43453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:40 +0000

    Add package roxbot/0.10.0/roxbot-0.10.0-py3-none-any.whl

---
+++ packages/roxbot/roxbot-0.10.0-py3-none-any.whl/roxbot/interfaces.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+ common interface definitions
+
+ Copyright (c) 2023 ROX Automation
+"""
+
+from typing import NamedTuple, Tuple
+
+
+class Pose(NamedTuple):
+    """2D pose interface"""
+
+    x: float = 0.0
+    y: float = 0.0
+    theta: float = 0.0
+
+    @property
+    def xy(self) -> Tuple[float, float]:
+        return self.x, self.y
+
+    def __str__(self) -> str:
+        return f"x={self.x:.2f}, y={self.y:.2f}, theta={self.theta:.2f}"
