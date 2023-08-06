a78c5bd1cc25bc07a8c106ab92f1e3e562f0f7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:47 +0000

    Add package yambs/1.10.3/yambs-1.10.3-py3-none-any.whl

---
+++ packages/yambs/yambs-1.10.3-py3-none-any.whl/yambs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=217418149725457b4f282d956da0a102
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Yet another meta build-system."
+PKG_NAME = "yambs"
+VERSION = "1.10.3"
---
+++ packages/yambs/yambs-1.10.3-py3-none-any.whl/yambs/config/native.py
@@ -0,0 +1,10 @@
+"""
+A module implementing a configuration interface for native builds.
+"""
+
+# internal
+from yambs.config.common import CommonConfig
+
+
+class Native(CommonConfig):
+    """The top-level configuration object for the package."""
