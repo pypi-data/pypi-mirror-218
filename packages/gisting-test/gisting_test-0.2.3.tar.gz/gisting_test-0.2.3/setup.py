b8d840ab7461647341ae0f6bec95dad023cd1c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:05 +0000

    Add package gisting-test/0.2.3/gisting_test-0.2.3.tar.gz

---
+++ packages/gisting-test/gisting_test-0.2.3.tar.gz/gisting_test-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="gisting_test",
+    version="0.2.3",
+    author="Owais Zahid",
+    email="owais.zahid@mail.utoronto.ca",
+    packages=['gisting_test','gisting_test/src','gisting_test/src/data'],
+    description="Test version for gisting",
+    url='https://github.com/jayelm/gisting',
+
+    classifiers=['Natural Language :: English',
+        'Programming Language :: Python :: 3']
+)
