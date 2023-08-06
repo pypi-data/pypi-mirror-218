cd1400ae4fc759b4747248766f18b7ec410243ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:27 +0000

    Add package gigs-client/0.0.1/gigs_client-0.0.1.tar.gz

---
+++ packages/gigs-client/gigs_client-0.0.1.tar.gz/gigs_client-0.0.1/gigs_client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from gigs_client.api.addons_api import AddonsApi
+from gigs_client.api.devices_api import DevicesApi
+from gigs_client.api.plans_api import PlansApi
+from gigs_client.api.portings_api import PortingsApi
+from gigs_client.api.projects_api import ProjectsApi
+from gigs_client.api.sims_api import SIMsApi
+from gigs_client.api.subscription_addons_api import SubscriptionAddonsApi
+from gigs_client.api.subscription_changes_api import SubscriptionChangesApi
+from gigs_client.api.subscriptions_api import SubscriptionsApi
+from gigs_client.api.usage_api import UsageApi
+from gigs_client.api.user_addresses_api import UserAddressesApi
+from gigs_client.api.users_api import UsersApi
+
