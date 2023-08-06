5440bb10d636d329199e0b29b2abfbf5b37ccccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:27 +0000

    Add package f3dasm/1.2.0/f3dasm-1.2.0.tar.gz

---
+++ packages/f3dasm/f3dasm-1.2.0.tar.gz/f3dasm-1.2.0/src/f3dasm/run_optimization.py
@@ -0,0 +1,221 @@
+"""
+Module to optimize benchmark optimization functions
+"""
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+import json
+import time
+from typing import Any, List, Type
+
+# Third-party
+import numpy as np
+import pandas as pd
+from pathos.helpers import mp
+
+from f3dasm.optimization import Optimizer
+from f3dasm.sampling import Sampler
+
+# Locals
+from .design import ExperimentData
+from .datageneration.functions import create_function_from_json
+from .datageneration.functions.function import Function
+from .logger import logger, time_and_log
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+
+class OptimizationResult:
+    def __init__(self, data: List[ExperimentData], optimizer: Optimizer, function: Function,
+                 sampler: Sampler, number_of_samples: int, seeds: List[int]):
+        """Optimization results object
+
+        Parameters
+        ----------
+        data
+            Data objects for each realization
+        optimizer
+            classname of the optimizer used
+        function
+            functionname that was optimized
+        sampler
+            classname of the initial sampling strategy
+        number_of_samples
+            number of initial samples, sampled by the sampling strategy
+        seeds
+            list of seeds that were used for each realization
+        """
+        self.data = data
+        self.optimizer = optimizer
+        self.function = function
+        self.sampler = sampler
+        self.number_of_samples = number_of_samples
+        self.seeds = seeds
+        self._log()
+
+    def to_json(self):
+        args = {'data': [d.to_json() for d in self.data],
+                'optimizer': self.optimizer.to_json(),
+                'function': self.function.to_json(),
+                'sampler': self.sampler.to_json(),
+                'number_of_samples': self.number_of_samples,
+                'seeds': self.seeds
+                }
+
+        return json.dumps(args)
+
+    def _log(self):
+        # Log
+        logger.info(
+            (f"Optimized {self.function.get_name()} function (seed={self.function.seed}, "
+             f"dim={self.function.dimensionality}, "
+             f"with {self.optimizer.get_name()} optimizer for "
+             f"{len(self.data)} realizations!")
+        )
+
+
+def run_optimization(
+    optimizer: Optimizer,
+    function: Function,
+    sampler: Sampler,
+    iterations: int,
+    seed: int,
+    number_of_samples: int = 30,
+) -> ExperimentData:
+    """Run optimization on some benchmark function
+
+    Parameters
+    ----------
+    optimizer
+        the optimizer used
+    function
+        the function to be optimized
+    sampler
+        the sampling strategy
+    iterations
+        number of iterations
+    seed
+        seed for the random number generator
+    number_of_samples, optional
+        number of initial samples, sampled by the sampling strategy
+
+    Returns
+    -------
+        Data object with the optimization data results
+    """
+
+    # Set function seed
+    # function.set_seed(seed)
+    optimizer.set_seed(seed)
+    sampler.set_seed(seed)
+
+    # Sample
+    samples = sampler.get_samples(numsamples=number_of_samples)
+
+    samples.add_output(output=function(samples), label="y")
+
+    optimizer.set_data(samples)
+
+    # Iterate
+    optimizer.iterate(iterations=iterations, function=function)
+    res = optimizer.extract_data()
+
+    # Reset the parameters
+    optimizer.__post_init__()
+
+    # Reset data
+    optimizer.data.reset_data()
+
+    return res
+
+
+@time_and_log
+def run_multiple_realizations(
+    optimizer: Optimizer,
+    function: Function,
+    sampler: Sampler,
+    iterations: int,
+    realizations: int,
+    number_of_samples: int = 30,
+    parallelization: bool = True,
+    verbal: bool = False,
+    seed: int or Any = None,
+) -> OptimizationResult:
+    """Run multiple realizations of the same algorithm on a benchmark function
+
+    Parameters
+    ----------
+    optimizer
+        the optimizer used
+    function
+        the function to be optimized
+    sampler
+        the sampling strategy
+    iterations
+        number of iterations
+    realizations
+        number of realizations
+    number_of_samples, optional
+        number of initial samples, sampled by the sampling strategy
+    parallelization, optional
+        set True to enable parallel execution of each realization
+    verbal, optional
+        set True to have more debug message
+    seed, optional
+        seed for the random number generator
+
+    Returns
+    -------
+        Object with the optimization data results
+    """
+    if seed is None:
+        seed = np.random.randint(low=0, high=1e5)
+
+    if parallelization:
+        args = [
+            (optimizer, function, sampler, iterations,
+             seed + index, number_of_samples)
+            for index, _ in enumerate(range(realizations))
+        ]
+
+        with mp.Pool() as pool:
+            # maybe implement pool.starmap_async ?
+            results = pool.starmap(run_optimization, args)
+
+    else:
+        results = []
+        for index in range(realizations):
+            args = {
+                "optimizer": optimizer,
+                "function": function,
+                "sampler": sampler,
+                "iterations": iterations,
+                "number_of_samples": number_of_samples,
+                "seed": seed + index,
+            }
+            results.append(run_optimization(**args))
+
+    return OptimizationResult(
+        data=results,
+        optimizer=optimizer,
+        function=function,
+        sampler=sampler,
+        number_of_samples=number_of_samples,
+        seeds=[seed + i for i in range(realizations)],
+    )
+
+
+def calculate_mean_std(results):  # OptimizationResult
+    mean_y = pd.concat([d.get_output_data().cummin()
+                       for d in results.data], axis=1).mean(axis=1)
+    std_y = pd.concat([d.get_output_data().cummin()
+                      for d in results.data], axis=1).std(axis=1)
+    return mean_y, std_y
---
+++ packages/f3dasm/f3dasm-1.2.0.tar.gz/f3dasm-1.2.0/src/f3dasm/sampling/sampler.py
@@ -0,0 +1,195 @@
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+import json
+from typing import Any, List, Union
+
+# Third-party core
+import numpy as np
+import pandas as pd
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+
+# Locals
+from ..design.domain import Domain
+from ..design.experimentdata import ExperimentData
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+
+class Sampler:
+    """Interface for sampling method
+
+    Parameters
+    ----------
+    design
+        design of experiments object
+    seed
+        seed for sampling
+    """
+
+    def __init__(self, design: Domain, seed: Union[Any, int] = None, number_of_samples: int = None):
+        self.design = design
+        self.seed = seed
+        self.number_of_samples = number_of_samples
+        self.__post_init__()
+
+    def __post_init__(self):
+        if self.seed:
+            np.random.seed(self.seed)
+
+    def __eq__(self, __o: object) -> bool:
+        return all((self.design == __o.design, self.seed == __o.seed))
+
+    @classmethod
+    def from_yaml(cls, config: DictConfig) -> 'Sampler':
+        """Create a sampler from a yaml configuration"""
+
+        args = {**config.sampler, 'design': None}
+        sampler: Sampler = instantiate(args)
+        sampler.design = Domain.from_yaml(config.design)
+        return sampler
+
+    def to_json(self):
+        args = {'design': self.design.to_json(),
+                'seed': self.seed}
+        name = self.__class__.__name__
+        return json.dumps((args, name))
+
+    def set_seed(self, seed: int):
+        """Set the seed of the sampler
+
+        Parameters
+        ----------
+        seed
+            the seed to be used
+        """
+        np.random.seed(seed)
+        self.seed = seed
+
+    def sample_continuous(self, numsamples: int) -> np.ndarray:
+        """Create N samples within the search space
+
+        Parameters
+        ----------
+        numsamples
+            number of samples
+
+        Returns
+        -------
+            samples
+        """
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    def get_samples(self, numsamples: Union[int, None] = None) -> ExperimentData:
+        """Receive samples of the search space
+
+        Parameters
+        ----------
+        numsamples
+            number of samples
+
+        Returns
+        -------
+            Data objects with the samples
+        """
+        # If numsamples is None, take the object attribute number_of_samples
+        if numsamples is None:
+            numsamples = self.number_of_samples
+
+        # First sample the continuous parameters
+        samples_continuous = self.sample_continuous(numsamples=numsamples)
+
+        # Sample discrete parameters
+        samples_discrete = self._sample_discrete(numsamples=numsamples)
+
+        # Sample categorical parameters
+        samples_categorical = self._sample_categorical(numsamples=numsamples)
+
+        # Sample constant parameters
+        samples_constant = self._sample_constant(numsamples=numsamples)
+
+        # Merge samples into array
+        samples = np.hstack(
+            (samples_continuous, samples_discrete, samples_categorical, samples_constant))
+
+        # TODO #60 : Fix this ordering issue
+        # Get the column names in this particular order
+        columnnames = [
+            ("input", name)
+            for name in self.design.get_continuous_input_names(
+            ) + self.design.get_discrete_input_names(
+            ) + self.design.get_categorical_input_names(
+            ) + self.design.get_constant_input_names()
+        ]
+
+        data = self._cast_to_data_object(
+            samples=samples, columnnames=columnnames)
+        return data
+
+    def _cast_to_data_object(self, samples: np.ndarray, columnnames: List[str]) -> ExperimentData:
+        """Cast the samples to a Data object"""
+        data = ExperimentData(design=self.design)
+
+        # First get an empty reference frame from the DoE
+        empty_frame = self.design._create_empty_dataframe()
+
+        # Then, create a new frame from the samples and columnnames
+        samples_frame = pd.DataFrame(data=samples, columns=columnnames)
+        df = pd.concat([empty_frame, samples_frame], sort=True)
+
+        # Add the samples to the Data object
+        data.add(data=df)
+
+        return data
+
+    def _sample_constant(self, numsamples: int):
+        constant = self.design.get_constant_input_parameters()
+        samples = np.empty(shape=(numsamples, len(constant)))
+        for dim, param in enumerate(constant.values()):
+            samples[:, dim] = param.value
+
+        return samples
+
+    def _sample_discrete(self, numsamples: int):
+        """Sample the descrete parameters, default randomly uniform"""
+        discrete = self.design.get_discrete_input_parameters()
+        samples = np.empty(shape=(numsamples, len(discrete)))
+        for dim, param in enumerate(discrete.values()):
+            samples[:, dim] = np.random.choice(
+                range(param.lower_bound,
+                      param.upper_bound + 1),
+                size=numsamples,
+            )
+
+        return samples
+
+    def _sample_categorical(self, numsamples: int):
+        """Sample the categorical parameters, default randomly uniform"""
+        categorical = self.design.get_categorical_input_parameters()
+        samples = np.empty(shape=(numsamples, len(categorical)), dtype=object)
+        for dim, param in enumerate(categorical.values()):
+            samples[:, dim] = np.random.choice(
+                param.categories, size=numsamples)
+
+        return samples
+
+    def _stretch_samples(self, samples: np.ndarray) -> np.ndarray:
+        """Stretch samples to their boundaries"""
+        continuous = self.design.get_continuous_input_parameters()
+        for dim, param in enumerate(continuous.values()):
+            samples[:, dim] = (
+                samples[:, dim] * (
+                    param.upper_bound - param.lower_bound
+                ) + param.lower_bound
+            )
+
+        return samples
