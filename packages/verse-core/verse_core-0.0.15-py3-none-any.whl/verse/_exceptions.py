2c27220685d9e165c021369dc7d3e103451a5bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package verse-core/0.0.15/verse_core-0.0.15-py3-none-any.whl

---
+++ packages/verse-core/verse_core-0.0.15-py3-none-any.whl/verse/_exceptions.py
@@ -0,0 +1,14 @@
+class NotFoundError(Exception):
+    pass
+
+
+class ExistsError(Exception):
+    pass
+
+
+class NotSupportedError(Exception):
+    pass
+
+
+class ProviderError(Exception):
+    pass
