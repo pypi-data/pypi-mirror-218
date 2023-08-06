31339a2e45c1dd2ee98670893bb051e5f5d48bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:11 +0000

    Add package pystacker/1.4.3/pystacker-1.4.3.tar.gz

---
+++ packages/pystacker/pystacker-1.4.3.tar.gz/pystacker-1.4.3/setup.py
@@ -0,0 +1,36 @@
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r', encoding='utf-8') as f:
+    license = f.read()
+
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    install_requires = f.read()
+
+setup(
+    author='remokasu',
+    name="pystacker",
+    version="1.4.3",
+    license=license,
+    url='https://github.com/remokasu/stacker',
+    install_requires=install_requires,
+    packages=find_packages(),
+    package_data={'stacker': ['data/*', 'plugins/*']},
+    description='Stacker: RPN Calculator in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='reverse-polish-calculator rpn terminal-app',
+    entry_points={
+        "console_scripts": [
+            "stacker = stacker.stacker:main",
+        ],
+    },
+)
---
+++ packages/pystacker/pystacker-1.4.3.tar.gz/pystacker-1.4.3/stacker/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from stacker import stacker
+
+__all__ = ["stacker"]
+
+# リソースフォルダへのパスを取得する
+resource_path = os.path.join(os.path.dirname(__file__), 'data')
+
+plugins_path = os.path.join(os.path.dirname(__file__), 'plugins')
+
+# モジュールにパスを追加する
+__path__.append(resource_path)
+__path__.append(plugins_path)
