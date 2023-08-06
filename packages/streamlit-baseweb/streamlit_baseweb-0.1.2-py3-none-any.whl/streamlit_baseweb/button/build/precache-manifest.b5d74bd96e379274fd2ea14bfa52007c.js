2f5be1976df018ee58fa0d18ac77916af3430523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:20 +0000

    Add package streamlit-baseweb/0.1.2/streamlit_baseweb-0.1.2-py3-none-any.whl

---
+++ packages/streamlit-baseweb/streamlit_baseweb-0.1.2-py3-none-any.whl/streamlit_baseweb/button/build/precache-manifest.b5d74bd96e379274fd2ea14bfa52007c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34edb36efd0aa8ae7d99e283a71cec1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f668e383ef372b8a800",
+    "url": "./static/js/2.c1418e57.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.c1418e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608be5f86e6eb2930098",
+    "url": "./static/js/main.a61d4437.chunk.js"
+  },
+  {
+    "revision": "a8efcc13a5c3acae5074",
+    "url": "./static/js/runtime-main.6f7b4229.js"
+  }
+]);
---
+++ packages/streamlit-baseweb/streamlit_baseweb-0.1.2-py3-none-any.whl/streamlit_baseweb/modal/build/precache-manifest.335beef11633fc996a1434855591b164.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a90e1063db48c1a6f9ae9d991abec7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f46c358eb971906587f",
+    "url": "./static/js/2.5aa23a28.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.5aa23a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdb153af0dbc271a886",
+    "url": "./static/js/main.fcdf4979.chunk.js"
+  },
+  {
+    "revision": "a8efcc13a5c3acae5074",
+    "url": "./static/js/runtime-main.6f7b4229.js"
+  }
+]);
