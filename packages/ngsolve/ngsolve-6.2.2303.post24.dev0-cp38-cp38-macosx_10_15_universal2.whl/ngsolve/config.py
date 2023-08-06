be5d3512209942fe0d4eb63c53210146ba5d7734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:43 +0000

    Add package ngsolve/6.2.2303.post24.dev0/ngsolve-6.2.2303.post24.dev0-cp38-cp38-macosx_10_15_universal2.whl

---
+++ packages/ngsolve/ngsolve-6.2.2303.post24.dev0-cp38-cp38-macosx_10_15_universal2.whl/ngsolve/config.py
@@ -0,0 +1,36 @@
+def _cmake_to_bool(s):
+    return s.upper() not in ['', '0','FALSE','OFF','N','NO','IGNORE','NOTFOUND']
+
+is_python_package    = _cmake_to_bool("TRUE")
+
+BUILD_STUB_FILES     = _cmake_to_bool("OFF")
+BUILD_UMFPACK        = _cmake_to_bool("")
+ENABLE_UNIT_TESTS    = _cmake_to_bool("OFF")
+INSTALL_DEPENDENCIES = _cmake_to_bool("OFF")
+USE_CCACHE           = _cmake_to_bool("ON")
+USE_HYPRE            = _cmake_to_bool("OFF")
+USE_LAPACK           = _cmake_to_bool("ON")
+USE_MKL              = _cmake_to_bool("OFF")
+USE_MKL              = _cmake_to_bool("OFF")
+USE_MUMPS            = _cmake_to_bool("OFF")
+USE_PARDISO          = _cmake_to_bool("OFF")
+USE_UMFPACK          = _cmake_to_bool("ON")
+
+NETGEN_DIR = "/Users/gitlab-runner/Library/Python/3.8/lib/python/site-packages"
+
+NGSOLVE_COMPILE_DEFINITIONS         = "HAVE_NETGEN_SOURCES;HAVE_DLFCN_H;HAVE_CXA_DEMANGLE;USE_TIMEOFDAY;MSG_NOSIGNAL=0;TCL;LAPACK;NGS_PYTHON;USE_UMFPACK"
+NGSOLVE_COMPILE_DEFINITIONS_PRIVATE = ""
+NGSOLVE_COMPILE_INCLUDE_DIRS        = ""
+NGSOLVE_COMPILE_OPTIONS             = "$<$<COMPILE_LANGUAGE:CXX>:-std=c++17>;$<$<COMPILE_LANGUAGE:CXX>:-Wno-undefined-var-template>;-DMAX_SYS_DIM=3"
+
+NGSOLVE_VERSION = "6.2.2303-24-g035559a14"
+NGSOLVE_VERSION_GIT = "v6.2.2303-24-g035559a14"
+NGSOLVE_VERSION_PYTHON = "6.2.2303.post24.dev"
+
+NGSOLVE_VERSION_MAJOR = "6"
+NGSOLVE_VERSION_MINOR = "2"
+NGSOLVE_VERSION_TWEAK = "24"
+NGSOLVE_VERSION_PATCH = "2303"
+NGSOLVE_VERSION_HASH = "g035559a14"
+
+version = NGSOLVE_VERSION_GIT
