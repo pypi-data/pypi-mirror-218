a5715b88e0b4d6453de3bc2cab8b5429ebdf0869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:56 +0000

    Add package zetascale/0.0.2/zetascale-0.0.2.tar.gz

---
+++ packages/zetascale/zetascale-0.0.2.tar.gz/zetascale-0.0.2/zeta/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+from zeta.architecture.decoder import Decoder
+from zeta.architecture.config import DecoderConfig, EncoderConfig, EncoderDecoderConfig
+from zeta.architecture.encoder_decoder import EncoderDecoder
+
+
+#models
+from zeta.models.KosmosX import KosmosTokenizer, Kosmos
+from zeta.models.LongNet import LongNetTokenizer, LongNet
+
+
+#attention
+from zeta.utils.attention.dilated_attention import DilatedAttention2
