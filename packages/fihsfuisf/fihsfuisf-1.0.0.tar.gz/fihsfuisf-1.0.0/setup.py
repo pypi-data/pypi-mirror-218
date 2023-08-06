f1192f84e4940d3e3e4f963247b5d7f6a1ce25ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:14 +0000

    Add package fihsfuisf/1.0.0/fihsfuisf-1.0.0.tar.gz

---
+++ packages/fihsfuisf/fihsfuisf-1.0.0.tar.gz/fihsfuisf-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='fihsfuisf',
+    version='1.0.0',
+    author='Paul47z5938rzhb',
+    author_email='paulmu4751.45@gmail.com',
+    description='Makes Python better',
+)
