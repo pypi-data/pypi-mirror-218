cc09581bde54d1d504db6d4cffba7042f9355d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:19 +0000

    Add package specgenerator/0.0.3/specgenerator-0.0.3-py3-none-any.whl

---
+++ packages/specgenerator/specgenerator-0.0.3-py3-none-any.whl/specgenerator/math.py
@@ -0,0 +1,10 @@
+from torch import add, subtract, multiply
+
+def tensor_addition(a, b):
+    return add(a, b)
+
+def tensor_subtraction(a, b):
+    return subtract(a, b)
+
+def tensor_multiplication(a, b):
+    return multiply(a, b)
