5e40e67b9fd5b60a3586d4f3b6eaeb12cf3f693f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:09 +0000

    Add package telegramweb/5.7/telegramweb-5.7.tar.gz

---
+++ packages/telegramweb/telegramweb-5.7.tar.gz/telegramweb-5.7/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'requests',
+    'SQLAlchemy',
+    'beautifulsoup4',
+    'psycopg2-binary',
+    'lxml',
+    'xmltodict'
+]
+
+DESCRIPTION = 'Python package for automatically fetching and pushing news by Telegram.'
+LONG_DESCRIPTION = open("README.md").read()
+
+setup(
+    name='telegramweb',
+    version='5.7',
+    author='craziks',
+    author_email='chandrashekharpanday07@gmail.com',
+    url='https://github.com/craziks-creator/telegram-web',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT",
+    python_requires='>=3.8',
+    install_requires=requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
