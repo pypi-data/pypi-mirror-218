3194c4103ce37fc59091cddf4e679a19646883a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:16 +0000

    Add package hawkflow/0.0.96/hawkflow-0.0.96-py3-none-any.whl

---
+++ packages/hawkflow/hawkflow-0.0.96-py3-none-any.whl/hawkflowclient/_endpoints.py
@@ -0,0 +1,44 @@
+import os
+import json
+
+from ._validation import _validate_timed_data
+from ._validation import _validate_metric_data
+from ._validation import _validate_exception_data
+
+
+def _timed_data(process: str, meta: str, uid: str) -> str:
+    _validate_timed_data(process, meta, uid)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "uid": uid
+    })
+
+    return x
+
+
+def _metric_data(process: str, meta: str, items: dict, df: int) -> str:
+    _validate_metric_data(process, meta, items, df)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "items": items,
+        "df": df
+    })
+
+    return x
+
+
+def _exception_data(process: str, meta: str, exception_text: str) -> str:
+    _validate_exception_data(process, meta, exception_text)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "exception": exception_text
+    })
+
+    return x
+
