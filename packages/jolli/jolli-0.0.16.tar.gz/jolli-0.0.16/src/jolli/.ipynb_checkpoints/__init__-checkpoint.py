e69c2499fa5e01a9b6b3779f7e102f1f49f61851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:30 +0000

    Add package jolli/0.0.16/jolli-0.0.16.tar.gz

---
+++ packages/jolli/jolli-0.0.16.tar.gz/jolli-0.0.16/src/jolli/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,14 @@
+from .jolliCode import Multiplot
+from .jolliCode import Open
+from .jolliCode import stdout2devnull
+from .jolliCode import get_globals
+
+test = "test"
+
+__all__ = [
+    "Multiplot",
+    "Open",
+    "stdout2devnull",
+    "get_globals",
+    "test",
+]
