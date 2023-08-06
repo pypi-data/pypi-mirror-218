03e4cc8252de3753be9190066eebf53022bee3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:07 +0000

    Add package xmindtotestcase/1.6.0/xmindtotestcase-1.6.0-py3-none-any.whl

---
+++ packages/xmindtotestcase/xmindtotestcase-1.6.0-py3-none-any.whl/xmind2testcase/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'xmindtotestcase'
+__description__ = 'XMindtoTestCase基于Python实现，提供了一个高效测试用例设计的解决方案！'
+__keywords__ = 'xmindtotestCase, testcase, test, testing, xmind, 思维导图, XMind思维导图',
+__url__ = 'https://github.com/huc141/xmindTotestcase.git'
+__author__ = 'yulong'
+__author_email__ = '1349983371@qq.com'
+__version__ = '1.6.0'
+__license__ = 'MIT'
+__cake__ = u'\u2728 \U0001f370 \u2728'
