df2136bf92b562280203e942a141c83429ed1d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:26 +0000

    Add package cs-models/0.0.596/cs_models-0.0.596-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.596-py3-none-any.whl/cs_models/resources/Context/models.py
@@ -0,0 +1,15 @@
+# pylint: disable=unnecessary-lambda
+from sqlalchemy import (
+    Column,
+    String,
+    Text,
+)
+
+from ...database import Base
+
+
+class ContextModel(Base):
+    __tablename__ = "contexts"
+
+    key = Column(String(64), primary_key=True)
+    value = Column(Text, nullable=False)
