5680d24c0714fcd663a5c2d2f00a9c8f1695be16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:57 +0000

    Add package bibleit/0.0.14/bibleit-0.0.14.tar.gz

---
+++ packages/bibleit/bibleit-0.0.14.tar.gz/bibleit-0.0.14/src/bibleit/config.py
@@ -0,0 +1,25 @@
+import importlib.resources
+import bibleit.translations as _translations
+
+# General
+debug = False
+color = False
+label = False
+screen = False
+application = "bibleit"
+version = "0.0.14"
+help = 'Type "help" for more information.'
+welcome = f"""
+    Welcome to {application} v{version}
+
+    {help}
+"""
+
+# Bible
+translation_dir = "translations"
+available_bible = importlib.resources.contents(_translations)
+default_bible = "nvi"
+
+# Repl
+history_length = 1000
+context_ps1 = ">"
