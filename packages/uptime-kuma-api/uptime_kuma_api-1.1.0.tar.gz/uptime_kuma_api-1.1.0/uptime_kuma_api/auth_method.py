5c30322f2f731c91c27b512b5a1f6e18e3198f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:35 +0000

    Add package uptime-kuma-api/1.1.0/uptime_kuma_api-1.1.0.tar.gz

---
+++ packages/uptime-kuma-api/uptime_kuma_api-1.1.0.tar.gz/uptime_kuma_api-1.1.0/uptime_kuma_api/auth_method.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class AuthMethod(str, Enum):
+    """Enumerate authentication methods for monitors."""
+
+    NONE = ""
+    """Authentication is disabled."""
+
+    HTTP_BASIC = "basic"
+    """HTTP Basic Authentication."""
+
+    NTLM = "ntlm"
+    """NTLM Authentication."""
+
+    MTLS = "mtls"
+    """mTLS Authentication."""
