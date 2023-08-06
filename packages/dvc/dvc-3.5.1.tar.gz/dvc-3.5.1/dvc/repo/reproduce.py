e69cae8bf88a2b306c4b6008f1f0f7a3197874ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:27 +0000

    Add package dvc/3.5.1/dvc-3.5.1.tar.gz

---
+++ packages/dvc/dvc-3.5.1.tar.gz/dvc-3.5.1/dvc/repo/reproduce.py
@@ -0,0 +1,199 @@
+import logging
+from typing import TYPE_CHECKING, Iterable, List, Optional, Union, cast
+
+from funcy import ldistinct
+
+from dvc.exceptions import ReproductionError
+from dvc.repo.scm_context import scm_context
+from dvc.stage.cache import RunCacheNotSupported
+from dvc.utils.collections import ensure_list
+
+from . import locked
+
+if TYPE_CHECKING:
+    from networkx import DiGraph
+
+    from dvc.stage import Stage
+
+    from . import Repo
+
+logger = logging.getLogger(__name__)
+
+
+def collect_stages(
+    repo: "Repo",
+    targets: Iterable[str],
+    recursive: bool = False,
+    glob: bool = False,
+) -> List["Stage"]:
+    stages: List["Stage"] = []
+    for target in targets:
+        stages.extend(repo.stage.collect(target, recursive=recursive, glob=glob))
+    return ldistinct(stages)
+
+
+def get_subgraph(
+    graph: "DiGraph",
+    nodes: Optional[List] = None,
+    pipeline: bool = False,
+    downstream: bool = False,
+) -> "DiGraph":
+    import networkx as nx
+
+    from .graph import get_pipeline, get_pipelines, get_subgraph_of_nodes
+
+    if not pipeline or not nodes:
+        return get_subgraph_of_nodes(graph, nodes, downstream=downstream)
+
+    pipelines = get_pipelines(graph)
+    used_pipelines = [get_pipeline(pipelines, node) for node in nodes]
+    return nx.compose_all(used_pipelines)
+
+
+def _remove_frozen_stages(graph: "DiGraph") -> "DiGraph":
+    g = cast("DiGraph", graph.copy())
+    for stage in graph:
+        if stage.frozen:
+            # NOTE: disconnect frozen stage from its dependencies
+            g.remove_edges_from(graph.out_edges(stage))
+    return g
+
+
+def get_active_graph(
+    graph: "DiGraph",
+    stages: Optional[List["Stage"]] = None,
+    pipeline: bool = False,
+    downstream: bool = False,
+) -> "DiGraph":
+    """Return the graph to operate."""
+    processed = _remove_frozen_stages(graph)
+    return get_subgraph(processed, stages, pipeline=pipeline, downstream=downstream)
+
+
+def plan_repro(
+    graph: "DiGraph",
+    stages: Optional[List["Stage"]] = None,
+    pipeline: bool = False,
+    downstream: bool = False,
+) -> List["Stage"]:
+    r"""Derive the evaluation of the given node for the given graph.
+
+    When you _reproduce a stage_, you want to _evaluate the descendants_
+    to know if it make sense to _recompute_ it. A post-ordered search
+    will give us an order list of the nodes we want.
+
+    For example, let's say that we have the following pipeline:
+
+                               E
+                              / \
+                             D   F
+                            / \   \
+                           B   C   G
+                            \ /
+                             A
+
+    The derived evaluation of D would be: [A, B, C, D]
+
+    In case that `downstream` option is specified, the desired effect
+    is to derive the evaluation starting from the given stage up to the
+    ancestors. However, the `networkx.ancestors` returns a set, without
+    any guarantee of any order, so we are going to reverse the graph and
+    use a reverse post-ordered search using the given stage as a starting
+    point.
+
+                   E                                   A
+                  / \                                 / \
+                 D   F                               B   C   G
+                / \   \        --- reverse -->        \ /   /
+               B   C   G                               D   F
+                \ /                                     \ /
+                 A                                       E
+
+    The derived evaluation of _downstream_ B would be: [B, D, E]
+    """
+    import networkx as nx
+
+    active = get_active_graph(graph, stages, pipeline=pipeline, downstream=downstream)
+    return list(nx.dfs_postorder_nodes(active))
+
+
+def _reproduce_stage(stage: "Stage", **kwargs) -> Optional["Stage"]:
+    if stage.frozen and not stage.is_import:
+        logger.warning(
+            "%s is frozen. Its dependencies are not going to be reproduced.",
+            stage,
+        )
+
+    ret = stage.reproduce(**kwargs)
+    if ret and not kwargs.get("dry", False):
+        stage.dump(update_pipeline=False)
+    return ret
+
+
+def _reproduce_stages(
+    stages: List["Stage"],
+    force_downstream: bool = False,
+    **kwargs,
+) -> List["Stage"]:
+    result: List["Stage"] = []
+    for i, stage in enumerate(stages):
+        try:
+            ret = _reproduce_stage(stage, upstream=stages[:i], **kwargs)
+        except Exception as exc:  # noqa: BLE001
+            raise ReproductionError(stage.addressing) from exc
+
+        if not ret:
+            continue
+
+        result.append(ret)
+        if force_downstream:
+            # NOTE: we are walking our pipeline from the top to the
+            # bottom. If one stage is changed, it will be reproduced,
+            # which tells us that we should force reproducing all of
+            # the other stages down below, even if their direct
+            # dependencies didn't change.
+            kwargs["force"] = True
+        if i < len(stages) - 1:
+            logger.info("")  # add a newline
+    return result
+
+
+@locked
+@scm_context
+def reproduce(
+    self: "Repo",
+    targets: Union[Iterable[str], str, None] = None,
+    recursive: bool = False,
+    pipeline: bool = False,
+    all_pipelines: bool = False,
+    downstream: bool = False,
+    single_item: bool = False,
+    glob: bool = False,
+    **kwargs,
+):
+    from dvc.dvcfile import PROJECT_FILE
+
+    if all_pipelines or pipeline:
+        single_item = False
+        downstream = False
+
+    if not kwargs.get("interactive", False):
+        kwargs["interactive"] = self.config["core"].get("interactive", False)
+
+    stages: List["Stage"] = []
+    if not all_pipelines:
+        targets_list = ensure_list(targets or PROJECT_FILE)
+        stages = collect_stages(self, targets_list, recursive=recursive, glob=glob)
+
+    if kwargs.get("pull", False) and kwargs.get("run_cache", True):
+        logger.debug("Pulling run cache")
+        try:
+            self.stage_cache.pull(None)
+        except RunCacheNotSupported as e:
+            logger.warning("Failed to pull run cache: %s", e)
+
+    steps = stages
+    if not single_item:
+        graph = self.index.graph
+        steps = plan_repro(graph, stages, pipeline=pipeline, downstream=downstream)
+    return _reproduce_stages(steps, **kwargs)
