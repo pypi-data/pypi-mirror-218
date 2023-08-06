09966faa84bfd750ed7bd94fc5379011a2ed966f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:11 +0000

    Add package prompttools/0.0.7/prompttools-0.0.7-py3-none-any.whl

---
+++ packages/prompttools/prompttools-0.0.7-py3-none-any.whl/prompttools/testing/threshold_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ThresholdType(Enum):
+    r"""
+    Defines the types of thresholds a user can specify for their test case.
+    """
+
+    MINIMUM = 1
+    MAXIMUM = 2
---
+++ packages/prompttools/prompttools-0.0.7-py3-none-any.whl/prompttools/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.0.7a0+9c6675e'
+git_version = '9c6675e1597b85c39a2f66f63b63ad522681d5f5'
