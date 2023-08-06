f6a1e377c3f9700ab5f1fe5d55c204035210671d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:02 +0000

    Add package pyanalyticsgit/0.4/pyanalyticsgit-0.4.tar.gz

---
+++ packages/pyanalyticsgit/pyanalyticsgit-0.4.tar.gz/pyanalyticsgit-0.4/analyticsgit/monitoramento.py
@@ -0,0 +1,9 @@
+from relatorio import Relatorio
+
+class monitoramento:
+  def rastreamento_git(self): 
+    obj = Relatorio()
+    obj.gerar_relatorio()
+
+obj = monitoramento()
+obj.rastreamento_git()
