c7c2501c2602fc7d04d94a51f6f97fc0d20a2af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package spamming/0.1.3/spamming-0.1.3.tar.gz

---
+++ packages/spamming/spamming-0.1.3.tar.gz/spamming-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='spamming',
+    version='0.1.3',
+    license='MIT',
+    description= "A spamming package",
+    long_description= open("README.md").read(),
+    long_description_content_type= "text/markdown",
+    author="gugu256",
+    author_email='gugu256@mail.com',
+    keywords=["spamming", "spam", "automation", "pyautogui"],
+    install_requires=[
+          'pyautogui',
+      ],
+    classifiers= [
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+
+)
