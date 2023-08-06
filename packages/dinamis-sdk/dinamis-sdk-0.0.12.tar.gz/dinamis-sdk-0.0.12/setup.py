dd5a395fc632569b4e1bb0d4326ea558574bb7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:43 +0000

    Add package dinamis-sdk/0.0.12/dinamis-sdk-0.0.12.tar.gz

---
+++ packages/dinamis-sdk/dinamis-sdk-0.0.12.tar.gz/dinamis-sdk-0.0.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "requests", "qrcode", "appdirs", "pydantic", "pystac", "pystac_client"
+]
+
+setup(
+    name="dinamis-sdk",
+    version="0.0.12",
+    description="DINAMIS SDK",
+    python_requires=">=3.8",
+    author="Remi Cresson",
+    author_email="remi.cresson@inrae.fr",
+    license="MIT",
+    zip_safe=False,
+    install_requires=install_requires,
+    packages=find_packages(),
+)
+
