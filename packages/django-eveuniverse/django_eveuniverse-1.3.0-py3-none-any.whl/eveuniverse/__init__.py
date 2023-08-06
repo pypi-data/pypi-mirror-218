6e751d7345304de53a26d8e0739d599589c4b9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:11 +0000

    Add package django-eveuniverse/1.3.0/django_eveuniverse-1.3.0-py3-none-any.whl

---
+++ packages/django-eveuniverse/django_eveuniverse-1.3.0-py3-none-any.whl/eveuniverse/__init__.py
@@ -0,0 +1,6 @@
+"""Complete set of Eve Universe models with on-demand loading from ESI."""
+
+default_app_config = "eveuniverse.apps.EveuniverseConfig"
+
+__version__ = "1.3.0"
+__title__ = "Eve Universe"
