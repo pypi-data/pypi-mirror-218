8fa9eed8f858c60665f70739c8ef028611a0fe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package myass/0.1.5/myass-0.1.5-py2.py3-none-any.whl

---
+++ packages/myass/myass-0.1.5-py2.py3-none-any.whl/myass/history.py
@@ -0,0 +1,17 @@
+import atexit
+import os
+import readline
+
+import appdirs
+
+
+def init(appname='myass', filename='history'):
+    data_dir = appdirs.user_data_dir(appname)
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+    histfile = os.path.join(data_dir, filename)
+    try:
+        readline.read_history_file(histfile)
+    except FileNotFoundError:
+        pass
+    atexit.register(readline.write_history_file, histfile)
