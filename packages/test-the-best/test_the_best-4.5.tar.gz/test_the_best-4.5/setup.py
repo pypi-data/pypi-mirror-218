543543bf8aa073d00fd392b63951cf3503a2964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:45 +0000

    Add package test-the-best/4.5/test_the_best-4.5.tar.gz

---
+++ packages/test-the-best/test_the_best-4.5.tar.gz/test_the_best-4.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='test_the_best',
+      version='4.5',
+      description='Тест загрузки библиотеки',
+      packages=['test_the_best'],
+      author_email='neso_hoshi_official@mail.ru',
+      readme="README.md",
+      zip_safe=False,
+     classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+])
