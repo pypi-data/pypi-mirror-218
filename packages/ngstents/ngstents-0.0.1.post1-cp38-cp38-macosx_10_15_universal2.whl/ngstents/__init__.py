95d676bc4beb2013e7120880980dfc34de12dc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package ngstents/0.0.1.post1/ngstents-0.0.1.post1-cp38-cp38-macosx_10_15_universal2.whl

---
+++ packages/ngstents/ngstents-0.0.1.post1-cp38-cp38-macosx_10_15_universal2.whl/ngstents/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ["TentSlab", "Tent", "conslaw", "utils"]
+
+import ngsolve # make sure all shared libs are loaded
+from ._pytents import TentSlab, Tent
+from . import conslaw
+from . import utils
+
+from .utils._drawtents import DrawPitchedTentsPlt
+from .utils._drawtents2d import DrawPitchedTents
+
+TentSlab.DrawPitchedTentsPlt = DrawPitchedTentsPlt
+TentSlab.DrawPitchedTents = DrawPitchedTents
+del DrawPitchedTentsPlt, DrawPitchedTents
