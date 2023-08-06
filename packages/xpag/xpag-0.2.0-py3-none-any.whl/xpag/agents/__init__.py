207850bc7b365c32e6cde895907a81931f60ad6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:33 +0000

    Add package xpag/0.2.0/xpag-0.2.0-py3-none-any.whl

---
+++ packages/xpag/xpag-0.2.0-py3-none-any.whl/xpag/agents/__init__.py
@@ -0,0 +1,7 @@
+from xpag.agents.agent import (
+    Agent,
+)
+from xpag.agents.sac.sac import SAC
+from xpag.agents.td3.td3 import TD3
+from xpag.agents.tqc.tqc import TQC
+from xpag.agents.sdqn.sdqn import SDQN, SDQNSetter
