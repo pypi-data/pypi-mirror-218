7bcea3a52120eef6ba1497c53bd8da75c051aca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package image-func/1.0.5/image_func-1.0.5-py3-none-any.whl

---
+++ packages/image-func/image_func-1.0.5-py3-none-any.whl/image_func/__init__.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from scipy.fftpack import fft2, ifft2
+import numpy as np
+import cv2
+
+def apply_convolution(image, kernel):
+    convolved_image = cv2.filter2D(image, -1, kernel)
+    return convolved_image
+
+def apply_correlation(image, kernel):
+    correlated_image = cv2.filter2D(image, -1, kernel)
+    return correlated_image
+
+def signaltonoise(a, axis=0, ddof=0):
+    a = np.asanyarray(a)
+    m = a.mean(axis)
+    sd = a.std(axis=axis, ddof=ddof)
+    return np.where(sd == 0, 0, m / sd)
+
+def process_image(image_path):
+    im = Image.open(image_path).convert('L')
+    im_array = np.array(im)
+    
+    freq = fft2(im_array)
+    im1 = ifft2(freq).real
+    
+    snr = signaltonoise(im1, axis=None)
+    
+    return im_array, im1, snr
