45d372111bc0d236ae2fad1b70e2e313beb428db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package fyerstest/0.3/fyerstest-0.3.tar.gz

---
+++ packages/fyerstest/fyerstest-0.3.tar.gz/fyerstest-0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='fyerstest',  
+     version='0.3',
+     author="Fyers-Tech",
+     author_email="support@fyers.in",
+     description="Fyers trading APIs.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/",
+     packages=setuptools.find_packages(),
+     package_data={
+         'fyerstest': ['/home/vinay/Documents/SDK/fyersSdk/fyerstest/map.json']
+     },
+     install_requires=[
+            'aiohttp==3.8.4',
+            'Requests==2.31.0',
+            'websocket_client==1.2.1',
+            'websocket_client==1.2.1',
+            'websockets==8.1',
+            'aws_lambda_powertools==2.19.0'
+          ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
