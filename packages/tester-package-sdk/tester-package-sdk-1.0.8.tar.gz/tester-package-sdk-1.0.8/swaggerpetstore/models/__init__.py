f3d9498952197b629792497bbc62c08e883acfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package tester-package-sdk/1.0.8/tester-package-sdk-1.0.8.tar.gz

---
+++ packages/tester-package-sdk/tester-package-sdk-1.0.8.tar.gz/tester-package-sdk-1.0.8/swaggerpetstore/models/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'pet',
+    'category',
+    'api_response',
+    'order',
+    'user',
+    'tag',
+    'o_auth_token',
+    'status_enum',
+    'status_1_enum',
+    'status_2_enum',
+    'o_auth_scope_enum',
+    'o_auth_provider_error_enum',
+]
