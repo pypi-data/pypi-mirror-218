15e004d73c9af433abeeb1a656e86aca9b0e7713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:07 +0000

    Add package pydaa/1.7.3/pydaa-1.7.3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pydaa/pydaa-1.7.3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pydaa.pyi
@@ -0,0 +1,14 @@
+"""Design and Analysis of Algorithms"""
+from __future__ import annotations
+import pydaa
+import typing
+
+__all__ = [
+    "dynamic_programming",
+    "graph",
+    "greedy",
+    "hashing",
+    "sorting"
+]
+
+
