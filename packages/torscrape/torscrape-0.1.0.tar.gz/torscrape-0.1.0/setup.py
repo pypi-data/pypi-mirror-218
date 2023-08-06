df50fae87e006afe4477eb67fcc48ceec84c9cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:21 +0000

    Add package torscrape/0.1.0/torscrape-0.1.0.tar.gz

---
+++ packages/torscrape/torscrape-0.1.0.tar.gz/torscrape-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+# Get the absolute path to the requirements.txt file
+requirements_path = r"C:\Users\Tobias\Desktop\Webscrape Course\requirements.txt"
+with open(requirements_path) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='torscrape',
+    version='0.01.0',
+    author='Tore.ofc, rxality',
+    author_email='tore.ofc.99@gmail.com',
+    description='A webscrape package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/toreofc/TorScrape/',
+    project_urls={
+        'Bug Tracker': 'https://github.com/toreofc/TorScrape/issues'
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': '.'},
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
