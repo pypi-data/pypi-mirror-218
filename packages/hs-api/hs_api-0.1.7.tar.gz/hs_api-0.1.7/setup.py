ddd3b8a50de5cef1c3de6ecd07f0c54bf380b3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:59 +0000

    Add package hs-api/0.1.7/hs_api-0.1.7.tar.gz

---
+++ packages/hs-api/hs_api-0.1.7.tar.gz/hs_api-0.1.7/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hs_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'bidict>=0.22.0,<0.23.0',
+ 'connectome-utils>=0.1.0,<0.2.0',
+ 'fxpmath>=0.4.8,<0.5.0',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'numba>=0.55.1,<0.56.0',
+ 'numpy>=1.18',
+ 'tqdm>=4.64.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'hs-api',
+    'version': '0.1.7',
+    'description': 'CRI User Software',
+    'long_description': 'None',
+    'author': 'Justin Frank & Abhinav Uppal',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
