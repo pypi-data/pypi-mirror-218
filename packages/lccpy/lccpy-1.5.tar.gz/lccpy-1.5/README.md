6b8e15309191af4729acca1f6c8851335f1c4ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:44 +0000

    Add package lccpy/1.5/lccpy-1.5.tar.gz

---
+++ packages/lccpy/lccpy-1.5.tar.gz/lccpy-1.5/README.md
@@ -0,0 +1,3 @@
+# 项目描述
+
+项目 \<[让Python更简单一点](https://github.com/lcctoor/lccpy#readme)\> 的依赖包。
---
+++ packages/lccpy/lccpy-1.5.tar.gz/lccpy-1.5/lccpy/vtype/__init__.py
@@ -0,0 +1,5 @@
+from ._vtype import (
+    vbool, vtrue, vfalse, uniset, empset, SysEmpty,
+    bidict, vnum, vdict, vstr, vbytes,
+    ToolPool, OrmIndexError
+)
