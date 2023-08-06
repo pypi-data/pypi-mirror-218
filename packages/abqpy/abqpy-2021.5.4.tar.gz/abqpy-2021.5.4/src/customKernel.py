d9331e6741bae16589c24fa352c0e3748cdea1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:00 +0000

    Add package abqpy/2021.5.4/abqpy-2021.5.4.tar.gz

---
+++ packages/abqpy/abqpy-2021.5.4.tar.gz/abqpy-2021.5.4/src/customKernel.py
@@ -0,0 +1,15 @@
+from abaqus.CustomKernel.CommandRegister import (  # noqa # pylint: disable=unused-import
+    CommandRegister,
+)
+from abaqus.CustomKernel.RegisteredDictionary import (  # noqa # pylint: disable=unused-import
+    RegisteredDictionary,
+)
+from abaqus.CustomKernel.RegisteredList import (  # noqa # pylint: disable=unused-import
+    RegisteredList,
+)
+from abaqus.CustomKernel.RegisteredTuple import (  # noqa # pylint: disable=unused-import
+    RegisteredTuple,
+)
+from abaqus.CustomKernel.RepositorySupport import (  # noqa # pylint: disable=unused-import
+    RepositorySupport,
+)
