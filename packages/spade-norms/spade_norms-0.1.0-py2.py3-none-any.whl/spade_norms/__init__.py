834130e8c5c881bd57e3c81008b2ce4ab90cf0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:55 +0000

    Add package spade-norms/0.1.0/spade_norms-0.1.0-py2.py3-none-any.whl

---
+++ packages/spade-norms/spade_norms-0.1.0-py2.py3-none-any.whl/spade_norms/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for SPADE Norms."""
+
+__author__ = """Miguel García"""
+__email__ = "migarbo1@upv.edu.es"
+__version__ = "0.1.0"
+
+from .norms import norm
+from .actions import normative_action
+from .spade_norms import NormativeMixin
