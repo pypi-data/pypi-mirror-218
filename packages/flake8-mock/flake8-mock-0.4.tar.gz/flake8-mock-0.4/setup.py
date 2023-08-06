ab338043df1ac5db13dd7cbe42e037203f400db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package flake8-mock/0.4/flake8-mock-0.4.tar.gz

---
+++ packages/flake8-mock/flake8-mock-0.4.tar.gz/flake8-mock-0.4/setup.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+from __future__ import with_statement
+from setuptools import setup
+
+
+def get_version(fname='flake8_mock.py'):
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    descr = []
+    for fname in ('README.rst',):
+        with open(fname) as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
+
+setup(
+    name='flake8-mock',
+    version=get_version(),
+    description="Provides checking for non-existent mock methods",
+    long_description=get_long_description(),
+    keywords=['flake8', 'mock', 'testing'],
+    author='Alejandro Pereira',
+    author_email='alepereira.dev@gmail.com',
+    url='https://github.com/aleGpereira/flake8-mock',
+    license='GNU',
+    py_modules=['flake8_mock'],
+    zip_safe=False,
+    entry_points={
+        'flake8.extension': [
+            'M = flake8_mock:MockChecker',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
