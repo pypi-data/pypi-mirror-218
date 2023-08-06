45af3510588df14f186ff70f1b8e6a5a1b9b29ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:43 +0000

    Add package qiskit-hangyul/0.2.0/qiskit_hangyul-0.2.0.tar.gz

---
+++ packages/qiskit-hangyul/qiskit_hangyul-0.2.0.tar.gz/qiskit_hangyul-0.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qiskit_hangyul",
+    version="0.2.0",
+    description="A quantum provider for Qiskit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["quantier"],
+    package_dir={"quantier": "quantier"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "qiskit",
+        "qiskit-aer"
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
