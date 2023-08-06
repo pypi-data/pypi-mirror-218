01d980ded8e67434db9381331481e507598885e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:36 +0000

    Add package f3dasm-simulate/1.0.0/f3dasm_simulate-1.0.0-py3-none-any.whl

---
+++ packages/f3dasm-simulate/f3dasm_simulate-1.0.0-py3-none-any.whl/f3dasm_simulate/abaqus/__init__.py
@@ -0,0 +1,36 @@
+"""
+f3dasm_simulate - Simulation extension of f3dasm
+the Bessa Group's conventions
+
+This package contains a template for Python packages following
+the Bessa Group's conventions.
+
+Usage:
+  import f3dasm_simulate
+
+Author: Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)
+"""
+
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+
+# Third-party
+
+
+# Local
+from ..example_module import add_one
+from . import (loading, material, microstructure, overwrite_inputs, simulator,
+               simulator_info)
+from .cddm import VonMisesPlasticElasticPathLoads
+from .simulator import AbaqusSimulator
+
+#                                                        Authorship and Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
