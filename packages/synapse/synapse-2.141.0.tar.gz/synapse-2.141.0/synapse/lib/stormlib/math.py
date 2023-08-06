178fa04dc8b7b5dcfe7f4f53f3612fd0df8ebf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:24 +0000

    Add package synapse/2.141.0/synapse-2.141.0.tar.gz

---
+++ packages/synapse/synapse-2.141.0.tar.gz/synapse-2.141.0/synapse/lib/stormlib/math.py
@@ -0,0 +1,35 @@
+import synapse.lib.stormtypes as s_stormtypes
+
+@s_stormtypes.registry.registerLib
+class MathLib(s_stormtypes.Lib):
+    '''
+    A Storm library for performing math operations.
+    '''
+    _storm_locals = (
+        {'name': 'number',
+         'desc': '''
+            Convert a value to a Storm Number object.
+
+            Storm Numbers are high precision fixed point decimals corresponding to the
+            the hugenum storage type.
+         ''',
+         'type': {'type': 'function', '_funcname': '_number',
+                  'args': (
+                      {'name': 'value', 'type': 'any',
+                       'desc': 'Value to convert.', },
+                  ),
+                  'returns': {'type': 'number', 'desc': 'A Number object.', }}},
+    )
+
+    _storm_lib_path = ('math',)
+
+    def getObjLocals(self):
+        return {
+            'number': self._number,
+        }
+
+    @s_stormtypes.stormfunc(readonly=True)
+    async def _number(self, value):
+        if isinstance(value, s_stormtypes.Number):
+            return value
+        return s_stormtypes.Number(value)
---
+++ packages/synapse/synapse-2.141.0.tar.gz/synapse-2.141.0/synapse/tests/test_lib_coro.py
@@ -0,0 +1,148 @@
+import sys
+import time
+import asyncio
+import threading
+
+import synapse.exc as s_exc
+import synapse.glob as s_glob
+import synapse.lib.coro as s_coro
+import synapse.tests.utils as s_t_utils
+
+class FakeError(Exception): pass
+
+def spawnfunc(x, y=10):
+    return x + y
+
+def spawnsleep(n=10):
+    time.sleep(n)
+    return True
+
+def spawnfakeit():
+    raise FakeError()
+
+def spawnexit():
+    sys.exit(0)
+
+def chkpool():
+    return s_coro.forkpool is None
+
+def synerr():
+    raise s_exc.SynErr(mesg='fail')
+
+def nopickle():
+    class Bar: pass
+    return Bar()
+
+class CoroTest(s_t_utils.SynTest):
+
+    async def test_coro_event(self):
+
+        evnt = s_coro.Event()
+
+        evnt.set()
+        self.true(await evnt.timewait())
+
+        evnt.clear()
+        self.false(await evnt.timewait(timeout=0.001))
+
+    async def test_coro_iscoro(self):
+
+        async def agen():
+            yield 42
+
+        def genr():
+            yield 'woot'
+
+        async def woot():
+            return 10
+
+        item = woot()
+        self.true(s_coro.iscoro(item))
+
+        await item
+
+        self.false(s_coro.iscoro(genr()))
+        self.false(s_coro.iscoro(agen()))
+
+    async def test_coro_genrhelp(self):
+
+        @s_coro.genrhelp
+        async def woot():
+            yield 1
+            yield 2
+            yield 3
+
+        self.none(await woot().spin())
+        self.eq([1, 2, 3], await woot().list())
+
+    async def test_executor(self):
+
+        def func(*args, **kwargs):
+            tid = threading.get_ident()
+            return tid, args, kwargs
+
+        future = s_coro.executor(func, 1, key='valu')
+        tid, args, kwargs = await future
+        # Ensure that we were not executed on the ioloop thread
+        self.ne(tid, s_glob._glob_thrd.ident)
+        # Ensure that args are passed as expected
+        self.eq(args, (1,))
+        self.eq(kwargs, {'key': 'valu'})
+
+        async def afunc():
+            tid = threading.get_ident()
+            return tid
+        # Ensure a generic coroutine is executed on the ioloop thread
+        self.eq(s_glob._glob_thrd.ident, await afunc())
+
+    async def test_lib_coro_spawn(self):
+
+        todo = (spawnfunc, (20,), {'y': 30})
+        self.eq(50, await s_coro.spawn(todo))
+
+        todo = (spawnsleep, (1.5,), {})
+        self.true(await s_coro.spawn(todo))
+
+        todo = (spawnsleep, (), {})
+        with self.raises(asyncio.TimeoutError):
+            await s_coro.spawn(todo, timeout=0.1)
+
+        todo = (spawnfakeit, (), {})
+        with self.raises(s_exc.SynErr) as cm:
+            await s_coro.spawn(todo)
+        self.eq('FakeError', cm.exception.get('name'))
+        self.isin('Error executing spawn function: FakeError', cm.exception.get('mesg'))
+
+        todo = (spawnexit, (), {})
+        with self.raises(s_exc.SpawnExit):
+            await s_coro.spawn(todo)
+
+        todo = (synerr, (), {})
+        with self.raises(s_exc.SynErr) as cm:
+            await s_coro.spawn(todo)
+        self.eq('fail', cm.exception.get('mesg'))
+
+        # by convention spawn functions should
+        # never return non-pickleable results
+        todo = (nopickle, (), {})
+        with self.raises(s_exc.SpawnExit) as cm:
+            await s_coro.spawn(todo)
+        self.eq(0, cm.exception.get('code'))
+        self.isin('without a result', cm.exception.get('mesg'))
+
+    async def test_lib_coro_forked(self):
+
+        self.true(await s_coro.forked(chkpool))
+
+        self.eq(50, await s_coro.forked(spawnfunc, 20, y=30))
+
+        with self.raises(FakeError):
+            await s_coro.forked(spawnfakeit)
+
+        self.eq(50, await s_coro.forked(spawnfunc, 20, y=30))
+
+        def newp():
+            return 23
+
+        with self.raises(Exception):
+            await s_coro.forked(newp)
