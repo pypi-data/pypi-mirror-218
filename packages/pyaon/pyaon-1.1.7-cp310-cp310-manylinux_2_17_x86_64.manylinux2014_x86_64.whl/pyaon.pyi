cfba20c5dca211abecf1d8b25a381558e1222ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:25 +0000

    Add package pyaon/1.1.7/pyaon-1.1.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyaon/pyaon-1.1.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyaon.pyi
@@ -0,0 +1,13 @@
+"""Interact with the AON (Andrezao Object Notation) file format"""
+from __future__ import annotations
+import pyaon
+import typing
+
+__all__ = [
+    "graph",
+    "hashing",
+    "knapsack",
+    "sorting"
+]
+
+
