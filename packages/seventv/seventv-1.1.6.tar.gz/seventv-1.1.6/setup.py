2612a59747fa133e3a09478a313c89b5da6853ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:56 +0000

    Add package seventv/1.1.6/seventv-1.1.6.tar.gz

---
+++ packages/seventv/seventv-1.1.6.tar.gz/seventv-1.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='seventv',
+    version='1.1.6',
+    author='Jässin Aouani',
+    author_email='jassin@aouani.de',
+    description='An asynchronous API-wrapper for 7tv.app',
+    long_description = "Placeholder",
+    packages=['seventv'],
+    install_requires=['aiohttp'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
