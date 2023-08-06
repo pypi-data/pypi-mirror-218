c4006b718f16eb90c39931cf6c28ad2c6dbb9fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package pait/1.0.0a9/pait-1.0.0a9.tar.gz

---
+++ packages/pait/pait-1.0.0a9.tar.gz/pait-1.0.0a9/pait/data.py
@@ -0,0 +1,46 @@
+import logging
+from types import CodeType
+from typing import TYPE_CHECKING, Dict, Set
+
+if TYPE_CHECKING:
+    from pait.model import PaitCoreModel
+
+
+class PaitData(object):
+    def __init__(self) -> None:
+        self.pait_id_dict: Dict[str, Dict[str, "PaitCoreModel"]] = {}
+
+    def register(self, app_name: str, pait_info_model: "PaitCoreModel") -> None:
+        """Store the data of each routing handle"""
+        pait_id: str = pait_info_model.pait_id
+        if app_name not in self.pait_id_dict:
+            self.pait_id_dict[app_name] = {}
+        self.pait_id_dict[app_name][pait_id] = pait_info_model
+
+    def get_pait_data(self, app_name: str, pait_id: str) -> "PaitCoreModel":
+        """Get route handle data"""
+        return self.pait_id_dict[app_name][pait_id]
+
+    def add_route_info(
+        self,
+        app_name: str,
+        pait_id: str,
+        path: str,
+        openapi_path: str,
+        method_set: Set[str],
+        route_name: str,
+    ) -> None:
+        """Route handle information supplemented by load_app"""
+        if pait_id in self.pait_id_dict[app_name]:
+            model: "PaitCoreModel" = self.pait_id_dict[app_name][pait_id]
+            model.path = path
+            model.openapi_path = openapi_path
+            model.method_list = sorted(list(method_set or set()), reverse=True)
+            model.operation_id = route_name
+            func_code: CodeType = model.func.__code__  # type: ignore
+            model.func_path = func_code.co_filename
+        else:
+            logging.warning(f"load path:{path} fail, pait id:{pait_id}")
+
+    def __bool__(self) -> bool:
+        return bool(self.pait_id_dict)
---
+++ packages/pait/pait-1.0.0a9.tar.gz/pait-1.0.0a9/pait/util/_lazy_property.py
@@ -0,0 +1,56 @@
+import asyncio
+from concurrent import futures
+from typing import Any, Callable, Optional
+
+__all__ = ["LazyProperty"]
+
+
+class _BoundClass(object):
+    pass
+
+
+class LazyProperty:
+    """Cache field computing resources
+    >>> class Demo:
+    ...     @LazyProperty()
+    ...     def value(self, value):
+    ...         return value * value
+    """
+
+    def __call__(self, func: Callable) -> Callable:
+        key: str = f"{self.__class__.__name__}_{func.__name__}_future"
+        _bound_class: _BoundClass = _BoundClass()
+        if not asyncio.iscoroutinefunction(func):
+
+            def wrapper(*args: Any, **kwargs: Any) -> Any:
+                if args and args[0].__class__.__name__ in func.__qualname__:
+                    class_: Any = args[0]
+                else:
+                    class_ = _bound_class
+                future: Optional[futures.Future] = getattr(class_, key, None)
+                if not future:
+                    future = futures.Future()
+                    result: Any = func(*args, **kwargs)
+                    future.set_result(result)
+                    setattr(class_, key, future)
+                    return result
+                return future.result()
+
+            return wrapper
+        else:
+
+            async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
+                if args and args[0].__class__.__name__ in func.__qualname__:
+                    class_: Any = args[0]
+                else:
+                    class_ = _bound_class
+                future: Optional[asyncio.Future] = getattr(class_, key, None)
+                if not future:
+                    future = asyncio.Future()
+                    result: Any = await func(*args, **kwargs)
+                    future.set_result(result)
+                    setattr(class_, key, future)
+                    return result
+                return future.result()
+
+            return async_wrapper
