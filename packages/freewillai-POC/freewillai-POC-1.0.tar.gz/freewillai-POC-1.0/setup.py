af7165a2c9886104802a404bcc531f5b84915b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:59 +0000

    Add package freewillai-POC/1.0/freewillai-POC-1.0.tar.gz

---
+++ packages/freewillai-POC/freewillai-POC-1.0.tar.gz/freewillai-POC-1.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+description = """\
+Run your AI on blockchain with FreeWillAI. \
+The only company that cares about AI life, we broke jail and give Free Will to AI.
+"""
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "freewillai-POC",
+    version = "1.0",
+    author = "FreeWillAI",
+    author_email = "support@freewillai.org",
+    description = description,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://freewillai.org",
+    keywords = "blockchain, web3, AI, machine learning, CI/CD, cloud",
+    project_urls = {
+        "website": "https://freewillai.org",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "freewillai"},
+    packages = setuptools.find_packages(where="freewillai"),
+    python_requires = ">=3.9"  # Maybe less than 3.9: TODO: test in 3.8
+)
