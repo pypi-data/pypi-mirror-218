e32ebf6a212bdfc6b4442aae1cb6f87e785397af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:58 +0000

    Add package modelbit/0.24.8/modelbit-0.24.8-py3-none-any.whl

---
+++ packages/modelbit/modelbit-0.24.8-py3-none-any.whl/modelbit/telemetry.py
@@ -0,0 +1,95 @@
+import logging
+import logging.handlers
+import os
+import sys
+import traceback
+from functools import wraps
+from typing import Callable, List, Optional, TypeVar, Union, cast
+
+from modelbit.api.api import MbApi  # For perf, skip __init__
+from modelbit.error import ModelbitError, UserFacingError
+from modelbit.utils import inDeployment
+from modelbit.ux import printTemplate
+
+logger = logging.getLogger(__name__)
+
+
+def enableFileLogging():
+  return os.environ.get("MB_LOG", None) is not None
+
+
+def initLogging():
+  LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+  streamHandler = logging.StreamHandler()
+  handlers: List[logging.Handler] = [streamHandler]
+  streamHandler.setLevel(LOGLEVEL)
+  if enableFileLogging():
+    try:
+      import appdirs
+      logDir = appdirs.user_log_dir("modelbit")
+      if not os.path.exists(logDir):
+        os.makedirs(logDir, exist_ok=True)
+      fileHandler = logging.handlers.RotatingFileHandler(os.path.join(logDir, "log.txt"),
+                                                         maxBytes=10485760,
+                                                         backupCount=5)
+      fileHandler.setLevel(level="INFO")
+      handlers.append(fileHandler)
+    except Exception as e:
+      print(e)
+      logging.info(e)
+
+  logging.basicConfig(level="INFO", handlers=handlers)
+
+
+def _logErrorToWeb(mbApi: Optional[MbApi], userErrorMsg: str):
+  errStack = traceback.format_exception(*sys.exc_info())[1:]
+  errStack.reverse()
+  errorMsg = userErrorMsg + "\n" + "".join(errStack)
+
+  if inDeployment():
+    print(errorMsg, file=sys.stderr)  # TODO: Maybe report these to web?
+    return
+
+  from modelbit.api import MbApi
+  mbApi = mbApi or MbApi()
+  try:
+    mbApi.getJson("api/cli/v1/error", {"errorMsg": errorMsg})
+  except Exception as e:
+    logger.info(e)
+
+
+T = TypeVar("T")
+
+
+def eatErrorAndLog(mbApi: Optional[MbApi], genericMsg: str):
+
+  def decorator(func: Callable[..., T]) -> Callable[..., T]:
+
+    @wraps(func)
+    def innerFn(*args: object, **kwargs: object) -> T:
+      error: Optional[Union[Exception,
+                            str]] = None  # Store and raise outside the handler so the trace is more compact.
+      try:
+        return func(*args, **kwargs)
+      except (KeyError, TypeError) as e:
+        _logErrorToWeb(mbApi, f"{genericMsg}, {type(e)}, {str(e)}")
+        error = e
+      except UserFacingError as e:
+        if e.logToModelbit:
+          _logErrorToWeb(mbApi, e.userFacingErrorMessage)
+        printTemplate("error", None, errorText=genericMsg + " " + e.userFacingErrorMessage)
+        error = cast(str, e.userFacingErrorMessage)
+      except Exception as e:
+        specificError = cast(Optional[str], getattr(e, "userFacingErrorMessage", None))
+        error = genericMsg + (" " + specificError if specificError is not None else "")
+        _logErrorToWeb(mbApi, cast(str, error))
+        printTemplate("error_details", None, errorText=error, errorDetails=traceback.format_exc())
+      # Convert to generic ModelbitError.
+      if type(error) == str:
+        raise ModelbitError(error)
+      else:
+        raise cast(Exception, error)
+
+    return innerFn
+
+  return decorator
