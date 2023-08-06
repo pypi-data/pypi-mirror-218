5bfcef765b19801f9031980acbaadb8c74511a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package magnify/0.4.2/magnify-0.4.2.tar.gz

---
+++ packages/magnify/magnify-0.4.2.tar.gz/magnify-0.4.2/src/magnify/pipeline.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from typing import Callable
+
+import confection
+from numpy.typing import ArrayLike
+import xarray as xr
+
+import magnify.registry as registry
+import magnify.utils as utils
+
+
+class Pipeline:
+    def __init__(self, reader: str, config: dict[str, dict[str, str]]):
+        self.reader: Callable[[ArrayLike | str], xr.Dataset] = registry.readers.get(reader)()
+        self.config = confection.Config(config)
+        self.components: list[Callable[[xr.Dataset], xr.Dataset]] = []
+
+    def __call__(
+        self,
+        data: ArrayLike | str,
+        times: Sequence[int] | None = None,
+        channels: Sequence[str] | None = None,
+    ) -> xr.Dataset | list[xr.Dataset]:
+        inputs = self.reader(data=data, times=times, channels=channels)
+        assays = []
+        for assay in inputs:
+            for name, component in self.components:
+                assay = component(assay)
+
+            assays.append(assay)
+
+        if len(assays) == 1:
+            assays = assays[0]
+
+        return assays
+
+    def add_pipe(self, name: str) -> None:
+        component_factory = registry.components.get(name)
+        component = component_factory(**utils.valid_kwargs(self.config, component_factory))
+        self.components.append((name, component))
