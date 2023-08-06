bdb8fa167196021c1c85768722a85b365decb0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:46 +0000

    Add package typetype/1.0.1/typetype-1.0.1.tar.gz

---
+++ packages/typetype/typetype-1.0.1.tar.gz/typetype-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='typetype',
+    version='1.0.1',
+    author='Ahmet Ozer',
+    url="https://github.com/ahmet8zer/typetype",
+    description='A command line typing game',
+    packages=['typetype'],
+    entry_points={
+        'console_scripts': [
+            'typetype=typetype.ahmetsgame:callmain'
+        ]
+    },
+    install_requires=[
+] + (['windows-curses'] if sys.platform == 'win32' else [])
+)
