2017166a03a37c1d95d718d29e6da35f13908fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:50 +0000

    Add package dynamic-sh/0.0.1a0/dynamic-sh-0.0.1a0.tar.gz

---
+++ packages/dynamic-sh/dynamic-sh-0.0.1a0.tar.gz/dynamic-sh-0.0.1a0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamic-sh',
+    version='0.0.1-alpha',
+    description='',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[
+        # List of package dependencies
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
