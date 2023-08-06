6a94f39fa60e149e23cd2d2c44045344bf594090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:22 +0000

    Add package EpubCrawler/2023.7.9.1/EpubCrawler-2023.7.9.1.tar.gz

---
+++ packages/EpubCrawler/EpubCrawler-2023.7.9.1.tar.gz/EpubCrawler-2023.7.9.1/EpubCrawler/config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+config = {
+    'name': '',
+    'url': '',
+    'link': '',
+    'title': 'title',
+    'content': '',
+    'remove': '',
+    'retry': 10,
+    'wait': 0,
+    'encoding': 'utf-8',
+    'credit': True,
+    'headers': {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+    },
+    'list': [],
+    'optiMode': 'quant',
+    'colors': 8,
+    'timeout': None,
+    'connTimeout': 10,
+    'readTimeout': 60,
+	'imgSrc': ['data-src', 'data-original-src', 'src'],
+    'proxy': [None],
+    'proxyOrder': 'sequential',
+    'textThreads': 8,
+    'imgThreads': 24,
+    'external': None,
+    'checkStatus': False,
+    'checkBlank': True,
+    'cache': True,
+    'waitContent': False,
+    'debug': False,
+    'selenium': False,
+    'sizeLimit': '50m',
+}
