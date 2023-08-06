3350262427f0ee9019069a5478c80562d35ebdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:23 +0000

    Add package calculator-MU/1.0.6/calculator_MU-1.0.6.tar.gz

---
+++ packages/calculator-MU/calculator_MU-1.0.6.tar.gz/calculator_MU-1.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator_MU",
+    author="Mantas Urbonas",
+    version="1.0.6",
+    author_email="mantas.urbonas0@gmail.com",
+    description="A simple calculator package",
+    packages=['calculator_MU'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
