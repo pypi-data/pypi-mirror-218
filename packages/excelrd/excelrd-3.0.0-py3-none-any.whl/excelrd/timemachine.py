223e0788915655eab07b8529ddb3bb8ebb43e996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:53 +0000

    Add package excelrd/3.0.0/excelrd-3.0.0-py3-none-any.whl

---
+++ packages/excelrd/excelrd-3.0.0-py3-none-any.whl/excelrd/timemachine.py
@@ -0,0 +1,24 @@
+##
+# <p>Copyright (c) 2006-2012 Stephen John Machin, Lingfo Pty Ltd</p>
+# <p>This module is part of the excelrd package, which is released under a BSD-style licence.</p>
+##
+
+# timemachine.py -- adaptation for single codebase.
+# Currently supported: 2.6 to 2.7, 3.2+
+# usage: from timemachine import *
+
+
+BYTES_LITERAL = lambda x: x.encode("latin1")
+BYTES_ORD = lambda byte: byte
+
+
+def fprintf(f, fmt, *vargs):
+    fmt = fmt.replace("%r", "%a")
+    if fmt.endswith("\n"):
+        print(fmt[:-1] % vargs, file=f)
+    else:
+        print(fmt % vargs, end=" ", file=f)
+
+
+EXCEL_TEXT_TYPES = (str, bytes, bytearray)  # xlwt: isinstance(obj, EXCEL_TEXT_TYPES)
+REPR = ascii
