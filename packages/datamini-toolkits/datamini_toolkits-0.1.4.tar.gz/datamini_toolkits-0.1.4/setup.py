717e7f89bebdc783081cc37d8de373ecb32451c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:54 +0000

    Add package datamini-toolkits/0.1.4/datamini_toolkits-0.1.4.tar.gz

---
+++ packages/datamini-toolkits/datamini_toolkits-0.1.4.tar.gz/datamini_toolkits-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datamini_toolkits',
+    version='0.1.4',
+    description='A toolkit for Data Using AI(e.g. LLM) by DataMini',
+    author='DataMini',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sql-data-maker = datamini_toolkits.mock_data.cli:main',
+            'cmysql = datamini_toolkits.cmysql.cli:main',
+        ]
+    }
+)
