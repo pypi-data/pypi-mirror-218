2af6938c621f61b4ee32063f4e84889201e83856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:21 +0000

    Add package zpz/0.5.1/zpz-0.5.1.tar.gz

---
+++ packages/zpz/zpz-0.5.1.tar.gz/zpz-0.5.1/README.md
@@ -0,0 +1,22 @@
+Python Utilities
+================
+
+This repo collects some small Python utilities that I have created (or collected) in practice,
+and organizes them as a package named `zpz`.
+
+These utilities are not "one-off" experiments; they are really useful code.
+However, they do not form a coherent set of utilities for a particular application domain.
+There is no plan to maintain this code as a coherent library.
+This package is uploaded to [pypi](https://pypi.org/project/zpz/).
+Consider it to be mainly for the author's personal convenience.
+Do not assume the releases will be maintained in a stable, regular, and backward-compatible way.
+
+One reasonable way to use it is to copy-paste whatever segments you find useful.
+
+To install, do
+
+```
+pip install zpz
+```
+
+There are a few optional dependencies specified by 'avro' and 'lineprofiler'.
---
+++ packages/zpz/zpz-0.5.1.tar.gz/zpz-0.5.1/src/zpz/datetime.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from pytz import timezone
+
+
+def tznow(tzname):
+    """
+    Get the current ``datetime`` in the specified timezone.
+
+    Args:
+        tzname: the ISO standard timezone name,
+        such as ``Africa/Cairo``, ``America/Los_Angeles``.
+    """
+    return datetime.now(timezone(tzname))
+
+
+def pacificnow():
+    return tznow("America/Los_Angeles")
+
+
+def make_timestamp() -> str:
+    """
+    This function creates a timestamp string with fixed format like
+
+        '2020-08-22T08:09:13.401346'
+
+    Strings created by this function can be compared to
+    determine time order. There is no need to parse the string
+    into `datetime` objects.
+
+    The returned string is often written as a timestamp file, like
+
+        open(file_name, 'w').write(make_timestamp())
+    """
+    return datetime.utcnow().isoformat(timespec="microseconds")
