708046a786dd47e263b0fd2f40921d5d143d4461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:18 +0000

    Add package zetascale/0.2.0/zetascale-0.2.0-py3-none-any.whl

---
+++ packages/zetascale/zetascale-0.2.0-py3-none-any.whl/zeta/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+from zeta.architecture.decoder import Decoder
+from zeta.architecture.config import DecoderConfig, EncoderConfig, EncoderDecoderConfig
+from zeta.architecture.encoder_decoder import EncoderDecoder
+
+
+#models
+from zeta.models.KosmosX import KosmosTokenizer, Kosmos
+from zeta.models.LongNet import LongNetTokenizer, LongNet
