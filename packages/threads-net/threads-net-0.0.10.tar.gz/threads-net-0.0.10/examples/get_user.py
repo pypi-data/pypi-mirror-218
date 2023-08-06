86f88680e916912dd17c648d86160ba94fc0a0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package threads-net/0.0.10/threads-net-0.0.10.tar.gz

---
+++ packages/threads-net/threads-net-0.0.10.tar.gz/threads-net-0.0.10/examples/get_user.py
@@ -0,0 +1,19 @@
+"""
+Provide example of getting a user implementation.
+"""
+import os
+import json
+
+from threads import Threads
+
+
+if __name__ == '__main__':
+    threads = Threads()
+    threads.load_settings('session.json')
+    threads.login(os.environ.get('INSTAGRAM_USERNAME'), os.environ.get('INSTAGRAM_PASSWORD'))
+
+    user_id = threads.get_user_id(username='zuck')
+    print(user_id)
+
+    user = threads.get_user(id=user_id)
+    print(json.dumps(user, indent=4))
