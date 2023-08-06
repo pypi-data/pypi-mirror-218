fad38beb679c9f55a36991f5b936ccdce6673d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:25 +0000

    Add package hak/0.0.59/hak-0.0.59.tar.gz

---
+++ packages/hak/hak-0.0.59.tar.gz/hak-0.0.59/hak/one/dict/period/financial_year/get_end_date.py
@@ -0,0 +1,12 @@
+from datetime import date
+from hak.one.string.print_and_return_false import f as pf
+from hak.one.dict.period.financial_year.make import f as mkfy
+
+# get_ω_date
+f = lambda x: date(x['final_year'], 6, 30)
+
+def t():
+  x = mkfy({'start_year': 2022})
+  y = date(2023, 6, 30)
+  z = f(x)
+  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
