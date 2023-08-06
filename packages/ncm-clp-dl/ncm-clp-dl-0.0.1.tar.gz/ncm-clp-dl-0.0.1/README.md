235c17cca4407dabde49eb5788950a871f556611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:06 +0000

    Add package ncm-clp-dl/0.0.1/ncm-clp-dl-0.0.1.tar.gz

---
+++ packages/ncm-clp-dl/ncm-clp-dl-0.0.1.tar.gz/ncm-clp-dl-0.0.1/README.md
@@ -0,0 +1,11 @@
+# 监视剪切板下载网易云音乐
+
+## 安装
+```bash
+pip install ncm-clp-dl
+```
+
+## 使用
+```bash
+ncm-clp-dl
+```
---
+++ packages/ncm-clp-dl/ncm-clp-dl-0.0.1.tar.gz/ncm-clp-dl-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ncm-clp-dl',
+    version="0.0.1",
+    description='监视剪切板下载网易云音乐',
+    long_description='',
+    author='luoyeah',
+    author_email='dao696@foxmail.com',
+    maintainer='',
+    maintainer_email='',
+    url='https://github.com/luoyeah/ncm-clp-dl',
+    license='MIT license',
+    keywords=[
+        '',
+    ],
+    install_requires=open("requirements.txt", 'r', encoding='utf-8').read().split("\n"),
+
+    entry_points={
+        'console_scripts': [
+            'ncm-clp-dl = ncm_clp_dl:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
