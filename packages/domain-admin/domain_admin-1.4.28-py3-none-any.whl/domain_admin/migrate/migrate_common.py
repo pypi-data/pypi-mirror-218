1b1d441d8694fbdcc67a4373be6c2a87b9e7431f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:43 +0000

    Add package domain-admin/1.4.28/domain_admin-1.4.28-py3-none-any.whl

---
+++ packages/domain-admin/domain_admin-1.4.28-py3-none-any.whl/domain_admin/migrate/migrate_common.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : migrate_common.py
+@Date    : 2023-06-16
+"""
+import traceback
+
+from peewee import MySQLDatabase, SqliteDatabase, PostgresqlDatabase
+from playhouse.cockroachdb import CockroachDatabase
+from playhouse.migrate import (
+    MySQLMigrator, SqliteMigrator,
+    SchemaMigrator, CockroachDBMigrator,
+    PostgresqlMigrator,
+    migrate
+)
+
+from domain_admin.log import logger
+
+
+def get_migrator(db):
+    """
+    不同的数据库，迁移方法不一样
+    :param db:
+    :return:
+    """
+
+    # mysql
+    if isinstance(db, MySQLDatabase):
+        return MySQLMigrator(db)
+    # sqlite
+    elif isinstance(db, SqliteDatabase):
+        return SqliteMigrator(db)
+    # Cockroach
+    elif isinstance(db, CockroachDatabase):
+        return CockroachDBMigrator(db)
+    # Postgresql
+    elif isinstance(db, PostgresqlDatabase):
+        return PostgresqlMigrator(db)
+    else:
+        return SchemaMigrator(db)
+
+
+def try_execute_migrate(migrate_rows: list):
+    """
+    执行迁移命令，旧版本可能因为字段缺失而报错
+    :param migrate_rows:
+    :return:
+    """
+    for migrate_row in migrate_rows:
+        try:
+            migrate(migrate_row)
+        except Exception as e:
+            logger.error(traceback.format_exc())
