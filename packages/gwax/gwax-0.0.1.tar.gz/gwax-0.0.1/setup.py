a1a683f181a91f3e5daf8d1c4a55f9f8070136b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:52 +0000

    Add package gwax/0.0.1/gwax-0.0.1.tar.gz

---
+++ packages/gwax/gwax-0.0.1.tar.gz/gwax-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+name = 'gwax'
+version = '0.0.1' # current 0.0.1
+
+with open('README.md' ,'r') as f:
+    long_description = f.read().strip()
+
+setup(
+    name=name,
+    version=version,
+    description=name,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/mdmould/{name}',
+    author='Matthew Mould',
+    author_email='mattdmould@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'jax',
+        'numpyro',
+    	],
+    python_requires='>=3.7',
+    )
+
