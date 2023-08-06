fd50ccca1644777bb755686a1f428f991a954be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:49 +0000

    Add package mydaoyipackage/0.0.3/mydaoyipackage-0.0.3.tar.gz

---
+++ packages/mydaoyipackage/mydaoyipackage-0.0.3.tar.gz/mydaoyipackage-0.0.3/mydaoyipackage/DaoYi.py
@@ -0,0 +1,15 @@
+"""
+@FILENAME: mydaoyipackage/daoyi
+@AUTHOR: DaoYi
+@TIME: 2023/7/11/上午 10:59
+@Describe: ...
+
+"""
+class daoyi:
+    path = "hello"
+
+
+
+    @classmethod
+    def love(cls):
+        print(cls.path)
---
+++ packages/mydaoyipackage/mydaoyipackage-0.0.3.tar.gz/mydaoyipackage-0.0.3/mydaoyipackage/__init__.py
@@ -0,0 +1,7 @@
+"""
+@FILENAME: mydaoyipackage/__init__.py
+@AUTHOR: DaoYi
+@TIME: 2023/7/10/下午 6:56
+@Describe: ...
+
+"""
