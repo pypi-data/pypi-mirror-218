3828a1bc6b4a25186d7f1fc6129aac151773523c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:26 +0000

    Add package moonwiki/2023.7.11.1/moonwiki-2023.7.11.1.tar.gz

---
+++ packages/moonwiki/moonwiki-2023.7.11.1.tar.gz/moonwiki-2023.7.11.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	name="moonwiki",
+	version="2023.7.11.1",
+	author="RixTheTyrunt",
+	author_email="rixthetyrunt@gmail.com",
+	description="moonwiki is a wiki application made with Python",
+	packages=["moonwiki"],
+	url="https://github.com/RixTheTyrunt/moonwiki",
+	python_requires=">=3",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown"
+)
