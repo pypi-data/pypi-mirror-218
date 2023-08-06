cfb463c2e5f2fc47c78b094744bdd3c298d70721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:55 +0000

    Add package dingtalk-stream/0.7.0/dingtalk-stream-0.7.0.tar.gz

---
+++ packages/dingtalk-stream/dingtalk-stream-0.7.0.tar.gz/dingtalk-stream-0.7.0/dingtalk_stream/__init__.py
@@ -0,0 +1,15 @@
+from .stream import DingTalkStreamClient
+from .credential import Credential
+from .handlers import EventHandler
+from .handlers import CallbackHandler
+from .handlers import SystemHandler
+from .frames import EventMessage
+from .frames import CallbackMessage
+from .frames import SystemMessage
+from .frames import AckMessage
+from .chatbot import ChatbotMessage
+from .chatbot import TextContent
+from .chatbot import AtUser
+from .chatbot import ChatbotHandler, AsyncChatbotHandler
+from .card_replier import AICardStatus, AICardReplier, CardReplier
+from .card_instance import MarkdownCardInstance, AIMarkdownCardInstance
