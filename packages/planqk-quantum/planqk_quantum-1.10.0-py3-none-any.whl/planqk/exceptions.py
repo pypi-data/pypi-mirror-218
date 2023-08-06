432a70af3162480c092b660bf714c62fff57019c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:35 +0000

    Add package planqk-quantum/1.10.0/planqk_quantum-1.10.0-py3-none-any.whl

---
+++ packages/planqk-quantum/planqk_quantum-1.10.0-py3-none-any.whl/planqk/exceptions.py
@@ -0,0 +1,11 @@
+class PlanqkError(Exception):
+    def __init__(self, *message):
+        super().__init__(' '.join(message))
+        self.message = ' '.join(message)
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class CredentialUnavailableError(PlanqkError):
+    pass
