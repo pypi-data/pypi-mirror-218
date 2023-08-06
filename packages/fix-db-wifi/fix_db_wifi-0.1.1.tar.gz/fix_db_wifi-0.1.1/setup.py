c36df48d43a824b982bb259bb8a30b70ff30ac8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:55 +0000

    Add package fix-db-wifi/0.1.1/fix_db_wifi-0.1.1.tar.gz

---
+++ packages/fix-db-wifi/fix_db_wifi-0.1.1.tar.gz/fix_db_wifi-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="fix_db_wifi",
+    version="0.1.1",
+    author="Leon Wolf",
+    author_email="fogxdev@gmail.com",
+    description="A tool to fix IP conflicts between Deutsche Bahn WIFI and Ubuntu/Docker",
+    long_description=long_description,
+    long_desciption_content_type="text/markdown",
+    url="https://github.com/fogx/fix_db_wifi",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "fix-db-wifi=fix_db_wifi.main:main",
+        ],
+    },
+)
