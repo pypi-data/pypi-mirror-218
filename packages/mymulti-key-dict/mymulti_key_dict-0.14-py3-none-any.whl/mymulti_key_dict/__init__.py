aab0eeb821001ea5eadf9fdca0978dd904e709ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:09 +0000

    Add package mymulti-key-dict/0.14/mymulti_key_dict-0.14-py3-none-any.whl

---
+++ packages/mymulti-key-dict/mymulti_key_dict-0.14-py3-none-any.whl/mymulti_key_dict/__init__.py
@@ -0,0 +1,113 @@
+import operator
+from pprint import pformat
+
+def convert_to_normal_dict_simple(di):
+    if isinstance(di, MultiKeyDict):
+        di = {k: convert_to_normal_dict_simple(v) for k, v in di.items()}
+    return di
+
+class MultiKeyDict(dict):
+
+    def __init__(self, seq=None, **kwargs):
+        if seq:
+            super().__init__(seq, **kwargs)
+
+        def convert_dict(di):
+            if (isinstance(di, dict) and not isinstance(di, self.__class__)) or (
+                (hasattr(di, "items") and hasattr(di, "keys") and hasattr(di, "keys"))
+            ):
+                ndi = self.__class__({},)
+                for k, v in di.items():
+                    ndi[k] = convert_dict(v)
+                return ndi
+            return di
+
+        for key in self:
+            self[key] = convert_dict(self[key])
+    def __str__(self):
+        return pformat({k:v for k,v in super().items()}, width=1)
+
+    def __missing__(self, key):
+        self[key] = self.__class__({})
+        return self[key]
+    def __repr__(self):
+        return self.__str__()
+    def __delitem__(self, i):
+        if isinstance(i, list):
+
+            lastkey = i[-1]
+            i = i[:-1]
+            it = iter(i)
+            firstkey = next(it)
+            value = self[firstkey]
+            for element in it:
+                value = operator.itemgetter(element)(value)
+            del value[lastkey]
+        else:
+            super().__delitem__(i)
+
+    def __getitem__(self, key, /):
+        if isinstance(key, list):
+            it = iter(key)
+            firstkey = next(it)
+            value = self[firstkey]
+            for element in it:
+                value = operator.itemgetter(element)(value)
+            return value
+        else:
+            return super().__getitem__(key)
+
+    def __setitem__(self, i, item):
+        if isinstance(i, list):
+            lastkey = i[-1]
+            i = i[:-1]
+            it = iter(i)
+            firstkey = next(it)
+            value = self[firstkey]
+            for element in it:
+                value = operator.itemgetter(element)(value)
+            value[lastkey] = item
+        else:
+            return super().__setitem__(i,item)
+    def to_dict(self):
+        return convert_to_normal_dict_simple(self)
+    def update(self, other, /, **kwds):
+        other = self.__class__(other)
+        super().update(other, **kwds)
+    def get(self, key, default=None):
+        v = default
+        if not isinstance(key, list):
+            return super().get(key,default)
+        else:
+            it = iter(key)
+            firstkey = next(it)
+            value = self[firstkey]
+            for element in it:
+                if element in value:
+                    value = operator.itemgetter(element)(value)
+                else:
+                    return default
+            return value
+    def pop(self, key, default=None):
+        if not isinstance(key, list):
+            v = super().pop(key, default)
+            return v
+        else:
+            return self._del_and_return(key,default)
+    def _del_and_return(self, key,default=None):
+        newkey = key[:-1]
+        delkey = key[-1]
+        it = iter(newkey)
+        firstkey = next(it)
+        value = self[firstkey]
+        for element in it:
+            if element in value:
+                value = operator.itemgetter(element)(value)
+            else:
+                return default
+
+        value1 = value[delkey]
+        del value[delkey]
+        return value1
+    def reversed(self):
+        return reversed(list(iter(self.keys())))
