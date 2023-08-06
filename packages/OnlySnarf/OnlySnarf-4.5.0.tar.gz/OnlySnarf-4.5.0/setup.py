a95089834143964cafad76f938e0d57f91659309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:53 +0000

    Add package OnlySnarf/4.5.0/OnlySnarf-4.5.0.tar.gz

---
+++ packages/OnlySnarf/OnlySnarf-4.5.0.tar.gz/OnlySnarf-4.5.0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OnlySnarf",
+    version="4.5.0",
+    author="Skeetzo",
+    author_email="WebmasterSkeetzo@gmail.com",
+    url = 'https://github.com/skeetzo/onlysnarf',
+    keywords = ['OnlyFans', 'OnlySnarf', 'selenium', 'snarf'],
+    description="OnlyFans Content Distribution Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # packages=setuptools.find_packages(),
+    packages=["OnlySnarf", "OnlySnarf/classes","OnlySnarf/conf","OnlySnarf/elements","OnlySnarf/lib","OnlySnarf/util"],
+    include_package_data=True,
+    install_requires=[
+        'ffmpeg',
+        'inquirer',
+        'wget',
+        'selenium',
+        'webdriver_manager'
+        ],
+    extras_require={
+        'dev': [
+            'pytest'
+        ]
+    },
+    entry_points={
+        'console_scripts' : [
+            'onlysnarf = OnlySnarf.menu:main',
+            'snarf = OnlySnarf.snarf:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Emulators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        "Operating System :: OS Independent"
+    ]
+)
