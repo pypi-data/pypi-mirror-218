ce14c032a42f14e4aed32ca2fb65dba1b4267fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:04 +0000

    Add package langcorn/0.0.11/langcorn-0.0.11.tar.gz

---
+++ packages/langcorn/langcorn-0.0.11.tar.gz/langcorn-0.0.11/langcorn/server/test_api.py
@@ -0,0 +1,42 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from examples import app
+
+from .api import create_service
+
+client = TestClient(create_service("examples.ex1:chain"))
+
+example_app = TestClient(app.app)
+
+
+@pytest.fixture(
+    scope="session",
+)
+def fn_executor():
+    yield None
+
+
+class TestRoutes:
+    def test_examples(self):
+        response = example_app.get("/")
+        assert response.status_code == 404
+
+    def test_read_main(self):
+        response = client.get("/")
+        assert response.status_code == 404
+
+    def test_state(self):
+        response = client.get("/ht")
+        assert response.status_code == 200
+        assert response.json()
+
+    # TODO: add error handling
+    @pytest.mark.parametrize(
+        "apps",
+        [("examples.ex1:chain",), ("examples.ex2:chain", "examples.ex1:chain")],
+    )
+    def test_create_service(self, apps):
+        client = TestClient(create_service(*apps))
+        response = client.get("/")
+        assert response.status_code == 404
