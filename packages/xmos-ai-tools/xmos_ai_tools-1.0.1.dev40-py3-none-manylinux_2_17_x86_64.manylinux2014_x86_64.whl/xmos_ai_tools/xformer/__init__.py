404ea2a991a2b5e135db725d47ef270fbf4b7a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:35 +0000

    Add package xmos-ai-tools/1.0.1.dev40/xmos_ai_tools-1.0.1.dev40-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xmos-ai-tools/xmos_ai_tools-1.0.1.dev40-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/xmos_ai_tools/xformer/__init__.py
@@ -0,0 +1,39 @@
+import subprocess
+import typing
+from pathlib import Path
+from typing import Union, List, Optional
+from .flash import generate_flash
+
+
+def convert(
+    filename: Union[str, Path],
+    outfile: Union[str, Path],
+    params: Optional[typing.Dict[str, Optional[str]]],
+) -> int:
+    args: List[str] = ["xcore-opt", "-o", str(outfile)]
+
+    if params is not None:
+        for key, val in params.items():
+            if len(key) > 1:
+                flag: str = "--" + str(key)
+            else:
+                flag = "-" + str(key)
+            if str(val) == "" or val is None:
+                args.append(flag)
+            else:
+                args.append(f"{flag}")
+                args.append(f"{val}")
+
+    args.append(str(filename))
+
+    process_call: subprocess.CompletedProcess = subprocess.run(
+        [arg for arg in args], check=True
+    )
+    return process_call.returncode
+
+
+def print_help(show_hidden: Optional[bool] = False) -> int:
+    if show_hidden:
+        return subprocess.run(["xcore-opt", "--help-list-hidden"]).returncode
+
+    return subprocess.run(["xcore-opt", "--help-list"]).returncode
