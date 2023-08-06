f509ea9dec754092890ee549702eba683da677aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:57 +0000

    Add package image-func/1.0.10/image_func-1.0.10-py3-none-any.whl

---
+++ packages/image-func/image_func-1.0.10-py3-none-any.whl/image_func/a.py
@@ -0,0 +1,62 @@
+from PIL import Image
+from scipy.fftpack import fft2, ifft2
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def apply_convolution(image, kernel):
+    convolved_image = cv2.filter2D(image, -1, kernel)
+    return convolved_image
+
+def apply_correlation(image, kernel):
+    correlated_image = cv2.filter2D(image, -1, kernel)
+    return correlated_image
+
+def signaltonoise(a, axis=0, ddof=0):
+    a = np.asanyarray(a)
+    m = a.mean(axis)
+    sd = a.std(axis=axis, ddof=ddof)
+    return np.where(sd == 0, 0, m / sd)
+
+def process_image(image_path):
+    im = Image.open(image_path).convert('L')
+    im_array = np.array(im)
+    
+    freq = fft2(im_array)
+    im1 = ifft2(freq).real
+    
+    snr = signaltonoise(im1, axis=None)
+    
+    return im_array, im1, snr
+
+def perform_fourier_transform(image_path):
+    # Read input image and convert to grayscale
+    img = cv2.imread(image_path, 0)
+
+    # Calculate optimal size for Fourier transform
+    optimalImg = cv2.copyMakeBorder(img, 0, cv2.getOptimalDFTSize(img.shape[0]) - img.shape[0], 0, cv2.getOptimalDFTSize(img.shape[1]) - img.shape[1], cv2.BORDER_CONSTANT, value=0)
+
+    # Calculate the discrete Fourier transform
+    dft_shift = np.fft.fftshift(cv2.dft(np.float32(optimalImg), flags=cv2.DFT_COMPLEX_OUTPUT))
+
+    # Calculate magnitude spectrum
+    magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]) + 1)
+
+    # Reconstruct the image using inverse Fourier transform
+    result = cv2.magnitude(cv2.idft(np.fft.ifftshift(dft_shift))[:, :, 0], cv2.idft(np.fft.ifftshift(dft_shift))[:, :, 1])
+
+    return optimalImg, magnitude_spectrum, result
+
+
+def apply_log_transform(image_path, output_path):
+    # Read the image
+    img = cv2.imread(image_path)
+
+    # Apply log transform
+    log_transformed = 255 * np.log(1 + img.astype(np.float32)) / np.log(1 + np.max(img))
+
+    # Convert the data type
+    log_transformed = log_transformed.astype(np.uint8)
+
+    # Save the output image
+    cv2.imwrite(output_path, log_transformed)
