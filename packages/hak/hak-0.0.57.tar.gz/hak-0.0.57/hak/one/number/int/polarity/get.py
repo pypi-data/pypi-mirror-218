e1659198a344c80bcb7718101c0b329c4bef1ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:48 +0000

    Add package hak/0.0.57/hak-0.0.57.tar.gz

---
+++ packages/hak/hak-0.0.57.tar.gz/hak-0.0.57/hak/one/number/int/polarity/get.py
@@ -0,0 +1,27 @@
+from hak.one.string.print_and_return_false import f as pf
+
+f = lambda x: int(x/abs(x) if x else 0)
+
+def t_negative():
+  x = -123
+  y = -1
+  z = f(x)
+  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+
+def t_zero():
+  x = 0
+  y = 0
+  z = f(x)
+  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+
+def t_positive():
+  x = 123
+  y = 1
+  z = f(x)
+  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+
+def t():
+  if not t_negative(): return pf('!t_negative')
+  if not t_zero(): return pf('!t_zero')
+  if not t_positive(): return pf('!t_positive')
+  return True
