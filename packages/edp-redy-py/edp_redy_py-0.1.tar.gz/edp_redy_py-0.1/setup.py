5067792c294ab46fab2338e4e5bb888f2c962ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:46 +0000

    Add package edp-redy-py/0.1/edp_redy_py-0.1.tar.gz

---
+++ packages/edp-redy-py/edp_redy_py-0.1.tar.gz/edp_redy_py-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='edp_redy_py',
+    version='0.1',
+    license='Apache 2.0',
+    author="Fábio Ferreira",
+    author_email='fabiorcferreira@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/skyborgff/edp_redy',
+    keywords='EDP',
+    install_requires=[],
+
+)
