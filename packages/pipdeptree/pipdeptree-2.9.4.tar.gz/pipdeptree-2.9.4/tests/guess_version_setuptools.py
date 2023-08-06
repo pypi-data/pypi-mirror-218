8c1cd9417b5beba6aac67b8cbee4f334c2132f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package pipdeptree/2.9.4/pipdeptree-2.9.4.tar.gz

---
+++ packages/pipdeptree/pipdeptree-2.9.4.tar.gz/pipdeptree-2.9.4/tests/guess_version_setuptools.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import sys
+
+import pipdeptree
+
+if sys.version_info >= (3, 8):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def raise_import_error(name):
+    raise ImportError(name)
+
+
+importlib_metadata.version = raise_import_error
+print(pipdeptree.guess_version("setuptools"), end="")  # noqa: T201
