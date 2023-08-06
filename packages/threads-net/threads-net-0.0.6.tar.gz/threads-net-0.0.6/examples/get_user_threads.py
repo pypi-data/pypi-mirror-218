e2bb012adaceaab64848a27111c107a22f155302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:59 +0000

    Add package threads-net/0.0.6/threads-net-0.0.6.tar.gz

---
+++ packages/threads-net/threads-net-0.0.6.tar.gz/threads-net-0.0.6/examples/get_user_threads.py
@@ -0,0 +1,12 @@
+"""
+Provide example of getting a user's threads implementation.
+"""
+import json
+
+from threads import Threads
+
+
+if __name__ == '__main__':
+    threads = Threads()
+    user_threads = threads.get_user_threads(id=314216)
+    print(json.dumps(user_threads, indent=4))
