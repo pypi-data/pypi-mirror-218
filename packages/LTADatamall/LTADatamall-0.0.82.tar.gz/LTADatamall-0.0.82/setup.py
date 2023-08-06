42e51b45d19decd5053ff1c93e3f19bb2400a8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package LTADatamall/0.0.82/LTADatamall-0.0.82.tar.gz

---
+++ packages/LTADatamall/LTADatamall-0.0.82.tar.gz/LTADatamall-0.0.82/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+    name='LTADatamall',
+    packages=['LTADatamall'],
+    version='0.0.82',
+    license='MIT',
+    description='LTA Datamall API Wrapper',
+    author='FishballNoodles',
+    author_email='joelkhor.work@gmail.com',
+    url='https://github.com/TheReaper62/ltadatamall',
+    download_url='https://github.com/TheReaper62/ltadatamall/archive/refs/tags/v0.0.82.tar.gz',
+    keywords=['lta','datamall','bus','timing','arrival','train','service','passenger volume','taxi'],
+    install_requires=[
+        'requests',
+        'aiohttp',
+        'asyncio',
+        'datetime',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
