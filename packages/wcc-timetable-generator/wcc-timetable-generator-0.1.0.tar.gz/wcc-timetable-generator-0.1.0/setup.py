f7fd27cbd68907dfabbb33490befef7299c83eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:31 +0000

    Add package wcc-timetable-generator/0.1.0/wcc-timetable-generator-0.1.0.tar.gz

---
+++ packages/wcc-timetable-generator/wcc-timetable-generator-0.1.0.tar.gz/wcc-timetable-generator-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wcc_timetable_generator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'wcc-timetable-generator',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': "# Timetable 📊\n\n## 📰 Description \nA CLI tool that allow you to generate a timetable for your school or university such that : \n - A subject can't be seen on two consecutive days\n - A subject have a minimum of 2 hours per week and a maximum of 6\n - The timetable is divided in slots of 2 hours\n - The subjects are distributed between monday morning and saturady morning\n - Morning classes begin at 8:30 and end at 12:30\n - Afternoon classes begin at 13:30 and end at 17:30\n\n ## 📦 Installation \n\n ### 💻 Local installation\n\n ### 🌐 Installation with PIP\n\n## 🖱 Usage\n\n## Roadmap\n- [x] Add the algorithm \n- [ ] Add GUI\n- [ ] Write tests\n- [ ] Publish to PyPI",
+    'author': 'Tsierenana Bôtramanagna Gracy',
+    'author_email': 'gtsierenana@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
