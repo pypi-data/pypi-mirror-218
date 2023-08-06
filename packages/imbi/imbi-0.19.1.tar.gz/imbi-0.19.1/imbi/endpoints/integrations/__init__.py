012101ea9df0208e39279ca41afa1effba8810dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package imbi/0.19.1/imbi-0.19.1.tar.gz

---
+++ packages/imbi/imbi-0.19.1.tar.gz/imbi-0.19.1/imbi/endpoints/integrations/__init__.py
@@ -0,0 +1,18 @@
+"""
+Integration Endpoints
+
+"""
+from tornado import web
+
+from . import gitlab, google, oauth2
+
+URLS = [
+    web.url(r'^/gitlab/auth', gitlab.RedirectHandler),
+    web.url(r'^/gitlab/namespaces', gitlab.UserNamespacesHandler),
+    web.url(r'^/gitlab/projects', gitlab.ProjectsHandler),
+    web.url(r'^/google/auth', google.RedirectHandler),
+    web.url(r'^/integrations$', oauth2.CollectionRequestHandler),
+    web.url(r'^/integrations/(?P<name>[\w_\-%\+]+)$',
+            oauth2.RecordRequestHandler,
+            name='integration')
+]
