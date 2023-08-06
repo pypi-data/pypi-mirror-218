6cff4e5b29d04a5f137fa5af2e80bf39df96a9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:36 +0000

    Add package ibis-framework/6.0.1.dev33/ibis_framework-6.0.1.dev33.tar.gz

---
+++ packages/ibis-framework/ibis_framework-6.0.1.dev33.tar.gz/ibis_framework-6.0.1.dev33/ibis/backends/snowflake/converter.py
@@ -0,0 +1,7 @@
+from ibis.formats.pandas import PandasData
+
+
+class SnowflakePandasData(PandasData):
+    convert_Struct = staticmethod(PandasData.convert_JSON)
+    convert_Array = staticmethod(PandasData.convert_JSON)
+    convert_Map = staticmethod(PandasData.convert_JSON)
