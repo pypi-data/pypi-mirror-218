4884c0a1f09238815f79b3b20ecfe187844acbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:38 +0000

    Add package RustAdmin/1.0.5/RustAdmin-1.0.5.tar.gz

---
+++ packages/RustAdmin/RustAdmin-1.0.5.tar.gz/RustAdmin-1.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RustAdmin',
+    version='1.0.5',
+    author='Pawan kumar',
+    author_email='control@vvfin.in',
+    url='https://github.com/yourusername/rustadmin',
+    py_modules=['rustadmin'],
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'rust-admin = admin.main:main',
+        ],
+    },
+)
