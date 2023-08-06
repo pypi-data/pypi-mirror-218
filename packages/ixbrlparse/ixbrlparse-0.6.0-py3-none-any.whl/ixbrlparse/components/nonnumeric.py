7fd38c14b6caab23867a99da1a46040d1a6c253b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package ixbrlparse/0.6.0/ixbrlparse-0.6.0-py3-none-any.whl

---
+++ packages/ixbrlparse/ixbrlparse-0.6.0-py3-none-any.whl/ixbrlparse/components/nonnumeric.py
@@ -0,0 +1,36 @@
+from copy import deepcopy
+from typing import Any, Dict, List, Optional, Union
+
+from bs4 import Tag
+
+from ixbrlparse.components import ixbrlContext
+from ixbrlparse.components.constants import NAME_SPLIT_EXPECTED
+
+
+class ixbrlNonNumeric:  # noqa: N801
+    def __init__(
+        self,
+        context: Union[ixbrlContext, str, None],
+        name: str,
+        format_: Optional[str],
+        value: str,
+        soup_tag: Optional[Tag] = None,
+    ) -> None:
+        name_split: List[str] = name.split(":", maxsplit=1)
+        if len(name_split) == NAME_SPLIT_EXPECTED:
+            self.schema = name_split[0]
+            self.name = name_split[1]
+        else:
+            self.schema = "unknown"
+            self.name = name_split[0]
+
+        self.context = context
+        self.format = format_
+        self.value = value
+        self.soup_tag = soup_tag
+
+    def to_json(self) -> Dict[str, Any]:
+        values = {k: deepcopy(v) for k, v in self.__dict__.items() if k != "soup_tag"}
+        if isinstance(self.context, ixbrlContext):
+            values["context"] = self.context.to_json()
+        return values
