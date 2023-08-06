14344e70735b885d15f60bfdef1b1f91b0676657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:35 +0000

    Add package PySortAlgos/0.1/PySortAlgos-0.1.tar.gz

---
+++ packages/PySortAlgos/PySortAlgos-0.1.tar.gz/PySortAlgos-0.1/PySortAlgos/__init__.py
@@ -0,0 +1,1 @@
+from .pysort import sort
---
+++ packages/PySortAlgos/PySortAlgos-0.1.tar.gz/PySortAlgos-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'PySortAlgos',
+  packages = ['PySortAlgos'],
+  version = '0.1',
+  license='MIT',
+  description = 'A simple repo that contains some commonly used sorting functions',
+  author = 'Bahir Hakimi',
+  author_email = 'bahirhakimy2020@gmail.com',
+  url = 'https://github.com/bahirhakimy/PySortAlgos',
+  download_url = 'https://github.com/BahirHakimy/PySortAlgos/archive/refs/tags/0.1.tar.gz',
+  keywords = ['sorting', 'algorithm', 'quicksort', 'array'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.9',
+  ],
+)
