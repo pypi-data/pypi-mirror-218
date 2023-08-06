3570fe3d945fea21a271701eac647e64917016be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:31 +0000

    Add package LongNet/0.1.3/LongNet-0.1.3.tar.gz

---
+++ packages/LongNet/LongNet-0.1.3.tar.gz/LongNet-0.1.3/LongNet/__init__.py
@@ -0,0 +1,6 @@
+# from LongNet.training import Train
+# from LongNet.model_test import LongNetTest
+# from LongNet.torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
+# from LongNet.model import LongNetTokenizer, LongNet
+from LongNet.torchscale import RelativePositionBias, XPOS
+from LongNet.attention import DilatedAttention
