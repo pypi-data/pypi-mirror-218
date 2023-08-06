4b02bec585286dc28785be7196865aef52b56acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:52 +0000

    Add package seleniumbase/4.15.12/seleniumbase-4.15.12.tar.gz

---
+++ packages/seleniumbase/seleniumbase-4.15.12.tar.gz/seleniumbase-4.15.12/examples/uc_cdp_events.py
@@ -0,0 +1,23 @@
+from pprint import pformat
+from seleniumbase import BaseCase
+
+if __name__ == "__main__":
+    from pytest import main
+    main([__file__, "--uc", "--uc-cdp", "--incognito", "-s"])
+
+
+class CDPTests(BaseCase):
+    def test_display_cdp_events(self):
+        if not (self.undetectable and self.uc_cdp_events and self.incognito):
+            self.get_new_driver(
+                undetectable=True, uc_cdp_events=True, incognito=True
+            )
+        # (To print everything, use "*". Otherwise select specific headers.)
+        # self.driver.add_cdp_listener("*", lambda data: print(pformat(data)))
+        self.driver.add_cdp_listener(
+            "Network.requestWillBeSentExtraInfo",
+            lambda data: print(pformat(data))
+        )
+        self.open("https://nowsecure.nl/#relax")
+        self.assert_text("OH YEAH, you passed!", "h1", timeout=7.25)
+        self.sleep(2)
