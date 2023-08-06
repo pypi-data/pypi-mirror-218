fe4e92ec3174791c23fa9663cde94032d3ff7925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:55 +0000

    Add package diz/0.0.7/diz-0.0.7.tar.gz

---
+++ packages/diz/diz-0.0.7.tar.gz/diz-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='diz',
+    version='0.0.7',
+    author='mjason',
+    description='用来构建大模型环境的工具',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'GitPython>=3.1.31',
+        'huggingface-hub>=0.16.4',
+        'PyYAML>=6.0',
+        'requests>=2.31.0',
+        'toolz>=0.12.0',
+        'typer>=0.9.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'diz = diz.main:main'
+        ]
+    }
+)
