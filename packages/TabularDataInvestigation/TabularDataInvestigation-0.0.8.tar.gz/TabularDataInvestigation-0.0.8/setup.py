8496704167243d36a88d114fefa7e07c31818525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:26 +0000

    Add package TabularDataInvestigation/0.0.8/TabularDataInvestigation-0.0.8.tar.gz

---
+++ packages/TabularDataInvestigation/TabularDataInvestigation-0.0.8.tar.gz/TabularDataInvestigation-0.0.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='TabularDataInvestigation',
+    packages=['TabularDataInvestigation'],
+    version='0.0.8',
+    license='MIT',
+    description='This package provide a fast tabular data investigation and it will eligible for ML model building and also helps to developers in their projects when needed ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Tanvir Islam',
+    author_email='islamtanvir659@gmail.com',
+    url='https://github.com/Tanvir223/TabularDataInvestigation',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Tanvir223/TabularDataInvestigation/issues"
+    },
+    install_requires=['requests'],
+
+    download_url="https://github.com/Tanvir223/TabularDataInvestigation/archive/refs/tags/0.0.8.tar.gz",
+    keywords=["pypi", "TabularDataInvestigation", "TabularData", "Data-Manupulation", "Data-Preprocessing", "Data Cleaning","Machine Learning", "Artificial Intelligence", "Industry Data" , "Data Science"],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
