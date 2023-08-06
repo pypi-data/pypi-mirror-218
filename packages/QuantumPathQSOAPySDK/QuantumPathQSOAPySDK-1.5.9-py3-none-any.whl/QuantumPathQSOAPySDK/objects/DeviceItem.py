6c4235f1a7d1de84816d6af829494dab35504497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:22 +0000

    Add package QuantumPathQSOAPySDK/1.5.9/QuantumPathQSOAPySDK-1.5.9-py3-none-any.whl

---
+++ packages/QuantumPathQSOAPySDK/QuantumPathQSOAPySDK-1.5.9-py3-none-any.whl/QuantumPathQSOAPySDK/objects/DeviceItem.py
@@ -0,0 +1,34 @@
+class DeviceItem:
+
+    # CONSTRUCTOR
+    def __init__(self, device: dict):
+        self.__deviceShortName = device['DeviceShortName']
+        self.__quantumMachineType = device['QuantumMachineType']
+        self.__isLocalSimulator = device['IsLocalSimulator']
+        self.__idVendor = device['idVendor'] ################################## idDevice
+        self.__vendorName = device['VendorName']
+        self.__description = device['Description']
+        self.__deviceName = device['DeviceName']
+    
+    
+    # GETTERS
+    def getDeviceShortName(self) -> str:
+        return self.__deviceShortName
+
+    def getQuantumMachineType(self) -> str:
+        return self.__quantumMachineType
+        
+    def getIsLocalSimulator(self) -> bool:
+        return self.__isLocalSimulator
+
+    def getIdVendor(self) -> int:
+        return self.__idVendor
+
+    def getVendorName(self) -> str:
+        return self.__vendorName
+
+    def getDescription(self) -> str:
+        return self.__description
+
+    def getDeviceName(self) -> str:
+        return self.__deviceName
