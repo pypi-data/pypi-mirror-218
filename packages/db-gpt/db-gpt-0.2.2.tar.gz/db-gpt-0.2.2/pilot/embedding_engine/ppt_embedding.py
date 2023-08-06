99f78b944fd0ca2a557fe3e21acf4b3522d950df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:15 +0000

    Add package db-gpt/0.2.2/db-gpt-0.2.2.tar.gz

---
+++ packages/db-gpt/db-gpt-0.2.2.tar.gz/db-gpt-0.2.2/pilot/embedding_engine/ppt_embedding.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from typing import List, Optional
+
+from langchain.document_loaders import UnstructuredPowerPointLoader
+from langchain.schema import Document
+from langchain.text_splitter import SpacyTextSplitter, RecursiveCharacterTextSplitter, TextSplitter
+
+from pilot.embedding_engine import SourceEmbedding, register
+
+
+class PPTEmbedding(SourceEmbedding):
+    """ppt embedding for read ppt document."""
+
+    def __init__(self, file_path, vector_store_config, text_splitter: Optional[TextSplitter] = None):
+        """Initialize ppt word path."""
+        super().__init__(file_path, vector_store_config, text_splitter=None)
+        self.file_path = file_path
+        self.vector_store_config = vector_store_config
+        self.text_splitter = text_splitter or None
+
+    @register
+    def read(self):
+        """Load from ppt path."""
+        loader = UnstructuredPowerPointLoader(self.file_path)
+        if self.text_splitter is None:
+            try:
+                self.text_splitter = SpacyTextSplitter(
+                    pipeline="zh_core_web_sm",
+                    chunk_size=100,
+                    chunk_overlap=100,
+                )
+            except Exception:
+                self.text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=100, chunk_overlap=50
+                )
+
+        return loader.load_and_split(self.text_splitter)
+
+    @register
+    def data_process(self, documents: List[Document]):
+        i = 0
+        for d in documents:
+            documents[i].page_content = d.page_content.replace("\n", "")
+            i += 1
+        return documents
---
+++ packages/db-gpt/db-gpt-0.2.2.tar.gz/db-gpt-0.2.2/pilot/embedding_engine/string_embedding.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+
+from langchain.schema import Document
+from langchain.text_splitter import TextSplitter
+
+from pilot.embedding_engine import SourceEmbedding, register
+
+
+class StringEmbedding(SourceEmbedding):
+    """string embedding for read string document."""
+
+    def __init__(self, file_path, vector_store_config, text_splitter: Optional[TextSplitter] = None):
+        """Initialize raw text word path."""
+        super().__init__(file_path=file_path, vector_store_config=vector_store_config)
+        self.file_path = file_path
+        self.vector_store_config = vector_store_config
+        self.text_splitter = text_splitter or None
+
+    @register
+    def read(self):
+        """Load from String path."""
+        metadata = {"source": "db_summary"}
+        return [Document(page_content=self.file_path, metadata=metadata)]
+
+    @register
+    def data_process(self, documents: List[Document]):
+        i = 0
+        for d in documents:
+            documents[i].page_content = d.page_content.replace("\n", "")
+            i += 1
+        return documents
