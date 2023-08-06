1f9c9edb21f40d0d25b60eb371fc2ecca1068b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package namespace-mahdimir/1.0.2/namespace_mahdimir-1.0.2.tar.gz

---
+++ packages/namespace-mahdimir/namespace_mahdimir-1.0.2.tar.gz/namespace_mahdimir-1.0.2/src/namespace_mahdimir/tse_github_data_url.py
@@ -0,0 +1,20 @@
+"""
+
+    """
+
+my_github_username = "imahdimir"
+m = my_github_username + "/"
+
+class GitHubDataUrl :
+    ind_ins = "d-Ind-Ins"
+    id_2_tic = 'd-TSETMC_ID-2-Ticker'
+    adj_ret = 'd-Adjusted-Returns'
+    rf = 'd-Iran-RiskFree-Rate-Monthly'
+    mkt_indx = 'd-TSE-Overall-Index-TEDPIX'
+    codal_ltrs = 'd-all-Codal-letters'
+    codal_tics_2_ftics = 'd-CodalTicker-2-FirmTicker'
+
+    def __init__(self) :
+        for ky , vl in vars(GitHubDataUrl).items() :
+            if not ky.startswith('_') :
+                setattr(self , ky , m + vl)
