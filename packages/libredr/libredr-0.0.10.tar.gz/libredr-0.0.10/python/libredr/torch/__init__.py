72c4d9b467ed17802c68590d0449e77eee6cdc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:02 +0000

    Add package libredr/0.0.10/libredr-0.0.10.tar.gz

---
+++ packages/libredr/libredr-0.0.10.tar.gz/libredr-0.0.10/python/libredr/torch/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["RayTracingFunction", "RayTracing", "PoolRayTracingFunction", "PoolRayTracing"]
+
+# PyTorch bindings
+from .ray_tracing import RayTracingFunction, RayTracing, PoolRayTracingFunction, PoolRayTracing
