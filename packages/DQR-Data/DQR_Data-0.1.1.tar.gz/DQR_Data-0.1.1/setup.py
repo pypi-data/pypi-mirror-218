2b0eee9f4d3d5568b28d99aeaeca5531f0c775f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:11 +0000

    Add package DQR-Data/0.1.1/DQR_Data-0.1.1.tar.gz

---
+++ packages/DQR-Data/DQR_Data-0.1.1.tar.gz/DQR_Data-0.1.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()  
+setup(
+    name='DQR_Data',
+    version='0.1.1',
+    description='A python library for cryptocurrency trading for Binance and Deribit',
+    long_description=long_description,    
+    long_description_content_type="text/markdown",
+    author='DAOQuantResearch',
+    author_email='DQR-contact@proton.me',
+    packages=['DQR_Data'],
+    url="https://github.com/DAOQuantResearch/data_package/tree/release_v1",
+    project_urls={
+        'Source': 'https://github.com/DAOQuantResearch/data_package/tree/release_v1',
+    },
+    install_requires=[
+        'requests',
+        'pandas',
+        'websocket-client',
+        'aiohttp',
+    ],
+)
