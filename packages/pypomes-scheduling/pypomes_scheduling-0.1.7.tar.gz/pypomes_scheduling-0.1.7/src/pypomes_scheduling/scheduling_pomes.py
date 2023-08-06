2a85a04a249b11403475dcf6fbf7c9a7c2bea6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:18 +0000

    Add package pypomes-scheduling/0.1.7/pypomes_scheduling-0.1.7.tar.gz

---
+++ packages/pypomes-scheduling/pypomes_scheduling-0.1.7.tar.gz/pypomes_scheduling-0.1.7/src/pypomes_scheduling/scheduling_pomes.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from pypomes_core import exc_format
+from typing import Final
+import logging
+import pytz
+import re
+import sys
+from .threaded_scheduler import __ThreadedScheduler
+
+__REGEX_VERIFY_CRON: Final[str] = "/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|" \
+                                  "(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})"
+
+__scheduler: __ThreadedScheduler
+
+
+def scheduler_create(errors: list[str], timezone: pytz.BaseTzInfo,
+                     retry_interval: int, logger: logging.Logger = None) -> bool:
+    """
+    Create the threaded job scheduler. This is a wrapper around the package *apscheduler*.
+
+    :param errors: Resulting errors, if any
+    :param timezone: The timezone to be used
+    :param retry_interval: Interval between retry attempts, in minutes
+    :param logger: Optional logger object
+    :return: True if scheduler was created, or False if an error prevented its creation
+    """
+    global __scheduler
+    # inicialize the return variable
+    result: bool = False
+
+    try:
+        __scheduler = __ThreadedScheduler(timezone, retry_interval, logger)
+        __scheduler.daemon = True
+        result = True
+    except Exception as e:
+        errors.append(f"Error creating the job scheduler: {exc_format(e, sys.exc_info())}")
+
+    return result
+
+
+def scheduler_schedule(errors: list[str],
+                       jobs: list[tuple[callable, str, str, str, datetime, tuple, dict]]) -> int:
+    """
+    Schedule the jobs in *jobs*, starting at the given *start*. Each element in the job list
+    is a *tuple* with the corresponding job data:
+    *(callable function, job id, job name, CRON expression, start timestamp, job args, job kwargs)*.
+    Only the first three data items are required.
+
+    :param errors: Resulting errors, if any
+    :param jobs: List of tuples with jobs to schedule
+    :return: The number of jobs effectively scheduled
+    """
+    global __scheduler
+    # inicializa a variável de retorno
+    result: int = 0
+
+    # traverse the job list and do the scheduling
+    for job in jobs:
+
+        # has a valid CRON expression been provided ?
+        if job[3] is not None and re.search(__REGEX_VERIFY_CRON, job[3]) is None:
+            # no, report the error
+            errors.append(f"Invalid CRON expression: '{job[4]}'")
+        else:
+            # yes, proceed with the scheduling
+            job_cron: str = job[3] if len(job) > 3 else None
+            job_start: datetime = job[4] if len(job) > 4 else None
+            job_args: tuple = job[5] if len(job) > 5 else None
+            job_kwargs: dict = job[6] if len(job) > 6 else None
+            try:
+                __scheduler.schedule_job(job[0], job[1], job[2], job_cron, job_start, job_args, job_kwargs)
+                result += 1
+            except Exception as e:
+                errors.append(f"Error scheduling the job '{job[2]}', id '{job[1]}', "
+                              f"with CRON '{job[3]}': {exc_format(e, sys.exc_info())}")
+
+    return result
+
+
+def scheduler_start():
+    """
+    Start the scheduler.
+    """
+    global __scheduler
+    __scheduler.start()
+
+
+def scheduler_stop():
+    """
+    Stop the scheduler.
+    """
+    global __scheduler
+    __scheduler.stop()
