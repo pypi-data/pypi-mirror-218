dab3e177a6edc579ad7db511159bf2cc4a434531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:16 +0000

    Add package kuantaz-api-client/1.0.1/kuantaz_api_client-1.0.1-py3-none-any.whl

---
+++ packages/kuantaz-api-client/kuantaz_api_client-1.0.1-py3-none-any.whl/kuantaz_api_client/__init__.py
@@ -0,0 +1,8 @@
+__project__ = 'kuantaz_api_client'
+__version__ = "1.0.1"
+
+from .Client import *
+from .KuantazException import *
+from .RequestHelper import *
+from .StatisticResult import *
+from .VerificationResult import *
