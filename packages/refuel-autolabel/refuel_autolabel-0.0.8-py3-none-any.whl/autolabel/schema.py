606a8dfd93054e92611099141a849ebbf0565754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package refuel-autolabel/0.0.8/refuel_autolabel-0.0.8-py3-none-any.whl

---
+++ packages/refuel-autolabel/refuel_autolabel-0.0.8-py3-none-any.whl/autolabel/schema.py
@@ -0,0 +1,171 @@
+from datetime import datetime
+from enum import Enum
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+from langchain.schema import Generation
+from pydantic import BaseModel
+
+from autolabel.configs import AutolabelConfig
+from autolabel.utils import calculate_md5
+
+
+class ModelProvider(str, Enum):
+    """Enum containing all LLM providers currently supported by autolabeler"""
+
+    OPENAI = "openai"
+    ANTHROPIC = "anthropic"
+    HUGGINGFACE_PIPELINE = "huggingface_pipeline"
+    REFUEL = "refuel"
+    GOOGLE = "google"
+    COHERE = "cohere"
+
+
+class TaskType(str, Enum):
+    """Enum containing all the types of tasks that autolabeler currently supports"""
+
+    CLASSIFICATION = "classification"
+    NAMED_ENTITY_RECOGNITION = "named_entity_recognition"
+    QUESTION_ANSWERING = "question_answering"
+    ENTITY_MATCHING = "entity_matching"
+    MULTILABEL_CLASSIFICATION = "multilabel_classification"
+
+
+class FewShotAlgorithm(str, Enum):
+    """Enum of supported algorithms for choosing which examples to provide the LLM in its instruction prompt"""
+
+    FIXED = "fixed"
+    SEMANTIC_SIMILARITY = "semantic_similarity"
+    MAX_MARGINAL_RELEVANCE = "max_marginal_relevance"
+    LABEL_DIVERSITY_RANDOM = "label_diversity_random"
+    LABEL_DIVERSITY_SIMILARITY = "label_diversity_similarity"
+
+
+class TaskStatus(str, Enum):
+    ACTIVE = "active"
+
+
+class Metric(str, Enum):
+    """Enum of supported performance metrics. Some metrics are always available (task agnostic), while others are only supported by certain types of tasks"""
+
+    # Task agnostic
+    SUPPORT = "support"
+    COMPLETION_RATE = "completion_rate"
+    # Classification metrics
+    ACCURACY = "accuracy"
+    CONFUSION_MATRIX = "confusion_matrix"
+    LABEL_DISTRIBUTION = "label_distribution"
+    F1 = "f1"
+    # Confidence metrics
+    AUROC = "auroc"
+    THRESHOLD = "threshold"
+
+
+class MetricResult(BaseModel):
+    """Contains performance metrics gathered from autolabeler runs"""
+
+    metric_type: Metric
+    name: str
+    value: Any
+
+
+class LLMAnnotation(BaseModel):
+    """Contains label information of a given data point, including the generated label, the prompt given to the LLM, and the LLMs response. Optionally includes a confidence_score if supported by the model"""
+
+    successfully_labeled: bool
+    label: Any
+    curr_sample: Optional[str] = ""
+    confidence_score: Optional[float] = None
+    generation_info: Optional[Dict[str, Any]] = None
+    raw_response: Optional[str] = ""
+    prompt: Optional[str] = ""
+
+
+class Dataset(BaseModel):
+    """Contains Dataset parameters, including input file path, indexes for state management (e.g. job batching and retries), and a unique ID"""
+
+    id: str
+    input_file: str
+    start_index: int
+    end_index: int
+
+    class Config:
+        orm_mode = True
+
+    @classmethod
+    def create_id(
+        self,
+        dataset: Union[str, pd.DataFrame],
+        config: AutolabelConfig,
+        start_index: int,
+        max_items: int,
+    ) -> str:
+        """
+        Generates a unique ID for the given Dataset configuration
+        Args:
+            dataset: either 1) input file name or 2) pandas Dataframe
+            config:  AutolabelConfig object containing project settings
+            start_index: index to begin labeling job at (used for job batching, retries, state management)
+            max_items: number of data points to label, beginning at start_index
+
+        Returns:
+            filehash: a unique ID generated from an MD5 hash of the functions parameters
+        """
+        if isinstance(dataset, str):
+            filehash = calculate_md5(
+                [open(dataset, "rb"), config._dataset_config, start_index, max_items]
+            )
+        else:
+            filehash = calculate_md5(
+                [dataset.to_csv(), config._dataset_config, start_index, max_items]
+            )
+        return filehash
+
+
+class Task(BaseModel):
+    id: str
+    task_type: TaskType
+    model_name: str
+    config: str
+
+    class Config:
+        orm_mode = True
+
+    @classmethod
+    def create_id(self, config: AutolabelConfig) -> str:
+        filehash = calculate_md5(config.config)
+        return filehash
+
+
+class TaskRun(BaseModel):
+    id: Optional[str] = None
+    created_at: datetime
+    task_id: str
+    dataset_id: str
+    current_index: int
+    output_file: str
+    status: TaskStatus
+    error: Optional[str] = None
+    metrics: Optional[Dict[str, Any]] = None
+
+    class Config:
+        orm_mode = True
+
+
+class Annotation(BaseModel):
+    id: Optional[str] = None
+    index: int
+    llm_annotation: Optional[LLMAnnotation] = None
+
+    class Config:
+        orm_mode = True
+
+
+class CacheEntry(BaseModel):
+    model_name: str
+    prompt: str
+    model_params: str
+    generations: Optional[List[Generation]] = None
+
+    class Config:
+        orm_mode = True
---
+++ packages/refuel-autolabel/refuel_autolabel-0.0.8-py3-none-any.whl/autolabel/tasks/utils.py
@@ -0,0 +1,50 @@
+import string
+import re
+
+
+def normalize_text(s: str) -> str:
+    """Removing articles and punctuation, and standardizing whitespace are all typical text processing steps."""
+
+    def remove_articles(text):
+        regex = re.compile(r"\b(a|an|the)\b", re.UNICODE)
+        return re.sub(regex, " ", text)
+
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def compute_f1(prediction: str, truth: str) -> float:
+    """
+    Compute models prediction accuracy based on given ground truth labels
+    Args:
+        prediction: model generated prediction
+        truth: ground truth label to compare against
+    Returns:
+        f1_score: values range from [0,1], with 1 indicating perfect accuracy
+    """
+    pred_tokens = normalize_text(prediction).split()
+    truth_tokens = normalize_text(truth).split()
+
+    # if either the prediction or the truth is no-answer then f1 = 1 if they agree, 0 otherwise
+    if len(pred_tokens) == 0 or len(truth_tokens) == 0:
+        return int(pred_tokens == truth_tokens)
+
+    common_tokens = set(pred_tokens) & set(truth_tokens)
+
+    # if there are no common tokens then f1 = 0
+    if len(common_tokens) == 0:
+        return 0
+
+    prec = len(common_tokens) / len(pred_tokens)
+    rec = len(common_tokens) / len(truth_tokens)
+
+    return 2 * (prec * rec) / (prec + rec)
