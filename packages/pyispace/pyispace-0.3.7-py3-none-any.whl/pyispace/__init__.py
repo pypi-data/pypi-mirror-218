75ff89be4a4cc93548ab102e6903a893c763c19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:09 +0000

    Add package pyispace/0.3.7/pyispace-0.3.7-py3-none-any.whl

---
+++ packages/pyispace/pyispace-0.3.7-py3-none-any.whl/pyispace/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+from .train import train_is
+from .pilot import pilot
+from .trace import trace
+
+
+__version__ = "0.3.7"
+
+
+_logger = logging.getLogger("pyispace")
+
+if not logging.root.handlers:
+    _logger.setLevel(logging.DEBUG)
+    if len(_logger.handlers) == 0:
+        handler = logging.StreamHandler()
+        _logger.addHandler(handler)
