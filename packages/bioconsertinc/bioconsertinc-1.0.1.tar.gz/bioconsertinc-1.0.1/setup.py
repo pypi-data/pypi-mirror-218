eb282c2b052032b89351a0e9f79a4eb3dfa20b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:32 +0000

    Add package bioconsertinc/1.0.1/bioconsertinc-1.0.1.tar.gz

---
+++ packages/bioconsertinc/bioconsertinc-1.0.1.tar.gz/bioconsertinc-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+def get_numpy_include_dirs():
+    import numpy
+    return [numpy.get_include()]
+
+setup(name='bioconsertinc',
+      version='1.0.1',
+      description='BioConsert, c implementation',
+      url='https://github.com/pierreandrieu/bioconsertinc',
+      long_description='BioConsert algorithm, c implementation',
+      author='Pierre Andrieu',
+      author_email='pierre.andrieu@lilo.org',
+      license='MIT',
+      ext_modules=[Extension("bioconsertinc", ["bioconsertinc.c"], include_dirs=get_numpy_include_dirs())],
+      python_requires='>=3',
+      zip_safe=False,
+      install_requires=['numpy>=1.13'],
+)
+
