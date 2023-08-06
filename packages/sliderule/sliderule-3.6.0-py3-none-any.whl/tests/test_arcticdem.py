8e03e24d8c7bba355e638e749f8edebc33659c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:27 +0000

    Add package sliderule/3.6.0/sliderule-3.6.0-py3-none-any.whl

---
+++ packages/sliderule/sliderule-3.6.0-py3-none-any.whl/tests/test_arcticdem.py
@@ -0,0 +1,106 @@
+"""Tests for sliderule arcticdem raster support."""
+
+import pytest
+from pathlib import Path
+import os.path
+import sliderule
+from sliderule import icesat2
+
+TESTDIR = Path(__file__).parent
+
+@pytest.mark.network
+class TestMosaic:
+    def test_vrt(self, domain, organization, desired_nodes):
+        icesat2.init(domain, organization=organization, desired_nodes=desired_nodes, bypass_dns=True)
+        rqst = {"samples": {"asset": "arcticdem-mosaic"}, "coordinates": [[-178.0,51.7]]}
+        rsps = sliderule.source("samples", rqst)
+
+        # old way of reading mosaics generated these results
+        # assert abs(rsps["samples"][0][0]["value"] - 80.713500976562) < 0.001
+        # assert rsps["samples"][0][0]["file"] == '/vsis3/pgc-opendata-dems/arcticdem/mosaics/v3.0/2m/70_09/70_09_2_1_2m_v3.0_reg_dem.tif'
+
+        # Note: sliderule code uses custom target WKT2 projections for PGC data sets.
+        # I don't know why sometimes returned value is 61.2 and sometimes 80.71
+        # This needs to be investigated with the science team. Code is correct because many other points always return the same value.
+        # This has never happened for strips but only for large VRT file.
+        # assert abs(rsps["samples"][0][0]["value"] - 61.2274551391602) < 0.001
+        assert abs(rsps["samples"][0][0]["value"] - 80.713500976562) < 0.001
+        assert rsps["samples"][0][0]["file"] ==  '/vsis3/pgc-opendata-dems/arcticdem/mosaics/v3.0/2m/2m_dem_tiles.vrt'
+
+
+    def test_nearestneighbour(self, domain, asset, organization, desired_nodes):
+        icesat2.init(domain, organization=organization, desired_nodes=desired_nodes, bypass_dns=True)
+        resource = "ATL03_20190314093716_11600203_005_01.h5"
+        region = sliderule.toregion(os.path.join(TESTDIR, "data/dicksonfjord.geojson"))
+        parms = { "poly": region['poly'],
+                  "cnf": "atl03_high",
+                  "ats": 20.0,
+                  "cnt": 10,
+                  "len": 40.0,
+                  "res": 20.0,
+                  "maxi": 1,
+                  "samples": {"mosaic": {"asset": "arcticdem-mosaic"}} }
+        gdf = icesat2.atl06p(parms, asset=asset, resources=[resource])
+        assert len(gdf) == 954
+        assert len(gdf.keys()) == 19
+        assert gdf["rgt"][0] == 1160
+        assert gdf["cycle"][0] == 2
+        assert gdf['segment_id'].describe()["min"] == 405231
+        assert gdf['segment_id'].describe()["max"] == 405900
+        assert abs(gdf["mosaic.value"].describe()["min"] - 605.48828125) < 0.0001
+
+    def test_zonal_stats(self, domain, asset, organization, desired_nodes):
+        icesat2.init(domain, organization=organization, desired_nodes=desired_nodes, bypass_dns=True)
+        resource = "ATL03_20190314093716_11600203_005_01.h5"
+        region = sliderule.toregion(os.path.join(TESTDIR, "data/dicksonfjord.geojson"))
+        parms = { "poly": region['poly'],
+                  "cnf": "atl03_high",
+                  "ats": 20.0,
+                  "cnt": 10,
+                  "len": 40.0,
+                  "res": 20.0,
+                  "maxi": 1,
+                  "samples": {"mosaic": {"asset": "arcticdem-mosaic", "radius": 10.0, "zonal_stats": True}} }
+        gdf = icesat2.atl06p(parms, asset=asset, resources=[resource])
+        assert len(gdf) == 954
+        assert len(gdf.keys()) == 26
+        assert gdf["rgt"][0] == 1160
+        assert gdf["cycle"][0] == 2
+        assert gdf['segment_id'].describe()["min"] == 405231
+        assert gdf['segment_id'].describe()["max"] == 405900
+        assert abs(gdf["mosaic.value"].describe()["min"] - 605.48828125) < 0.0001
+        assert gdf["mosaic.count"].describe()["max"] == 81
+        assert gdf["mosaic.stdev"].describe()["count"] == 954
+
+        # old 'way' time was for the tile/strip rasters
+        # assert gdf["mosaic.time"][0] == 1176076818.0
+
+        # new 'way' time is for the vrt which was created by PGC for sliderule recently
+        assert gdf["mosaic.time"][0] == 1358108640000.0
+
+@pytest.mark.network
+class TestStrips:
+    def test_vct(self, domain, organization, desired_nodes):
+        icesat2.init(domain, organization=organization, desired_nodes=desired_nodes, bypass_dns=True)
+        region_of_interest = [  {'lon': -46.76533411521963, 'lat': 65.4938164756588},
+                                {'lon': -46.34013213284274, 'lat': 65.49860245693627},
+                                {'lon': -46.35015561146599, 'lat': 65.67523503534576},
+                                {'lon': -46.77853429879463, 'lat': 65.67041017142712},
+                                {'lon': -46.76533411521963, 'lat': 65.4938164756588}  ]
+        parms = { "poly": region_of_interest,
+                  "cnf": "atl03_high",
+                  "ats": 10.0,
+                  "cnt": 5,
+                  "len": 40.0,
+                  "res": 120.0,
+                  "maxi": 5,
+                  "rgt": 658,
+                  "time_start":'2020-01-01',
+                  "time_end":'2021-01-01',
+                  "samples": {"strips": {"asset": "arcticdem-strips", "with_flags": True}} }
+        gdf = icesat2.atl06p(parms, asset="icesat2", resources=['ATL03_20191108234307_06580503_005_01.h5'])
+        assert len(gdf.attrs['file_directory']) == 16
+        for file_id in range(16):
+            assert file_id in gdf.attrs['file_directory'].keys()
+            assert '/pgc-opendata-dems/arcticdem/strips/' in gdf.attrs['file_directory'][file_id]
+
