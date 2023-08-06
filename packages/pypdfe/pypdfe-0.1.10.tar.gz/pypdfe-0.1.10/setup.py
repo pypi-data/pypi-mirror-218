aa5faca1b66b0dff0ad519dd75e5297cb19699d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:01 +0000

    Add package pypdfe/0.1.10/pypdfe-0.1.10.tar.gz

---
+++ packages/pypdfe/pypdfe-0.1.10.tar.gz/pypdfe-0.1.10/setup.py
@@ -0,0 +1,37 @@
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+from glob import glob
+import os
+
+
+__version__ = "0.1.10"
+
+sources = sorted(
+    glob(f'src{os.sep}**{os.sep}*.cpp', recursive=True))
+depends = sorted(glob(f'src{os.sep}**{os.sep}*.h', recursive=True))
+sources = [source for source in sources if 'mvPDFMain.cpp' not in source]
+
+ext_modules = [
+    Pybind11Extension("pypdfe._pypdfe",
+                      sources=sources,
+                      depends=depends,
+                      )
+    ]
+
+setup(
+    name="pypdfe",
+    version=__version__,
+    author="Nate Mauney",
+    author_email="nmauney4@uncc.edu",
+    url="https://github.com/CHANGE_ME",
+    description="A Python package for PDF estimation using Dr. Jennifer Farmer's PDFe and multivariate PDF libraries.",
+    long_description="",
+    ext_modules=ext_modules,
+    packages=['pypdfe'],
+    package_data={'pypdfe': ['_pypdfe.*']},
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=['numpy'],
+)
