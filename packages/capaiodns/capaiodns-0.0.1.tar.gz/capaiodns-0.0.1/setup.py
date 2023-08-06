c94b1413f6c685558f3561a7bd747d07c8f578cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:58 +0000

    Add package capaiodns/0.0.1/capaiodns-0.0.1.tar.gz

---
+++ packages/capaiodns/capaiodns-0.0.1.tar.gz/capaiodns-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="capaiodns",
+    version="0.0.1",
+    description="None",
+    packages=['capaiodns'],
+    long_description='Long long',
+    long_description_content_type="text/markdown",
+    url="https://github.com/DevCapitalizer/capaiodns",
+    author="DDjango",
+    author_email="ddjango.786@gmail.com",
+    license="MIT",
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Operating System :: OS Independent",
+    # ],
+    python_requires=">=3.9",
+)
