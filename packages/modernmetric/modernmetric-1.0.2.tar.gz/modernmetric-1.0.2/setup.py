1a6ca7824dd9dd17d0c3a537b117d8c6dfdc97c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:24 +0000

    Add package modernmetric/1.0.2/modernmetric-1.0.2.tar.gz

---
+++ packages/modernmetric/modernmetric-1.0.2.tar.gz/modernmetric-1.0.2/setup.py
@@ -0,0 +1,49 @@
+import subprocess
+import setuptools
+
+_long_description = "See https://github.com/startupos/multimetric for documentation"
+_long_description_content_type = "text/plain"
+try:
+    _long_description = subprocess.check_output(
+        ["pandoc", "--from", "markdown", "--to", "rst", "README.md"]).decode("utf-8")
+    _long_description_content_type = "text/x-rst"
+except (subprocess.CalledProcessError, FileNotFoundError):
+    pass
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="modernmetric",
+    version="1.0.2",
+    author="Jason Nichols",
+    author_email="jason@startupos.dev",
+    description="Calculate code metrics in various languages",
+    long_description=_long_description,
+    long_description_content_type=_long_description_content_type,
+    url="https://github.com/startupos/multimetric",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+        entry_points={
+        "console_scripts": [
+            "modernmetric = modernmetric.__main__:main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
