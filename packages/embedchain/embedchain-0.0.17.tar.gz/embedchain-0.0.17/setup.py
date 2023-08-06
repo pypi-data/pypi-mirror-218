83309bb8eb057c031f6f3e68f027c50f71baa501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package embedchain/0.0.17/embedchain-0.0.17.tar.gz

---
+++ packages/embedchain/embedchain-0.0.17.tar.gz/embedchain-0.0.17/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="embedchain",
+    version="0.0.17",
+    author="Taranjeet Singh",
+    author_email="reachtotj@gmail.com",
+    description="embedchain is a framework to easily create LLM powered bots over any dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/embedchain/embedchain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["embedchain"],
+    install_requires=[
+        "langchain>=0.0.205",
+        "requests",
+        "openai",
+        "chromadb>=0.3.26",
+        "youtube-transcript-api",
+        "beautifulsoup4",
+        "pypdf",
+        "pytube",
+        "gpt4all",
+        "sentence_transformers",
+        "docx2txt",
+    ],
+)
