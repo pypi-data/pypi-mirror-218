54420e305b9f516013408a3344ba6a2d36e39610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:13 +0000

    Add package crossval-ensemble/0.0.0/crossval_ensemble-0.0.0.tar.gz

---
+++ packages/crossval-ensemble/crossval_ensemble-0.0.0.tar.gz/crossval_ensemble-0.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crossval_ensemble',
+    version='0.0.0',
+    description='A scikit-learn wrapper for CrossValidation Ensembles',
+    long_description='CrossvalEnsemble is an ML library in Python that allows to create\
+    CrossValidation Ensembles leveraging scikit-learn.',
+    long_description_content_type='text/markdown',
+    author='Pierre-Alexis Thoumieu',
+    author_email='pierre-alexis@liberkeys.com',
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.25.0',
+        'pandas>=2.0.3',
+        'tqdm>=4.65.0',
+        'python-dotenv>=1.0.0',
+        'flake8>=6.0.0',
+        'scikit-learn>=1.3.0',
+        'ipywidget>=8.0.6',
+        'catboost>=1.2',
+        'xgboost>=1.7.6',
+        'lightgbm>=3.3.5',
+        'sklearn-pandas>=2.2.0',
+    ]
+)
