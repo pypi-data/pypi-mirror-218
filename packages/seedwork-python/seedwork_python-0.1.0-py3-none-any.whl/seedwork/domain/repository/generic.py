e2348a012a3e8de33dd92e10f34ec8517ee0a5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:32 +0000

    Add package seedwork-python/0.1.0/seedwork_python-0.1.0-py3-none-any.whl

---
+++ packages/seedwork-python/seedwork_python-0.1.0-py3-none-any.whl/seedwork/domain/repository/generic.py
@@ -0,0 +1,47 @@
+from abc import ABCMeta, abstractmethod
+from typing import Generic, Optional, TypeVar
+
+from seedwork.domain.entity import Entity, EntityId
+
+__all__ = ['GenericRepository']
+
+T = TypeVar('T', bound=Entity)
+
+
+class GenericRepository(Generic[T], metaclass=ABCMeta):
+    @abstractmethod
+    def get(self, entity_id: EntityId) -> Optional[T]:
+        """Get an entity based on the given primary key identifier.
+
+        Returns:
+            The entity instance or `None` if not found.
+
+        """
+
+    @abstractmethod
+    def create(self, entity: T) -> T:
+        """Create an entity."""
+
+    @abstractmethod
+    def update(
+        self, entity_id: EntityId, entity: T, commit: bool = True
+    ) -> Optional[T]:
+        """Update and return an entity based on the given primary key identifier.
+
+        Returns:
+            The updated entity instance or `None` if not found.
+
+        """
+
+    @abstractmethod
+    def delete(self, entity_id: EntityId) -> None:
+        """Delete an entity."""
+
+    @abstractmethod
+    def commit(self) -> None:
+        """Commit the current transaction.
+
+        If the implementation isn't transactional, this method does nothing.
+
+
+        """
