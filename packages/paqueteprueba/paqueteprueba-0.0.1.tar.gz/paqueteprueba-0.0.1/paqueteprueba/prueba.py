ba5173b550819073aa3741d44bef89a85672c11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:48 +0000

    Add package paqueteprueba/0.0.1/paqueteprueba-0.0.1.tar.gz

---
+++ packages/paqueteprueba/paqueteprueba-0.0.1.tar.gz/paqueteprueba-0.0.1/paqueteprueba/prueba.py
@@ -0,0 +1,24 @@
+"""
+Esta es la documentacion de prueba
+"""
+
+class Prueba:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def prueba(self, cancion):
+        """
+        Reproduce la cancion que recibio como parametro
+
+        Parameters:
+        cancion (str): este es un string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 se reproduce con exito,
+        en caso de fracaso devuelve 0
+        """
+        print("reproduciendo cancion")
+
+    def para_prueba(self):
+        print("detine la cancion")
