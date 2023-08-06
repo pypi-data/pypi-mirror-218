e38081a900e46a396c695525e6e2c2317a91276b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:57 +0000

    Add package service-markets/0.3.12/service_markets-0.3.12-py3-none-any.whl

---
+++ packages/service-markets/service_markets-0.3.12-py3-none-any.whl/service_markets/api/main.py
@@ -0,0 +1,102 @@
+import asyncio
+import logging
+import os
+from os import listdir
+from typing import Optional
+
+from aars import AARS, Record
+from aleph.sdk.vm.app import AlephApp
+from aleph_message.models import PostMessage
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_walletauth import authorization_routes
+
+from ..core.constants import API_MESSAGE_FILTER, SERVICE_MARKETS_MESSAGE_CHANNEL
+from ..core.session import initialize_aars
+from .routers import (
+    services,
+    users,
+)
+
+logger = (
+    logging.getLogger(__name__)
+    if __name__ != "__main__"
+    else logging.getLogger("uvicorn")
+)
+http_app = FastAPI()
+
+origins = ["*"]
+
+http_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+http_app.include_router(services.router)
+http_app.include_router(users.router)
+http_app.include_router(authorization_routes)
+
+app = AlephApp(http_app=http_app)
+
+
+async def re_index():
+    logger.info(f"API re-indexing channel {AARS.channel}")
+    await asyncio.wait_for(AARS.sync_indices(), timeout=None)
+    logger.info("API re-indexing done")
+
+
+@app.on_event("startup")
+async def startup():
+    app.aars = await initialize_aars()
+    print("Syncing indices...")
+    await re_index()
+
+
+@app.get("/")
+async def index():
+    if os.path.exists("/opt/venv"):
+        opt_venv = list(listdir("/opt/venv"))
+    else:
+        opt_venv = []
+    return {
+        "vm_name": SERVICE_MARKETS_MESSAGE_CHANNEL,
+        "endpoints": [
+            "/docs",
+        ],
+        "files_in_volumes": {
+            "/opt/venv": opt_venv,
+        },
+    }
+
+
+@app.post("/event")
+async def event(event: PostMessage):
+    await fishnet_event(event)
+
+
+@app.get("/address")
+async def address():
+    return AARS.account.get_address()
+
+
+@app.event(filters=API_MESSAGE_FILTER)
+async def fishnet_event(event: PostMessage):
+    record: Optional[Record]
+    print("fishnet_event", event)
+    api_message_filter_without_amend = API_MESSAGE_FILTER.copy()
+    api_message_filter_without_amend.remove("amend")
+    if event.content.type in api_message_filter_without_amend:
+        if Record.is_indexed(event.item_hash):
+            return
+        cls: Record = globals()[event.content.type]
+        record = await cls.from_post(event)
+    else:  # amend
+        if Record.is_indexed(event.content.ref):
+            return
+        record = await Record.fetch(event.content.ref).first()
+    assert record
+    for inx in record.get_indices():
+        inx.add_record(record)
---
+++ packages/service-markets/service_markets-0.3.12-py3-none-any.whl/service_markets/core/heimdall.py
@@ -0,0 +1,95 @@
+# Desc: FastAPI Depends plugin for controlling access to API endpoints
+# It will raise a 403 if the user is not allowed to access the endpoint.
+# For the first request, the Aleph network will be queried to see if the user is allowed to access the endpoint.
+import asyncio
+from typing import Optional, List
+
+from aars import AARS, Index
+from fastapi import HTTPException, FastAPI
+from fastapi_walletauth import WalletAuth
+from fastapi_walletauth.core import SignatureChallengeTokenAuth
+from starlette.requests import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from .model import Permission, Service
+from .session import initialize_aars
+
+
+Index(Service, "url")
+Index(Permission, ["user_address", "service_id"])
+
+
+class ServicePermissionAuth(SignatureChallengeTokenAuth):
+    aars: AARS
+    service_record: Optional[Service] = None
+    cached_permissions = {}
+    ready = False
+
+    def __init__(
+        self,
+        service_url: str,
+    ):
+        super().__init__()
+        self.service_url = service_url
+
+    def __call__(self, request: Request) -> WalletAuth:
+        """
+        Check if the user has the given permission for the given service.
+        """
+        wallet_auth: WalletAuth = super().__call__(request)
+        if self.cached_permissions.get(wallet_auth.address):
+            return wallet_auth
+        loop = self.aars.session.http_session.loop
+        permission_record = loop.run_until_complete(Permission.filter(
+            user_address=wallet_auth.address,
+            service_id=self.service_record.item_hash,
+        ).all())
+        if not permission_record:
+            raise HTTPException(
+                status_code=403,
+                detail="User does not have permission to access this service",
+            )
+        self.cached_permissions[wallet_auth.address] = permission_record
+        return wallet_auth
+
+    async def setup(self, **kwargs):
+        self.aars = await initialize_aars(**kwargs)
+        print(f"Heimdall re-indexing channel {AARS.channel}")
+        await asyncio.wait_for(AARS.sync_indices(), timeout=None)
+        services = await Service.fetch_objects().all()
+        service = next(filter(lambda s: s.url == self.service_url, services), None)
+        if not service:
+            raise ValueError(
+                f"Service with url {self.service_url} is not registered on service.markets"
+            )
+        print(f"Service {self.service_url} successfully loaded. Heimdall is ready.")
+        self.service_record = service
+        self.ready = True
+
+
+class HeimdallMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI, backend: ServicePermissionAuth, open_endpoints: List[str], **kwargs):
+        super().__init__(app)
+        self.backend = backend
+        self.open_endpoints = open_endpoints
+        self.kwargs = kwargs
+
+    async def dispatch(self, request, call_next):
+        if not request.url.path.startswith("/authorization") or not request.url.path.startswith("/docs") or not request.url.path.startswith("/openapi.json") or not request.url.path.startswith("/redoc") or not request.url.path.startswith("/"):
+            print(request.url.path)
+            if not self.backend.ready:
+                await self.backend.setup(**self.kwargs)
+            request.state.wallet_auth = self.backend(request)
+        return await call_next(request)
+
+
+def setup_heimdall(app: FastAPI, service_url: str, **kwargs):
+    """
+    Setup Heimdall middleware for the given app. This will check if the user has permission to access the given service.
+    Permission is checked by querying the Aleph network.
+    """
+    app.add_middleware(
+        HeimdallMiddleware,
+        backend=ServicePermissionAuth(service_url),
+        **kwargs,
+    )
