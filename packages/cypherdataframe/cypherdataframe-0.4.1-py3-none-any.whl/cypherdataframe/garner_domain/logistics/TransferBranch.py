bb1ee1e8129a6e8171454da5dd7ac7fd84beb4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:50 +0000

    Add package cypherdataframe/0.4.1/cypherdataframe-0.4.1-py3-none-any.whl

---
+++ packages/cypherdataframe/cypherdataframe-0.4.1-py3-none-any.whl/cypherdataframe/garner_domain/logistics/TransferBranch.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+from cypherdataframe.garner_domain.BranchMaker import BranchMaker
+from cypherdataframe.garner_domain.properties_defaults import \
+    TRANSFER_RELATIONSHIP, \
+    TRANSFER_RETURN_ID, TRANSFER_LABEL
+from cypherdataframe.model.Property import Property
+
+
+@dataclass
+class TransferBranch(BranchMaker):
+    props: list[Property]
+    label: str = TRANSFER_LABEL
+    relationship: str = field(default_factory=lambda: TRANSFER_RELATIONSHIP)
+    relationship_postfix: str | None = field(default_factory=lambda: TRANSFER_RETURN_ID)
+    required: bool = False
+    archived: bool = True
+    domain_label: str | None = None
