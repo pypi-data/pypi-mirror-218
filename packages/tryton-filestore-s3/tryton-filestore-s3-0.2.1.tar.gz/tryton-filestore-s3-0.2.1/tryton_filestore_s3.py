9c91032ab460eb2caa73740cbf102a01f86f0b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:32 +0000

    Add package tryton-filestore-s3/0.2.1/tryton-filestore-s3-0.2.1.tar.gz

---
+++ packages/tryton-filestore-s3/tryton-filestore-s3-0.2.1.tar.gz/tryton-filestore-s3-0.2.1/tryton_filestore_s3.py
@@ -0,0 +1,59 @@
+# This file is part of filestore-s3. The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+import boto3
+import uuid
+from trytond.config import config
+from trytond.filestore import FileStore
+
+
+class FileStoreS3(FileStore):
+
+    def get(self, id, prefix=''):
+        s3, bucket = get_client()
+        key = name(id, prefix)
+        response = s3.get_object(Bucket=bucket, Key=key)
+        return response['Body'].read()
+
+    def getmany(self, ids, prefix=''):
+        return [self.get(id, prefix) for id in ids]
+
+    def size(self, id, prefix=''):
+        s3, bucket = get_client()
+        key = name(id, prefix)
+        response = s3.head_object(Bucket=bucket, Key=key)
+        return response['ContentLength']
+
+    def sizemany(self, ids, prefix=''):
+        return [self.size(id, prefix) for id in ids]
+
+    def set(self, data, prefix=''):
+        s3, bucket = get_client()
+        id = uuid.uuid4().hex
+        key = name(id, prefix)
+        s3.put_object(Bucket=bucket, Key=key, Body=data)
+        return id
+
+    def setmany(self, data, prefix=''):
+        return [self.set(d, prefix) for d in data]
+
+
+def name(id, prefix=''):
+    return '/'.join(filter(None, [prefix, id]))
+
+
+def get_client():
+    access_key = config.get('database', 's3_access_key')
+    if access_key is None:
+        access_key = config.get('database', 'access_key')
+    secret_key = config.get('database', 's3_secret_key')
+    if secret_key is None:
+        secret_key = config.get('database', 'secret_key')
+    bucket = config.get('database', 'bucket')
+    endpoint_url = config.get('database', 's3_endpoint')
+    client = boto3.client(
+        's3',
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        endpoint_url=endpoint_url,
+    )
+    return client, bucket
