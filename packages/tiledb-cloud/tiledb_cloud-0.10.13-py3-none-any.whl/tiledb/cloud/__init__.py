e94cc47c4422844b310d15b06ed7f59c84e5c9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:54 +0000

    Add package tiledb-cloud/0.10.13/tiledb_cloud-0.10.13-py3-none-any.whl

---
+++ packages/tiledb-cloud/tiledb_cloud-0.10.13-py3-none-any.whl/tiledb/cloud/__init__.py
@@ -0,0 +1,81 @@
+# This file imports specifically to re-export.
+
+from tiledb.cloud import compute
+from tiledb.cloud import dag
+from tiledb.cloud import sql
+from tiledb.cloud import udf
+from tiledb.cloud._common import pickle_compat as _pickle_compat
+from tiledb.cloud.array import array_activity
+from tiledb.cloud.array import deregister_array
+from tiledb.cloud.array import info
+from tiledb.cloud.array import list_shared_with
+from tiledb.cloud.array import register_array
+from tiledb.cloud.array import share_array
+from tiledb.cloud.array import unshare_array
+from tiledb.cloud.client import Config
+from tiledb.cloud.client import Ctx
+from tiledb.cloud.client import list_arrays
+from tiledb.cloud.client import list_public_arrays
+from tiledb.cloud.client import list_shared_arrays
+from tiledb.cloud.client import login
+from tiledb.cloud.client import organization
+from tiledb.cloud.client import organizations
+from tiledb.cloud.client import user_profile
+from tiledb.cloud.file import create_file
+from tiledb.cloud.file import export_file
+from tiledb.cloud.notebook import download_notebook_contents
+from tiledb.cloud.notebook import download_notebook_to_file
+from tiledb.cloud.notebook import rename_notebook
+from tiledb.cloud.notebook import upload_notebook_contents
+from tiledb.cloud.notebook import upload_notebook_from_file
+from tiledb.cloud.rest_api import models
+from tiledb.cloud.tasks import last_sql_task
+from tiledb.cloud.tasks import last_udf_task
+from tiledb.cloud.tasks import task
+from tiledb.cloud.tiledb_cloud_error import TileDBCloudError
+
+_pickle_compat.patch_cloudpickle()
+_pickle_compat.patch_pandas()
+
+try:
+    from tiledb.cloud.version import version as __version__
+except ImportError:
+    __version__ = "0.0.0.local"
+
+ResultFormat = models.ResultFormat
+UDFResultType = ResultFormat
+
+__all__ = (
+    "compute",
+    "dag",
+    "sql",
+    "udf",
+    "array_activity",
+    "deregister_array",
+    "info",
+    "list_shared_with",
+    "register_array",
+    "share_array",
+    "unshare_array",
+    "Config",
+    "Ctx",
+    "list_arrays",
+    "list_public_arrays",
+    "list_shared_arrays",
+    "login",
+    "organization",
+    "organizations",
+    "user_profile",
+    "create_file",
+    "export_file",
+    "rename_notebook",
+    "upload_notebook_from_file",
+    "upload_notebook_contents",
+    "download_notebook_to_file",
+    "download_notebook_contents",
+    "last_sql_task",
+    "last_udf_task",
+    "task",
+    "TileDBCloudError",
+    "__version__",
+)
