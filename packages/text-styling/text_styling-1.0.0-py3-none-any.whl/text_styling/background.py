1b37c1b79ba2bf3400e35aa285377ad1b4b6e8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package text-styling/1.0.0/text_styling-1.0.0-py3-none-any.whl

---
+++ packages/text-styling/text_styling-1.0.0-py3-none-any.whl/text_styling/background.py
@@ -0,0 +1,12 @@
+"""Módulo que contém variáveis de cores para fundo do texto"""
+
+# Define as cores de fundo do texto como variáveis
+bg_reset = "\033[0m"  # Retorna ao padrão
+bg_black = "\033[40m"  # Preto
+bg_red = "\033[41m"  # Vermelho
+bg_green = "\033[42m"  # Verde
+bg_yellow = "\033[43m"  # Amarelo
+bg_blue = "\033[44m"  # Azul
+bg_magenta = "\033[45m"  # Magenta
+bg_cyan = "\033[46m"  # Ciano
+bg_white = "\033[47m"  # Branco
---
+++ packages/text-styling/text_styling-1.0.0-py3-none-any.whl/text_styling/colors.py
@@ -0,0 +1,13 @@
+"""Módulo que contém variáveis de cores para texto"""
+
+# Define as cores de texto como variáveis
+reset = "\033[0m"  # Retorna ao padrão
+black = "\033[30m"  # Preto
+red = "\033[31m"  # Vermelho
+green = "\033[32m"  # Verde
+yellow = "\033[33m"  # Amarelo
+blue = "\033[34m"  # Azul
+magenta = "\033[35m"  # Magenta
+cyan = "\033[36m"  # Ciano
+white = "\033[37m"  # Branco
+inversion = "\033[7m" # Inverte cor do fundo com cor do texto
