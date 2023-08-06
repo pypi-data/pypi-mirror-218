1d94dde0d282517870925271dcb730c5eeba5f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:42 +0000

    Add package ixbrlparse/0.5.4/ixbrlparse-0.5.4-py3-none-any.whl

---
+++ packages/ixbrlparse/ixbrlparse-0.5.4-py3-none-any.whl/ixbrlparse/components/context.py
@@ -0,0 +1,50 @@
+import datetime
+from copy import deepcopy
+from typing import Any, Dict, List, Optional
+
+
+class ixbrlContext:
+    def __init__(
+        self,
+        _id: str,
+        entity: Dict[str, Optional[str]],
+        segments: Optional[List[Dict]],
+        instant: Optional[str],
+        startdate: Optional[str],
+        enddate: Optional[str],
+    ):
+        self.id = _id
+        self.entity = entity
+        self.segments = segments
+        self.instant: Optional[datetime.date] = (
+            datetime.datetime.strptime(instant.strip(), "%Y-%m-%d").date()
+            if instant
+            else None
+        )
+        self.startdate: Optional[datetime.date] = (
+            datetime.datetime.strptime(startdate.strip(), "%Y-%m-%d").date()
+            if startdate
+            else None
+        )
+        self.enddate: Optional[datetime.date] = (
+            datetime.datetime.strptime(enddate.strip(), "%Y-%m-%d").date()
+            if enddate
+            else None
+        )
+
+    def __repr__(self) -> str:
+        if self.startdate and self.enddate:
+            datestr = "{} to {}".format(self.startdate, self.enddate)
+        else:
+            datestr = str(self.instant)
+
+        segmentstr = " (with segments)" if self.segments else ""
+
+        return "<IXBRLContext {} [{}]{}>".format(self.id, datestr, segmentstr)
+
+    def to_json(self) -> Dict[str, List[Dict[str, Any]]]:
+        values = deepcopy(self.__dict__)
+        for i in ["startdate", "enddate", "instant"]:
+            if isinstance(values[i], datetime.date):
+                values[i] = str(values[i])
+        return values
---
+++ packages/ixbrlparse/ixbrlparse-0.5.4-py3-none-any.whl/ixbrlparse/components/numeric.py
@@ -0,0 +1,72 @@
+from copy import deepcopy
+from typing import Dict, Optional, Union
+
+from bs4 import Tag
+
+from .context import ixbrlContext
+from .transform import get_format, ixbrlFormat
+
+
+class ixbrlNumeric:
+    # contextref
+    # decimals
+    # format
+    # name
+    # scale
+    # sign
+    # text
+    # unitref
+    # xmlns:ix
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        unit: Optional[str] = None,
+        value: Optional[Union[str, int, float]] = None,
+        text: Optional[Union[str, int, float]] = None,
+        context: Union[ixbrlContext, str, None] = None,
+        soup_tag: Optional[Tag] = None,
+        **attrs,
+    ) -> None:
+        self.name: Optional[str] = name
+        self.schema: str = "unknown"
+        if isinstance(name, str):
+            name_value = name.split(":", maxsplit=1)
+            if len(name_value) == 2:
+                self.schema = name_value[0]
+                self.name = name_value[1]
+            else:
+                self.schema = "unknown"
+                self.name = name_value[0]
+
+        if not isinstance(value, (str, int, float)):
+            value = text
+        if not isinstance(value, (str, int, float)):
+            raise ValueError("Must provide either value or text")
+        self.text: Union[str, int, float] = value
+        self.context: Union[ixbrlContext, str, None] = context
+        self.unit: Optional[str] = unit
+        self.value: Optional[Union[int, float]] = None
+        self.soup_tag = soup_tag
+
+        format_ = {
+            "format_": attrs.get("format"),
+            "decimals": attrs.get("decimals", "0"),
+            "scale": attrs.get("scale", 0),
+            "sign": attrs.get("sign", ""),
+        }
+        self.format: Optional[ixbrlFormat] = get_format(format_["format_"])(**format_)
+
+        try:
+            if isinstance(self.format, ixbrlFormat):
+                self.value = self.format.parse_value(self.text)
+        except ValueError:
+            print(attrs)
+            raise
+
+    def to_json(self) -> Dict:
+        values = {k: deepcopy(v) for k, v in self.__dict__.items() if k != "soup_tag"}
+        if isinstance(self.format, ixbrlFormat):
+            values["format"] = self.format.to_json()
+        if isinstance(self.context, ixbrlContext):
+            values["context"] = self.context.to_json()
+        return values
