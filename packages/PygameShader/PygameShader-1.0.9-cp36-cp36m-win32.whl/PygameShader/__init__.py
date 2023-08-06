6d57e2f5fd8e0dabe003f0e293ac354a7ba030ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:16 +0000

    Add package PygameShader/1.0.9/PygameShader-1.0.9-cp36-cp36m-win32.whl

---
+++ packages/PygameShader/PygameShader-1.0.9-cp36-cp36m-win32.whl/PygameShader/__init__.py
@@ -0,0 +1,7 @@
+"""
+PygameShader library loading
+"""
+from PygameShader.shader import *
+# from PygameShader.shader_gpu import *
+from PygameShader.misc import *
+from PygameShader.Palette import *
