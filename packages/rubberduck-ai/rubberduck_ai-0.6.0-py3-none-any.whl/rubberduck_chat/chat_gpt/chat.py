1767c686179cc18cefa700d5bce9420dce45f8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:54 +0000

    Add package rubberduck-ai/0.6.0/rubberduck_ai-0.6.0-py3-none-any.whl

---
+++ packages/rubberduck-ai/rubberduck_ai-0.6.0-py3-none-any.whl/rubberduck_chat/chat_gpt/chat.py
@@ -0,0 +1,238 @@
+import re
+
+import inquirer
+import openai
+import pyperclip
+from halo import Halo
+from rich.console import Console
+from rich.syntax import Syntax
+
+from rubberduck_chat.chat_gpt.session_store import *
+from rubberduck_chat.utils import get_datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class GptChatSessionConfigs:
+  chat_gpt_model: str
+  max_messages_per_request: int
+  snippet_header_background_color: str
+  snippet_theme: str
+
+
+class GptChatSession:
+  snippet_end_pattern = r'\s*```'
+  snippet_start_pattern = r'\s*```(\S+)?'
+  quote_pattern = re.compile(r'`([^`]*)`')
+
+  def __init__(self, session_id, configs: GptChatSessionConfigs, session_metadata: GptSessionMetadata,
+               system_message: GptSystemMessage,
+               turns: List[GptChatTurn]):
+
+    self.session_id: str = session_id
+    self.configs: GptChatSessionConfigs = configs
+    self.session_metadata: GptSessionMetadata = session_metadata
+    self.system_message: GptSystemMessage = system_message
+    self.turns: List[GptChatTurn] = turns
+    self.console: Console = Console()
+    self.snippets: List[str] = []
+
+  @classmethod
+  def create_new(cls, configs: GptChatSessionConfigs):
+    message = GptSystemMessage.from_system_message('You are a helpful assistant')
+    session_id = str(uuid4())
+    return cls(session_id, configs, GptSessionMetadata(session_id, int(time.time())), message, [])
+
+  @classmethod
+  def from_session_id(cls, session_id: str, configs: GptChatSessionConfigs):
+    lines: List[str] = fetch_session_data(session_id)
+
+    gpt_session_metadata: GptSessionMetadata = GptSessionMetadata.from_line(lines[0])
+    gpt_system_message: GptSystemMessage = GptSystemMessage.from_json_string(lines[1])
+    gpt_chat_turns: List[GptChatTurn] = []
+    turn_ids: set[str] = set()
+
+    for line in lines[-1:1:-1]:
+      turn = GptChatTurn.from_json_string(line)
+
+      if turn.id in turn_ids:
+        continue
+
+      turn_ids.add(turn.id)
+      gpt_chat_turns.append(turn)
+
+    gpt_chat_turns.reverse()
+
+    return cls(session_id, configs, gpt_session_metadata, gpt_system_message, gpt_chat_turns)
+
+  def print_current_session(self, print_time=False):
+    for turn in self.turns:
+      if print_time:
+        create_time = f'[{get_datetime(turn.created_time)}] '
+      else:
+        create_time = ''
+
+      print(f'>>>{create_time}{turn.user_prompt}')
+      assistant_response = turn.get_assistant_response()
+      if assistant_response:
+        self.print_assistant_response(assistant_response)
+
+  def process_prompt(self, prompt: str, configs: GptChatSessionConfigs):
+    current_turn = GptChatTurn.from_user_prompt(prompt)
+    self.store_chat_turn(current_turn)
+    self.turns.append(current_turn)
+    messages: List[dict] = [self.system_message.get_chat_gpt_request_message()]
+
+    for turn in self.turns[-(self.configs.max_messages_per_request + 1):]:
+      messages.append(turn.get_user_prompt_message())
+      assistant_response_message = turn.get_assistant_response_message()
+      if assistant_response_message:
+        messages.append(assistant_response_message)
+
+    response = None
+    error_message = None
+
+    with Halo(text='Fetching', spinner='dots'):
+      try:
+        response = openai.ChatCompletion.create(model=configs.chat_gpt_model, messages=messages)
+      except Exception as error:
+        error_message = str(error)
+
+    if error_message:
+      print(error_message)
+      return
+
+    if response:
+      current_turn.updated_response(response)
+      self.store_chat_turn(current_turn)
+      self.print_assistant_response(current_turn.get_assistant_response())
+    else:
+      print('No results found')
+
+  def print_assistant_response(self, message: str):
+    new_snippets: List[str] = []
+    message_parts = message.split('\n')
+
+    snippet = None
+    snippet_language = None
+    snippet_count = 0
+
+    for part in message_parts:
+      if re.search(self.snippet_end_pattern, part) and snippet is not None:
+        self.print_header(snippet_language, snippet_count)
+        self.print_code(snippet_language, snippet)
+        new_snippets.append(snippet)
+        snippet = None
+        snippet_language = None
+      elif re.search(self.snippet_start_pattern, part):
+        matches = re.findall(self.snippet_start_pattern, part)
+        if matches:
+          snippet_language = matches[0]
+        else:
+          snippet_language = None
+        snippet = ''
+        snippet_count += 1
+      elif snippet is not None:
+        snippet += part + '\n'
+      elif not part:
+        print()
+      else:
+        self.print_text(part)
+
+    if new_snippets:
+      self.snippets = new_snippets
+
+    print('')
+
+  def print_header(self, language: str, count: int):
+    copy_message = f'Enter "{count}" to copy snippet'
+
+    if language:
+      header = f' {language.upper()} | {copy_message}'
+    else:
+      header = f' {copy_message}'
+
+    syntax = Syntax(header, 'text', theme=self.configs.snippet_theme,
+                    background_color=self.configs.snippet_header_background_color)
+    self.console.print(syntax, overflow='fold')
+
+  def print_code(self, language: str, code: str):
+    if not language:
+      language = 'text'
+
+    syntax = Syntax(code, language, theme=self.configs.snippet_theme)
+    self.console.print(syntax, overflow='fold')
+
+  def print_text(self, text: str):
+    highlighted_text = self.quote_pattern.sub(r'`\033[1m\1\033[0m`', text)
+    print(highlighted_text)
+
+  def has_snippet(self, snippet_index: int) -> bool:
+    return snippet_index <= len(self.snippets)
+
+  def copy_snippet(self, snippet_index: int):
+    if snippet_index <= len(self.snippets):
+      pyperclip.copy(self.snippets[snippet_index - 1])
+      print('Snippet copied to clipboard')
+    else:
+      print('No snippet to copy')
+
+  def store_chat_turn(self, gpt_chat_turn: GptChatTurn):
+    if self.turns:
+      store_chat_turn_to_file(self.session_id, gpt_chat_turn)
+    else:
+      store_metadata_to_file(self.session_id, self.session_metadata)
+      store_system_message_to_file(self.session_id, self.system_message)
+      store_chat_turn_to_file(self.session_id, gpt_chat_turn)
+
+
+class GptChat:
+
+  def __init__(self, session: GptChatSession, configs: GptChatSessionConfigs):
+    self.session = session
+    self.configs = configs
+
+  def process_prompt(self, prompt: str):
+    self.session.process_prompt(prompt, self.configs)
+
+  def create_new_session(self):
+    self.session = GptChatSession.create_new(self.configs)
+    set_active_session_id(self.session.session_id)
+    print('Started new session')
+
+  def update_configs(self, configs: GptChatSessionConfigs):
+    self.configs = configs
+    self.session.configs = configs
+
+  def has_snippet(self, snippet_index: int) -> bool:
+    return self.session.has_snippet(snippet_index)
+
+  def copy_snippet(self, snippet_index: int):
+    self.session.copy_snippet(snippet_index)
+
+  def print_current_session(self):
+    self.session.print_current_session(print_time=True)
+
+  def change_session(self):
+    session_previews = get_all_session_previews()
+
+    if not session_previews:
+      print('No previous session found')
+      return
+
+    message = f'Select from {len(session_previews)} sessions'
+
+    previews = []
+
+    for session_preview in session_previews:
+      previews.append((session_preview.session_preview, session_preview))
+
+    options = [inquirer.List('option', message=message, choices=previews)]
+    answers = inquirer.prompt(options)
+
+    if answers:
+      preview: GptSessionPreview = answers['option']
+      self.session = GptChatSession.from_session_id(preview.session_id, self.configs)
+      self.session.print_current_session(print_time=True)
+      set_active_session_id(preview.session_id)
+      print(f'Loaded session: {preview.session_preview}')
---
+++ packages/rubberduck-ai/rubberduck_ai-0.6.0-py3-none-any.whl/rubberduck_chat/configs.py
@@ -0,0 +1,241 @@
+import configparser
+import os
+import platform
+import re
+from dataclasses import dataclass
+from typing import Optional, List
+
+import inquirer
+
+from rubberduck_chat.store import rubberduck_dir_name
+
+configs_filename = 'configs.ini'
+default_config_section_name = 'default'
+config_array_delimiter = ','
+
+configs = configparser.ConfigParser()
+
+
+def is_valid_int(value: str) -> bool:
+  if value.isdigit() and int(value) >= 0:
+    return True
+  else:
+    return False
+
+
+def is_valid_bool(value: str) -> bool:
+  return value.lower() in ['true', 'false']
+
+
+def is_valid_color_code(value: str) -> bool:
+  regex = r'^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
+  return re.match(regex, value) is not None
+
+
+@dataclass
+class ConfigEntry:
+  name: str
+  default_value: str
+  description: str
+  value_varifier: Optional[callable]
+
+  def get_value(self) -> str:
+    return configs[default_config_section_name][self.name]
+
+  def set_value(self, value):
+    configs[default_config_section_name][self.name] = value
+    with open(get_configs_path(), 'w') as configfile:
+      configs.write(configfile)
+
+  def get_int_value(self) -> int:
+    try:
+      value = int(self.get_value())
+      if value >= 0:
+        return value
+      else:
+        return int(self.default_value)
+    except ValueError:
+      return int(self.default_value)
+
+  def get_bool_value(self) -> bool:
+    return self.get_value().lower() == 'true'
+
+
+@dataclass
+class ConfigSet:
+  max_saved_session_count = ConfigEntry(
+    'max_saved_session_count',
+    str(100),
+    'Maximum number of sessions saved locally',
+    is_valid_int
+  )
+  always_continue_last_session = ConfigEntry(
+    'always_continue_last_session',
+    'false',
+    'Always continue from your previous session',
+    is_valid_bool
+  )
+  inactive_session_cutoff_time_in_seconds = ConfigEntry(
+    'inactive_session_cutoff_time_in_seconds',
+    str(172800),
+    'Creat new session if previous session is inactive for this many seconds',
+    is_valid_int
+  )
+  chat_gpt_model = ConfigEntry(
+    'chat_gpt_model',
+    'gpt-3.5-turbo',
+    'ChatGPT model; [gpt-3.5-turbo/gpt-4]',
+    None
+  )
+  max_messages_per_request = ConfigEntry(
+    'max_messages_per_request',
+    str(10),
+    'Maximum number of previous chat user prompts used to generating new responses',
+    is_valid_int
+  )
+  snippet_header_background_color = ConfigEntry(
+    'snippet_header_background_color',
+    '#707070',
+    'Snippet header background color',
+    is_valid_color_code
+  )
+  snippet_theme = ConfigEntry(
+    'snippet_theme',
+    'monokai',
+    'Snippet theme',
+    None
+  )
+  exit_command_trigger = ConfigEntry(
+    'exit_command_trigger',
+    config_array_delimiter.join(['.exit', '.e']),
+    'Commands to exit session',
+    None
+  )
+  help_command_trigger = ConfigEntry(
+    'help_command_trigger',
+    config_array_delimiter.join(['.help', '.h']),
+    'Commands to print help message',
+    None
+  )
+  change_session_command_trigger = ConfigEntry(
+    'change_session_command_trigger',
+    config_array_delimiter.join(['.sessions', '.s']),
+    'Commands to change current session',
+    None
+  )
+  print_session_command_trigger = ConfigEntry(
+    'print_session_command_trigger',
+    config_array_delimiter.join(['.print', '.p']),
+    'Commands to print current session',
+    None
+  )
+  new_session_command_trigger = ConfigEntry(
+    'new_session_command_trigger',
+    config_array_delimiter.join(['.new', '.n']),
+    'Commands to start new session',
+    None
+  )
+  update_key_command_trigger = ConfigEntry(
+    'update_key_command_trigger',
+    config_array_delimiter.join(['.key', '.k']),
+    'Commands to update openai API key',
+    None
+  )
+  update_config_command_trigger = ConfigEntry(
+    'update_config_command_trigger',
+    config_array_delimiter.join(['.config', '.c']),
+    'Commands to change configs',
+    None
+  )
+  supported_command_cli = ConfigEntry(
+    'supported_command_cli',
+    config_array_delimiter.join(['cls', 'dir', 'cd'] if platform.system() == 'Windows' else ['clear', 'ls', 'cd']),
+    'CLI commands supported by the chat',
+    None
+  )
+
+
+config_collection = ConfigSet()
+config_collection_list: List[ConfigEntry] = [
+  config_collection.max_saved_session_count,
+  config_collection.always_continue_last_session,
+  config_collection.inactive_session_cutoff_time_in_seconds,
+  config_collection.chat_gpt_model,
+  config_collection.max_messages_per_request,
+  config_collection.snippet_header_background_color,
+  config_collection.snippet_theme,
+  config_collection.exit_command_trigger,
+  config_collection.help_command_trigger,
+  config_collection.change_session_command_trigger,
+  config_collection.print_session_command_trigger,
+  config_collection.new_session_command_trigger,
+  config_collection.update_key_command_trigger,
+  config_collection.update_config_command_trigger,
+  config_collection.supported_command_cli,
+]
+
+
+def get_configs_path() -> str:
+  return os.path.join(os.path.expanduser('~'), rubberduck_dir_name, configs_filename)
+
+
+def setup_default_config(override_existing_configs: bool = False):
+  config_filepath = get_configs_path()
+  configs.read(get_configs_path())
+
+  if default_config_section_name not in configs:
+    configs.add_section(default_config_section_name)
+
+  for config in config_collection_list:
+    if override_existing_configs or config.name not in configs[default_config_section_name]:
+      configs[default_config_section_name][config.name] = config.default_value
+
+  with open(config_filepath, 'w') as configfile:
+    configs.write(configfile)
+
+
+def update_config():
+  options: List[tuple[str, ConfigEntry]] = [
+    ('Reset all configs', ConfigEntry('reset_all', '', '', None))
+  ]
+
+  for config in config_collection_list:
+    description = f'{config.description}: {config.get_value()}'
+    options.append((description, config))
+
+  print('Select a config to update, press Ctrl+C to exit.')
+  answers = inquirer.prompt([inquirer.List('config', message='Selected', choices=options)])
+
+  if answers:
+    config = answers["config"]
+
+    if config.name == 'reset_all':
+      setup_default_config(override_existing_configs=True)
+      print('Config reset to default.')
+    else:
+      while True:
+        questions = [
+          inquirer.Text("new_value", message=f"New value", default=config.get_value())
+        ]
+
+        result = inquirer.prompt(questions)
+
+        if not result:
+          break
+
+        new_value = result['new_value']
+
+        if new_value:
+          if not config.value_varifier:
+            config.set_value(new_value)
+            print(f'Config updated with value: {new_value}')
+            break
+          elif config.value_varifier:
+            if config.value_varifier(new_value):
+              config.set_value(new_value)
+              print(f'Config updated with value: {new_value}')
+              break
+            else:
+              print('Invalid value, please try again.')
+        else:
+          break
