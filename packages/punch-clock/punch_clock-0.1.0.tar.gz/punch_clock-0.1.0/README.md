644ca80a630896662425600dc666c2c0f429880b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:35 +0000

    Add package punch-clock/0.1.0/punch_clock-0.1.0.tar.gz

---
+++ packages/punch-clock/punch_clock-0.1.0.tar.gz/punch_clock-0.1.0/README.md
@@ -0,0 +1,6 @@
+# punchClock
+![Status](https://img.shields.io/static/v1?style=flat&logo=github&label=status&message=finished&color=red) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](http://makeapullrequest.com)  [![GitHub forks](https://img.shields.io/github/forks/debOliveira/punchClock.svg?style=social&label=Fork&maxAge=2592000)](https://GitHub.com/debOliveira/punchClock/network/) [![GitHub stars](https://img.shields.io/github/stars/debOliveira/punchClock.svg?style=social&label=Star&maxAge=2592000)](https://GitHub.com/debOliveira/punchClock/stargazers/) [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FdebOliveira%2FpunchClock&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+This is a simple punch clock report generating py.
+
+I use to keep track of on going activities in R&D projects I mentor.
---
+++ packages/punch-clock/punch_clock-0.1.0.tar.gz/punch_clock-0.1.0/punch_clock/punch_clock.py
@@ -0,0 +1,52 @@
+import git
+import os
+from . import utils
+import click
+
+def get_repo() -> git.Repo:
+    return git.Repo(".")
+
+def get_name(git_config: git.GitConfigParser) -> str:
+    name = git_config.get_value("user", "name")
+    name = name.replace(" ", "-")
+    name = "".join([c if c.isalnum() else "-" for c in name])
+    name = "_".join(name.split("-"))
+    name = name.lower()
+    return name
+
+def get_email(git_config: git.GitConfigParser) -> str:
+    return git_config.get_value("user", "email")
+
+def push_repo(repo: git.Repo, m: str, dir: str, name: str) -> None:
+    filename = f"{dir}/{name}.md"
+    repo.index.add(filename)
+    repo.index.commit(m)
+    repo.remotes.origin.push()
+    pass
+
+def entry_in(name: str, dir: str) -> None:
+    try:
+        with open(f"{dir}/{name}.md", "r+") as f:
+            f.seek(0, os.SEEK_END)
+            f.seek(f.tell() - 1, os.SEEK_SET)
+            if f.read(1) == "\n":
+                f.write(f"| {utils.get_time()} |")
+            else:
+                raise Exception("!!! already punched in !!!")
+    except Exception as e:
+        raise e
+
+
+def entry_out(name: str, dir: str) -> None:
+    try:
+        with open(f"{dir}/{name}.md", "r+") as f:
+            f.seek(0, os.SEEK_END)
+            f.seek(f.tell() - 1, os.SEEK_SET)
+            if f.read(1) != "\n":
+                content = click.prompt(">> what did you do?", type=str)
+                f.write(f" {utils.get_time()} | {content} |\n")
+            else:
+                raise Exception("!!! already punched out !!!")
+    except Exception as e:
+        raise e
+        
