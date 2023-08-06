bf6d7220f675739d4276abadf0cd10b3e456edc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package prolog-primitives/1.0.2/prolog_primitives-1.0.2-py3-none-any.whl

---
+++ packages/prolog-primitives/prolog_primitives-1.0.2-py3-none-any.whl/prolog_primitives/ml_lib/transformations/schema_trasformation.py
@@ -0,0 +1,32 @@
+from prolog_primitives.basic import DistributedElements
+from typing import Generator
+from prolog_primitives.basic import Utils
+from ..collections import SharedCollections
+from .transformationClass import Pipeline
+
+class __SchemaTrasformation(DistributedElements.DistributedPrimitive):
+    
+    def solve(self, request: DistributedElements.DistributedRequest) -> Generator[DistributedElements.DistributedResponse, None, None]:
+        schema_ref = request.arguments[0]
+        transf_ref = request.arguments[1]
+        
+        if(not schema_ref.HasField('var') and transf_ref.HasField('var')):
+            schema_id = Utils.parseArgumentMsg(schema_ref)
+            
+            id = SharedCollections().addPipeline(Pipeline(schema_id))
+            yield request.replySuccess(substitutions={
+                transf_ref.var: Utils.buildConstantArgumentMsg(id)
+                }, hasNext=False)
+        elif(schema_ref.HasField('var') and not transf_ref.HasField('var')):
+            transformation_id = Utils.parseArgumentMsg(transf_ref)
+            transformation: Pipeline = SharedCollections().getPipeline(transformation_id)
+            
+            id = SharedCollections().addSchema(transformation.computeFinalSchema())
+            yield request.replySuccess(substitutions = {
+                schema_ref.var: Utils.buildConstantArgumentMsg(id)
+            }, hasNext=False)            
+        else:
+            yield request.replyFail()
+            
+            
+schemaTrasformation = DistributedElements.DistributedPrimitiveWrapper("schema_trasformation", 2, __SchemaTrasformation())
