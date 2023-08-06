6abbd2369861dd63a83f39c72dad3eb608e0d4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package asmek-authcogs/1.0.4/asmek_authcogs-1.0.4.tar.gz

---
+++ packages/asmek-authcogs/asmek_authcogs-1.0.4.tar.gz/asmek_authcogs-1.0.4/asmek_authcogs/auth_hooks.py
@@ -0,0 +1,18 @@
+"""Hooking into the auth system"""
+
+from allianceauth import hooks
+
+from . import app_settings
+
+
+@hooks.register("discord_cogs_hook")
+def register_cogs():
+    """
+    Registering our discord cogs
+    """
+
+    return [
+        "asmek_authcogs.cogs.about",
+        "asmek_authcogs.cogs.auth",
+        "asmek_authcogs.cogs.links",
+    ]
