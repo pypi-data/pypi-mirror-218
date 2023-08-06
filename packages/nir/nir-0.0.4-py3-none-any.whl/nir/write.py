8c92a5b8b350d2c3030eb7025d3e2bb2ab5747e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:22 +0000

    Add package nir/0.0.4/nir-0.0.4-py3-none-any.whl

---
+++ packages/nir/nir-0.0.4-py3-none-any.whl/nir/write.py
@@ -0,0 +1,85 @@
+import typing
+import pathlib
+
+import h5py
+import numpy as np
+
+import nir
+
+
+def write(filename: typing.Union[str, pathlib.Path], graph: nir.NIR) -> None:
+    def convert_node(node: nir.NIRNode) -> dict:
+        if isinstance(node, nir.Delay):
+            return {"type": "Delay", "delay": node.delay}
+        elif isinstance(node, nir.I):
+            return {"type": "I", "r": node.r}
+        elif isinstance(node, nir.Input):
+            return {
+                "type": "Input",
+                "shape": node.shape,
+            }
+        elif isinstance(node, nir.LI):
+            return {
+                "type": "LI",
+                "tau": node.tau,
+                "r": node.r,
+                "v_leak": node.v_leak,
+            }
+        elif isinstance(node, nir.LIF):
+            return {
+                "type": "LIF",
+                "tau": node.tau,
+                "r": node.r,
+                "v_leak": node.v_leak,
+                "v_threshold": node.v_threshold,
+            }
+        elif isinstance(node, nir.Linear):
+            return {
+                "type": "Linear",
+                "weights": node.weights,
+                "bias": node.bias,
+            }
+        elif isinstance(node, nir.Conv1d):
+            return {
+                "type": "Conv1d",
+                "weights": node.weights,
+                "stride": node.stride,
+                "padding": node.padding,
+                "dilation": node.dilation,
+                "groups": node.groups,
+                "bias": node.bias,
+            }
+        elif isinstance(node, nir.Conv2d):
+            return {
+                "type": "Conv2d",
+                "weights": node.weights,
+                "stride": node.stride,
+                "padding": node.padding,
+                "dilation": node.dilation,
+                "groups": node.groups,
+                "bias": node.bias,
+            }
+        elif isinstance(node, nir.Output):
+            return {
+                "type": "Output",
+            }
+        elif isinstance(node, nir.Threshold):
+            return {"type": "Threshold", "threshold": node.threshold}
+        else:
+            raise ValueError(f"Unknown node type: {node}")
+
+    """Write a NIR to a HDF5 file."""
+    with h5py.File(filename, "w") as f:
+        nodes_group = f.create_group("nodes")
+        for i, node in enumerate(graph.nodes):
+            d = convert_node(node)
+            node_group = nodes_group.create_group(str(i))
+            for k, v in d.items():
+                if isinstance(v, str):
+                    node_group.create_dataset(k, data=v, dtype=h5py.string_dtype())
+                elif isinstance(v, np.ndarray):
+                    node_group.create_dataset(k, data=v, dtype=v.dtype)
+                else:
+                    node_group.create_dataset(k, data=v)
+                    # raise ValueError(f"Unknown type: {type(v)}")
+        f.create_dataset("edges", data=graph.edges)
---
+++ packages/nir/nir-0.0.4-py3-none-any.whl/tests/test_readwrite.py
@@ -0,0 +1,97 @@
+import tempfile
+
+import numpy as np
+
+import nir
+
+
+def factory_test_graph(ir: nir.NIR):
+    tmp = tempfile.mktemp()
+    nir.write(tmp, ir)
+    ir2 = nir.read(tmp)
+    for i in range(len(ir.nodes)):
+        for k, v in ir.nodes[i].__dict__.items():
+            if isinstance(v, np.ndarray) or isinstance(v, list):
+                assert np.array_equal(v, getattr(ir2.nodes[i], k))
+            else:
+                assert v == getattr(ir2.nodes[i], k)
+
+
+def test_simple():
+    ir = nir.NIR(nodes=[nir.Linear(weights=[1, 2, 3], bias=4)], edges=[(0, 0)])
+    factory_test_graph(ir)
+
+
+def test_integrator():
+    ir = nir.NIR(
+        nodes=[nir.Linear(weights=[1], bias=0), nir.I(r=2)],
+        edges=[(0, 0)],
+    )
+    factory_test_graph(ir)
+
+
+def test_leaky_integrator():
+    ir = nir.NIR(
+        nodes=[nir.Linear(weights=[1], bias=0), nir.LI(tau=1, r=2, v_leak=3)],
+        edges=[(0, 0)],
+    )
+    factory_test_graph(ir)
+
+
+def test_leaky_integrator_and_fire():
+    ir = nir.NIR(
+        nodes=[
+            nir.Linear(weights=[1], bias=0),
+            nir.LIF(tau=1, r=2, v_leak=3, v_threshold=4),
+        ],
+        edges=[(0, 0)],
+    )
+    factory_test_graph(ir)
+
+
+def test_simple_with_read_write():
+    ir = nir.NIR(
+        nodes=[
+            nir.Input(
+                shape=[
+                    3,
+                ]
+            ),
+            nir.Linear(weights=[1, 2, 3], bias=4),
+            nir.Output(),
+        ],
+        edges=[(0, 1), (1, 2)],
+    )
+    factory_test_graph(ir)
+
+
+def test_delay():
+    ir = nir.NIR(
+        nodes=[
+            nir.Input(
+                shape=[
+                    3,
+                ]
+            ),
+            nir.Delay(delay=[1, 2, 3]),
+            nir.Output(),
+        ],
+        edges=[(0, 1), (1, 2)],
+    )
+    factory_test_graph(ir)
+
+
+def test_threshold():
+    ir = nir.NIR(
+        nodes=[
+            nir.Input(
+                shape=[
+                    3,
+                ]
+            ),
+            nir.Threshold(threshold=[2.0, 2.5, 2.8]),
+            nir.Output(),
+        ],
+        edges=[(0, 1), (1, 2)],
+    )
+    factory_test_graph(ir)
