5e7343ff8ad34891fd51b5f3b83c7c9f37fcceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:19 +0000

    Add package findpython/1.0.0a2/findpython-1.0.0a2-cp37-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/findpython/findpython-1.0.0a2-cp37-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/findpython/__init__.pyi
@@ -0,0 +1,122 @@
+import abc
+from pathlib import Path
+from typing import Any, Sequence
+
+from _typeshed import StrPath
+
+class Version:
+    dev: Any
+    epoch: Any
+    post: Any
+    pre: Any
+    release: Any
+    major: Any
+    minor: Any
+    micro: Any
+
+    @classmethod
+    def __init__(cls, *args, **kwargs) -> None: ...
+    def any_prerelease(self, *args, **kwargs) -> Any: ...
+    def parse_star(self, *args, **kwargs) -> Any: ...
+    def __eq__(self, other) -> Any: ...
+    def __ge__(self, other) -> Any: ...
+    def __gt__(self, other) -> Any: ...
+    def __hash__(self) -> Any: ...
+    def __le__(self, other) -> Any: ...
+    def __lt__(self, other) -> Any: ...
+    def __ne__(self, other) -> Any: ...
+
+class PythonVersion:
+    def __init__(
+        self,
+        executable: StrPath,
+        *,
+        interpreter: StrPath | None = None,
+        version: Version | None = None,
+        architecture: str | None = None,
+        keep_symlink: bool = False,
+    ) -> None: ...
+    @property
+    def executable(self) -> Path: ...
+    @property
+    def interpreter(self) -> Path: ...
+    @property
+    def real_path(self) -> Path: ...
+    @property
+    def version(self) -> Version: ...
+    @property
+    def architecture(self) -> str: ...
+    def is_valid(self) -> bool: ...
+    def matches(
+        self,
+        major: int | None = None,
+        minor: int | None = None,
+        patch: int | None = None,
+        pre: bool | None = None,
+        dev: bool | None = None,
+        name: str | None = None,
+        architecture: str | None = None,
+    ) -> bool: ...
+    def __eq__(self, __value: object) -> bool: ...
+    def __lt__(self, __value: object) -> bool: ...
+    def __hash__(self) -> int: ...
+
+class Finder:
+    resolve_symlinks: bool
+    same_file: bool
+    same_interpreter: bool
+
+    def __init__(
+        self,
+        resolve_symlinks: bool = False,
+        no_same_file: bool = False,
+        no_same_interpreter: bool = False,
+        selected_providers: Sequence[str] | None = None,
+    ) -> None: ...
+    def find_all(
+        self,
+        major: str | int | None = None,
+        minor: int | None = None,
+        patch: int | None = None,
+        pre: bool | None = None,
+        dev: bool | None = None,
+        name: str | None = None,
+        architecture: str | None = None,
+    ) -> list[PythonVersion]: ...
+    def find(
+        self,
+        major: str | int | None = None,
+        minor: int | None = None,
+        patch: int | None = None,
+        pre: bool | None = None,
+        dev: bool | None = None,
+        name: str | None = None,
+        architecture: str | None = None,
+    ) -> PythonVersion | None: ...
+    def add_provider(self, provider: BaseProvider, pos: int | None = None) -> None: ...
+
+def find_all(
+    major: str | int | None = None,
+    minor: int | None = None,
+    patch: int | None = None,
+    pre: bool | None = None,
+    dev: bool | None = None,
+    name: str | None = None,
+    architecture: str | None = None,
+) -> list[PythonVersion]: ...
+def find(
+    major: str | int | None = None,
+    minor: int | None = None,
+    patch: int | None = None,
+    pre: bool | None = None,
+    dev: bool | None = None,
+    name: str | None = None,
+    architecture: str | None = None,
+) -> PythonVersion | None: ...
+
+class BaseProvider(abc.ABC):
+    @abc.abstractmethod
+    def find_pythons(self) -> list[PythonVersion]: ...
+    def find_pythons_from_path(
+        self, path: StrPath, as_interpreter: bool = False
+    ) -> list[PythonVersion]: ...
