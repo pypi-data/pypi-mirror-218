f436c8d33d3863693ea3be70e7ca3f09fdff78b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package AQIPython/1.0.1/AQIPython-1.0.1.tar.gz

---
+++ packages/AQIPython/AQIPython-1.0.1.tar.gz/AQIPython-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="AQIPython",
+    version="1.0.1",
+    description="A Multistandard python AQI calculator library ",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Spritan/AQIPython",
+    author="Spritan",
+    author_email="proypabsab@gmail.com",
+    license="MIT",
+    keywords = ["AQI", "Air Quality", "Sensors","IOT"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Healthcare Industry',
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    # install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    # python_requires=">=3.10",
+)
