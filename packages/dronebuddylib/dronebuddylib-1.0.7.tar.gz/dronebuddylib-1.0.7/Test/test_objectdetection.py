5acf81098b34b0109a2c1ff513e1f7d547aad403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:12 +0000

    Add package dronebuddylib/1.0.7/dronebuddylib-1.0.7.tar.gz

---
+++ packages/dronebuddylib/dronebuddylib-1.0.7.tar.gz/dronebuddylib-1.0.7/Test/test_objectdetection.py
@@ -0,0 +1,26 @@
+import unittest
+
+import cv2
+
+import offline.atoms as dbl
+
+
+# read input image
+
+
+class TestObjectDetection(unittest.TestCase):
+
+    def test_upload_image(self):
+        image = cv2.imread('test_image_clear.jpg')
+
+        labels = dbl.get_label_yolo(image)
+        assert len(labels) > 0 and 'chair' in labels
+
+        print(labels)
+
+    def test_me(self):
+        print('test_me')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/dronebuddylib/dronebuddylib-1.0.7.tar.gz/dronebuddylib-1.0.7/dronebuddylib/utils/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.7"
+
+from .enums import DroneCommands
+from .enums import Language
+from .enums import ObjectDetectionReturnTypes
+from .logging_config import get_logger
+from .exceptions import FileWritingException
