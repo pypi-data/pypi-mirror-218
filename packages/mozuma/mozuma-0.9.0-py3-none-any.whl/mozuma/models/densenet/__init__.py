3aec4e01491709264fcddb85d84bf30086bef8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:11 +0000

    Add package mozuma/0.9.0/mozuma-0.9.0-py3-none-any.whl

---
+++ packages/mozuma/mozuma-0.9.0-py3-none-any.whl/mozuma/models/densenet/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["TorchDenseNetModule", "torch_densenet_imagenet", "torch_densenet_places365"]
+
+from .modules import TorchDenseNetModule
+from .pretrained import torch_densenet_imagenet, torch_densenet_places365
