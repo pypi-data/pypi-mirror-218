b02be33129f40c1518be18ee4d0b2f346248c72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:30 +0000

    Add package aimojicommit/1.0.0/aimojicommit-1.0.0-py2.py3-none-any.whl

---
+++ packages/aimojicommit/aimojicommit-1.0.0-py2.py3-none-any.whl/src/ConfigManager.py
@@ -0,0 +1,61 @@
+import yaml
+import constants
+import os
+import requests
+from pathlib import Path
+
+class ConfigManager:
+    def __init__(self):
+        homedir = str(Path.home())
+        self.root_folder = os.path.join(homedir, constants.aimoji_root_folder_name)
+        self.file_path = os.path.join(self.root_folder, constants.config_file_name)
+        self.config = None
+
+    def load_config(self) -> None:
+        if not os.path.exists(self.root_folder):
+            os.makedirs(self.root_folder)
+
+        if not os.path.exists(self.file_path):
+            resp = requests.get(constants.remote_default_config_url, stream=True)
+            with open(self.file_path, 'wb') as file:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    file.write(chunk)
+        
+        with open(self.file_path, "r") as file:
+            self.config = yaml.load(file, Loader=yaml.FullLoader)
+
+    def get_value(self, key: str, default = None):
+        if self.config is None:
+            self.load_config()
+        return self.config.get(key, default)
+
+    def get_commit_types(self):
+        if self.config is None:
+            self.load_config()
+        return self.get_value('commit_types')
+
+    def get_openai_api_key(self):
+        if self.config is None:
+            self.load_config()
+        return self.get_value('openai_api_key')
+    
+    def set_openai_api_key(self, key: str):
+        if self.config is None:
+            self.load_config()
+        self.config['openai_api_key'] = key
+        self.save_config()
+
+    def get_openai_chat_model(self):
+        if self.config is None:
+            self.load_config()
+        return self.get_value('openai_chat_model')
+    
+    def set_openai_chat_model(self, model: str):
+        if self.config is None:
+            self.load_config()
+        self.config['openai_chat_model'] = model
+        self.save_config()
+
+    def save_config(self) -> None:
+        with open(self.file_path, "w") as file:
+            yaml.dump(self.config, file)
---
+++ packages/aimojicommit/aimojicommit-1.0.0-py2.py3-none-any.whl/src/aimojicommit.py
@@ -0,0 +1,140 @@
+import click
+from ChoicesManager import ChoicesManager
+from ConfigManager import ConfigManager
+from GitManager import GitManager
+from AIManager import AIManager, Model
+
+def model_id(model: Model):
+    return model.id
+
+def setup_config(config_manager: ConfigManager):
+    openai_key = config_manager.get_openai_api_key()
+    chat_model = config_manager.get_openai_chat_model()
+    if openai_key is None:
+        set_key(None)
+    if chat_model is None:
+        set_model(None)
+
+
+def set_key(key: str or None):
+    config_manager = ConfigManager()
+    config_manager.load_config()
+    if not key:
+        key = click.prompt("Please enter your OpenAI API key", type=str)
+
+    config_manager.set_openai_api_key(key)
+
+
+def set_model(model: str or None):
+    config_manager = ConfigManager()
+    config_manager.load_config()
+
+    models = AIManager(config_manager).list_available_models()
+
+    if not model:
+        model = ChoicesManager(config_manager).choose_option(
+            "Please choose an OpenAI chat model (*-32k suggested)", list(map(model_id, models))
+        )
+
+    if model not in list(map(model_id, models)):
+        click.echo("Error: Model not found.")
+        raise click.Abort()
+
+    config_manager.set_openai_chat_model(model)
+
+
+@click.group(invoke_without_command=True)
+@click.option("-t", "--commit-type", help="Specify the commit type", default=None)
+def aimojicommit(commit_type):
+    click.echo("🤖 Aimoji Commit")
+    config_manager = ConfigManager()
+    config_manager.load_config()
+    click.echo(f"Loaded config from {config_manager.file_path}")
+
+    setup_config(config_manager)
+
+    choices_manager = ChoicesManager(config_manager)
+    ai_manager = AIManager(config_manager)
+
+    if not GitManager.is_git_installed():
+        click.echo("Error: Git is not installed. Exiting.")
+        raise click.Abort()
+
+    if not GitManager.is_inside_repository():
+        click.echo("Error: Not inside a git repository. Exiting.")
+        raise click.Abort()
+
+    diffs = GitManager.get_diff_changes()
+    stats = GitManager.get_diff_stats()
+
+    if len(diffs) == 0 or len(stats) == 0:
+        click.echo("Error: No changes added to commit. Exiting.")
+        raise click.Abort()
+
+    # Optionally ask for the commit type
+    if not commit_type:
+        commit_type = choices_manager.choose_commit_type()
+
+    # Check if there is a preconfigured commit message
+    merge_msg = GitManager.get_merge_commit_message()
+    if merge_msg is not None and len(merge_msg) > 0:
+        click.echo(f"Found preconfigured merge commit message: {merge_msg}")
+        reply = click.prompt(
+            "Do you want to use this message? (Y/n)",
+            default="Y",
+            show_choices=True,
+            type=click.Choice(["Y", "n"]),
+        )
+        if reply == "Y":
+            GitManager.commit_with_message(merge_msg)
+            raise click.Abort()
+
+    model_id = config_manager.get_openai_chat_model()
+    model = ai_manager.get_model_info(model_id)
+
+    # No preconfigured commit message, so generate
+    can_gen = ai_manager.can_generate_commit_message(model, stats, diffs, commit_type)
+    if not can_gen:
+        click.echo(
+            "Changes are too large to generate a commit message. Opening editor."
+        )
+        GitManager.commit_with_message(commit_type)
+    else:
+        need_regenerate = True
+        while need_regenerate:
+            commit_msg = ai_manager.generate_commit_message(
+                model, stats, diffs, commit_type['value']
+            )
+            click.echo(f"Generated commit message: {commit_msg}")
+            reply = click.prompt(
+                "Continue, regenerate or quit?",
+                default="C",
+                show_choices=True,
+                type=click.Choice(["C", "r", "q"]),
+            )
+            if reply == "C":
+                GitManager.commit_with_message(commit_msg)
+                need_regenerate = False
+            elif reply == "r":
+                need_regenerate = True
+            elif reply == "q":
+                raise click.Abort()
+            else:
+                click.echo("Error: Invalid choice.")
+                raise click.Abort()
+
+
+@aimojicommit.command("set-model")
+@click.argument("model", default=None)
+def set_model_cmd(model):
+    set_model(model)
+
+
+@aimojicommit.command("set-key")
+@click.argument("key", default=None)
+def set_key_cmd(key):
+    set_key(key)
+
+
+if __name__ == "__main__":
+    aimojicommit()
