a2d13af28979b1ca642f928a3319947be7bb7334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:40 +0000

    Add package fsapp/1.0.4/fsapp-1.0.4.tar.gz

---
+++ packages/fsapp/fsapp-1.0.4.tar.gz/fsapp-1.0.4/fsapp/__init__.py
@@ -0,0 +1,11 @@
+from fsapp.base import app
+from fsapp.classes import BaseExecutor
+from fsapp.core.config import settings
+from fsapp.core.handlers import handlers
+
+__all__ = [
+    "BaseExecutor",
+    "app",
+    "settings",
+    "handlers"
+]
