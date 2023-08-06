ee34cb9933a4849a9c962479c6bdad00ef543f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:05 +0000

    Add package omnivorex/0.0.11/omnivorex-0.0.11-py3-none-any.whl

---
+++ packages/omnivorex/omnivorex-0.0.11-py3-none-any.whl/omnivorex/components/login_screen.py
@@ -0,0 +1,55 @@
+from textual.widgets import (
+    Input,
+    Button,
+    Static,
+)
+from textual.screen import ModalScreen
+from textual import events
+
+
+class LoginScreen(ModalScreen[str]):
+    DEFAULT_CSS = """
+    LoginScreen {
+        layout: vertical;
+        align: center middle;
+        border: thick $background 80%;
+        background: $surface;
+    }
+    LoginScreen * {
+        text-align: center;
+        max-width: 40;
+        align: center middle;
+        width: 100%;
+    }
+    .hidden {
+        display: none;   
+    }
+    #error {
+        color: red;
+    }
+    #login {
+        width: 100%;
+    }
+    """
+
+    def compose(self):
+        yield Static("Enter your Omnivore API Token:")
+        yield Input(placeholder="Token")
+        yield Static("Please enter a valid API token!", classes="hidden", id="error")
+        yield Button("Login", variant="primary", id="login")
+
+    def get_text_from_input(self):
+        api_token = self.query_one("Input").value
+        if api_token:
+            self.dismiss(api_token)
+        else:
+            self.query_one("#error").remove_class("hidden")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "login":
+            self.get_text_from_input()
+
+    def on_key(self, event: events.Key) -> None:
+        # if the user presses the enter key, dismiss the screen
+        if event.key == "enter":
+            self.get_text_from_input()
