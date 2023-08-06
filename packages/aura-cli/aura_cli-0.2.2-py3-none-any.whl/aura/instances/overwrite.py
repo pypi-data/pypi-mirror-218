6fab5adfd21e0c2caf9a89fcaa147552f19693d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package aura-cli/0.2.2/aura_cli-0.2.2-py3-none-any.whl

---
+++ packages/aura-cli/aura_cli-0.2.2-py3-none-any.whl/aura/instances/overwrite.py
@@ -0,0 +1,20 @@
+import json
+import click
+from aura.api_command import api_command
+
+from aura.api_repository import make_api_call
+from aura.util.get_instance_id import get_instance_id
+
+# POST /instances/:instanceId/overwrite
+
+@api_command(help="Overwrite an instance")
+@click.option('--instance-id', '-id', help="The instance ID")
+@click.option('--source-instance', '-s', prompt=True, help="The source instance ID")
+@click.option('--name', '-n', help="The instance name")
+def overwrite(instance_id, source_instance, name):
+    instance_id = get_instance_id(instance_id, name)
+
+    data = {"source_instance_id": source_instance}
+    path = f"/instances/{instance_id}/overwrite"
+
+    return make_api_call("POST", path, data=json.dumps(data))
---
+++ packages/aura-cli/aura_cli-0.2.2-py3-none-any.whl/aura/instances/update.py
@@ -0,0 +1,24 @@
+import json
+import click
+from aura.api_command import api_command
+from aura.api_repository import make_api_call
+from aura.util.get_instance_id import get_instance_id
+
+# PATCH /instances/:instanceId
+
+@api_command(help="Update an instance")
+@click.option('--instance-id', '-id', help="The instance ID")
+@click.option('--memory', '-m', help="A new instance memory size (for resizing)")
+@click.option('--new-name', help="A new instance name (for renaming)")
+@click.option('--name', '-n', help="The instance name")
+def update(instance_id, memory, new_name, name):
+    instance_id = get_instance_id(instance_id, name)
+    
+    data = {}
+    if memory:
+        data["memory"] = f"{memory}GB"
+    if new_name:
+        data["name"] = new_name
+    path = f"/instances/{instance_id}"
+
+    return make_api_call("PATCH", path, data=json.dumps(data))
