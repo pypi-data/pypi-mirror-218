d082ebf3325c1d923d8f0dc4209d4754d1499b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package model-tracker-framework/1.2.1/model_tracker_framework-1.2.1.tar.gz

---
+++ packages/model-tracker-framework/model_tracker_framework-1.2.1.tar.gz/model_tracker_framework-1.2.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import pathlib
+import setuptools
+
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name='model_tracker_framework',
+    version='1.2.1',
+    description="An object-orientated framework for tracking machine learning projects.",
+    long_description=README,
+    packages=setuptools.find_packages(where="src"),
+    author="Joshua Spear",
+    author_email="josh.spear9@gmail.com",
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/kaggle-tings/mtf",
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=["pandas"]
+)
