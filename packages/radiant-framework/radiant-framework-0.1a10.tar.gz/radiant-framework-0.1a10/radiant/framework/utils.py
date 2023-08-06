dbddbf115cecf010ba3a34a48dd686f33fa3f875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:29 +0000

    Add package radiant-framework/0.1a10/radiant-framework-0.1a10.tar.gz

---
+++ packages/radiant-framework/radiant-framework-0.1a10.tar.gz/radiant-framework-0.1a10/radiant/framework/utils.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import multiprocessing
+
+
+
+########################################################################
+class Environ_:
+    """"""
+
+
+    # ----------------------------------------------------------------------
+    def __call__(self, value, default=None):
+        """"""
+        return os.getenv(value, default)
+
+
+    # ----------------------------------------------------------------------
+    def __getattr__(self, value):
+        """"""
+        return os.getenv(value, None)
+
+
+environ = Environ_()
+
+
+
+
+
+# ----------------------------------------------------------------------
+def run_script(script, port):
+    """"""
+    def worker(script, port):
+        os.system(f"python {script} {port}")
+    p = multiprocessing.Process(target=worker, args=(script, port))
+    p.start()
