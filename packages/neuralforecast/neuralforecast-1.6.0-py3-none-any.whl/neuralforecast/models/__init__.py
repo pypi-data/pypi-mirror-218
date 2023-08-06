d3feb57bad865c7141a96bb5e4d273a3bc0b9c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:39 +0000

    Add package neuralforecast/1.6.0/neuralforecast-1.6.0-py3-none-any.whl

---
+++ packages/neuralforecast/neuralforecast-1.6.0-py3-none-any.whl/neuralforecast/models/__init__.py
@@ -0,0 +1,22 @@
+__all__ = ['RNN', 'GRU', 'LSTM', 'TCN', 'DilatedRNN',
+           'MLP', 'NHITS', 'NBEATS', 'NBEATSx',
+           'TFT', 'VanillaTransformer', 'Informer', 'Autoformer', 'PatchTST', 'FEDformer',
+           'StemGNN', 'HINT']
+
+from .rnn import RNN
+from .gru import GRU
+from .lstm import LSTM
+from .tcn import TCN
+from .dilated_rnn import DilatedRNN
+from .mlp import MLP
+from .nhits import NHITS
+from .nbeats import NBEATS
+from .nbeatsx import NBEATSx
+from .tft import TFT
+from .stemgnn import StemGNN
+from .vanillatransformer import VanillaTransformer
+from .informer import Informer
+from .autoformer import Autoformer
+from .fedformer import FEDformer
+from .patchtst import PatchTST
+from .hint import HINT
