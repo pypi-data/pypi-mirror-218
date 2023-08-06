7eed116437acc75ef7f85fc205e27dceb0bb2614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:46 +0000

    Add package cubicweb-postgis/1.0.0/cubicweb_postgis-1.0.0-py3-none-any.whl

---
+++ packages/cubicweb-postgis/cubicweb_postgis-1.0.0-py3-none-any.whl/cubicweb_postgis/__init__.py
@@ -0,0 +1,11 @@
+"""cubicweb-postgis application package
+
+Wrap Postgis into CubicWeb.
+"""
+
+_GEOM_PARAMETERS = (
+    "srid",
+    "geom_type",
+    "coord_dimension",
+)  # XXX: coord_dimension -> z and m ?
+_GEOG_PARAMETERS = ("srid", "geom_type", "coord_dimension")
