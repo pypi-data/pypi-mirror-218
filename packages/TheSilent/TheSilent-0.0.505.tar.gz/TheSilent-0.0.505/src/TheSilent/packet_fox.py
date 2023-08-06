74a2f5ae463d70c8c41065666d1931389ccd1733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:19 +0000

    Add package TheSilent/0.0.505/TheSilent-0.0.505.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.505.tar.gz/TheSilent-0.0.505/src/TheSilent/packet_fox.py
@@ -0,0 +1,25 @@
+import binascii
+import socket
+from TheSilent.clear import clear
+
+CYAN = "\033[1;36m"
+
+def packet_fox():
+    interfaces = socket.if_nameindex()
+    sniffer_socket = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.ntohs(3))
+    for interface in interfaces:
+        if interface[1] != "lo":
+            try:
+                sniffer_socket.bind((interface[1],interface[0]))
+                print(CYAN + f"listening on interface:{interface[1]} port:{interface[0]}")
+
+            except OSError:
+                continue
+
+    while True:
+        packet = sniffer_socket.recv(65536)
+        data = binascii.hexlify(packet).decode()
+        with open("hex_dump.txt", "a") as file:
+            file.write(data + "\n")
+
+packet_fox()
