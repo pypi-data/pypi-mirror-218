458a6c96ade7277395020eef2c9f92e6db84ab7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:09 +0000

    Add package pyreference/0.7.4/pyreference-0.7.4-py3-none-any.whl

---
+++ packages/pyreference/pyreference-0.7.4-py3-none-any.whl/pyreference-0.7.4.data/scripts/pyreference_biotype.py
@@ -0,0 +1,214 @@
+#!python
+
+from __future__ import print_function, absolute_import
+from argparse import ArgumentParser
+from collections import Counter, defaultdict
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+from pyreference import Reference, ReferenceArgumentParser
+from pyreference.utils import iv_iterators
+from pyreference.utils.file_utils import name_from_file_name, mk_path_for_file
+from pyreference.utils.genomics_utils import opposite_strand, format_chrom
+import HTSeq
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import six
+
+
+def handle_args():
+    parser = ReferenceArgumentParser(description='Collect stats on read length and biotype')
+    parser.add_argument("--intervals", help='.bed/.gtf etc file')
+    parser.add_argument("--intervals-name", help="Used in graphs")
+    parser.add_argument("--reverse-strand", action='store_true',
+                        help="Reverse strand before testing region, useful when you have stranded sequencing and "
+                             "the read sequenced is anti-sense")
+    parser.add_argument("bam")
+    return parser.parse_args()
+
+
+def get_counts_by_length(bam, regions_array, has_chr, reverse_strand):
+    """ bam: bam file path
+        regions_array: genomic array of biotypes which is the output from create_biotype_regions_array
+        has_chr: Boolean, do reference chromosome names have "chr"? Can be obtained using reference.has_chr
+        reverse_strand: switch the strand of the alignment before counting reads
+        Returns: pandas dataframe of counts for each biotype for each read length."""
+    
+    length_counters = defaultdict(Counter)
+
+    for aln in HTSeq.BAM_Reader(bam):
+        length = len(aln.read)
+        read_region = None
+        if aln.aligned:
+            aln.iv.chrom = format_chrom(aln.iv.chrom, has_chr)
+            if reverse_strand:
+                aln.iv.strand = opposite_strand(aln.iv.strand)
+
+            region_overlap_length = 0
+            for iv, r in regions_array[aln.iv].steps():
+                if iv.length > region_overlap_length:
+                    read_region = r
+                    region_overlap_length = iv.length
+
+            if read_region is None:
+                read_region = "intergenic"
+        else:
+            read_region = "unaligned"
+        length_counters[length][read_region] += 1
+    
+    df = pd.DataFrame(length_counters, columns=sorted(list(length_counters)))
+    df = df.fillna(0).astype(int)
+    df = df.sort_index().T
+    return df
+
+
+def create_biotype_regions_array(reference, interesting_biotypes=None):
+    """ Reference: reference object,
+        interesting_biotypes : List of Strings corresponding to biotype keys (everything else is 'other') """
+
+    if interesting_biotypes is None:
+        interesting_biotypes = ['protein_coding', 'rRNA', 'lincRNA', 'misc_RNA', 'snRNA', 'miRNA', 'snoRNA', 'tRNA']
+
+    other_biotypes = []  # Everything non interesting is counted as "other"
+    for biotype in reference.genes_by_biotype.keys():
+        if biotype not in interesting_biotypes:
+            other_biotypes.append(biotype)
+
+    def get_biotype(gene):
+        if gene.biotype in other_biotypes:
+            return "other"
+        elif gene.biotype == "misc_RNA":
+            if gene.name and gene.name.startswith("RNY"):
+                return "yRNA"
+        return gene.biotype
+
+    regions = HTSeq.GenomicArray("auto", stranded=True, typecode='O')
+    for transcript in six.itervalues(reference.transcripts):
+        # Antisense: Read is in the region of a transcript, but on the opposite strand.
+        antisense_iv = transcript.iv.copy()
+        antisense_iv.strand = opposite_strand(antisense_iv.strand)
+        regions[antisense_iv] = "anti-sense"
+
+    for gene in six.itervalues(reference.genes):
+        for transcript in gene.transcripts:
+            regions[transcript.iv] = "introns"
+
+    for biotype in other_biotypes + interesting_biotypes:
+        genes = reference.genes_by_biotype.get(biotype, [])
+        for gene in genes:
+            for t in gene.transcripts:
+                for exon in t.get_features("exon"):
+                    regions[exon.iv] = get_biotype(gene)
+    return regions
+
+
+def add_intervals_to_regions_array(regions_array, intervals, intervals_name, has_chr):
+    intervals_name = intervals_name or name_from_file_name(intervals)
+
+    iv_iterator = iv_iterators.load_iv_iterator(intervals)
+    for iv in iv_iterators.format_chrom_iterator(iv_iterator, has_chr):
+        # Fix for 'Non-stranded index used for stranded GenomicArray.'
+        if iv.strand == '.':
+            iv.strand = '+'
+            regions_array[iv] = intervals_name
+            iv.strand = '-'
+            regions_array[iv] = intervals_name
+        else:
+            regions_array[iv] = intervals_name
+
+
+def main():
+    args = handle_args()
+    
+    #Set up  
+    sample_name = name_from_file_name(args.bam)
+    csv_file = "%s.read_counts.regions.csv" % sample_name
+    graph_image =  "%s.read_counts.regions.png" % sample_name
+    
+    reference = args.reference
+    print("Reference is", reference) #To confirm the annotation you're using is what you intended. 
+    
+    regions_array = create_biotype_regions_array(reference)
+
+    if args.intervals:
+        add_intervals_to_regions_array(regions_array, args.intervals, args.intervals_name, reference.has_chr)
+
+    print("Loaded reference - reading BAM")
+    df = get_counts_by_length(args.bam, regions_array, reference.has_chr, args.reverse_strand)
+    
+    biotype_colors = {'other': "red",
+                      'protein_coding': "DarkGreen",
+                      'rRNA': "orange",
+                      'lincRNA': "purple",
+                      'misc_RNA': "brown",
+                      'snRNA': "yellow",
+                      'miRNA': "blue",
+                      'snoRNA': "cyan",
+                      "unaligned": "black",
+                      "tRNA": "magenta",
+                      "intergenic": "grey",
+                      "introns": "pink",
+                      "anti-sense": "silver",
+                      "yRNA": "Coral"}
+    if args.intervals:
+        biotype_colors[args.intervals_name] = "lightgreen"
+    
+    # Add empty columns (biotypes) for those which had zero counts
+    df[sorted(set(biotype_colors.keys()).difference(df.columns))] = 0
+    
+    # Add empty rows (read lengths) for those which had zero counts
+    smallest = min(df.index)
+    largest = max(df.index)
+    all_read_lengths = range(smallest, largest + 1)
+    missing_read_lengths = (sorted(set(all_read_lengths).difference(df.index)))
+    if missing_read_lengths:
+        missing_df = pd.DataFrame(index=missing_read_lengths, dtype=int, columns=df.columns, data=0)
+        df = pd.concat([df, missing_df])
+
+    df = df.sort_index()
+    df.to_csv(csv_file)
+    
+    # Graph data
+    labels = sorted(biotype_colors.keys())
+    colors = []
+    for k in labels:
+        colors.append(biotype_colors[k])
+    
+    sns.set_theme(context='paper', style="ticks", font_scale=1.1)
+
+    print("Total read counts:")
+    print(df.sum(axis=0))  # A summary of total counts for all read lengths.
+    
+    fig = Figure(dpi=300, figsize=(4.8, 3.1))
+    
+    fig.patch.set_facecolor('white')
+    ax = fig.add_subplot(111)
+     
+    # Make stacked bar chart
+    bottom = np.zeros(len(df.index), dtype='i')
+    for label in df.columns:
+        counts = df[label]
+        color = biotype_colors[label]
+        _ = ax.bar(df.index, counts, label=label, color=color, bottom=bottom, linewidth=0)
+        bottom += counts
+    
+    # Format chart
+    ax.set_xlabel("Length (nt)")
+    ax.set_ylabel("Read counts")
+    
+    _, ymax = ax.get_ylim()
+    ax.set_ylim(ymin=0, ymax=ymax*1.02)  # Move maximum slightly above highest bar
+    ax.set_xlim(xmin=(min(df.index) - 0.7), xmax=(max(df.index) + 0.7))
+    
+    # Shrink to fit legend
+    fig.tight_layout(rect=[0, 0, 0.7, 1])  # left, bottom, right, top
+     
+    ax.legend(loc='center left', prop={'size': 8.5}, bbox_to_anchor=(1.01, 0.5))
+    mk_path_for_file(graph_image)
+     
+    canvas = FigureCanvasAgg(fig)
+    canvas.print_png(graph_image)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pyreference/pyreference-0.7.4-py3-none-any.whl/pyreference/transcript.py
@@ -0,0 +1,240 @@
+from __future__ import print_function, absolute_import
+
+import HTSeq
+from collections import defaultdict
+from lazy import lazy
+
+from pyreference.genomic_region import GenomicRegion
+from pyreference.settings import START, END, CHROM, STRAND
+from pyreference.utils.genomics_utils import GenomicInterval_from_directional, dict_to_iv
+
+
+class NotOnTranscriptException(Exception):
+    """ For when a transcription position is not on a transcript """
+    pass
+
+
+class Transcript(GenomicRegion):
+    def __init__(self, *args, **kwargs):
+        gene = kwargs.pop("gene", None)
+        super(Transcript, self).__init__(*args, **kwargs)
+        self.gene = gene
+    
+    def get_gene_id(self):
+        return self.gene.get_id() 
+
+    @lazy
+    def is_coding(self):
+        return "start_codon" in self._dict
+
+    @lazy
+    def tags(self):
+        return set(self._dict.get("tag", "").split(","))
+
+    @property
+    def is_forward_strand(self):
+        return self._dict["strand"] == "+"
+
+    def get_representative_transcript(self):
+        return self
+
+    def get_features_length(self, feature_type):
+        length = 0
+        for feature in self.get_features_in_stranded_order(feature_type):
+            length += feature[END] - feature[START] 
+        return length
+
+    def get_features(self, feature_type):
+        """ returns list of HTSeq.GenomicFeature """
+        genomic_features = []
+        for f in self.get_features_in_stranded_order(feature_type):
+            iv = dict_to_iv(f)
+            genomic_feature = HTSeq.GenomicFeature(self.get_id(), feature_type, iv)
+            genomic_features.append(genomic_feature)
+        
+        return genomic_features 
+
+    @lazy
+    def features_by_type(self):
+        """ These are redundant so we re-generate them from JSON """
+        fbt = defaultdict(list)
+
+        # All in genomic order
+        (left_utr, right_utr) = ("5PUTR", "3PUTR")
+        if not self.is_forward_strand:  # Switch
+            (left_utr, right_utr) = (right_utr, left_utr)
+
+        cds_start = self._dict.get("cds_start")
+        cds_end = self._dict.get("cds_end")
+
+        if self.is_coding:
+            left_codon_feature = {START: cds_start, END: cds_start+3}
+            right_codon_feature = {START: cds_end - 3, END: cds_end}
+            # cds_start/cds_end INCLUDE the start/stop codons, while the "CDS" features only includes start_codon
+            cds_feature_start = cds_start
+            cds_feature_end = cds_end
+            if self.is_forward_strand:
+                fbt["start_codon"].append(left_codon_feature)
+                fbt["stop_codon"].append(right_codon_feature)
+                cds_feature_end -= 3
+            else:
+                fbt["start_codon"].append(right_codon_feature)
+                fbt["stop_codon"].append(left_codon_feature)
+                cds_feature_start += 3
+        else:
+            cds_feature_start = None
+            cds_feature_end = None
+
+        for exon in self._dict["exons"]:  # exons in genomic order
+            exon_start = exon[0]
+            exon_end = exon[1]
+            exon_feature = {
+                START: exon_start,
+                END: exon_end,
+            }
+            fbt["exon"].append(exon_feature)
+
+            if self.is_coding:
+                if exon_start <= cds_feature_end and exon_end >= cds_feature_start:
+                    start_coding = max(cds_feature_start, exon_start)
+                    stop_coding = min(cds_feature_end, exon_end)
+
+                    cds_feature = {START: start_coding,
+                                   END: stop_coding}
+                    fbt["CDS"].append(cds_feature)
+
+                if exon_start < cds_start:
+                    end_non_coding = min(cds_start, exon_end)
+                    utr_feature = {START: exon_start,
+                                   END: end_non_coding}
+                    fbt[left_utr].append(utr_feature)
+
+                if exon_end > cds_end:
+                    start_non_coding = max(cds_end, exon_start)
+                    utr_feature = {START: start_non_coding,
+                                   END: exon_end}
+                    fbt[right_utr].append(utr_feature)
+
+        return fbt
+
+    def get_features_in_stranded_order(self, feature_type):
+        """features returned sorted 5' -> 3' """
+
+        is_reversed = self._dict["strand"] == '-'
+        features = self.features_by_type.get(feature_type, [])
+        if features:
+            # Need to add this as not in there by default
+            transcript_chrom = self._dict[CHROM]
+            transcript_strand = self._dict[STRAND]
+
+            for f in features:
+                f[CHROM] = transcript_chrom
+                f[STRAND] = transcript_strand
+
+            features = sorted(features, key=lambda x: x[START], reverse=is_reversed)
+        return features
+
+    @lazy
+    def length(self):
+        return sum([exon[1] - exon[0] for exon in self._dict["exons"]])
+
+    def get_transcript_length(self):
+        return self.length
+
+    def get_sequence_from_features(self, feature_type):
+        features = self.get_features_in_stranded_order(feature_type)
+        return self.reference.get_sequence_from_features(features)
+
+    def get_transcript_sequence(self):
+        return self.get_sequence_from_features("exon")
+
+    @lazy
+    def exons(self):
+        """ Returns list of exon features """
+        return self.get_features("exon")
+    
+    @lazy
+    def threeputr(self):
+        """ Returns the exon regions which contain 3'UTR as list of features """
+        return self.get_features("3PUTR") 
+    
+    @lazy
+    def fiveputr(self):
+        """ Returns the exon regions which contain 5'UTR as list of features """
+        return self.get_features("5PUTR")
+
+    def get_coding_sequence(self):
+        """ Warning: There are frame shift issues not handled here.
+            Do not naively turn this into a protein - better to use existing databases """
+        return self.get_sequence_from_features("CDS")
+
+    def get_5putr_sequence(self):
+        return self.get_sequence_from_features("5PUTR")
+
+    def get_3putr_sequence(self):
+        return self.get_sequence_from_features("3PUTR")
+
+    def get_intron_ivs(self):
+        """
+        Purpose: Get list of intron intervals for the the transcript
+        Output: A list of HTSeq Genomic Interval instances for all introns in the transcript (ordered according to strand)
+        """
+        intron_ivs = []
+        previous_exon = None
+        for exon in self.get_features("exon"):  # This is in stranded order
+            if previous_exon:
+                # HTSeq ends are 1 past the last base of the sequence.
+                # Thus for touching sequences like exons/introns, first_seq.end = second_seq.start
+                intron_start = previous_exon.iv.end_d
+                intron_end = exon.iv.start_d
+                intron_length = abs(intron_end - intron_start)
+                intron = GenomicInterval_from_directional(exon.iv.chrom, intron_start, intron_length, exon.iv.strand)
+                intron_ivs.append(intron)
+            previous_exon = exon
+        return intron_ivs
+    
+    def get_intron_sequences(self):
+        """
+        Get list of intron sequences for transcript
+        Output: List of sequences (intron order and sequences are 5' to 3')
+        """
+        list_of_intron_intervals = self.get_intron_ivs()
+        intron_sequences = []
+        for intron in list_of_intron_intervals:
+            intron_sequences.append(self.reference.get_sequence_from_iv(intron))
+        return intron_sequences
+    
+    def get_genomic_position(self, pos_on_transcript):
+        """
+        Converts 0-based position on a transcript into 0-based position on the chromosome
+        Arguments -- position relative to 5' end of transcript (int) 
+        Returns -- position on chromosome (int)
+        """
+        #logging.debug("Searching %s for %d", self.get_id(), pos_on_transcript)
+        
+        running_exon_length = 0
+        previous_running_exon_length = 0
+        #go through exons in order, adding to running_exon_length, until you find the one with the miR seed start position in it
+        for exon in self.get_features("exon"):
+            running_exon_length += exon.iv.length
+            if pos_on_transcript < running_exon_length: #match start is in this exon
+                #logging.debug("found the exon, %r running exon length is: %r", exon, running_exon_length)
+                #logging.debug("previous_running_exon_length is %r:", previous_running_exon_length)
+                #logging.debug("exon start_d and end_d are: %r, %r", exon.iv.start_d, exon.iv.end_d)
+                genomic_pos_of_exon_start = exon.iv.start_d
+                pos_on_this_exon = pos_on_transcript - previous_running_exon_length
+                #logging.debug("position on this exon is %r", pos_on_this_exon)
+                if exon.iv.strand == '+':
+                    genomic_position_of_match_start = genomic_pos_of_exon_start + pos_on_this_exon
+                elif exon.iv.strand == '-':
+                    genomic_position_of_match_start = genomic_pos_of_exon_start - pos_on_this_exon
+                else:
+                    raise ValueError("strand must be + or -, not: %r" % exon.iv.strand)
+                return genomic_position_of_match_start
+            previous_running_exon_length += exon.iv.length
+
+        raise NotOnTranscriptException("%s didn't contain %s" % (self.get_id(), pos_on_transcript))
+
+    def __repr__(self):
+        coding_str = " (coding)" if self.is_coding else ""
+        return "Transcript %s%s: length %d" % (self.get_id(), coding_str, self.length)
