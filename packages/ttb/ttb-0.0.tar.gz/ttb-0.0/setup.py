6e3988eb64f3fed6bd14f8e9f9c25e3fa2b8014c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:34 +0000

    Add package ttb/0.0/ttb-0.0.tar.gz

---
+++ packages/ttb/ttb-0.0.tar.gz/ttb-0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ttb',
+      version='0.0',
+      description='Тест загрузки библиотеки2',
+      long_description=open('README.md').read(),
+      packages=['ttb'],
+      author_email='neso_hoshi_official@mail.ru',
+      zip_safe=False,
+      author='Neso Hiroshi',
+      classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+])
