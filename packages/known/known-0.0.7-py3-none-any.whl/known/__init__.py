c5aee53048fa9f8110ff12cb8df87b1922a28d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:35 +0000

    Add package known/0.0.7/known-0.0.7-py3-none-any.whl

---
+++ packages/known/known-0.0.7-py3-none-any.whl/known/__init__.py
@@ -0,0 +1,26 @@
+# known
+__doc__=r"""
+**known** is a collection of reusable python code. Use pip to install **known**
+
+.. code-block:: console
+
+   $ pip install known
+   
+The package is frequently updated by adding new functionality, make sure to have the latest version. To check version use
+
+.. code-block:: console
+
+   >>> import known
+   >>> known.__version___
+
+:py:mod:`known/__init__.py`
+"""
+__version__ = '0.0.7'
+#print(f'known.{__version__}')
+
+
+# from .basic import *
+# from .imgu import *
+# from .mailer import *
+# from .store import *
+
