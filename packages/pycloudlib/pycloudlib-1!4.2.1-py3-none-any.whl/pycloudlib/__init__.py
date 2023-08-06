c3489f51d73ad9f63afc948adea84a12ca555d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:03 +0000

    Add package pycloudlib/1!4.2.1/pycloudlib-1!4.2.1-py3-none-any.whl

---
+++ packages/pycloudlib/pycloudlib-1!4.2.1-py3-none-any.whl/pycloudlib/__init__.py
@@ -0,0 +1,26 @@
+# This file is part of pycloudlib. See LICENSE file for license information.
+"""Main pycloud module __init__."""
+
+import logging
+
+from pycloudlib.azure.cloud import Azure
+from pycloudlib.ec2.cloud import EC2
+from pycloudlib.gce.cloud import GCE
+from pycloudlib.ibm.cloud import IBM
+from pycloudlib.lxd.cloud import LXD, LXDContainer, LXDVirtualMachine
+from pycloudlib.oci.cloud import OCI
+from pycloudlib.openstack.cloud import Openstack
+
+__all__ = [
+    "Azure",
+    "EC2",
+    "GCE",
+    "IBM",
+    "LXD",
+    "LXDContainer",
+    "LXDVirtualMachine",
+    "OCI",
+    "Openstack",
+]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
