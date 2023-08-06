acd5a34c3b9c638eaa2bd50a6ebf2a346249ef77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:50 +0000

    Add package biadab/9.9.9/biadab-9.9.9.tar.gz

---
+++ packages/biadab/biadab-9.9.9.tar.gz/biadab-9.9.9/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as rd:
+    long_description = rd.read()
+
+setup(
+    name="biadab",
+    version="9.9.9",
+    author="Unknown",
+    author_email="opetbae6@gmail.com",
+    description="Client library for mengontol",
+    url="https://github.com/kastaid/pytelibs",
+    license="AGPL",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pyrogram', 'tgcrypto'],
+    keywords=['pypi', 'mengontol', 'python', 'pyrogram', 'telebot'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
