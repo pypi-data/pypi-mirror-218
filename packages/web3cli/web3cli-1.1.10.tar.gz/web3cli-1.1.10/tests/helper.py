710cc066beffe472b9c4f85bbff6781c129d64d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package web3cli/1.1.10/web3cli-1.1.10.tar.gz

---
+++ packages/web3cli/web3cli-1.1.10.tar.gz/web3cli-1.1.10/tests/helper.py
@@ -0,0 +1,42 @@
+"""Helper functions to better run tests"""
+
+import os
+import secrets
+import string
+from typing import Any
+
+from web3cli.main import Web3Cli
+from web3core.helpers import yaml
+
+
+def get_test_config_file() -> str:
+    """Path to the config file that the app will use while running tests"""
+    return os.path.join(
+        os.path.expanduser("~"), ".web3cli", "config", "web3cli_test.yml"
+    )
+
+
+def delete_test_config_file(app: Web3Cli) -> None:
+    """Delete the configuration file for the test app"""
+    file = get_test_config_file()
+    if os.path.isfile(file):
+        os.remove(file)
+
+
+def update_setting_in_test_config_file(setting: str, value: Any) -> None:
+    """Set the value of a setting in the test config file"""
+    yaml.set(
+        filepath=get_test_config_file(),
+        setting=setting,
+        value=value,
+        logger=None,
+        section="web3cli",
+    )
+
+
+def get_random_string(
+    n: int = 6, set: str = string.ascii_lowercase + string.digits
+) -> str:
+    """Return a random string of length 'n', picking characters from
+    the given set"""
+    return "".join(secrets.choice(set) for _ in range(n))
