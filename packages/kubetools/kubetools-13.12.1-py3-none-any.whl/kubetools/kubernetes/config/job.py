0eeea7e8536e1907b3fc2507bbab4a1b900f9d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:46 +0000

    Add package kubetools/13.12.1/kubetools-13.12.1-py3-none-any.whl

---
+++ packages/kubetools/kubetools-13.12.1-py3-none-any.whl/kubetools/kubernetes/config/job.py
@@ -0,0 +1,106 @@
+import shlex
+from uuid import uuid4
+
+from .container import make_container_config
+from .util import copy_and_update
+
+
+def make_job_config(
+    config,
+    app_name=None,
+    labels=None,
+    annotations=None,
+    envvars=None,
+    job_name=None,
+    container_name="upgrade",
+):
+    '''
+    Builds a Kubernetes job configuration dict.
+    '''
+
+    # We want a copy of these because we'll modify them below
+    labels = labels or {}
+    envvars = envvars or {}
+    annotations = annotations or {}
+
+    # Generate name
+    job_id = str(uuid4())
+    if job_name is None:
+        job_name = job_id
+
+    # Attach the ID to labels
+    labels = copy_and_update(labels, {
+        'job-id': job_id,
+    })
+
+    # Figure out the command
+    command = config['command']
+
+    if isinstance(command, str):
+        command = shlex.split(command)
+
+    # Get/create description
+    description = config.get('description', 'Run: {0}'.format(command))
+
+    # Attach description to annotations
+    annotations = copy_and_update(annotations, {
+        'description': description,
+    })
+
+    # Update global envvars with job specific ones
+    envvars = copy_and_update(
+        envvars,
+        config.get('envars'),  # legacy support TODO: remove!
+        config.get('envvars'),
+        {'KUBE_JOB_ID': job_id},
+    )
+
+    # Make our container
+    container = make_container_config(
+        job_id,
+        {
+            'name': container_name,
+            'command': command,
+            'image': config['image'],
+            'chdir': config.get('chdir', '/'),
+            'resources': config.get('resources', {}),
+        },
+        envvars=envvars,
+        labels=labels,
+        annotations=annotations,
+    )
+
+    # Completions default to 1, same as Kubernetes
+    completions = config.get('completions', 1)
+    # Parallelism defaults to completions, also as Kubernetes
+    parallelism = config.get('parallelism', completions)
+
+    job_config = {
+        # Normal Kubernetes job config
+        'apiVersion': 'batch/v1',
+        'kind': 'Job',
+        'metadata': {
+            'name': job_name,
+            'labels': labels,
+            'annotations': annotations,
+        },
+        'spec': {
+            'completions': completions,
+            'parallelism': parallelism,
+            'selector': labels,
+            'template': {
+                'metadata': {
+                    'labels': labels,
+                },
+                'spec': {
+                    'restartPolicy': 'Never',
+                    'containers': [container],
+                },
+            },
+        },
+    }
+
+    if 'ttl_seconds_after_finished' in config:
+        job_config['spec']['ttlSecondsAfterFinished'] = config.get('ttl_seconds_after_finished')
+
+    return job_config
