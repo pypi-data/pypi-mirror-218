ff1873ba70efb01a2a859af862408426988ba751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:31 +0000

    Add package HemanDistributions/0.1/HemanDistributions-0.1.tar.gz

---
+++ packages/HemanDistributions/HemanDistributions-0.1.tar.gz/HemanDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='HemanDistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['HemanDistributions'],
+      zip_safe=False)
