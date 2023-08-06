3c35e4af587f5375bb706987791deea4c5ea1358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:52 +0000

    Add package GameWidgets/0.1.8/GameWidgets-0.1.8-py3-none-any.whl

---
+++ packages/GameWidgets/GameWidgets-0.1.8-py3-none-any.whl/GameWidgets/__init__.py
@@ -0,0 +1,32 @@
+'''try:
+    from GameWidgets.Widgets import *
+except:
+    print('GameWidgets.Widgets File has been edited, moved, or deleted')
+try:
+    from GameWidgets.GameWidgets import *
+except:
+    print('GameWidgets.GameWidgets File has been edited, moved, or deleted')
+try:
+    from GameWidgets.SetUp import *
+except:
+    print('GameWidgets.SetUp File has been edited, moved, or deleted')'''
+from GameWidgets import Constants
+print('GameWidgets Active!')
+__author__='Manomay Tyagi'
+__credits__='Tyagi Family'
+'''__Widgets__=Automatic Import:
+Btn
+Clock
+Colors
+Sound
+Cursor
+DialogeBox
+ScreenCommands
+Animatrix
+Draw
+Required Imports:
+Sprite
+Tile
+Hat
+JoyStick
+R1_R2'''
