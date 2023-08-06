d498ba6db676c1ee74beba398d3fff9b78b60c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:34 +0000

    Add package flet-lite/1.7/flet_lite-1.7.tar.gz

---
+++ packages/flet-lite/flet_lite-1.7.tar.gz/flet_lite-1.7/flet/utils/all_props_posible.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+def all_posible_props ():
+    return [
+        "value",
+        "color",
+        "bgcolor",
+        "font_family",
+        "italic",
+        "max_lines",
+        "no_wrap",
+        "selectable",
+        "semantics_label",
+        "size", "spans", "style",
+        "value", "weight", "autofocus",
+        "border_width", "can_reveal_password", "counter_text",
+        "cursor_color", "cursor_height", "cursor_radius", "cursor_width",
+        "dense", "error_style", "filled", "focused_bgcolor", "focused_border_color", 
+        "helper_style", "helper_text", "hint_text", "label", "max_length", "max_lines",
+        "min_lines", "multiline", "password", "read_only", "prefix_text", "text_size",
+        "expand", "disabled", "alignment", "width", "height", "border_radius", "text",
+        "scroll", "auto_scroll", "icon", "name", "src", "src_base64"
+    ]
