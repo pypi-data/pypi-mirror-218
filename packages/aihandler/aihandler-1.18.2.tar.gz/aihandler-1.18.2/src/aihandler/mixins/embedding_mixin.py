0fe72a0f650b5fc054055441c45b8a14f6bc4070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:18 +0000

    Add package aihandler/1.18.2/aihandler-1.18.2.tar.gz

---
+++ packages/aihandler/aihandler-1.18.2.tar.gz/aihandler-1.18.2/src/aihandler/mixins/embedding_mixin.py
@@ -0,0 +1,38 @@
+import os
+import torch
+from aihandler.logger import logger
+
+
+class EmbeddingMixin:
+    def load_learned_embed_in_clip(self):
+        learned_embeds_path = self.settings_manager.settings.embeddings_path.get()
+        if not os.path.exists(learned_embeds_path):
+            learned_embeds_path = os.path.join(self.model_base_path, "embeddings")
+        if self.embeds_loaded:
+            return
+        embeddings_not_supported = False
+        self.embeds_loaded = True
+        if os.path.exists(learned_embeds_path):
+            logger.info("Loading embeddings...")
+            try:
+                for f in os.listdir(learned_embeds_path):
+                    path = os.path.join(learned_embeds_path, f)
+                    words = f.split(".")
+                    if words[-1] in ["pt", "ckpt", "pth", "safetensors"]:
+                        words = words[:-1]
+                    token = ".".join(words).lower()
+                    try:
+                        self.pipe.load_textual_inversion(path, token=token, weight_name=f)
+                    except Exception as e:
+                        logger.warning("Failed to load " + path)
+                        logger.warning(e)
+
+            except AttributeError as e:
+                if "load_textual_inversion" in str(e):
+                    embeddings_not_supported = True
+                else:
+                    raise e
+            except RuntimeError as e:
+                embeddings_not_supported = True
+            if embeddings_not_supported:
+                logger.warning("Embeddings not supported in this model")
