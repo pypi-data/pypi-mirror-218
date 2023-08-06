6e733d1c67439776f9c353d22b9a87a32c81a947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:36 +0000

    Add package lfinancial/0.2.2/lfinancial-0.2.2-py3-none-any.whl

---
+++ packages/lfinancial/lfinancial-0.2.2-py3-none-any.whl/lfinancial/factory.py
@@ -0,0 +1,39 @@
+from lfinancial.generators.bank import BankGenerator
+from lfinancial.generators.contry import CountryGenerator
+from lfinancial.generators.currency import CurrencyGenerator
+from lfinancial.generators.document import IDCodeGenerator
+from lfinancial.generators.mail import EmailGenerator
+from lfinancial.generators.name import NameGenerator
+from lfinancial.generators.phone import PhoneGenerator
+from lfinancial.generators.swift import SwiftGenerator
+
+
+class GeneratorFactory:
+    def __init__(self):
+        self.generators = {
+            "SSN": IDCodeGenerator(),
+            "IDCard": IDCodeGenerator(),
+            "Passport": IDCodeGenerator(),
+            "NRIC": IDCodeGenerator(),
+            "MyNumber": IDCodeGenerator(),
+            "first_name": NameGenerator(),
+            "middle_name": NameGenerator(),
+            "last_name": NameGenerator(),
+            "cn_name": NameGenerator(),
+            "kana_name": NameGenerator(),
+            "cellphone": PhoneGenerator(),
+            "area_code": PhoneGenerator(),
+            "high_risk": CountryGenerator(),
+            "email": EmailGenerator(),
+            "bank": BankGenerator(),
+            "currency": CurrencyGenerator(),
+            "swift_code": SwiftGenerator(),
+        }
+
+    def register_generator(self, name, generator):
+        self.generators[name] = generator
+
+    def create_generator(self, name):
+        if name in self.generators:
+            return self.generators[name]
+        raise ValueError(f"Unsupported generator: {name}.")
