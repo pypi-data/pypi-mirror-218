91f4fc7e2ab124cab36342c6f5e328e1c13693ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:43 +0000

    Add package autotraders/1.6.2/autotraders-1.6.2-py3-none-any.whl

---
+++ packages/autotraders/autotraders-1.6.2-py3-none-any.whl/autotraders/shared_models/trait.py
@@ -0,0 +1,11 @@
+class Trait:
+    def __init__(self, data):
+        self.symbol: str = data["symbol"]
+        self.name: str = data["name"]
+        self.description: str = data["description"]
+
+    def __str__(self):
+        return self.symbol
+
+    def __eq__(self, other):
+        return self.symbol == str(other)
