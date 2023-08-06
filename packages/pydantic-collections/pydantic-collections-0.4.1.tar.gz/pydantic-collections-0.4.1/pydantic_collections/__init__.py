47e87ed36eb92db826cc39277cae8009745684ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:51 +0000

    Add package pydantic-collections/0.4.1/pydantic-collections-0.4.1.tar.gz

---
+++ packages/pydantic-collections/pydantic-collections-0.4.1.tar.gz/pydantic-collections-0.4.1/pydantic_collections/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'pydantic-collections'
+__version__ = '0.4.1'
+
+from ._base_collection_model import BaseCollectionModel
+
+__all__ = (
+    '__title__',
+    '__version__',
+    'BaseCollectionModel',
+)
