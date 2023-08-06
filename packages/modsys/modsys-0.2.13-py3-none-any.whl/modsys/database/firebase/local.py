be8fd1900f952b6705dd08596d920009ea89e569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:15 +0000

    Add package modsys/0.2.13/modsys-0.2.13-py3-none-any.whl

---
+++ packages/modsys/modsys-0.2.13-py3-none-any.whl/modsys/database/firebase/local.py
@@ -0,0 +1,50 @@
+# Copyright 2023 Apollo API, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from modsys.database.firebase.base import AbstractFirebaseClient
+
+
+class FakeFirebaseClient(AbstractFirebaseClient):
+    def execute(self, nosql):
+        pass
+
+    def init_with_service_account(self):
+        """
+        Initialize the Firestore DB client using a service account
+        :param file_path: path to service account
+        :return: firestore
+        """
+
+    def init_with_project_id(self):
+        """
+        Initialize the Firestore DB client using a GCP project ID
+        :param project_id: The GCP project ID
+        :return: firestore
+        """
+
+    def init_with_database_url(self):
+        pass
+
+    def close():
+        pass
+
+    def execute(self, nosql):
+        pass
+
+    def execute(self, collection_name):
+        pass
+
+    def list_tables(self):
+        pass
