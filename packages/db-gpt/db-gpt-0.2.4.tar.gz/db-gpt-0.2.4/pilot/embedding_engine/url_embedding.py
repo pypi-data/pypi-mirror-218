5ccbb6a0140961fb7a51550b7f365bf67404aa97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:45 +0000

    Add package db-gpt/0.2.4/db-gpt-0.2.4.tar.gz

---
+++ packages/db-gpt/db-gpt-0.2.4.tar.gz/db-gpt-0.2.4/pilot/embedding_engine/url_embedding.py
@@ -0,0 +1,50 @@
+from typing import List, Optional
+
+from bs4 import BeautifulSoup
+from langchain.document_loaders import WebBaseLoader
+from langchain.schema import Document
+from langchain.text_splitter import SpacyTextSplitter, RecursiveCharacterTextSplitter, TextSplitter
+
+from pilot.embedding_engine import SourceEmbedding, register
+
+
+
+class URLEmbedding(SourceEmbedding):
+    """url embedding for read url document."""
+
+    def __init__(self, file_path, vector_store_config, text_splitter: Optional[TextSplitter] = None):
+        """Initialize url word path."""
+        super().__init__(file_path, vector_store_config, text_splitter=None)
+        self.file_path = file_path
+        self.vector_store_config = vector_store_config
+        self.text_splitter = text_splitter or None
+
+    @register
+    def read(self):
+        """Load from url path."""
+        loader = WebBaseLoader(web_path=self.file_path)
+        if self.text_splitter is None:
+            try:
+                self.text_splitter = SpacyTextSplitter(
+                    pipeline="zh_core_web_sm",
+                    chunk_size=100,
+                    chunk_overlap=100,
+                )
+            except Exception:
+                self.text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=100, chunk_overlap=50
+                )
+
+        return loader.load_and_split(self.text_splitter)
+
+    @register
+    def data_process(self, documents: List[Document]):
+        i = 0
+        for d in documents:
+            content = d.page_content.replace("\n", "")
+            soup = BeautifulSoup(content, "html.parser")
+            for tag in soup(["!doctype", "meta"]):
+                tag.extract()
+            documents[i].page_content = soup.get_text()
+            i += 1
+        return documents
