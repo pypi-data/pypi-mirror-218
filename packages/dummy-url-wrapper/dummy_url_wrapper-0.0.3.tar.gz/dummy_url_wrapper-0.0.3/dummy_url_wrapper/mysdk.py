8ace2322139b7c5cb1bc92ec4e36645d9fac030c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:19 +0000

    Add package dummy-url-wrapper/0.0.3/dummy_url_wrapper-0.0.3.tar.gz

---
+++ packages/dummy-url-wrapper/dummy_url_wrapper-0.0.3.tar.gz/dummy_url_wrapper-0.0.3/dummy_url_wrapper/mysdk.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+test_url = os.getenv("TEST_URL")
+
+
+class URLWrapperClass:
+    def __init__(self, env_path=None):
+        load_dotenv()
+
+    def wrap_url(config, payload):
+        config_data = config
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "None",
+        }
+        requestOptions = {
+            "method": "GET",
+            "headers": headers,
+        }
+
+        response = requests.get(test_url, **requestOptions)
+        response.raise_for_status()
+        response_data = {
+            "response": response,
+            "config": config_data,
+            "payload": payload,
+        }
+        return response_data
