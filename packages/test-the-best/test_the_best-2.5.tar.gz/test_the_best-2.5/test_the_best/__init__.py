9eb0127b04446c73efc76ff50666487b15244a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:34 +0000

    Add package test-the-best/2.5/test_the_best-2.5.tar.gz

---
+++ packages/test-the-best/test_the_best-2.5.tar.gz/test_the_best-2.5/test_the_best/__init__.py
@@ -0,0 +1,30 @@
+from time import sleep
+from sys import stdout
+
+def test(x = None):
+  JUMP_LEFT_SEQ = '\u001b[100D'
+  DELAY = 0.05
+  for i in range(0, 101):
+    sleep(DELAY)
+    print(JUMP_LEFT_SEQ, end='')
+    print(f'Progress: {i:0>3}%', end='')
+    stdout.flush()
+    print()
+  y = "Тест пройден?"
+  if x == y:
+    x = "Пройден!"
+    return x
+  if x != None:
+    return x
+  else:
+    x = "Тест успешно пройден!"
+    return x
+
+def loading(DELAY):
+    JUMP_LEFT_SEQ = '\u001b[100D'
+    for i in range(0, 101):
+        sleep(DELAY)
+        print(JUMP_LEFT_SEQ, end='')
+        print(f'Progress: {i:0>3}%', end='')
+        stdout.flush()
+    return
