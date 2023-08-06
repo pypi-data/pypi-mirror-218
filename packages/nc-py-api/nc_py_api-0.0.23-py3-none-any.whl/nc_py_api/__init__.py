8ef1aad55bdd58f78b4776e8dee857ec9ba4cc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:24 +0000

    Add package nc-py-api/0.0.23/nc_py_api-0.0.23-py3-none-any.whl

---
+++ packages/nc-py-api/nc_py_api-0.0.23-py3-none-any.whl/nc_py_api/__init__.py
@@ -0,0 +1,15 @@
+"""
+Import all possible stuff that can be used.
+"""
+
+from .constants import ApiScope, LogLvl
+from .exceptions import NextcloudException, check_error
+from .files import FsNodeInfo
+from .integration_fastapi import (
+    enable_heartbeat,
+    nc_app,
+    set_enabled_handler,
+    set_scopes,
+)
+from .nextcloud import Nextcloud, NextcloudApp
+from .ui_files_actions_menu import UiActionFileInfo, UiFileActionHandlerInfo
