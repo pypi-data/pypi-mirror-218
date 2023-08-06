11d4021375d7bd774980bc93d1b27a7b6d544bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package nip-config/0.10.2/nip-config-0.10.2.tar.gz

---
+++ packages/nip-config/nip-config-0.10.2.tar.gz/nip-config-0.10.2/tests/test_multi.py
@@ -0,0 +1,13 @@
+# import time
+# from multiprocessing import Pool
+#
+#
+# def f(n):
+#     # time.sleep(n)
+#     return n
+#
+#
+# pool = Pool(3)
+# result = pool.imap(f, (3, 1, 2))
+# # result.
+# print([res for res in result])
