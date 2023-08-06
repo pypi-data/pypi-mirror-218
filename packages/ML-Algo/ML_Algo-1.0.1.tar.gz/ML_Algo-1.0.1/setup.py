087da7541fac3eed1175b6bbbde1ed717381d063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:33 +0000

    Add package ML-Algo/1.0.1/ML_Algo-1.0.1.tar.gz

---
+++ packages/ML-Algo/ML_Algo-1.0.1.tar.gz/ML_Algo-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='ML_Algo',
+    version='1.0.1',
+    author='Mr Raj',
+    author_email='arunraj14092002@gmail.com',
+    description='A package for calculating moving averages',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['pandas'],
+)
