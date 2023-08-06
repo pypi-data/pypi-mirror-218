81f750f8898b58bad20e999d1acb73cad9cc4acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:34 +0000

    Add package golfy/1.5.1/golfy-1.5.1.tar.gz

---
+++ packages/golfy/golfy-1.5.1.tar.gz/golfy-1.5.1/golfy/__init__.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+from .initialization import init
+from .optimization import optimize
+from .solution import Solution
+from .validity import is_valid, count_violations, violations_per_replicate
+
+__version__ = "1.5.1"
+
+__all__ = [
+    "init",
+    "optimize",
+    "count_violations",
+    "is_valid",
+    "violations_per_replicate",
+    "__version__",
+]
