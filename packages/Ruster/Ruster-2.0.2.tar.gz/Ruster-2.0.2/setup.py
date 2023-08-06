166b901fbdfbe7a75e0180c69fabc2100d17614a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:56 +0000

    Add package Ruster/2.0.2/Ruster-2.0.2.tar.gz

---
+++ packages/Ruster/Ruster-2.0.2.tar.gz/Ruster-2.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Ruster',
+    version='2.0.2',
+    author='Pawan Kumar',
+    author_email='control@vvfin.in',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        "werkzueg",
+        "uuid",
+        "rustadmin",
+        "cryptography",
+        "pyjwt",
+        "jinja2",
+        "itsdangerous",
+        "nexusdb"
+    ],
+    entry_points={
+        'console_scripts': [
+            'ruster = ruster.app',
+        ],
+    },
+)
