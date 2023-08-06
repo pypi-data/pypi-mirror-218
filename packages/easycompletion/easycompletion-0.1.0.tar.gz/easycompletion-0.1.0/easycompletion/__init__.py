32c557d1e9242e45d1525d8243a2410eb0d40296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:29 +0000

    Add package easycompletion/0.1.0/easycompletion-0.1.0.tar.gz

---
+++ packages/easycompletion/easycompletion-0.1.0.tar.gz/easycompletion-0.1.0/easycompletion/__init__.py
@@ -0,0 +1,9 @@
+"""
+easycompletion
+
+Leveraging conversational AI for bicameral decision making.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Moon (https://github.com/lalalune)'
+__credits__ = 'https://github.com/lalalune/easycompletion'
