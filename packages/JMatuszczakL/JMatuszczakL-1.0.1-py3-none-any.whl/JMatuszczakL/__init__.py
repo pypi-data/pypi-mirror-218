b033ab37be782d120335e973e52543a4ed1e30cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:27 +0000

    Add package JMatuszczakL/1.0.1/JMatuszczakL-1.0.1-py3-none-any.whl

---
+++ packages/JMatuszczakL/JMatuszczakL-1.0.1-py3-none-any.whl/JMatuszczakL/__init__.py
@@ -0,0 +1,32 @@
+# jm_library.py
+
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+
+class JM:
+    @staticmethod
+    def CreateWindow(app_title, calculate_text, calculate_function):
+        app = QApplication(sys.argv)
+        window = QWidget()
+        window.setWindowTitle(app_title)
+
+        layout = QVBoxLayout()
+
+        input_label = QLabel("Input:")
+        input_field = QLineEdit()
+        layout.addWidget(input_label)
+        layout.addWidget(input_field)
+
+        output_label = QLabel("Output:")
+        output_field = QLabel()
+        layout.addWidget(output_label)
+        layout.addWidget(output_field)
+
+        calculate_button = QPushButton(calculate_text)
+        calculate_button.clicked.connect(lambda: JM.calculate(input_field, output_field, calculate_function))
+        layout.addWidget(calculate_button)
+
+        window.setLayout(layout)
+        window.show()
+
+        sys.exit(app.exec())
