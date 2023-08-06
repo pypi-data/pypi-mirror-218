5f07f21a4feae8d64c902a94f535dcf485b215ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package f3dasm-optimize/1.0.0/f3dasm_optimize-1.0.0.tar.gz

---
+++ packages/f3dasm-optimize/f3dasm_optimize-1.0.0.tar.gz/f3dasm_optimize-1.0.0/src/f3dasm_optimize/__init__.py
@@ -0,0 +1,75 @@
+"""
+Some API information about the opitmizers
+"""
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+import sys
+from itertools import chain
+from os import path
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+# Local
+from ._imports import _IntegrationModule
+
+if TYPE_CHECKING:
+    from ._all_optimizers import OPTIMIZERS
+    from ._version import __version__
+    from .adam import Adam, Adam_Parameters
+    from .adamax import Adamax, Adamax_Parameters
+    from .bayesianoptimization import (BayesianOptimization,
+                                       BayesianOptimization_Parameters)
+    from .cmaes import CMAES, CMAES_Parameters
+    from .differentialevolution import (DifferentialEvolution,
+                                        DifferentialEvolution_Parameters)
+    from .ftrl import Ftrl, Ftrl_Parameters
+    from .nadam import Nadam, Nadam_Parameters
+    from .pso import PSO, PSO_Parameters
+    from .rmsprop import RMSprop, RMSprop_Parameters
+    from .sade import SADE, SADE_Parameters
+    from .sea import SEA, SEA_Parameters
+    from .sga import SGA, SGA_Parameters
+    from .sgd import SGD, SGD_Parameters
+    from .simulatedannealing import (SimulatedAnnealing,
+                                     SimulatedAnnealing_Parameters)
+    from .xnes import XNES, XNES_Parameters
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+_import_structure: dict = {
+    "adam": ["Adam", "Adam_Parameters"],
+    "adamax": ["Adamax", "Adamax_Parameters"],
+    "bayesianoptimization": ["BayesianOptimization", "BayesianOptimization_Parameters"],
+    "cmaes": ["CMAES", "CMAES_Parameters"],
+    "differentialevolution": ["DifferentialEvolution", "DifferentialEvolution_Parameters"],
+    "ftrl": ["Ftrl", "Ftrl_Parameters"],
+    "nadam": ["Nadam", "Nadam_Parameters"],
+    "pso": ["PSO", "PSO_Parameters"],
+    "rmsprop": ["RMSprop", "RMSprop_Parameters"],
+    "sade": ["SADE", "SADE_Parameters"],
+    "sea": ["SEA", "SEA_Parameters"],
+    "sga": ["SGA", "SGA_Parameters"],
+    "sgd": ["SGD", "SGD_Parameters"],
+    "simulatedannealing": ["SimulatedAnnealing", "SimulatedAnnealing_Parameters"],
+    "xnes": ["XNES", "XNES_Parameters"],
+    "_all_optimizers": ["OPTIMIZERS"],
+    "_version": ["__version__"],
+}
+
+if not TYPE_CHECKING:
+    class _LocalIntegrationModule(_IntegrationModule):
+        __file__ = globals()["__file__"]
+        __path__ = [path.dirname(__file__)]
+        __all__ = list(chain.from_iterable(_import_structure.values()))
+        _import_structure = _import_structure
+
+    sys.modules[__name__] = _LocalIntegrationModule(__name__)
