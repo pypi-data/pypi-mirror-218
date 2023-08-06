4bc96b4c3ba4287a44f4379ee8343bbd95c91706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package plugen/0.8/plugen-0.8.tar.gz

---
+++ packages/plugen/plugen-0.8.tar.gz/plugen-0.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'plugen = main',
+        ],
+    },
+    name='plugen',
+    version='0.8',
+    license='MIT',
+    author="Clarembeau Alexis",
+    author_email='alexis.clarembeau@gmail.com',
+    packages=find_packages('./'),
+    package_dir={'': './'},
+    url='https://github.com/aclarembeau/plugen',
+    keywords='static website generator',
+    install_requires=[],
+)
