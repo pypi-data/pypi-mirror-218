a523b3ed7ca8b4356d100047b53e8101dbaec33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:17 +0000

    Add package datumaro/1.4.0rc1/datumaro-1.4.0rc1-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/datumaro/datumaro-1.4.0rc1-cp39-cp39-macosx_10_9_x86_64.whl/datumaro/__init__.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2019-2023 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from . import errors as errors
+from . import ops as ops
+from . import project as project
+from .components.algorithms import RISE, LossDynamicsAnalyzer
+from .components.annotation import (
+    NO_GROUP,
+    Annotation,
+    AnnotationType,
+    Bbox,
+    BinaryMaskImage,
+    Caption,
+    Categories,
+    Colormap,
+    CompiledMask,
+    CompiledMaskImage,
+    Cuboid3d,
+    Ellipse,
+    IndexMaskImage,
+    Label,
+    LabelCategories,
+    Mask,
+    MaskCategories,
+    Points,
+    PointsCategories,
+    Polygon,
+    PolyLine,
+    RgbColor,
+    RleMask,
+)
+from .components.cli_plugin import CliPlugin
+from .components.dataset import Dataset, eager_mode
+from .components.dataset_base import (
+    DEFAULT_SUBSET_NAME,
+    CategoriesInfo,
+    DatasetBase,
+    DatasetItem,
+    IDataset,
+    SubsetBase,
+)
+from .components.environment import Environment, PluginRegistry
+from .components.exporter import Exporter, ExportErrorPolicy, FailingExportErrorPolicy
+from .components.hl_ops import HLOps
+from .components.importer import FailingImportErrorPolicy, Importer, ImportErrorPolicy
+from .components.launcher import Launcher
+from .components.media import ByteImage, Image, MediaElement, Video, VideoFrame
+from .components.media_manager import MediaManager
+from .components.progress_reporting import (
+    NullProgressReporter,
+    ProgressReporter,
+    SimpleProgressReporter,
+    TQDMProgressReporter,
+)
+from .components.transformer import ItemTransform, ModelTransform, Transform
+from .components.validator import Validator
+from .components.visualizer import Visualizer
+from .version import __version__
