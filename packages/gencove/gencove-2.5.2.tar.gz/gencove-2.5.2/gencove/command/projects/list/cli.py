a0dd873e0fcb855d12f100b1976ef0d693a02481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:24 +0000

    Add package gencove/2.5.2/gencove-2.5.2.tar.gz

---
+++ packages/gencove/gencove-2.5.2.tar.gz/gencove-2.5.2/gencove/command/projects/list/cli.py
@@ -0,0 +1,17 @@
+"""List projects shell command definition."""
+import click
+
+from gencove.command.common_cli_options import add_options, common_options
+from gencove.constants import Credentials, Optionals
+
+from .main import List
+
+
+@click.command("list")
+@add_options(common_options)
+def list_projects(host, email, password, api_key):
+    """List your projects."""
+    List(
+        Credentials(email=email, password=password, api_key=api_key),
+        Optionals(host=host),
+    ).run()
