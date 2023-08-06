68f965ef34e31bbb44bf1d4915948b1fbe2f7ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:40 +0000

    Add package benimang/0.3.8/benimang-0.3.8.tar.gz

---
+++ packages/benimang/benimang-0.3.8.tar.gz/benimang-0.3.8/beni/btype.py
@@ -0,0 +1,8 @@
+from typing import Any, Callable, Coroutine, TypeVar, cast
+
+Func = TypeVar("Func", bound=Callable[..., Any])
+AsyncFunc = TypeVar("AsyncFunc", bound=Callable[..., Coroutine[Any, Any, Any]])
+AnyType = TypeVar("AnyType")
+IntFloatStr = TypeVar("IntFloatStr", int, float, str)
+Null = cast(Any, None)
+Function = Callable[..., Any]
