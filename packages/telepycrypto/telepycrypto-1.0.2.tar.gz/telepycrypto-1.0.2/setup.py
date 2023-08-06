77c4a9ab34f897b7b15053a0d8045df6acc740b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:04 +0000

    Add package telepycrypto/1.0.2/telepycrypto-1.0.2.tar.gz

---
+++ packages/telepycrypto/telepycrypto-1.0.2.tar.gz/telepycrypto-1.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='telepycrypto',
+	version='1.0.2',
+	author='Redpiar',
+	author_email='Regeonwix@gmail.com',
+	description='soon',
+	packages=['telepycrypto'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
