679e24cb9c4dc01f413ca09c7e46c533934ac700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:31 +0000

    Add package hat-monitor/0.7.2/hat_monitor-0.7.2-cp310.cp311-none-any.whl

---
+++ packages/hat-monitor/hat_monitor-0.7.2-cp310.cp311-none-any.whl/hat/monitor/server/main.py
@@ -0,0 +1,126 @@
+"""Monitor server main"""
+
+from pathlib import Path
+import argparse
+import asyncio
+import contextlib
+import logging.config
+import sys
+
+import appdirs
+
+from hat import aio
+from hat import json
+
+from hat.monitor.server import common
+import hat.monitor.server.master
+import hat.monitor.server.server
+import hat.monitor.server.slave
+import hat.monitor.server.ui
+
+
+mlog: logging.Logger = logging.getLogger('hat.monitor.server.main')
+"""Module logger"""
+
+user_conf_dir: Path = Path(appdirs.user_config_dir('hat'))
+"""User configuration directory path"""
+
+
+def create_argument_parser() -> argparse.ArgumentParser:
+    """Create argument parser"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--conf', metavar='PATH', type=Path, default=None,
+        help="configuration defined by hat-monitor://main.yaml# "
+             "(default $XDG_CONFIG_HOME/hat/monitor.{yaml|yml|json})")
+    return parser
+
+
+def main():
+    """Monitor Server"""
+    parser = create_argument_parser()
+    args = parser.parse_args()
+
+    conf_path = args.conf
+    if not conf_path:
+        for suffix in ('.yaml', '.yml', '.json'):
+            conf_path = (user_conf_dir / 'monitor').with_suffix(suffix)
+            if conf_path.exists():
+                break
+
+    if conf_path == Path('-'):
+        conf = json.decode_stream(sys.stdin)
+    else:
+        conf = json.decode_file(conf_path)
+
+    sync_main(conf)
+
+
+def sync_main(conf: json.Data):
+    """Sync main entry point"""
+    aio.init_asyncio()
+
+    common.json_schema_repo.validate('hat-monitor://main.yaml#', conf)
+
+    logging.config.dictConfig(conf['log'])
+
+    with contextlib.suppress(asyncio.CancelledError):
+        aio.run_asyncio(async_main(conf))
+
+
+async def async_main(conf: json.Data):
+    """Async main entry point"""
+    async_group = aio.Group()
+    server = None
+    master = None
+    ui = None
+    slave_runner = None
+
+    async def cleanup():
+        if ui:
+            await ui.async_close()
+        if server:
+            await server.async_close()
+        if slave_runner:
+            await slave_runner.async_close()
+        if master:
+            await master.async_close()
+        await async_group.async_close()
+        await asyncio.sleep(0.1)
+
+    try:
+        mlog.debug('starting server')
+        server = await hat.monitor.server.server.create(conf['server'])
+        async_group.spawn(aio.call_on_done, server.wait_closing(),
+                          async_group.close)
+
+        mlog.debug('starting master')
+        master = await hat.monitor.server.master.create(conf['master'])
+        async_group.spawn(aio.call_on_done, master.wait_closing(),
+                          async_group.close)
+
+        mlog.debug('starting ui')
+        ui = await hat.monitor.server.ui.create(conf['ui'], server)
+        async_group.spawn(aio.call_on_done, ui.wait_closing(),
+                          async_group.close)
+
+        mlog.debug('starting slave runner')
+        slave_runner = hat.monitor.server.slave.Runner(conf['slave'], server,
+                                                       master)
+        async_group.spawn(aio.call_on_done, slave_runner.wait_closing(),
+                          async_group.close)
+
+        mlog.debug('monitor started')
+        await async_group.wait_closing()
+
+    except Exception as e:
+        mlog.warning('async main error: %s', e, exc_info=e)
+
+    finally:
+        mlog.debug('stopping monitor')
+        await aio.uncancellable(cleanup())
+
+
+if __name__ == '__main__':
+    sys.argv[0] = 'hat-monitor'
+    sys.exit(main())
---
+++ packages/hat-monitor/hat_monitor-0.7.2-cp310.cp311-none-any.whl/hat/monitor/server/ui.py
@@ -0,0 +1,112 @@
+"""Implementation of web server UI"""
+
+import contextlib
+import functools
+import importlib.resources
+import logging
+import urllib
+
+from hat import aio
+from hat import json
+from hat import juggler
+import hat.monitor.server.server
+
+
+mlog: logging.Logger = logging.getLogger(__name__)
+"""Module logger"""
+
+autoflush_delay: float = 0.2
+"""Juggler autoflush delay"""
+
+
+async def create(conf: json.Data,
+                 server: hat.monitor.server.server.Server
+                 ) -> 'WebServer':
+    """Create user interface
+
+    Args:
+        conf: configuration defined by
+            ``hat-monitor://main.yaml#/definitions/ui``
+        server: local monitor server
+
+    """
+    srv = WebServer()
+    srv._server = server
+
+    exit_stack = contextlib.ExitStack()
+    try:
+        ui_path = exit_stack.enter_context(
+            importlib.resources.as_file(
+                importlib.resources.files(__package__) / 'ui'))
+
+        state = json.Storage()
+        update_state = functools.partial(_update_state, state, server)
+        exit_stack.enter_context(server.register_change_cb(update_state))
+        update_state()
+
+        addr = urllib.parse.urlparse(conf['address'])
+        srv._srv = await juggler.listen(host=addr.hostname,
+                                        port=addr.port,
+                                        request_cb=srv._on_request,
+                                        static_dir=ui_path,
+                                        autoflush_delay=autoflush_delay,
+                                        state=state)
+
+        try:
+            srv.async_group.spawn(aio.call_on_cancel, exit_stack.close)
+
+        except BaseException:
+            await aio.uncancellable(srv.async_close())
+            raise
+
+    except BaseException:
+        exit_stack.close()
+        raise
+
+    mlog.debug("web server listening on %s", conf['address'])
+    return srv
+
+
+class WebServer(aio.Resource):
+    """WebServer
+
+    For creating new instance of this class see `create` coroutine.
+
+    """
+
+    @property
+    def async_group(self) -> aio.Group:
+        """Async group"""
+        return self._srv.async_group
+
+    async def _on_request(self, conn, name, data):
+        if name == 'set_rank':
+            mlog.debug("received set_rank request")
+            self._server.set_rank(data['cid'], data['rank'])
+
+        else:
+            raise Exception('received invalid message type')
+
+
+def _update_state(state, server):
+    local_components = [{'cid': i.cid,
+                         'name': i.name,
+                         'group': i.group,
+                         'data': i.data,
+                         'rank': i.rank}
+                        for i in server.local_components]
+    global_components = [
+        {'cid': i.cid,
+         'mid': i.mid,
+         'name': i.name,
+         'group': i.group,
+         'data': i.data,
+         'rank': i.rank,
+         'blessing_req': {'token': i.blessing_req.token,
+                          'timestamp': i.blessing_req.timestamp},
+         'blessing_res': {'token': i.blessing_res.token,
+                          'ready': i.blessing_res.ready}}
+        for i in server.global_components]
+    state.set([], {'mid': server.mid,
+                   'local_components': local_components,
+                   'global_components': global_components})
