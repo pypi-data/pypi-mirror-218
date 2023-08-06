524cf3dcc74757ffb84f6d15c1bc0fe9053c7ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package pd-data-store/0.1/pd_data_store-0.1.tar.gz

---
+++ packages/pd-data-store/pd_data_store-0.1.tar.gz/pd_data_store-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pd_data_store",
+    version="0.1",
+    packages=find_packages(),
+    author="lml",
+    description="A simple package for password-based data encryption/decryption",
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+)
