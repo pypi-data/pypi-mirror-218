978c95dc73d7ffa272b796b0b0b70e4c61b2b979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:26 +0000

    Add package napari-script-editor/0.2.10/napari_script_editor-0.2.10-py3-none-any.whl

---
+++ packages/napari-script-editor/napari_script_editor-0.2.10-py3-none-any.whl/napari_script_editor/_scripts_directory.py
@@ -0,0 +1,63 @@
+import os
+from napari_tools_menu import register_action
+
+CODE_TEMPLATE = """# This is a template script for the napari-script-editor.
+# See this folder for more examples:
+# https://github.com/haesleinhuepf/napari-script-editor/tree/main/example_scripts
+import napari
+if 'viewer' not in globals():
+    viewer = napari.Viewer()
+print("Hello world, napari has", len(viewer.layers), "layers")
+"""
+
+def _init_scripts_directory():
+    """
+    Sets up the <home>/.napari-scripts folder. If it doesn't exist, it puts a template python file there.
+    """
+    home_dir = os.path.expanduser('~')
+    sub_dir = "/.napari-scripts/"
+    scripts_dir = home_dir + sub_dir
+
+    if not os.path.isdir(scripts_dir):
+        os.mkdir(scripts_dir)
+
+    if not os.path.isdir(scripts_dir + _new_template_filename()):
+        with open(scripts_dir + _new_template_filename(), 'w') as f:
+            f.write(CODE_TEMPLATE)
+
+    return scripts_dir
+
+def _new_template_filename():
+    return '.new_template.py'
+
+def _search_scripts():
+    """
+    Searches scripts in the <home>/.napari-scripts folder and attaches them to the Tools > Scripts menu as actions
+    """
+    file_list = os.listdir(_init_scripts_directory())
+    files = [file for file in file_list if file.endswith(".py") and not file.startswith(".")]
+
+    for i in files:
+        _register_menu(_init_scripts_directory() + i)
+
+def _register_menu(filename):
+    """
+    Adds a specific python file to the Tools > Scripts menu
+    """
+    with open(filename) as f:
+        lines = f.readlines()
+    code = "\n".join(lines)
+
+    menu = filename.replace("\\", "/").split("/")[-1].replace("_", " ").replace(".py", "")
+
+    def callback(viewer):
+        _exec_code(code, viewer)
+
+    register_action(callback, "Scripts > " + menu)
+
+def _exec_code(code, viewer):
+    """
+    Executes given code and provides the viewer as global variable to that code.
+    """
+    globs = {"viewer": viewer}
+    exec(code, globs)
---
+++ packages/napari-script-editor/napari_script_editor-0.2.10-py3-none-any.whl/napari_script_editor/_tests/test_dock_widget.py
@@ -0,0 +1,29 @@
+from napari_script_editor import ScriptEditor
+import pytest
+
+def test_something_with_viewer(make_napari_viewer):
+    viewer = make_napari_viewer()
+
+
+    script_editor = ScriptEditor.get_script_editor_from_viewer(viewer, _for_testing=True)
+
+    num_dw = len(viewer.window._dock_widgets)
+
+    script_editor = ScriptEditor(viewer, _for_testing = True)
+    viewer.window.add_dock_widget(
+        script_editor
+    )
+    assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    script_editor._on_new_click()
+    script_editor._on_save_click(filename="test.py")
+    script_editor._on_load_click(filename="test.py")
+    script_editor._on_run_click()
+    script_editor.set_code("print('hello world')")
+
+    script_editor = ScriptEditor.get_script_editor_from_viewer(viewer, _for_testing=True)
+
+def test_scripts_directory():
+    from napari_script_editor._scripts_directory import _init_scripts_directory, _search_scripts
+    _init_scripts_directory()
+    _search_scripts()
