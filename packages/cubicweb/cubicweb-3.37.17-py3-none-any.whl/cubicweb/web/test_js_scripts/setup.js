9269995c44fb9f10436e6cffce5c76ab3e23098b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:20 +0000

    Add package cubicweb/3.37.17/cubicweb-3.37.17-py3-none-any.whl

---
+++ packages/cubicweb/cubicweb-3.37.17-py3-none-any.whl/cubicweb/web/test_js_scripts/setup.js
@@ -0,0 +1,9 @@
+window.pageid = "TESTTESTTESTTESTTEST";
+window.BASE_URL = "";
+window.$ = window.jQuery = require("../data/jquery.js");
+
+require("../data/cubicweb.js");
+require("../data/cubicweb.htmlhelpers.js");
+require("../data/cubicweb.python.js");
+require("../data/cubicweb.compat.js");
+require("../data/cubicweb.ajax.js");
