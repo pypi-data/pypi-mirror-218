9aa0e0845f27e83d7c3ce5d353815e907ea0d5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:21 +0000

    Add package discoverable-garage-door/0.1.1/discoverable_garage_door-0.1.1.tar.gz

---
+++ packages/discoverable-garage-door/discoverable_garage_door-0.1.1.tar.gz/discoverable_garage_door-0.1.1/discoverable_garage_door/__main__.py
@@ -0,0 +1,30 @@
+import logging
+import logging.config
+import sys
+import signal
+from .util import logger
+
+from ha_mqtt_discoverable import Settings, __version__
+
+from discoverable_garage_door.config import Config, config
+from discoverable_garage_door.cover import CoverInfo, Cover, Settings
+
+print(__version__)
+sys.exit(0)
+mqtt_settings = Settings.MQTT(
+    host=config.mqtt_broker.host,
+    username=config.mqtt_broker.username,
+    password=config.mqtt_broker.password,
+    discovery_prefix=config.mqtt_broker.discovery_prefix,
+    state_prefix=config.mqtt_broker.state_prefix,
+)
+doors: [Cover] = []
+for door in config.gpio.doors:
+    doors.append(
+        Cover.cover(mqtt=mqtt_settings, gpio_config=config.gpio, door_config=door)
+    )
+
+try:
+    signal.pause()
+except KeyboardInterrupt:
+    print("\nStopping ...")
