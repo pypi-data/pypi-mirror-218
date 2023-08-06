ea9f0dd461ebc751b1e22a81ae17e1f60a5035ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:37 +0000

    Add package hmtaipy/1.0.1/hmtaipy-1.0.1.tar.gz

---
+++ packages/hmtaipy/hmtaipy-1.0.1.tar.gz/hmtaipy-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hmtaipy",
+    version="1.0.1",
+    license='MIT',
+    author="TheCuteOwl",
+    description="API Wrapper of hmtai NPM package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['requests'],
+    packages=setuptools.find_packages(),
+    scripts=['bin/hmtaipy.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
