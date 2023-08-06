dbe8d1759ba4f80a845df25eb46b85deb5b1b0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package threads-net/0.0.1/threads-net-0.0.1.tar.gz

---
+++ packages/threads-net/threads-net-0.0.1.tar.gz/threads-net-0.0.1/examples/get_post.py
@@ -0,0 +1,18 @@
+
+
+
+
+from threads import Threads
+threads = Threads()
+post = threads.get_post(id=3141055616164096839)
+print(post)
+
+user = threads.get_user(id=314216)
+print(user)
+
+user_threads = threads.get_user_threads(id=314216)
+print(user_threads)
+
+user_replies = threads.get_user_replies(id=314216)
+print(user_replies)
+
