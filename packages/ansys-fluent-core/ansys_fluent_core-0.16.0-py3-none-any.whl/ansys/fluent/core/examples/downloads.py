cfc9f41b55fb2d8234daf53dda3ac927cb394a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:47 +0000

    Add package ansys-fluent-core/0.16.0/ansys_fluent_core-0.16.0-py3-none-any.whl

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.16.0-py3-none-any.whl/ansys/fluent/core/examples/downloads.py
@@ -0,0 +1,78 @@
+"""Functions to download sample datasets from the PyAnsys data repository.
+
+Examples
+--------
+
+>>> from ansys.fluent.core import examples
+>>> filename = examples.download_file("bracket.iges", "geometry")
+>>> filename
+'/home/user/.local/share/ansys_fluent_core/examples/bracket.iges'
+"""
+import os
+import re
+import shutil
+from typing import Optional
+import urllib.request
+import zipfile
+
+import ansys.fluent.core as pyfluent
+
+
+def delete_downloads() -> bool:
+    """Delete all downloaded examples to free space or update the files."""
+    shutil.rmtree(pyfluent.EXAMPLES_PATH)
+    os.makedirs(pyfluent.EXAMPLES_PATH)
+    return True
+
+
+def _decompress(filename: str) -> None:
+    zip_ref = zipfile.ZipFile(filename, "r")
+    zip_ref.extractall(pyfluent.EXAMPLES_PATH)
+    return zip_ref.close()
+
+
+def _get_file_url(filename: str, directory: Optional[str] = None) -> str:
+    if directory:
+        return (
+            "https://github.com/ansys/example-data/raw/master/"
+            f"{directory}/{filename}"
+        )
+    return f"https://github.com/ansys/example-data/raw/master/{filename}"
+
+
+def _retrieve_file(url: str, filename: str, save_path: Optional[str] = None) -> str:
+    if save_path is None:
+        save_path = pyfluent.EXAMPLES_PATH
+    else:
+        save_path = os.path.abspath(save_path)
+    local_path = os.path.join(save_path, os.path.basename(filename))
+    local_path_no_zip = re.sub(".zip$", "", local_path)
+    # First check if file has already been downloaded
+    print("Checking if specified file already exists...")
+    if os.path.isfile(local_path_no_zip) or os.path.isdir(local_path_no_zip):
+        print(f"File already exists. File path:\n{local_path_no_zip}")
+        return local_path_no_zip
+
+    print("File does not exist. Downloading specified file...")
+
+    # Check if save path exists
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # grab the correct url retriever
+    urlretrieve = urllib.request.urlretrieve
+
+    # Perform download
+    saved_file, _ = urlretrieve(url, filename=local_path)
+    if local_path.endswith(".zip"):
+        _decompress(local_path)
+        local_path = local_path_no_zip
+    print(f"Download successful. File path:\n{local_path}")
+    return local_path
+
+
+def download_file(
+    filename: str, directory: Optional[str] = None, save_path: Optional[str] = None
+):
+    url = _get_file_url(filename, directory)
+    return _retrieve_file(url, filename, save_path)
