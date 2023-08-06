845b960fd5f045e2078564d33d2d1a4bd87e95fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package zein/0.0.10/zein-0.0.10.tar.gz

---
+++ packages/zein/zein-0.0.10.tar.gz/zein-0.0.10/zein/text/text_module.py
@@ -0,0 +1,19 @@
+# in main.py
+import sys
+from . import ar
+
+class TextFilter:
+    def __init__(self, lang='ar'):
+      self.lang = lang.lower()
+
+    def __getattr__(self, name):
+        # get the module that matches the lang attribute
+        module = globals()[self.lang]
+        # get the nested class from the module
+        subclass = getattr(module, self.lang)
+        # create an instance of the nested class
+        obj = subclass()
+        # get the function that matches the name argument
+        func = getattr(obj, name)
+        # return the function
+        return func
