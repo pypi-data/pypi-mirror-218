f173a85f937605f216c887cf398a7a1301b31b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:06 +0000

    Add package qvsed/1.4.1/qvsed-1.4.1.tar.gz

---
+++ packages/qvsed/qvsed-1.4.1.tar.gz/qvsed-1.4.1/qvsed/config_default.py
@@ -0,0 +1,15 @@
+# This config file serves as a template and should not be modified directly; please make any necessary changes in config.py.
+# pylint: disable=all
+
+# Font
+font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
+font_size = 11
+
+# Colours
+text_color = "#FFFFFF"
+background_color = "#282C34"
+button_color = "#393F4A"
+button_hover_color = "#31353F"
+button_pressed_color = "#282C34"
+text_area_color = "#31353F"
+echo_area_color = "#31353F"
