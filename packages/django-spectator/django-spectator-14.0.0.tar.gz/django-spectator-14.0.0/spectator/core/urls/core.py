97d412c538e96735a8ec1ec12640e5060ac5ff36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:39 +0000

    Add package django-spectator/14.0.0/django-spectator-14.0.0.tar.gz

---
+++ packages/django-spectator/django-spectator-14.0.0.tar.gz/django-spectator-14.0.0/spectator/core/urls/core.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .. import views
+
+# Only the home page.
+# This should be under the namespace 'spectator:core'.
+
+app_name = "core"
+
+urlpatterns = [
+    path("", view=views.HomeView.as_view(), name="home"),
+]
