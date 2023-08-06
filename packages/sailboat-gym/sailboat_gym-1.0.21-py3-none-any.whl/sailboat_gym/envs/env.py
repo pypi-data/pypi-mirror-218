6cb81b5ab97d8dcaf1e4cf69a5d8925d128ce0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:31 +0000

    Add package sailboat-gym/1.0.21/sailboat_gym-1.0.21-py3-none-any.whl

---
+++ packages/sailboat-gym/sailboat_gym-1.0.21-py3-none-any.whl/sailboat_gym/envs/env.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+import numpy as np
+from typing import Any, Tuple, Callable
+
+from ..types import GymObservation, GymAction, Observation, Action, ResetInfo
+from ..utils import ProfilingMeta
+from ..abstracts import AbcRender
+
+
+class SailboatEnv(gym.Env, metaclass=ProfilingMeta):
+    NB_STEPS_PER_SECONDS: int # Hz
+
+    render_mode: str
+    metadata: dict
+
+    action_space = GymAction
+    observation_space = GymObservation
+
+    def reset(self, **kwargs) -> Tuple[Observation, ResetInfo]:
+        super().reset(**kwargs)
+
+    def step(self, action: Action) -> Tuple[Observation, float, bool, Any]:
+        super().step(action)
+
+    def render(self, draw_extra_fct: Callable[[AbcRender, np.ndarray, Observation], None] = None) -> np.ndarray:
+        super().render()
+
+    def close(self) -> None:
+        super().close()
