51fbc33a8311a66d15f7b93f360336c03f8760e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package laminci/0.7.3/laminci-0.7.3.tar.gz

---
+++ packages/laminci/laminci-0.7.3.tar.gz/laminci-0.7.3/laminci/_docs.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+from ._env import load_project_yaml
+
+
+def move_built_docs_to_docs_slash_project_slug():
+    if os.environ["GITHUB_EVENT_NAME"] != "push":
+        return
+    yaml = load_project_yaml()
+    shutil.move("_build/html", f"_build/{yaml['project_slug']}")
+    os.makedirs("_build/html/docs")
+    shutil.move(f"_build/{yaml['project_slug']}", "_build/html/docs")
