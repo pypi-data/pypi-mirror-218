d9923c14b8d9dfa22f3aed466c945005dea280c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:16 +0000

    Add package findpython/1.0.0a3/findpython-1.0.0a3.tar.gz

---
+++ packages/findpython/findpython-1.0.0a3.tar.gz/findpython-1.0.0a3/src/cli.rs
@@ -0,0 +1,97 @@
+use anyhow::anyhow;
+use clap::Parser;
+
+use crate::{Finder, MatchOptions};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Return all matching Python versions
+    #[arg(short, long)]
+    all: bool,
+
+    /// Resolve symlinks and remove duplicate results
+    #[arg(long)]
+    resolve_symlinks: bool,
+
+    /// Remove duplicate results that are the same binary
+    #[arg(long = "no-same-file", action=clap::ArgAction::SetFalse, default_value_t = true)]
+    same_file: bool,
+
+    /// Remove duplicate results that wrap the same Python interpreter
+    #[arg(long = "no-same-python", action=clap::ArgAction::SetFalse, default_value_t = true)]
+    same_python: bool,
+
+    /// Select provider names(comma-separated) to use
+    #[arg(long)]
+    providers: Option<String>,
+
+    /// The output format
+    #[arg(short, long, value_parser = ["default", "json", "path"], default_value = "default")]
+    output: String,
+
+    /// The version spec to find, e.g. 3|3.8|python3
+    version_spec: Option<String>,
+}
+
+pub fn main(cli: Cli) -> anyhow::Result<()> {
+    let mut finder = Finder::default()
+        .resolve_symlinks(cli.resolve_symlinks)
+        .same_file(cli.same_file)
+        .same_interpreter(cli.same_python);
+
+    if let Some(names) = cli.providers {
+        let v = names
+            .split(',')
+            .into_iter()
+            .map(|n| {
+                if !crate::providers::ALL_PROVIDERS.contains(&n) {
+                    Err(anyhow!(format!("Provider {} not found", n)))
+                } else {
+                    Ok(n)
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        finder = finder.select_providers(&v)?;
+    }
+
+    let mut options = MatchOptions::default();
+    if let Some(version_spec) = cli.version_spec.as_deref() {
+        options = options.version_spec(version_spec);
+    }
+
+    let paths = if cli.all {
+        finder.find_all(options)
+    } else {
+        finder.find(options).map_or(vec![], |v| vec![v])
+    };
+
+    if paths.len() == 0 {
+        return Err(anyhow!("No matching Python versions found"));
+    }
+    eprintln!("Found matching Python versions:");
+    match cli.output.as_str() {
+        "default" => {
+            for path in paths {
+                println!("{}", path);
+            }
+        }
+        "path" => {
+            for path in paths {
+                println!("{}", path.executable.to_string_lossy());
+            }
+        }
+        "json" => {
+            let json = if cli.all {
+                serde_json::to_string_pretty(&paths)?
+            } else {
+                serde_json::to_string_pretty(&paths[0])?
+            };
+            println!("{}", json);
+        }
+        _ => {
+            return Err(anyhow!("Unsupported output format"));
+        }
+    }
+    Ok(())
+}
---
+++ packages/findpython/findpython-1.0.0a3.tar.gz/findpython-1.0.0a3/src/providers/mod.rs
@@ -0,0 +1,76 @@
+use std::path::PathBuf;
+
+use crate::helpers;
+use crate::PythonVersion;
+use lazy_static::lazy_static;
+
+mod asdf;
+mod path;
+mod pyenv;
+mod rye;
+
+#[cfg(feature = "pyo3")]
+pub mod pyobject;
+
+#[cfg(windows)]
+mod winreg;
+
+#[cfg(windows)]
+lazy_static! {
+    pub static ref ALL_PROVIDERS: [&'static str; 5] = ["path", "pyenv", "rye", "asdf", "winreg"];
+}
+
+#[cfg(not(windows))]
+lazy_static! {
+    pub static ref ALL_PROVIDERS: [&'static str; 4] = ["path", "pyenv", "rye", "asdf"];
+}
+
+pub trait Provider: Send + Sync {
+    fn create() -> Option<Self>
+    where
+        Self: Sized;
+
+    fn find_pythons(&self) -> Vec<PythonVersion>;
+}
+
+pub fn get_provider(name: &str) -> Option<Box<dyn Provider>> {
+    match name {
+        "path" => path::PathProvider::create().map(|p| Box::new(p) as Box<dyn Provider>),
+        "pyenv" => pyenv::PyenvProvider::create().map(|p| Box::new(p) as Box<dyn Provider>),
+        "rye" => rye::RyeProvider::create().map(|p| Box::new(p) as Box<dyn Provider>),
+        "asdf" => asdf::AsdfProvider::create().map(|p| Box::new(p) as Box<dyn Provider>),
+        #[cfg(windows)]
+        "winreg" => winreg::WinRegProvider::create().map(|p| Box::new(p) as Box<dyn Provider>),
+        _ => None,
+    }
+}
+
+/// Find all Python versions under the given path.
+/// ### Arguments:
+///
+/// path: The path to search for Python versions under.
+/// as_interpreter: Whether to use the path as an interpreter.
+///     Must not be true if it might be a wrapper script.
+///
+/// ### Returns:
+/// A list of Python versions found under the given path.
+pub fn find_pythons_from_path(path: &PathBuf, as_interpreter: bool) -> Vec<PythonVersion> {
+    match path.read_dir() {
+        Ok(entries) => entries
+            .into_iter()
+            .filter_map(|entry| {
+                let path = entry.ok()?.path();
+                if helpers::path_is_python(&path) {
+                    let mut python = PythonVersion::new(path.to_owned());
+                    if as_interpreter {
+                        python = python.with_interpreter(path.to_owned());
+                    }
+                    Some(python)
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        Err(_) => vec![],
+    }
+}
