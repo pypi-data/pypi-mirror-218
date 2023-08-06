75a8a499c8e4baeafa07cfb2cc97a39e13579980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:25 +0000

    Add package ragas/0.0.4/ragas-0.0.4.tar.gz

---
+++ packages/ragas/ragas-0.0.4.tar.gz/ragas-0.0.4/tests/benchmarks/benchmark_eval.py
@@ -0,0 +1,21 @@
+import os
+
+from datasets import Dataset
+from torch.cuda import is_available
+
+from ragas import evaluate
+from ragas.metrics import answer_relevancy, context_relevancy, faithfulness
+
+DEVICE = "cuda" if is_available() else "cpu"
+
+PATH_TO_DATSET_GIT_REPO = "../../../datasets/fiqa/"
+assert os.path.isdir(PATH_TO_DATSET_GIT_REPO), "Dataset not found"
+ds = Dataset.from_json(os.path.join(PATH_TO_DATSET_GIT_REPO, "gen_ds.json"))
+assert isinstance(ds, Dataset)
+
+if __name__ == "__main__":
+    result = evaluate(
+        ds,
+        metrics=[answer_relevancy, context_relevancy, faithfulness],
+    )
+    print(result)
