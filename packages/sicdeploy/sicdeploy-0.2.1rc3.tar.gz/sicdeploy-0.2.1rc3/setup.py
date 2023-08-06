f2373c5bbc5d32be7a3cc79aee5157c392498435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:00 +0000

    Add package sicdeploy/0.2.1rc3/sicdeploy-0.2.1rc3.tar.gz

---
+++ packages/sicdeploy/sicdeploy-0.2.1rc3.tar.gz/sicdeploy-0.2.1rc3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+setup(
+    name='sicdeploy',
+    version='0.2.1rc3',
+    license="LICENSE",
+    packages=find_packages(),
+    package_data={
+        "sicdeploy": ["commands/*", "templates/*"]
+    },
+    entry_points={
+        'console_scripts': ['sicd=sicd.main:run']
+    },
+    install_requires=[str(ir.requirement) for ir in parse_requirements("requirements.txt", session='build')]
+)
