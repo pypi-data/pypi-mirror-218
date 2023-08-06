061fc7aa06d50af016f4bcf2085e9a89407a1629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:22 +0000

    Add package xtb-trading/0.1.0/xtb_trading-0.1.0-py3-none-any.whl

---
+++ packages/xtb-trading/xtb_trading-0.1.0-py3-none-any.whl/xtb_trading/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from XTBApi.api import Client
+
+
+def main():
+    USER_ID = os.environ["USER_ID"]
+    PASSWORD = os.environ["PASSWORD"]
+
+    client = Client()
+    client.login("{USER_ID}", "{PASSWORD}", mode={demo,real})
+
+# CHECK IF MARKET IS OPEN FOR EURUSD
+    client.check_if_market_open([EURUSD])
+
+# # BUY ONE VOLUME (FOR EURUSD THAT CORRESPONDS TO 100000 units)
+# # client.open_trade('buy', EURUSD, 1)
+# # SEE IF ACTUAL GAIN IS ABOVE 100 THEN CLOSE THE TRADE
+# trades = client.update_trades() # GET CURRENT TRADES
+# trade_ids = [trade_id for trade_id in trades.keys()]
+# for trade in trade_ids:
+#     actual_profit = client.get_trade_profit(trade) # CHECK PROFIT
+#     if actual_profit >= 100:
+#         client.close_trade(trade) # CLOSE TRADE
+# # CLOSE ALL OPEN TRADES
+# client.close_all_trades()
+
+    client.logout()
