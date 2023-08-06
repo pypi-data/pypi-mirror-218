5fbdafce432eb18d9e5babd8f896289443285ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:22 +0000

    Add package egttools/0.1.12.1/egttools-0.1.12.1.tar.gz

---
+++ packages/egttools/egttools-0.1.12.1.tar.gz/egttools-0.1.12.1/tests/test_openmp.py
@@ -0,0 +1,33 @@
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+# os.environ['KMP_STACKSIZE'] = '4m'
+# os.environ['KMP_VERSION'] = '.TRUE.'
+
+def test_openmp_simulations():
+    import numpy as np
+    import egttools as egt
+
+    # Payoff matrix
+    V = 2
+    D = 3
+    T = 1
+    Z = 100
+    A = np.array([
+        [(V - D) / 2, V],
+        [0, (V / 2) - T],
+    ])
+
+    game = egt.games.NormalFormGame(1, A)
+    assert game.type() == 'NormalFormGame'
+
+    evolver = egt.numerical.PairwiseComparisonNumerical(Z, game, 1000)
+    dist = evolver.estimate_stationary_distribution_sparse(10, 100000, 1000, 1, 1e-3)
+
+    assert dist.toarray().shape == (1, Z + 1)
+
+
+if __name__ == "__main__":
+    test_openmp_simulations()
