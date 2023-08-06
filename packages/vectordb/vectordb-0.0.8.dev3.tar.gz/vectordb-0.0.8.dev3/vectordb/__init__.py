75ec61af275535622ce7da8ad7ad68bc5621b2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package vectordb/0.0.8.dev3/vectordb-0.0.8.dev3.tar.gz

---
+++ packages/vectordb/vectordb-0.0.8.dev3.tar.gz/vectordb-0.0.8.dev3/vectordb/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.8.dev3'
+
+from vectordb.db.inmemory_exact_vectordb import InMemoryExactNNVectorDB
+from vectordb.db.hnsw_vectordb import HNSWVectorDB
+from vectordb.client import Client
