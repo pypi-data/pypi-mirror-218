257bee5e66c0d3e955d45b1c074ad532b0b10a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:41 +0000

    Add package otio-hls-playlist-adapter/1.0.0/otio_hls_playlist_adapter-1.0.0.tar.gz

---
+++ packages/otio-hls-playlist-adapter/otio_hls_playlist_adapter-1.0.0.tar.gz/otio_hls_playlist_adapter-1.0.0/README.md
@@ -0,0 +1,23 @@
+# OpenTimelineIO HLS Playlist Adapter
+
+The `hls_playlist` adapter is part of OpenTimelineIO's contrib adapter plugins.
+This adapter supports authoring of HLS playlists within OpenTimelineIO by using clips to represent media fragments.
+
+# License
+
+OpenTimelineIO and the "hls playlist" adapter are open source software.
+Please see the [LICENSE](LICENSE) for details.
+
+Nothing in the license file or this project grants any right to use Pixar or
+any other contributor’s trade names, trademarks, service marks, or product names.
+
+## Contributions
+
+If you want to contribute to the project,
+please see: https://opentimelineio.readthedocs.io/en/latest/tutorials/contributing.html
+
+# Contact
+
+For more information, please visit http://opentimeline.io/
+or https://github.com/AcademySoftwareFoundation/OpenTimelineIO
+or join our discussion forum: https://lists.aswf.io/g/otio-discussion
---
+++ packages/otio-hls-playlist-adapter/otio_hls_playlist_adapter-1.0.0.tar.gz/otio_hls_playlist_adapter-1.0.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='otio-hls-playlist-adapter',
+    version='1.0.0',
+    description='OpenTimelineIO HLS Playlist Adapter',
+    long_description='# OpenTimelineIO HLS Playlist Adapter\n\nThe `hls_playlist` adapter is part of OpenTimelineIO\'s contrib adapter plugins.\nThis adapter supports authoring of HLS playlists within OpenTimelineIO by using clips to represent media fragments.\n\n# License\n\nOpenTimelineIO and the "hls playlist" adapter are open source software.\nPlease see the [LICENSE](LICENSE) for details.\n\nNothing in the license file or this project grants any right to use Pixar or\nany other contributor’s trade names, trademarks, service marks, or product names.\n\n## Contributions\n\nIf you want to contribute to the project,\nplease see: https://opentimelineio.readthedocs.io/en/latest/tutorials/contributing.html\n\n# Contact\n\nFor more information, please visit http://opentimeline.io/\nor https://github.com/AcademySoftwareFoundation/OpenTimelineIO\nor join our discussion forum: https://lists.aswf.io/g/otio-discussion\n',
+    author_email='Contributors to the OpenTimelineIO project <otio-discussion@lists.aswf.io>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Multimedia :: Video :: Non-Linear Editor',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'opentimelineio>=0.15.0',
+    ],
+    entry_points={
+        'opentimelineio.plugins': [
+            'otio_hls_playlist_adapter = otio_hls_playlist_adapter',
+        ],
+    },
+    packages=[
+        'otio_hls_playlist_adapter',
+    ],
+    package_dir={'': 'src'},
+)
