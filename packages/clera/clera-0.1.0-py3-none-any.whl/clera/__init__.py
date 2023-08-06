61a31a6d5512a6c4279c1dacac13a733e321064d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:46 +0000

    Add package clera/0.1.0/clera-0.1.0-py3-none-any.whl

---
+++ packages/clera/clera-0.1.0-py3-none-any.whl/clera/__init__.py
@@ -0,0 +1,9 @@
+from .core import *
+from .stubs import *
+from .scripts.editor import editor
+
+__all__ = core.__all__
+
+version = __version__ = '0.1.0 Released 11-JULY-2023'
+
+__version__ = ver = version.split()[0]
