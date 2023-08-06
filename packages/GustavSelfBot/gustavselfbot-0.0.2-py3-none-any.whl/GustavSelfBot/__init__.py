4412cc4aabcf9c3fbb938399ed83b3a00a374a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:52 +0000

    Add package GustavSelfBot/0.0.2/gustavselfbot-0.0.2-py3-none-any.whl

---
+++ packages/GustavSelfBot/gustavselfbot-0.0.2-py3-none-any.whl/GustavSelfBot/__init__.py
@@ -0,0 +1,15 @@
+"""
+My self bot!
+"""
+
+__version__ = "0.0.2"
+
+from GustavSelfBot.__logging__ import *
+from GustavSelfBot.__config__ import Config
+from GustavSelfBot.__bot__ import bot
+
+log.info("Main module loaded!")
+
+if Config["token"] == "":
+    log.error("No token provided!")
+    raise Exception("No token provided!")
