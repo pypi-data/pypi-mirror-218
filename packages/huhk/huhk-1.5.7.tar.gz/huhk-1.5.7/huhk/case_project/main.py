f5465071d341bbf562b22c42074674f3ccfb153c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:14 +0000

    Add package huhk/1.5.7/huhk-1.5.7.tar.gz

---
+++ packages/huhk/huhk-1.5.7.tar.gz/huhk-1.5.7/huhk/case_project/main.py
@@ -0,0 +1,28 @@
+import click
+from huhk.case_project.version import version as _version
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.command()
+@click.option('-v', '--version', help='线索版本', required=False)
+@click.option('-i', '--init', help='项目key', required=False)
+@click.option('-u', '--update', help='项目key', required=False)
+def main(version, init, update):
+    """Simple program that greets NAME for a total of COUNT times."""
+    if version:
+        click.echo('版本：' + _version)
+        click.echo(type(version))
+    elif init:
+        click.echo('init：' + _version)
+        click.echo(type(init))
+    elif update:
+        click.echo('update：' + _version)
+        click.echo(type(update))
+
+
+if __name__ == '__main__':
+    main()
