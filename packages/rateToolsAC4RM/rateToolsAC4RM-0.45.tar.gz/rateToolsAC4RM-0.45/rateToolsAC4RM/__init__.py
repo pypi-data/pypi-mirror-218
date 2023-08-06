34b4a7ff723afcf4231a7467a0989591cf20e728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:50 +0000

    Add package rateToolsAC4RM/0.45/rateToolsAC4RM-0.45.tar.gz

---
+++ packages/rateToolsAC4RM/rateToolsAC4RM-0.45.tar.gz/rateToolsAC4RM-0.45/rateToolsAC4RM/__init__.py
@@ -0,0 +1,20 @@
+from .rateTools import d
+from .rateTools import pv
+from .rateTools import fwd
+from .rateTools import rates2fwds
+from .rateTools import fwds2dcf
+from .rateTools import dDCF
+from .rateTools import dFWDS
+from .rateTools import bulletSwap
+from .rateTools import swapMkt
+from .rateTools import swap1YrFwds01
+from .rateTools import swap1YrFwdsCF
+from .rateTools import sumCF
+from .rateTools import portfolioHedge
+from .rateTools import hedgeCF
+from .rateTools import psaSimulation
+
+
+
+
+
---
+++ packages/rateToolsAC4RM/rateToolsAC4RM-0.45.tar.gz/rateToolsAC4RM-0.45/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rateToolsAC4RM',
+      version='0.45',
+      description='rate tools for fwd deltas',
+      packages=['rateToolsAC4RM'],
+      author_email='tl2034@columbia.edu',
+      zip_safe=False)
