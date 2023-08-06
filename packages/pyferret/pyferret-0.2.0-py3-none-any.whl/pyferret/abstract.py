24e2bedc24d519a62af3ebb67d50f0306a3f8bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:49 +0000

    Add package pyferret/0.2.0/pyferret-0.2.0-py3-none-any.whl

---
+++ packages/pyferret/pyferret-0.2.0-py3-none-any.whl/pyferret/abstract.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import Callable, Generic, TypeVar
+
+T = TypeVar("T", covariant=True)
+S = TypeVar("S", covariant=True)
+
+
+class Context(Generic[T]):
+    """
+    Base class of container that stores some value
+    """
+
+    __slots__ = ("_value",)
+
+    def __init__(self, v: T) -> None:
+        self._value = v
+
+
+class Functor(Context[T]):
+    @abstractmethod
+    def fmap(self, func: Callable[[T], S]) -> Functor[S]:
+        """
+        Applying `func` on inner value of context
+        """
+
+        raise NotImplementedError
+
+
+class Applicative(Functor[T]):
+    @abstractmethod
+    def applicate(self, func: Applicative[Callable[[T], S]]) -> Applicative[S]:
+        raise NotImplementedError
+
+
+class Monad(Applicative[T]):
+    @abstractmethod
+    def bind(self, func: Callable[[T], Monad[S]]) -> Monad[S]:
+        """
+        Applying `func` which returns a Monad on inner value of context and return its
+        result
+        """
+        raise NotImplementedError
