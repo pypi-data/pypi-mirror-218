5f23a3839a8680082e6946954f116166d1fda998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:59 +0000

    Add package whyPrintll/0.0.1/whyPrintll-0.0.1.tar.gz

---
+++ packages/whyPrintll/whyPrintll-0.0.1.tar.gz/whyPrintll-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+ 
+setup(
+    name='whyPrintll',  # 对外模块的名字
+    version='0.0.1',  # 版本号
+    description='测试本地发布模块',  # 描述
+    author='why',  # 作者
+    author_email='why2011_0723@163.com',
+    py_modules=['whyPrintll'],  # 要发布的模块
+)
