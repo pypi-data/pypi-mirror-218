eccd7fc32347ebeb0568cc199b6893ce6d33e394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:35 +0000

    Add package offat/0.2.0/offat-0.2.0.tar.gz

---
+++ packages/offat/offat-0.2.0.tar.gz/offat-0.2.0/offat/__main__.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+from asyncio import run
+# from pprint import pprint as print
+from .openapi import OpenAPIParser
+from .tester.test_generator import TestGenerator
+from .tester.test_runner import TestRunner
+from .tester.test_results import TestResultTable
+
+
+def start():
+    '''Starts cli tool'''
+    parser = ArgumentParser(prog='offat')
+    parser.add_argument('-f','--file', dest='fpath', type=str, help='path of openapi/swagger specification file', required=True)
+    
+    args = parser.parse_args()
+
+    api_parser = OpenAPIParser(args.fpath)
+
+    # create tester objs
+    test_runner = TestRunner()
+    test_table_generator = TestResultTable()
+    test_generator = TestGenerator()
+
+    
+    # test for unsupported http methods
+    # unsupported_http_endpoint_tests = test_generator.check_unsupported_http_methods(api_parser.base_url, api_parser._get_endpoints())
+    # test_results = run(test_runner.run_tests(unsupported_http_endpoint_tests))
+    # results = test_table_generator.generate_result_table(test_results)
+    # print(results)
+
+
+    # sqli fuzz test
+    sqli_fuzz_tests = test_generator.sqli_fuzz_params(api_parser)
+    test_results = run(test_runner.run_tests(sqli_fuzz_tests))
+    results = test_table_generator.generate_result_table(test_results)
+    print(results)
+
+    # generate results
+
+if __name__ == '__main__':
+    start()
