01c8e71d88161635fad2462597b8b5c915203228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:35 +0000

    Add package zaber-motion/3.4.3/zaber_motion-3.4.3-py2.py3-none-any.whl

---
+++ packages/zaber-motion/zaber_motion-3.4.3-py2.py3-none-any.whl/zaber_motion/ascii/oscilloscope_capture_properties.py
@@ -0,0 +1,49 @@
+﻿# ===== THIS FILE IS GENERATED FROM A TEMPLATE ===== #
+# ============== DO NOT EDIT DIRECTLY ============== #
+# pylint: disable=W0201
+
+from ..protobufs import main_pb2
+
+
+class OscilloscopeCaptureProperties:
+    """
+    The public properties of one channel of recorded oscilloscope data.
+    """
+
+    @property
+    def setting(self) -> str:
+        """
+        The name of the recorded setting.
+        """
+
+        return self._setting
+
+    @setting.setter
+    def setting(self, value: str) -> None:
+        self._setting = value
+
+    @property
+    def axis_number(self) -> int:
+        """
+        The number of the axis the data was recorded from, or 0 for the controller.
+        """
+
+        return self._axis_number
+
+    @axis_number.setter
+    def axis_number(self, value: int) -> None:
+        self._axis_number = value
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+    @staticmethod
+    def from_protobuf(
+        pb_data: main_pb2.OscilloscopeCaptureProperties
+    ) -> 'OscilloscopeCaptureProperties':
+        instance = OscilloscopeCaptureProperties.__new__(
+            OscilloscopeCaptureProperties
+        )  # type: OscilloscopeCaptureProperties
+        instance.setting = pb_data.setting
+        instance.axis_number = pb_data.axis_number
+        return instance
---
+++ packages/zaber-motion/zaber_motion-3.4.3-py2.py3-none-any.whl/zaber_motion/measurement.py
@@ -0,0 +1,61 @@
+﻿# ===== THIS FILE IS GENERATED FROM A TEMPLATE ===== #
+# ============== DO NOT EDIT DIRECTLY ============== #
+# pylint: disable=W0201
+
+from typing import Optional  # pylint: disable=unused-import
+from .protobufs import main_pb2
+from .units import UnitsAndLiterals, Units, units_from_literals
+
+
+class Measurement:
+    """
+    Represents a numerical value with optional units specified.
+    """
+
+    def __init__(
+            self: 'Measurement',
+            value: float,
+            unit: Optional[UnitsAndLiterals] = None
+    ) -> None:
+        self._value = value
+        self._unit = unit
+
+    @property
+    def value(self) -> float:
+        """
+        Value of the measurement.
+        """
+
+        return self._value
+
+    @value.setter
+    def value(self, value: float) -> None:
+        self._value = value
+
+    @property
+    def unit(self) -> Optional[UnitsAndLiterals]:
+        """
+        Optional units of the measurement.
+        """
+
+        return self._unit
+
+    @unit.setter
+    def unit(self, value: Optional[UnitsAndLiterals]) -> None:
+        self._unit = value
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+    @staticmethod
+    def to_protobuf(source: 'Optional[Measurement]') -> main_pb2.Measurement:
+        if source is None:
+            return main_pb2.Measurement()
+
+        if not isinstance(source, Measurement):
+            raise TypeError("Provided value is not Measurement.")
+
+        pb_data = main_pb2.Measurement()
+        pb_data.value = source.value
+        pb_data.unit = units_from_literals(source.unit or Units.NATIVE).value
+        return pb_data
