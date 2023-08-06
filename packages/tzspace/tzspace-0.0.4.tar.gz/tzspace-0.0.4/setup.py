a609ecdbeecfa8c835763add6671f5aa1606f611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:02 +0000

    Add package tzspace/0.0.4/tzspace-0.0.4.tar.gz

---
+++ packages/tzspace/tzspace-0.0.4.tar.gz/tzspace-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="tzspace",
+  version="0.0.4",
+  author="Jie Guan",
+  author_email="jguanisme@163.com",
+  description="tzspace sdk for ior standard",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/tzspace",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
