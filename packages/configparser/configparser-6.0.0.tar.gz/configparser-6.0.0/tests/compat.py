104cf851feb4023c6f1f0c8a7f9dfa215800d912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package configparser/6.0.0/configparser-6.0.0.tar.gz

---
+++ packages/configparser/configparser-6.0.0.tar.gz/configparser-6.0.0/tests/compat.py
@@ -0,0 +1,21 @@
+import test.support
+import contextlib
+import sys
+import types
+
+
+def check__all__(*args, **kwargs):
+    if sys.version_info < (3, 10):  # pragma: no cover
+        with contextlib.suppress(KeyError):
+            kwargs.update(blacklist=kwargs.pop('not_exported'))
+    return test.support.check__all__(*args, **kwargs)
+
+
+support = types.SimpleNamespace(**dict(vars(test.support), check__all__=check__all__))
+
+
+try:
+    from test.support import os_helper
+except ImportError:  # pragma: no cover
+    # Python 3.9
+    import test.support as os_helper  # noqa: F401
