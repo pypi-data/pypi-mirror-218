1f40efcfb8a0104a5eaf6ed13de624f0ba0f1ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:21 +0000

    Add package CronoAPI/1.1.1/CronoAPI-1.1.1.tar.gz

---
+++ packages/CronoAPI/CronoAPI-1.1.1.tar.gz/CronoAPI-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CronoAPI',
+    version='1.1.1',
+    description='Biblioteca de datas e horas',
+    author='Sabio_',
+    packages=find_packages(),
+    install_requires=['datetime'],
+)
