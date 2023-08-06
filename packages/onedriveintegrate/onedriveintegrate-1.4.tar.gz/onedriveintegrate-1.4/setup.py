68f4ba15954b7ed8ae4e98d7ac681327595f3f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:17 +0000

    Add package onedriveintegrate/1.4/onedriveintegrate-1.4.tar.gz

---
+++ packages/onedriveintegrate/onedriveintegrate-1.4.tar.gz/onedriveintegrate-1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# Read contents of README.md file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+      name='onedriveintegrate',
+      version='1.4',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      description='Package for OneDrive File Management',
+      author='William Guo',
+      license='MIT',
+      packages=['onedriveintegrate'],
+      zip_safe=False
+)
