9de65ea9faf216a584386316f9210ad3d6fb8e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:34 +0000

    Add package JMatuszczakL/1.0.3/JMatuszczakL-1.0.3.tar.gz

---
+++ packages/JMatuszczakL/JMatuszczakL-1.0.3.tar.gz/JMatuszczakL-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='JMatuszczakL',
+    version='1.0.3',
+    author='JMatuszczak',
+    author_email='J@matuszczak.org',
+    description='Easy window making',
+    packages=['JMatuszczakL'],
+    install_requires=['PyQt6'],  # Add any dependencies your library requires
+)
+
