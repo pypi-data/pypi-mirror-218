adb74dd31555037f58144b4de790a0ed8efbd2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:03 +0000

    Add package cs-models/0.0.598/cs-models-0.0.598.tar.gz

---
+++ packages/cs-models/cs-models-0.0.598.tar.gz/cs-models-0.0.598/src/cs_models/resources/AssistantSearchCopilot/schemas.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema, fields
+
+
+class AssistantSearchCopilotResourceSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    assistant_user_query_id = fields.Integer(allow_none=True)
+    updated_at = fields.DateTime(dump_only=True)
---
+++ packages/cs-models/cs-models-0.0.598.tar.gz/cs-models-0.0.598/src/cs_models/resources/Context/schemas.py
@@ -0,0 +1,12 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class ContextResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    key = fields.String(required=True)
+    value = fields.String(validate=not_blank, required=True)
