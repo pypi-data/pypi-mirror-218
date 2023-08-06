2420ba5870b02788046c3bcbe7c73ec2fd444024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:53 +0000

    Add package rally-cli/0.1.11/rally_cli-0.1.11.tar.gz

---
+++ packages/rally-cli/rally_cli-0.1.11.tar.gz/rally_cli-0.1.11/rallycli/apis/timebox_api.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from logging import getLogger
+from typing import List
+
+import urllib3
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from rallycli.base_api import BaseAPI
+from rallycli.models import RallyTypeGeneric, type_names
+
+## logger definition
+logger = getLogger(__name__)
+## Disable certificate warnings for testing pourposes
+urllib3.disable_warnings()
+
+
+class TimeboxAPI(BaseAPI):
+    """Class for accessing Time Boxes domain"""
+
+    ## Releases
+
+    def get_releases_for_project(self, project_ref: str, fetch: str = "true") -> List[RallyTypeGeneric]:
+        return self.query(f"(Project = {project_ref})", type_names.RELEASE, fetch=fetch)
+
+    ## Iterations
+
+    def get_active_iterations_for_project(self, project_ref: str, fetch: str = "true") -> List[RallyTypeGeneric]:
+        return self.query(f"( (Project = {project_ref}) AND (State != Accepted))",
+                          type_names.ITERATION, fetch=fetch)
