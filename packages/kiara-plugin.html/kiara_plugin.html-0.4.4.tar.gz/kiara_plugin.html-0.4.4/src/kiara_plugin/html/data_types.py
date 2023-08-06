15b84bd5fbbba15cdf39da8f15f27dce8c5ce45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:51 +0000

    Add package kiara-plugin.html/0.4.4/kiara_plugin.html-0.4.4.tar.gz

---
+++ packages/kiara-plugin.html/kiara_plugin.html-0.4.4.tar.gz/kiara_plugin.html-0.4.4/src/kiara_plugin/html/data_types.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains the value type classes that are used in the ``kiara_plugin.html`` package.
+"""
+from kiara.data_types.included_core_types import StringType
+
+
+class HtmlType(StringType):
+
+    _data_type_name = "html"
