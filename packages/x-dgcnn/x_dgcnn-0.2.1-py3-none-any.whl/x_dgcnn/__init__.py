d838524390392e72fe0b009c7b9b6cd29ae9f95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package x-dgcnn/0.2.1/x_dgcnn-0.2.1-py3-none-any.whl

---
+++ packages/x-dgcnn/x_dgcnn-0.2.1-py3-none-any.whl/x_dgcnn/__init__.py
@@ -0,0 +1,11 @@
+from x_dgcnn.dgcnn import (
+    EdgeConv,
+    SpatialTransformNet,
+    DGCNN_Cls,
+    DGCNN_Seg,
+    disable_keops
+)
+
+from x_dgcnn.x_dgcnn import XEdgeConv, XSpatialTransformNet, XDGCNN_Cls
+
+from x_dgcnn.route import subset_topk
