e44104e9ee375bdaa977d2c145a842203db62199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:38 +0000

    Add package diz/0.0.1/diz-0.0.1-py3-none-any.whl

---
+++ packages/diz/diz-0.0.1-py3-none-any.whl/utils/dir.py
@@ -0,0 +1,12 @@
+import os
+
+
+def is_empty(directory_path):
+    # 列出目录中的文件和子目录
+    contents = os.listdir(directory_path)
+
+    # 判断目录是否为空
+    if len(contents) == 0:
+        return True
+    else:
+        return False
