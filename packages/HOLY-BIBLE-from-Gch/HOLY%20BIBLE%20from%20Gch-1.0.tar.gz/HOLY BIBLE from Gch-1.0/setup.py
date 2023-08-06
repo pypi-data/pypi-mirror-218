ee8c6ebe403b3b8f00f486e223d54fdc71aabd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:24 +0000

    Add package HOLY-BIBLE-from-Gch/1.0/HOLY%20BIBLE%20from%20Gch-1.0.tar.gz

---
+++ packages/HOLY-BIBLE-from-Gch/HOLY%20BIBLE%20from%20Gch-1.0.tar.gz/HOLY BIBLE from Gch-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name= "HOLY BIBLE from Gch",
+    version="1.0",
+    author="C. Praiseline Christina",
+    description="HOLY BIBLE",
+    long_description="HOLY BIBLE designed where the package loads the entire bible and displays ultiple related references with the input keyword and selected verses displays",
+    keywords="HOLY BIBLE",
+    python_requires=">=3.3",
+    install_requires=[
+        'python==3.4',
+        'tkinter'],
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+)
