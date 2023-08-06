b1d8305899f1171838692934bcee699f0d7f9566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:45 +0000

    Add package tiled/0.1.0a98/tiled-0.1.0a98.tar.gz

---
+++ packages/tiled/tiled-0.1.0a98.tar.gz/tiled-0.1.0a98/docs/source/how-to/docker.md
@@ -0,0 +1,65 @@
+# Run Tiled using Docker
+
+To run tiled using docker we must first obtain a docker image containing a tiled installation.
+To do this run
+
+```
+docker pull ghcr.io/bluesky/tiled:main
+```
+
+In this example we will use the docker image to serve a directory of files as explained [here](../tutorials/serving-files.md).
+
+First generate a directory of example files using a utility provided by Tiled.
+
+```
+python -m tiled.examples.generate_files example_files/
+```
+
+The docker container runs tiled via [gunicorn](https://gunicorn.org/) which provides horizontal scaling over workers.
+Because of this we cannot configure tiled using commandline arguments and must specify a config file.
+
+Take the following server configuration below:
+```yaml
+# config.yml
+trees:
+  - path: /
+    tree: tiled.adapters.files:DirectoryAdapter.from_directory
+    args:
+      directory: "example_files"
+authentication:
+  allow_anonymous_access: true
+  single_user_api_key: SECRET
+```
+and serve it using the docker container
+```
+docker run --rm -p 8000:8000 \
+  --mount type=bind,source="$(pwd)",target=/deploy \
+  --env TILED_CONFIG=/deploy/config.yml \
+  ghcr.io/bluesky/tiled:main
+```
+Note that we make the data and the configuration file available to the
+container via bind mounds and point tiled to the configuration file using the
+`TILED_CONFIG` environment variable.
+We must supply the `single_user_api_key` in the configuration so that all
+workers use the same key.
+
+This invocation can be simplified by writing a `docker-compose.yml` file.
+
+```yaml
+# docker-compose.yml
+
+version: "3.2" # higher config versions may also work; lower will not
+services:
+  tiled-server:
+    image: ghcr.io/bluesky/tiled:main
+    volumes:
+      - type: bind
+        source: .
+        target: /deploy
+    environment:
+      - TILED_CONFIG=/deploy/config.yml
+    ports:
+      - 8000:8000
+```
+
+With this file the tiled server can be brought up by simply running `docker-compose up`.
---
+++ packages/tiled/tiled-0.1.0a98.tar.gz/tiled-0.1.0a98/tiled/_tests/conftest.py
@@ -0,0 +1,114 @@
+import os
+from pathlib import Path
+
+import pytest
+import pytest_asyncio
+
+from .. import profiles
+from ..catalog import from_uri, in_memory
+from ..client import context
+from ..server.settings import get_settings
+from .utils import enter_password as utils_enter_password
+from .utils import temp_postgres
+
+
+@pytest.fixture(autouse=True)
+def reset_settings():
+    """
+    Reset the FastAPI Settings.
+
+    Fast API uses a global singleton for Settings.  It is difficult to get
+    around this, so our best option is to reset it.
+    """
+    get_settings.cache_clear()
+    yield
+
+
+@pytest.fixture(autouse=True)
+def set_tiled_cache_dir(tmpdir):
+    """
+    Use a tmpdir instead of ~/.cache/tiled/tokens
+    """
+    original = context.TILED_CACHE_DIR
+    context.TILED_CACHE_DIR = Path(tmpdir)
+    yield
+    context.TILED_CACHE_DIR = original
+
+
+@pytest.fixture
+def tmp_profiles_dir(tmpdir):
+    """
+    Use a tmpdir instead of ~/.config/tiled/profiles
+    """
+    original = profiles.paths
+    profiles.paths.clear()
+    profiles.paths.extend([Path(tmpdir)])
+    profiles.load_profiles.cache_clear()
+    yield
+    profiles.paths.clear()
+    profiles.paths.extend(original)
+
+
+@pytest.fixture
+def enter_password():
+    """
+    DEPRECATED: Use the normal (non-fixture) context manager in .utils.
+    """
+    return utils_enter_password
+
+
+@pytest.fixture(scope="module")
+def tmpdir_module(request, tmpdir_factory):
+    """A tmpdir fixture for the module scope. Persists throughout the module."""
+    # Source: https://stackoverflow.com/a/31889843
+    return tmpdir_factory.mktemp(request.module.__name__)
+
+
+# This can un-commented to debug leaked threads.
+# import threading
+# import time
+#
+# def poll_enumerate():
+#     while True:
+#         time.sleep(1)
+#         print("THREAD COUNT", len(threading.enumerate()))
+#
+# thread = threading.Thread(target=poll_enumerate, daemon=True)
+# thread.start()
+
+
+# To test with postgres, start a container like:
+#
+# docker run --name tiled-test-postgres -p 5432:5432 -e POSTGRES_PASSWORD=secret -d docker.io/postgres
+# and set this env var like:
+#
+# TILED_TEST_POSTGRESQL_URI=postgresql+asyncpg://postgres:secret@localhost:5432
+
+TILED_TEST_POSTGRESQL_URI = os.getenv("TILED_TEST_POSTGRESQL_URI")
+
+
+@pytest_asyncio.fixture(params=["sqlite", "postgresql"])
+async def adapter(request, tmpdir):
+    """
+    Adapter instance
+
+    Note that startup() and shutdown() are not called, and must be run
+    either manually (as in the fixture 'a') or via the app (as in the fixture 'client').
+    """
+    if request.param == "sqlite":
+        adapter = in_memory(writable_storage=str(tmpdir))
+        yield adapter
+    elif request.param == "postgresql":
+        if not TILED_TEST_POSTGRESQL_URI:
+            raise pytest.skip("No TILED_TEST_POSTGRESQL_URI configured")
+        # Create temporary database.
+        async with temp_postgres(TILED_TEST_POSTGRESQL_URI) as uri_with_database_name:
+            # Build an adapter on it.
+            adapter = from_uri(
+                uri_with_database_name,
+                writable_storage=str(tmpdir),
+                initialize_database_at_startup=True,
+            )
+            yield adapter
+    else:
+        assert False
