e2b825ca4e009395ad9049198586379f464762d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:14 +0000

    Add package capsolver/1.0.6/capsolver-1.0.6-py3-none-any.whl

---
+++ packages/capsolver/capsolver-1.0.6-py3-none-any.whl/capsolver/capsolver.py
@@ -0,0 +1,56 @@
+import time
+
+import capsolver.error
+from capsolver.capsolver_object import CapsolverObject
+
+_DEFAULT_RETRIES = 60
+_DEFAULT_INTERVAL = 1
+
+
+class Capsolver(CapsolverObject):
+
+    def __init__(self, api_key=None, api_base=None, **params):
+        super().__init__(api_key, api_base, **params)
+
+    
+    @classmethod
+    def postTask(cls, params:dict):
+        return cls().request("post", "/createTask", {"task":params})
+
+    @classmethod
+    def getTask(cls, **params):
+        for _ in range(_DEFAULT_RETRIES):
+            time.sleep(_DEFAULT_INTERVAL)
+            r = cls().request("post", "/getTaskResult", params)
+            print("get Task",r)
+            if isinstance(r,capsolver.error.CapsolverError):
+                raise r
+            if r["status"]=="processing":
+                continue
+            if r["status"] =="idle":
+                continue
+            return r
+        raise capsolver.error.Timeout('Failed to get results')
+
+    @classmethod
+    def solve(cls,params:dict):
+        r = cls.postTask(params)
+        if isinstance(r, capsolver.error.CapsolverError):
+            raise r
+        if r['status']=="ready":
+            print("test2",r)
+            return r['solution']
+        r = cls.getTask(taskId=r['taskId'])
+        if isinstance(r, capsolver.error.CapsolverError):
+            raise r
+        print("teset1",r)
+        return r['solution']
+
+
+    @classmethod
+    def balance(cls):
+        r = cls().request("post", "/getBalance",{})
+        return r
+
+
+
