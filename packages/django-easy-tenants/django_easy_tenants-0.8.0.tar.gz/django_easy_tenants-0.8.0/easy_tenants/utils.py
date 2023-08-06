865e294cda7d2c8795a889aa141549c6f523db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package django-easy-tenants/0.8.0/django_easy_tenants-0.8.0.tar.gz

---
+++ packages/django-easy-tenants/django_easy_tenants-0.8.0.tar.gz/django_easy_tenants-0.8.0/easy_tenants/utils.py
@@ -0,0 +1,46 @@
+import threading
+from contextlib import contextmanager
+
+from easy_tenants.exceptions import TenantError
+
+state_local = threading.local()
+
+
+def get_state():
+    state_default = {
+        "enabled": True,
+        "tenant": None,
+    }
+    state = getattr(state_local, "state", state_default)
+    return state
+
+
+def get_current_tenant():
+    state = get_state()
+
+    if state["enabled"] and state["tenant"] is None:
+        raise TenantError("Tenant is required in context.")
+
+    return state["tenant"]
+
+
+@contextmanager
+def tenant_context(tenant=None, enabled=True):
+    previous_state = get_state()
+
+    new_state = previous_state.copy()
+    new_state["enabled"] = enabled
+    new_state["tenant"] = tenant
+
+    state_local.state = new_state
+
+    try:
+        yield
+    finally:
+        state_local.state = previous_state
+
+
+@contextmanager
+def tenant_context_disabled():
+    with tenant_context(enabled=False):
+        yield
