2eda1fb925326087800378ba47a6f0841f98dd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:50 +0000

    Add package shubhlipi/0.0.0.18/shubhlipi-0.0.0.18.tar.gz

---
+++ packages/shubhlipi/shubhlipi-0.0.0.18.tar.gz/shubhlipi-0.0.0.18/shubhlipi/crypted.py
@@ -0,0 +1,46 @@
+import base64
+import hashlib
+import uuid
+from .kry import get_type, from_base64, to_base64
+try:
+    from cryptography.fernet import Fernet
+except:
+    pass
+
+
+def bin_str(val) -> str:
+    if get_type(val) == "bytes":
+        return val
+    else:
+        return bytes(val, "utf-8")
+
+
+def salt() -> str:
+    return uuid.uuid4().hex
+
+
+def hash_256(val) -> str:
+    return hashlib.sha3_256(bin_str(val)).hexdigest()
+
+
+def hash_512(val) -> str:
+    return hashlib.sha3_512(bin_str(val)).hexdigest()
+
+
+def hash_md5(val) -> str:
+    return hashlib.md5(bin_str(val)).hexdigest()
+
+
+def encrypt(val, key: str) -> bytes:
+    """Used to encrypt `bytes` or `str`"""
+    f = Fernet(to_base64(hash_md5(key)))
+    return f.encrypt(bin_str(val))
+
+
+def decrypt(val, key: str) -> bytes:
+    f = Fernet(to_base64(hash_md5(key)))
+    return f.decrypt(bin_str(val))
+
+
+def decrypt_text(val, key: str):
+    return decrypt(val, key).decode("utf-8")
