930e6e56508d7e3ec2207d9a7e4090bf95a346b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:44 +0000

    Add package whyPrint/0.0.1/whyPrint-0.0.1.tar.gz

---
+++ packages/whyPrint/whyPrint-0.0.1.tar.gz/whyPrint-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='whyPrint',
+      version='0.0.1',
+      description='本次更新：修复list模块对中文的支持',
+      author='why',
+      author_email='why2011_0723@163.com',
+      url='https://gitee.com/why2011_0723/why/',
+      packages=['whyPrint'],
+     )
