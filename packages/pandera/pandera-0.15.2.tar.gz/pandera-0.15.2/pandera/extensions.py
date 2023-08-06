b202793ad413e209b4c0a9d6c0e27269f028b7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:24 +0000

    Add package pandera/0.15.2/pandera-0.15.2.tar.gz

---
+++ packages/pandera/pandera-0.15.2.tar.gz/pandera-0.15.2/pandera/extensions.py
@@ -0,0 +1,10 @@
+"""Extensions module, for backwards compatibility."""
+
+# pylint: disable=unused-import
+from pandera.api.extensions import (
+    register_builtin_check,
+    register_builtin_hypothesis,
+    CheckType,
+    register_check_method,
+    register_check_statistics,
+)
