c2f02b40cb86aadd65777330ee6e664ea2538b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:43 +0000

    Add package dataclasses-struct/0.5.0/dataclasses_struct-0.5.0.tar.gz

---
+++ packages/dataclasses-struct/dataclasses_struct-0.5.0.tar.gz/dataclasses_struct-0.5.0/dataclasses_struct/__init__.py
@@ -0,0 +1,49 @@
+# flake8: noqa
+
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
+
+from .dataclass import (
+    dataclass,
+    NATIVE_ENDIAN_ALIGNED,
+    NATIVE_ENDIAN,
+    LITTLE_ENDIAN,
+    BIG_ENDIAN,
+    NETWORK_ENDIAN,
+)
+from .types import (
+    Char,
+    Int8,
+    Uint8,
+    Bool,
+    Int16,
+    Uint16,
+    Int32,
+    Uint32,
+    Int64,
+    Uint64,
+    Size,
+    SSize,
+    Pointer,
+    Float32,
+    Float,
+    Float64,
+    Double,
+    PadBefore,
+    PadAfter,
+)
+from .field import (
+    BoolField,
+    CharField,
+    IntField,
+    SignedIntField,
+    UnsignedIntField,
+    FloatField,
+    DoubleField,
+    SizeField,
+    SignedSizeField,
+    UnsignedSizeField,
+    PointerField,
+    BytesField,
+)
---
+++ packages/dataclasses-struct/dataclasses_struct-0.5.0.tar.gz/dataclasses_struct-0.5.0/dataclasses_struct/types.py
@@ -0,0 +1,56 @@
+from typing_extensions import Annotated
+
+from . import field
+
+# Single char type
+Char = Annotated[bytes, field.CharField()]
+
+# Boolean type
+Bool = Annotated[bool, field.BoolField()]
+
+# Integer types
+Int8 = Annotated[int, field.SignedIntField(1)]
+Uint8 = Annotated[int, field.UnsignedIntField(1)]
+Int16 = Annotated[int, field.SignedIntField(2)]
+Uint16 = Annotated[int, field.UnsignedIntField(2)]
+Int32 = Annotated[int, field.SignedIntField(4)]
+Uint32 = Annotated[int, field.UnsignedIntField(4)]
+Int64 = Annotated[int, field.SignedIntField(8)]
+Uint64 = Annotated[int, field.UnsignedIntField(8)]
+
+# Native size types
+Size = Annotated[int, field.UnsignedSizeField()]
+SSize = Annotated[int, field.SignedSizeField()]
+Pointer = Annotated[int, field.PointerField()]
+
+# Floating point types
+Float32 = Annotated[float, field.FloatField()]
+Float = Float32
+Float64 = Annotated[float, field.DoubleField()]
+Double = Float64
+
+
+class _Padding:
+    before: bool
+
+    def __init__(self, size: int):
+        if size < 0:
+            raise ValueError('size must be non-negative')
+        self.size = size
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self.size})'
+
+
+class PadBefore(_Padding):
+    before = True
+
+    def __init__(self, size: int):
+        super().__init__(size)
+
+
+class PadAfter(_Padding):
+    before = False
+
+    def __init__(self, size: int):
+        super().__init__(size)
