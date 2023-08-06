7fcdc2fbbc80f43d4d9b4bacbf61563751f41eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package qvsed/1.2.5/qvsed-1.2.5.tar.gz

---
+++ packages/qvsed/qvsed-1.2.5.tar.gz/qvsed-1.2.5/qvsed/config.py
@@ -0,0 +1,7 @@
+# This is QVSED's config file, you can change its options here.
+
+# Font
+font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
+font_size = 11
+
+# That's it for now.
---
+++ packages/qvsed/qvsed-1.2.5.tar.gz/qvsed-1.2.5/qvsed/config_default.py
@@ -0,0 +1,7 @@
+# This config file serves as a template and should not be modified directly; please make any necessary changes in config.py.
+
+# Font
+font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
+font_size = 11
+
+# That's it for now.
