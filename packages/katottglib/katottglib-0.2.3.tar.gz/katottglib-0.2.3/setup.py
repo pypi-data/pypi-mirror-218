6dadf4264820310391afac5f1646696321ea0432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:33 +0000

    Add package katottglib/0.2.3/katottglib-0.2.3.tar.gz

---
+++ packages/katottglib/katottglib-0.2.3.tar.gz/katottglib-0.2.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='katottglib',
+    version='0.2.3',
+    packages=['katottglib'],
+    install_requires=[
+    ],
+    author='Denis Senchishen',
+    author_email='dsenchishen@icloud.com',
+    description='A Python lib to operate KATOTTG addresses (classified administrative division of Ukraine).',
+    url='https://github.com/sen-den/katottglib',
+)
