dfe7e2ff8a7b15a2c7f330afedaeed493720fccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:19 +0000

    Add package thiggle/0.0.1/thiggle-0.0.1.tar.gz

---
+++ packages/thiggle/thiggle-0.0.1.tar.gz/thiggle-0.0.1/thiggle/apis/paths/v1_categorize.py
@@ -0,0 +1,7 @@
+from thiggle.paths.v1_categorize.post import ApiForpost
+
+
+class V1Categorize(
+    ApiForpost,
+):
+    pass
---
+++ packages/thiggle/thiggle-0.0.1.tar.gz/thiggle-0.0.1/thiggle/models/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from thiggle.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from thiggle.model.categorize_request import CategorizeRequest
+from thiggle.model.categorize_response import CategorizeResponse
