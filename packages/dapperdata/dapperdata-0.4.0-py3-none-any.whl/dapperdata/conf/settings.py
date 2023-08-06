a2f527f023ae46083ac94c1cb79a9bbe76c81c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package dapperdata/0.4.0/dapperdata-0.4.0-py3-none-any.whl

---
+++ packages/dapperdata/dapperdata-0.4.0-py3-none-any.whl/dapperdata/conf/settings.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from typing import List, Set
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
+
+default_data = {}
+if os.path.exists("pyproject.toml"):
+    with open("pyproject.toml", "rb") as f:
+        toml_data = tomllib.load(f)
+    default_data.update(toml_data.get("tool", {}).get("dapperdata", {}))
+
+
+if os.path.exists(".gitignore"):
+    gitignore_directories = set([])
+    with open(".gitignore", "rb") as f:
+        for line in f.readlines():
+            line_string = line.decode("utf-8")
+            if "*" in line_string:
+                continue
+            if line_string.startswith("#"):
+                continue
+            if "#" in line_string:
+                line_string = line_string.split("#")[0]
+            line_string = line_string.strip().strip("/")
+            if len(line_string) == 0:
+                continue
+            gitignore_directories.add(line_string)
+    if len(gitignore_directories) > 0:
+        default_data["exclude_paths"] = set(default_data.get("exclude_paths", [])) | gitignore_directories
+
+
+class Settings(BaseSettings):
+    project_name: str = "pretty-config"
+    debug: bool = False
+
+    exclude_paths: Set[str] = default_data.get("exclude_paths", [])
+
+    model_config = SettingsConfigDict(env_prefix="dapperdata_")
