5c98a356aa1bf9568e04ac50751d2ebc1aee61ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:54 +0000

    Add package yokkaichi/1.5.2/yokkaichi-1.5.2-py3-none-any.whl

---
+++ packages/yokkaichi/yokkaichi-1.5.2-py3-none-any.whl/yokkaichi/__main__.py
@@ -0,0 +1,298 @@
+# Import modules
+import argparse
+import pathlib
+import platform
+import time
+from datetime import datetime
+
+import IP2Location
+import requests
+import tomli
+
+from yokkaichi import __version__
+
+from . import config_loader
+from .args_to_cfg import args_to_cfg
+from .constants.rich_console import console
+from .enums.MasscanMethods import MasscanMethods
+from .MasscanScan import MasscanScan
+from .port_parser import parse_port_range
+from .ServerScan import ServerScan
+
+
+def display_version():
+    console.print(
+        f"yokkaichi [bold cyan]{__version__}[/bold cyan] on [bold cyan]{platform.python_implementation()} {platform.python_version()}[/bold cyan]",
+        style="green",
+    )
+    exit()
+
+
+def get_country_ips(countries):
+    country_ip_list = []
+    for country in countries:
+        # Download CIDRs for country
+        cidr_list = requests.get(
+            f"https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/{country.lower()}.cidr"
+        )
+        # Check is the country valid
+        if cidr_list.text == "404: Not Found":
+            console.print(
+                f"[bold white]{country}[/bold white] is not a proper 2-letter code!",
+                style="red",
+            )
+            continue
+        # Add IPs to IP list
+        for ip in cidr_list.text.splitlines():
+            country_ip_list.append(ip)
+
+    return country_ip_list
+
+
+def verify_ip2location(db):
+    if not pathlib.Path(db).is_file():
+        console.print("IP2Location database doesn't exist", style="bold red")
+        exit(1)
+
+    try:
+        IP2Location.IP2Location(db)
+    except ValueError:
+        # IP2Location's default exception messages are okay
+        console.print("The IP2Location database is corrupted", style="bold red")
+        exit(1)
+
+
+def load_ip_list(ip_list_location):
+    ips = []
+    # Load file
+    try:
+        with open(ip_list_location, "r") as f:
+            ip_list = f.readlines()
+            for ip in ip_list:
+                ips.append(ip.strip())
+    except FileNotFoundError:
+        console.print("ERROR! IP LIST/MASSCAN LIST NOT FOUND!", style="red")
+        exit(1)
+
+    return ips
+
+
+def main(cfg):
+    if args.show_version:
+        # Show the version and exit
+        display_version()
+
+    # Load the config file
+    if args.config_file != None:
+        try:
+            cfg = config_loader.parse_cfg(args.config_file)
+            # TODO: Make use of it
+        except tomli.TOMLDecodeError:
+            console.print(
+                "Config file is invalid! (Failed parsing TOML)", style="bold red"
+            )
+        except FileNotFoundError:
+            console.print(
+                f"[bold white]{args.config_file}[/bold white] doesn't exist. Create a sample config in this location? (y/n) ",
+                style="yellow",
+                end="",
+            )
+            if input().lower() == "y":
+                config_loader.write_cfg(args.config_file)
+                console.print(
+                    f"Created a new config file at [bold white]{args.config_file}[/bold white]. Adjust it to your preferences",
+                    style="green",
+                )
+                exit(0)
+
+    # Check does output file exists
+    if pathlib.Path(cfg.output).is_file():
+        console.print(
+            "Output file exists. Continuing will overwrite this file. Proceed? (y/n) ",
+            style="yellow",
+            end="",
+        )
+        if input().lower() == "n":
+            exit(0)
+    else:
+        # Touch the file
+        pathlib.Path(cfg.output).touch()
+
+    if cfg.use_ip2location:
+        verify_ip2location(cfg.ip2location_db)
+
+    scan_start = time.time()
+
+    if cfg.ip_list_scan:
+        console.print("Loading IPs", style="cyan")
+        ip_list = load_ip_list(cfg.ip_list)
+        console.print(f"Loaded {len(ip_list)} IPs", style="green")
+    else:
+        ip_list = None
+
+    if cfg.masscan_scan:
+        masscan_ips_from_file = []
+        masscan_ips_for_countries = []
+        if cfg.masscan_ip_source == MasscanMethods.COUNTRIES:
+            # Get CIDR ranges for countries
+            masscan_ips = get_country_ips(cfg.masscan_country_list)
+        elif cfg.masscan_ip_source == MasscanMethods.LIST:
+            # Load masscan IP list
+            masscan_ips = load_ip_list(cfg.masscan_ip_list)
+
+        # Combine two sources of masscan IPs together
+        # masscan_ips = masscan_ips_from_file + masscan_ips_for_countries
+
+        # Start masscan
+        masscan_scanner = MasscanScan(cfg, masscan_ips)
+        masscan_results = masscan_scanner.start_scan()
+    else:
+        masscan_results = None
+
+    # print(clr.Fore.CYAN + "Loading IPs")
+    # ips = load_file()
+
+    scanner = ServerScan(cfg=cfg, masscan_list=masscan_results, ip_list=ip_list)
+    scanner.start_scan()
+
+    scan_end = time.time()
+
+    # Show results
+    scan_start_time = datetime.fromtimestamp(scan_start).isoformat()
+    scan_end_time = datetime.fromtimestamp(scan_end).isoformat()
+    scan_time = time.strftime("%H:%M:%S", time.gmtime(scan_end - scan_start))
+    console.print(
+        f"[bold white]{len(scanner.results)}[/bold white] servers found.\nStarted: [bold white]{scan_start_time}[/bold white].\nEnded: [bold white]{scan_end_time}[/bold white].\nTook [bold white]{scan_time}[/bold white].",
+        style="magenta",
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument(
+        "-j", "--java", dest="java", help="Scan for Java servers", action="store_true"
+    )
+    parser.add_argument(
+        "-b",
+        "--bedrock",
+        dest="bedrock",
+        help="Scan for Bedrock servers",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--ip-list-scan",
+        dest="ip_list_scan",
+        help="Scan from IP list (slow and outdated, use masscan instead)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--ip-list", dest="ip_list", help="Location to IP List", type=str, default=""
+    )
+    parser.add_argument(
+        "--masscan",
+        dest="masscan",
+        help="Enable scanning with masscan",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--masscan-method",
+        dest="masscan_method",
+        help="Where to look for IPs for masscan? Possible values: countries, list",
+        type=str,
+        default="countries",
+    )
+    parser.add_argument(
+        "--masscan-ip-list",
+        dest="masscan_ip_list",
+        help="Location to IP (or CIDR) list to scan by masscan before scanning with mcserver scanner",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "--masscan-countries",
+        dest="masscan_countries",
+        help="Countries to scan in 2-letter format",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--masscan-args",
+        dest="masscan_args",
+        help="Arguments for masscan (example: --max-rate 1000)",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "--masscan-output",
+        dest="masscan_json_output",
+        help="Output results to a file. To be used for debugging purposes.",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "-p",
+        "--ports",
+        dest="ports",
+        help="Ports to scan on. Example format: 25560-25569,42069. Uses 25565 by default",
+        type=str,
+        default="25565",
+    )
+    parser.add_argument(
+        "--query",
+        dest="query",
+        help="Query servers, required for player list but slows down the script (Note: might be broken at the moment)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--ip2location-db",
+        dest="ip2location_db",
+        help="IP2Location BIN database location, required for providing geolocation info",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "--ip2location-cache",
+        dest="ip2location_cache",
+        help="Cache IP2Location database to RAM. Make sure you have enough RAM to use this feature",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        dest="thread_count",
+        help="Number of threads (default: 100)",
+        type=int,
+        default=100,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output_file",
+        help="Output JSON file",
+        default="out.json",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        dest="config_file",
+        help="Configuration file (example one will be created if it doesn't exist)",
+        default=None,
+        const="yokkaichi.toml",
+        nargs="?",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        dest="show_version",
+        help="Show version and quit",
+        action="store_true",
+    )
+    parser.set_defaults(java=False, bedrock=False, query=False)
+    args = parser.parse_args()
+
+    cfg = args_to_cfg(args)
+
+    # if args.java and args.bedrock == False:
+    #     print("You need to choose either Java or Bedrock")
+    #     exit(1)
+
+    main(cfg)
---
+++ packages/yokkaichi/yokkaichi-1.5.2-py3-none-any.whl/yokkaichi/config_loader.py
@@ -0,0 +1,125 @@
+import tomli
+
+from .constants.rich_console import console
+from .enums.MasscanMethods import MasscanMethods
+from .enums.Platforms import Platforms
+from .port_parser import parse_port_range
+from .structs.CFG import CFG
+
+CONFIG_VERSION = "1"
+# TODO: Bring this back to a separate file
+SAMPLE_CFG = """
+# This is an example configuration file for Yokkaichi.
+# Configure this for your preferences.
+# Pass the location to this config file with -c as an argument.
+# Example: python -m yokkaichi -c sample.toml
+# You can pass this without the file location and it will look for yokkaichi.toml in your current location.
+# Do not comment any of the config's options, it will currently cause a crash
+
+version = "1" # Changed for every change breaking config compatibility
+
+[platforms]
+java = true
+bedrock = false
+
+[platforms.additional]
+java_query = false # Use the Query protocol (more info, but a little bit broken and slow right now)
+
+[type]
+masscan = true # Recommended, fast
+ip_list = false # Not recommended, slow, outdated
+
+[type.options_masscan]
+ip_source = "countries" # Availible types: "countries", "list"
+args = "" # Additional arguments for masscan
+output = false
+output_location = "masscan_out.json" # Where to output masscan's results (not required)
+
+[type.options_masscan.countries]
+countries = ["US", "DE"] # Standard TOML array, use ISO 3166-1 alpha-2 codes (the 2 letter ones)
+
+[type.options_masscan.list]
+list = "masscan_ips.txt" # Location to the list of IPs to scan with masscan, separated by newlines
+
+[type.options_ip_list]
+list = "ips.txt" # Location to the list of IPs to scan, separated by newlines
+
+[scanner]
+ports = "25564-25566,25569" # Port list (not TOML's format! Similarly to nmap and masscan splits ports by commas and sets ranges with hyphens)
+threads = 100 # Leave this at default unless you have a lot of servers
+output = "out.json" # IMPORTANT! That's where the servers go!
+
+[ip2location]
+enabled = false # Enable getting the location of the server
+db = "IP2LOCATION-LITE-DB11.BIN" # IP2Location .BIN database (get from lite.ip2location.com)
+cache = true # Enable for faster speed at the cost of RAM
+""".strip()
+
+
+def load_cfg(cfg_location):
+    with open(cfg_location, "rb") as f:
+        cfg_file = tomli.load(f)
+    return cfg_file
+
+
+def parse_cfg(cfg_location):
+    # Read the config file
+    cfg_file = load_cfg(cfg_location)
+
+    cfg = CFG()
+
+    # TODO: Check if the file is a valid Yokkaichi config!
+    cfg_file_ver = cfg_file["version"]
+    if cfg_file_ver != CONFIG_VERSION:
+        console.print(
+            f"Wrong config version detected! Please update your config. Your config: {cfg_file_ver}. Expected: {CONFIG_VERSION}",
+            style="bold red",
+        )
+        exit(1)
+        return True
+
+    if cfg_file["platforms"]["java"]:
+        cfg.platforms.append(Platforms.JAVA)
+    if cfg_file["platforms"]["bedrock"]:
+        cfg.platforms.append(Platforms.BEDROCK)
+
+    cfg.query_java = cfg_file["platforms"]["additional"]["java_query"]
+
+    cfg.masscan_scan = cfg_file["type"]["masscan"]
+    cfg.ip_list_scan = cfg_file["type"]["ip_list"]
+
+    if (
+        cfg_file["type"]["options_masscan"]["ip_source"]
+        == MasscanMethods.COUNTRIES.value
+    ):
+        cfg.masscan_ip_source = MasscanMethods.COUNTRIES
+    elif cfg_file["type"]["options_masscan"]["ip_source"] == MasscanMethods.LIST.value:
+        cfg.masscan_ip_source = MasscanMethods.LIST
+
+    cfg.masscan_args = cfg_file["type"]["options_masscan"]["args"]
+    cfg.masscan_output = cfg_file["type"]["options_masscan"]["output"]
+    cfg.masscan_output_location = cfg_file["type"]["options_masscan"]["output_location"]
+
+    cfg.masscan_country_list = cfg_file["type"]["options_masscan"]["countries"][
+        "countries"
+    ]
+
+    cfg.masscan_ip_list = cfg_file["type"]["options_masscan"]["list"]["list"]
+
+    cfg.ip_list = cfg_file["type"]["options_ip_list"]["list"]
+
+    cfg.ports = parse_port_range(cfg_file["scanner"]["ports"])
+    cfg.threads = cfg_file["scanner"]["threads"]
+    cfg.output = cfg_file["scanner"]["output"]
+
+    cfg.use_ip2location = cfg_file["ip2location"]["enabled"]
+    cfg.ip2location_db = cfg_file["ip2location"]["db"]
+    cfg.ip2location_cache = cfg_file["ip2location"]["cache"]
+
+    return cfg
+
+
+def write_cfg(cfg_location):
+    # Write the sample config
+    with open(cfg_location, "w") as f:
+        f.write(SAMPLE_CFG)
