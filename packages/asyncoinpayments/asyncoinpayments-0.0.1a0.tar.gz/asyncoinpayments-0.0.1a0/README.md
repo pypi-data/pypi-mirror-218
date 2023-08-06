9c38f716b27af9172973e1f32e7afd80a91d5e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:08 +0000

    Add package asyncoinpayments/0.0.1a0/asyncoinpayments-0.0.1a0.tar.gz

---
+++ packages/asyncoinpayments/asyncoinpayments-0.0.1a0.tar.gz/asyncoinpayments-0.0.1a0/README.md
@@ -0,0 +1,20 @@
+# AsynCoinpayments  
+
+An async/await compatible python wrapper for the CoinPayments API  
+
+---
+
+## How to install  
+
+In the command prompt, type `pip install asyncoinpayments`
+
+## Features  
+- Fast to setup and simple to use
+- Easy to use async/await interface
+- API calls using aiohttp requests  
+- create your own API calls
+
+### Contact me
+
+**Email** : <flalugli.dev@gmail.com>
+**Discord** : flalugli 
---
+++ packages/asyncoinpayments/asyncoinpayments-0.0.1a0.tar.gz/asyncoinpayments-0.0.1a0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1a'
+DESCRIPTION = 'Python Asynchronous Wrapper of the CoinPayments API'
+
+
+# Setting up
+setup(
+    name="asyncoinpayments",
+    version=VERSION,
+    author="fla (Flavio Lugli)",
+    author_email="<flalugli.dev@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/flalugli/asyncoinpayments",
+    license="Apache License 2.0",
+    packages=find_packages(),
+    install_requires=['aiohttp', 'tenacity'],
+    keywords=['python', 'crypto', 'cryptocurrency', 'payment gateway', 'async', 'aiohttp'],
+    extras_require={"dev": ["twine>=4.0.2"]},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8"
+)
