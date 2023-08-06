7af63dabf1c819cd251db60999e4fc18bcb628ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:42 +0000

    Add package klcreqs/2023.7.7/klcreqs-2023.7.7.tar.gz

---
+++ packages/klcreqs/klcreqs-2023.7.7.tar.gz/klcreqs-2023.7.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="klcreqs",
+        version="2023.07.07",
+        description="library for streamlining api requests",
+        long_description="coming soon",
+        readme="README.md",
+        author='Thomas Nemechek',
+        author_email='tnemechek@klcapital.com',
+        classifiers=["Programming Language :: Python"],
+        packages=['klcreqs'],
+        include_package_data=True,
+        install_requires=['numpy>=1.20.3',
+        'pandas>=1.4.3',
+        'requests>=2.26.0']
+        )
