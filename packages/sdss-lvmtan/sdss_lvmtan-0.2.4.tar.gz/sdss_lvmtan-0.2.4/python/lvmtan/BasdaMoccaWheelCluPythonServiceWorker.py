c5a65477941b4eaac21c4af58441a4f3903b3195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:05 +0000

    Add package sdss-lvmtan/0.2.4/sdss_lvmtan-0.2.4.tar.gz

---
+++ packages/sdss-lvmtan/sdss_lvmtan-0.2.4.tar.gz/sdss_lvmtan-0.2.4/python/lvmtan/BasdaMoccaWheelCluPythonServiceWorker.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-06-15
+# @Filename: BasdaMoccaWheelCluPythonServiceWorker.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+import BasdaMoccaException
+import BasdaMoccaX
+import BasdaService
+import Nice
+import numpy as np
+
+from .BasdaMoccaCluPythonServiceWorker import *
+
+
+class BasdaMoccaWheelCluPythonServiceWorker(BasdaMoccaCluPythonServiceWorker):
+    "python clu worker"
+
+    def __init__(self, _svcName):
+        BasdaMoccaCluPythonServiceWorker.__init__(self, _svcName)
+
+
+    @command_parser.command("scanAllReferenceSwitches")
+    @BasdaCluPythonServiceWorker.wrapper
+    async def scanAllReferenceSwitches(self):
+        """Scan all reference switches"""
+        try:
+            self.service.scanAllReferenceSwitchesStart()
+            while not self.service.scanAllReferenceSwitchestCompletion().isDone():
+                command.info(
+                    Position=self.service.getPosition(),
+                    DeviceEncoder={"Position": self.service.getDeviceEncoderPosition("STEPS"), "Unit": "STEPS"},
+                    Velocity=self.service.getVelocity(),
+                    AtHome=self.service.isAtHome(),
+                    AtLimit=self.service.isAtLimit(),
+                )
+            self.service.scanAllReferenceSwitchesWait()
+            return command.finish(**self._status(self.service.isReachable()))
+
+        except Exception as e:
+            command.fail(error=e)
---
+++ packages/sdss-lvmtan/sdss_lvmtan-0.2.4.tar.gz/sdss_lvmtan-0.2.4/python/lvmtan/exceptions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+# @Author: Florian Briegel
+# @Date:   2022-09-05 12:01:21
+
+
+class LvmTanError(Exception):
+    """Core exception"""
+
+class LvmTanNotImplemented(LvmTanError):
+    """Not yet implemented features."""
+
+class LvmTanApiAuthError(LvmTanError):
+    """API authentication errors"""
+
+class LvmTanOutOfRange(LvmTanError):
+    """Out of range error"""
+
+class LvmTanMotorIsStillMoving(LvmTanError):
+    """Motor is still in movement"""
+
