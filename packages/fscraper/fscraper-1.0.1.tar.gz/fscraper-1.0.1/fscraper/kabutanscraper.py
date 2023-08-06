e63c93dd64e76830548cad8e1b570c4be2e47cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:17 +0000

    Add package fscraper/1.0.1/fscraper-1.0.1.tar.gz

---
+++ packages/fscraper/fscraper-1.0.1.tar.gz/fscraper-1.0.1/fscraper/kabutanscraper.py
@@ -0,0 +1,7 @@
+
+def get_minutes_chart():
+    """
+    The info could retrieve from "https://kabutan.jp/stock/chart?code=7203" with a GET request.
+    :NOTE: The format of response is plain html 
+    """
+    return
