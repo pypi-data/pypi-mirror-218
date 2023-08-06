e9d99e5d1f09d82c103e169559691f354de85807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:33 +0000

    Add package zfl-ai/0.0.4/zfl_ai-0.0.4.tar.gz

---
+++ packages/zfl-ai/zfl_ai-0.0.4.tar.gz/zfl_ai-0.0.4/ai/cat_cnn.py
@@ -0,0 +1,27 @@
+﻿"""
+
+画像が挿入されると、猫・ライオン・チーターを学習したCNNモデルをロードして.
+
+与えられた画像に対しての予測結果を出力する.
+
+"""
+
+import os
+
+from tensorflow.keras.models import load_model  # type: ignore
+
+classes = ["猫", "ライオン", "チーター"]
+
+
+def predict(img):
+    """
+    モデルのロード
+    """
+
+    file_path = "ai/catai_cnn_new.h5"
+    if not os.path.isfile(file_path):
+        file_path = "catai_cnn_new.h5"
+    model = load_model(file_path)
+    result = model.predict([img])[0]
+    predicted = result.argmax()
+    return str(classes[predicted])
