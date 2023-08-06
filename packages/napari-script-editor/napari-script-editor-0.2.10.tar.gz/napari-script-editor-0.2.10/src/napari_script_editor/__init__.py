45817216366eb31c0b503fc4a5415087fd571255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:26 +0000

    Add package napari-script-editor/0.2.10/napari-script-editor-0.2.10.tar.gz

---
+++ packages/napari-script-editor/napari-script-editor-0.2.10.tar.gz/napari-script-editor-0.2.10/src/napari_script_editor/__init__.py
@@ -0,0 +1,11 @@
+
+__version__ = "0.2.10"
+
+
+
+
+from ._dock_widget import napari_experimental_provide_dock_widget, ScriptEditor
+
+from ._scripts_directory import _search_scripts
+
+_search_scripts()
