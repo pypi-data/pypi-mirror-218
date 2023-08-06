8b51067d20a04a09826a2e9c82dd2da3bfc48d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:16 +0000

    Add package logPPP/1.0.1/logPPP-1.0.1.tar.gz

---
+++ packages/logPPP/logPPP-1.0.1.tar.gz/logPPP-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='logPPP',
+    version='1.0.1',
+    description='logPPP',
+    author='Aurorax-own',
+    author_email='15047150695@163.com',
+    packages=['logPPP'],
+    install_requires=[
+    ]
+)
