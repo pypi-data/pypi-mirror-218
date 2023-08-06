03c2029a709eacb478fe325997196100aeb618a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package intentify/0.0.3/intentify-0.0.3.tar.gz

---
+++ packages/intentify/intentify-0.0.3.tar.gz/intentify-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'An intent classification library for AI Models.'
+
+# Setting up
+setup(
+    name="intentify",
+    version=VERSION,
+    author="Ice (Rishan Pancham)",
+    author_email="<rishanpan@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'tensorflow', 'nltk', 'keras'],
+    keywords=['python', 'intent', 'intents', 'ai', 'intent classification'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
