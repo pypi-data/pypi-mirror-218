39f10eaf6efd0f1368d504db706e83638f9d6794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:57 +0000

    Add package roboto-sdk/0.1.10/roboto_sdk-0.1.10.tar.gz

---
+++ packages/roboto-sdk/roboto_sdk-0.1.10.tar.gz/roboto_sdk-0.1.10/src/roboto_sdk/exceptions/__init__.py
@@ -0,0 +1,15 @@
+from .domain import (
+    RobotoDomainException,
+    RobotoHttpExceptionParse,
+    RobotoNoOrgProvidedException,
+    RobotoNotFoundException,
+    RobotoUnauthorizedException,
+)
+
+__all__ = [
+    "RobotoDomainException",
+    "RobotoHttpExceptionParse",
+    "RobotoNotFoundException",
+    "RobotoNoOrgProvidedException",
+    "RobotoUnauthorizedException",
+]
