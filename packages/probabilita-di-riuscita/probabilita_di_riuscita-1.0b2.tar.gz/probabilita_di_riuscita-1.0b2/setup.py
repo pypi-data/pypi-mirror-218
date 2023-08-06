be4080529a07f63d30a83aa3949939058cbfb84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package probabilita-di-riuscita/1.0b2/probabilita_di_riuscita-1.0b2.tar.gz

---
+++ packages/probabilita-di-riuscita/probabilita_di_riuscita-1.0b2.tar.gz/probabilita_di_riuscita-1.0b2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabilita_di_riuscita',
+      version='1.0b2',
+      description='Gaussian and Binomial distributions',
+      packages=['probabilita_di_riuscita'],
+      author = 'Roberto Giorgi',
+      author_email = 'rjnenjo3@testmail.com',
+      zip_safe=False)
