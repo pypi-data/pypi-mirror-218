c968b96a235f60aa7b7900f1d794162b25cf69b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:51 +0000

    Add package windmill-api/1.127.1/windmill_api-1.127.1.tar.gz

---
+++ packages/windmill-api/windmill_api-1.127.1.tar.gz/windmill_api-1.127.1/windmill_api/models/preview_language.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class PreviewLanguage(str, Enum):
+    PYTHON3 = "python3"
+    DENO = "deno"
+    GO = "go"
+    BASH = "bash"
+    POSTGRESQL = "postgresql"
+    NATIVETS = "nativets"
+    BUN = "bun"
+
+    def __str__(self) -> str:
+        return str(self.value)
