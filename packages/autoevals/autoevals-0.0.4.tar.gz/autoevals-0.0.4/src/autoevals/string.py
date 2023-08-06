e233cb524117f4fb8d31f81ece7439cf94984135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:49 +0000

    Add package autoevals/0.0.4/autoevals-0.0.4.tar.gz

---
+++ packages/autoevals/autoevals-0.0.4.tar.gz/autoevals-0.0.4/src/autoevals/string.py
@@ -0,0 +1,19 @@
+from Levenshtein import distance
+
+from .base import Evaluator
+
+
+class LevenshteinEvaluator(Evaluator):
+    async def _run_eval_sync(self, output, expected=None, **kwargs):
+        if expected is None:
+            raise ValueError("LevenshteinEvaluator requires an expected value")
+
+        output, expected = str(output), str(expected)
+        max_len = max(len(x) for x in [output, expected])
+        if max_len == 0:
+            return 1
+
+        return 1 - (distance(output, expected) / max(len(x) for x in [output, expected]))
+
+
+__all__ = ["LevenshteinEvaluator"]
