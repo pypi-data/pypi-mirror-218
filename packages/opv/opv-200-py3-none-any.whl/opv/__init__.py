6334c209b8b3a2486e1c0c7c9647dd6d67554ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:29 +0000

    Add package opv/200/opv-200-py3-none-any.whl

---
+++ packages/opv/opv-200-py3-none-any.whl/opv/__init__.py
@@ -0,0 +1,11 @@
+# This file is placed in the Public Domain.
+
+
+"""clean namespace"""
+
+
+__author__ = "Bart Thate <programmingobject@gmail.com>"
+
+
+from opv.objects import *
+from opv.objects import __all__, __dir__
