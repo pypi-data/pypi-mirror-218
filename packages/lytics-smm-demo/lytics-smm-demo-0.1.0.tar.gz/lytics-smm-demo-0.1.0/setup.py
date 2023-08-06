a0b21cc8a90de90dc95c07c8a3b4f5427d079aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:31 +0000

    Add package lytics-smm-demo/0.1.0/lytics-smm-demo-0.1.0.tar.gz

---
+++ packages/lytics-smm-demo/lytics-smm-demo-0.1.0.tar.gz/lytics-smm-demo-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lytics-smm-demo",
+    version="0.1.0",
+    author="Lytics SMM",
+    description="A Python package for interacting with the Lytics SMM API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/accesstokens/lytics-smm-demo",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
