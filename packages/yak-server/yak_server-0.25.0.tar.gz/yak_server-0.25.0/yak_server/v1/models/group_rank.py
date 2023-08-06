965e09783fbe11a2efe4b16437a01ca41b350f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:46 +0000

    Add package yak-server/0.25.0/yak_server-0.25.0.tar.gz

---
+++ packages/yak-server/yak_server-0.25.0.tar.gz/yak_server-0.25.0/yak_server/v1/models/group_rank.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel
+
+from .generic import PositiveOrZeroInt
+from .teams import TeamOut
+
+if TYPE_CHECKING:
+    from yak_server.database.models import GroupPositionModel
+    from yak_server.helpers.language import Lang
+
+
+class GroupPositionOut(BaseModel):
+    team: TeamOut
+    played: PositiveOrZeroInt
+    won: PositiveOrZeroInt
+    drawn: PositiveOrZeroInt
+    lost: PositiveOrZeroInt
+    goals_for: PositiveOrZeroInt
+    goals_against: PositiveOrZeroInt
+    goals_difference: int
+    points: PositiveOrZeroInt
+
+    @classmethod
+    def from_instance(
+        cls,
+        group_position: "GroupPositionModel",
+        lang: "Lang",
+    ) -> "GroupPositionOut":
+        return cls(
+            team=TeamOut.from_instance(group_position.team, lang),
+            played=group_position.played,
+            won=group_position.won,
+            drawn=group_position.drawn,
+            lost=group_position.lost,
+            goals_for=group_position.goals_for,
+            goals_against=group_position.goals_against,
+            goals_difference=group_position.goals_difference,
+            points=group_position.points,
+        )
