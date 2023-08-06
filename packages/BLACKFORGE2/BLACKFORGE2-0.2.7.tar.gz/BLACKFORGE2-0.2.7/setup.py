75028d4cfc5381a9578d8b862f64c420ecdb9cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:35 +0000

    Add package BLACKFORGE2/0.2.7/BLACKFORGE2-0.2.7.tar.gz

---
+++ packages/BLACKFORGE2/BLACKFORGE2-0.2.7.tar.gz/BLACKFORGE2-0.2.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='BLACKFORGE2',
+    version='0.2.7',    
+    description='A Framework For Building 2d Games With Python+Pygame.',
+    url='https://github.com/setoyuma/BLACKFORGE2',
+    author='Setoichi',
+    author_email='setoichi.dev@gmail.com',
+    license='MIT',
+    packages=['BLACKFORGE2'],
+    install_requires=['pygame-ce',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+)
