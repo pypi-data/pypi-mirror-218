b1ceaf8dd4c6e204a80116a9882af39093bce2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package elsasa/0.1/elsasa-0.1.tar.gz

---
+++ packages/elsasa/elsasa-0.1.tar.gz/elsasa-0.1/elsasa/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="elsasa_pack", 
+ version="0.11",
+ author = 'Mostafa Elhadidy',
+ description="Gaussian distributions", 
+ packages=['elsasa_pack'],
+ zip_safe=False)
