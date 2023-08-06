df1959573e186b6bfb977e36815e6f1c6c4e0136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:44 +0000

    Add package cs-models/0.0.600/cs-models-0.0.600.tar.gz

---
+++ packages/cs-models/cs-models-0.0.600.tar.gz/cs-models-0.0.600/src/cs_models/resources/Collection/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    String,
+)
+
+from ...database import Base
+
+
+class CollectionModel(Base):
+    __tablename__ = "collections"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    type = Column(String(50), nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
