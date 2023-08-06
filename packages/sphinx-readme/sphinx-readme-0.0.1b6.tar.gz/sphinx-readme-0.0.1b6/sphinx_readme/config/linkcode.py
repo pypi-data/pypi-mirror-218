e4c20b91bbd403d942d8c67136557e76b3cd4a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:17 +0000

    Add package sphinx-readme/0.0.1b6/sphinx-readme-0.0.1b6.tar.gz

---
+++ packages/sphinx-readme/sphinx-readme-0.0.1b6.tar.gz/sphinx-readme-0.0.1b6/sphinx_readme/config/linkcode.py
@@ -0,0 +1,221 @@
+import os
+import re
+import sys
+import inspect
+import subprocess
+import pkg_resources
+from pathlib import Path
+from typing import Dict, Optional, Callable
+from sphinx.errors import ExtensionError
+from sphinx_readme.utils import logger
+
+
+def get_linkcode_url(blob_url: Optional[str] = None,
+                     repo_url: Optional[str] = None,
+                     context: Optional[Dict] = None,
+                     blob: Optional[str] = None) -> str:
+    """Get the template URL for linking to highlighted GitHub source code
+
+    Formatted into the final link by ``linkcode_resolve()``
+    """
+    if blob_url is None:
+        if repo_url is None:
+            if context is None:
+                raise ExtensionError(
+                    "``sphinx_readme:`` config value ``html_context`` is missing")
+            else:
+                repo_url = get_repo_url(context)
+        blob_url = get_blob_url(repo_url, blob, context)
+
+    if 'bitbucket' in blob_url:
+        return blob_url + "/{filepath}#lines-{linestart}:{linestop}"
+    else:
+        return blob_url + "/{filepath}#L{linestart}-L{linestop}"
+
+
+def get_repo_url(context: Dict):
+    for host in ('github', 'gitlab', 'bitbucket'):
+        user = context.get(f"{host}_user")
+        repo = context.get(f"{host}_repo")
+
+        if not all((user, repo)):
+            continue
+
+        if host == "bitbucket":
+            tld = "org"
+        else:
+            tld = "com"
+
+        return f"https://{host}.{tld}/{user}/{repo}"
+
+    raise ExtensionError(
+        "``sphinx_readme``: unable to determine repo url")
+
+
+def get_blob_url(repo_url: str, blob: Optional[str] = None, context: Optional[Dict] = None) -> str:
+    """Generate the url for a specific blob of a repository
+
+    If ``blob`` and ``context`` are not provided, the most recent commit hash will be used
+    """
+    if context:
+        host = get_repo_host(repo_url)
+        blob = context.get(f"{host}_version")
+
+    if blob is not None:
+        # Use blob from kwarg/html_context
+        blob = get_linkcode_revision(blob)
+    else:
+        # Use hash of the most recent commit
+        blob = get_linkcode_revision('head')
+
+    if "bitbucket" in repo_url:
+        return repo_url.strip('/') + f"/src/{blob}"
+    else:
+        return repo_url.strip("/") + f"/blob/{blob}"
+
+
+def get_repo_host(url: str):
+    if match := re.match(r"https?://(\w+)\.(?:com|org)", url):
+        return match.group(1)
+    return None
+
+
+def get_linkcode_revision(blob: str) -> str:
+    """Get the blob to link to
+
+    .. note::
+
+       The value of ``blob`` can be any of ``"head"``, ``"last_tag"``, or ``"{blob}"``
+
+       * ``head`` (default): links to the most recent commit hash; if this commit is tagged, uses the tag instead
+       * ``last_tag``: links to the most recently tagged commit; if no tags exist, uses ``head``
+       * ``blob``: links to any blob you want, for example ``"master"`` or ``"v2.0.1"``
+    """
+    if blob == "head":
+        return get_head()
+    if blob == 'last_tag':
+        return get_last_tag()
+    # Link to the branch/tree/blob you provided, ex. "master"
+    return blob
+
+
+def get_head(errors: bool = False) -> Optional[str]:
+    """Gets the most recent commit hash or tag
+
+    :raises subprocess.CalledProcessError: if the commit can't be found and ``errors`` is ``True``
+    """
+    cmd = "git log -n1 --pretty=%H"
+    try:
+        # get most recent commit hash
+        head = subprocess.check_output(cmd.split()).strip().decode('utf-8')
+
+        # if head is a tag, use tag as reference
+        cmd = "git describe --exact-match --tags " + head
+        try:
+            tag = subprocess.check_output(cmd.split(" ")).strip().decode('utf-8')
+            return tag
+
+        except subprocess.CalledProcessError:
+            return head
+
+    except subprocess.CalledProcessError as e:
+        if errors:
+            raise e
+        else:
+            return logger.error("Failed to get head")  # so no head?
+
+
+def get_last_tag() -> str:
+    """Get the most recent commit tag
+
+    :raises RuntimeError: if there are no tagged commits
+    """
+    try:
+        cmd = "git describe --tags --abbrev=0"
+        return subprocess.check_output(cmd.split(" ")).strip().decode('utf-8')
+
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("No tags exist for the repo") from e
+
+
+def get_repo_dir() -> Path:
+    """Get the root directory of the repository
+
+    :raises RuntimeError: if the directory can't be determined
+    """
+    try:
+        cmd = "git rev-parse --show-toplevel"
+        repo_dir = Path(subprocess.check_output(cmd.split(" ")).strip().decode('utf-8'))
+
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("Unable to determine the repository directory") from e
+
+    # For ReadTheDocs, repo is cloned to /path/to/<repo_dir>/checkouts/<version>/
+    if repo_dir.parent == "checkouts":
+        return repo_dir.parent.parent
+    else:
+        return repo_dir
+
+
+def get_linkcode_resolve(linkcode_url: str) -> Callable:
+    """Defines and returns a ``linkcode_resolve`` function for your package
+
+    Used by default if ``linkcode_resolve`` isn't defined in ``conf.py``
+    """
+
+    repo_dir = get_repo_dir()
+    pkg = pkg_resources.require(repo_dir.name)[0]
+    top_level = pkg.get_metadata('top_level.txt').strip()
+
+    def linkcode_resolve(domain, info):
+        """Returns a link to the source code on GitHub, with appropriate lines highlighted
+
+        :By:
+            Adam Korn (https://github.com/tdkorn)
+        :Adapted From:
+            nlgranger/SeqTools (https://github.com/nlgranger/seqtools/blob/master/docs/conf.py)
+        """
+        if domain != 'py' or not info['module']:
+            return None
+
+        modname = info['module']
+        fullname = info['fullname']
+
+        submod = sys.modules.get(modname)
+        if submod is None:
+            return None
+
+        obj = submod
+        for part in fullname.split('.'):
+            try:
+                obj = getattr(obj, part)
+            except AttributeError:
+                return None
+
+        try:
+            filepath = os.path.relpath(inspect.getsourcefile(obj), repo_dir)
+            if filepath is None:
+                return
+        except Exception:
+            return None
+
+        try:
+            source, lineno = inspect.getsourcelines(obj)
+        except OSError:
+            return None
+        else:
+            linestart, linestop = lineno, lineno + len(source) - 1
+
+        # Fix links with "../../../" or "..\\..\\..\\"
+        filepath = '/'.join(filepath[filepath.find(top_level):].split('\\'))
+
+        # Example: https://github.com/TDKorn/my-magento/blob/docs/magento/models/model.py#L28-L59
+        final_link = linkcode_url.format(
+            filepath=filepath,
+            linestart=linestart,
+            linestop=linestop
+        )
+        logger.debug(f"Final Link for {fullname}: {final_link}")
+        return final_link
+
+    return linkcode_resolve
---
+++ packages/sphinx-readme/sphinx-readme-0.0.1b6.tar.gz/sphinx-readme-0.0.1b6/sphinx_readme/config/main.py
@@ -0,0 +1,268 @@
+import os
+import re
+import copy
+from pathlib import Path
+from collections import defaultdict
+from typing import Union, List, Dict
+from functools import cached_property
+
+from sphinx.application import Sphinx
+from sphinx.errors import ExtensionError
+
+from sphinx_readme.utils import get_conf_val, set_conf_val, logger, replace_only_directives, remove_raw_directives
+from sphinx_readme.config import get_repo_dir, get_repo_host, get_blob_url, get_repo_url, get_linkcode_url, get_linkcode_resolve
+
+
+class READMEConfig:
+
+    REFERENCE_MAPPING = {
+        "replace": None,
+        "target": None,
+    }
+
+    def __init__(self, app: Sphinx):
+        self.logger = logger
+        self.src_dir = Path(app.srcdir)
+        self.repo_dir = get_repo_dir()
+        self.out_dir = get_conf_val(app, 'readme_out_dir')
+        self.src_files = get_conf_val(app, 'readme_src_files', [])
+        self.html_context = get_conf_val(app, "html_context")
+        self.html_baseurl = get_conf_val(app, "html_baseurl", "").rstrip("/")
+        self.docs_url_type = get_conf_val(app, 'readme_docs_url_type')
+        self.replace_attrs = get_conf_val(app, 'readme_replace_attrs')
+        self.inline_markup = get_conf_val(app, 'readme_inline_markup')
+        self.raw_directive = get_conf_val(app, 'readme_raw_directive')
+        self.rubric_heading = get_conf_val(app, 'readme_rubric_heading')
+        self.admonition_icons = get_conf_val(app, 'readme_admonition_icons')
+        self.include_directive = get_conf_val(app, 'readme_include_directive')
+        self.default_admonition_icon = get_conf_val(app, 'readme_default_admonition_icon')
+
+        self.repo_blob = get_conf_val(app, "readme_blob")
+        self.repo_url = self.get_repo_url()
+        self.blob_url = get_blob_url(
+            repo_url=self.repo_url,
+            blob=self.repo_blob
+        )
+        self.docs_url = self.get_docs_url()
+        self.ref_map = self.get_ref_map()
+        self.source_files = self.read_source_files()
+
+        if self.docs_url_type == "code":
+            self.setup_linkcode_resolve(app)
+
+    def get_repo_url(self) -> str:
+        """Generates the repository URL from the :external+sphinx:confval:`html_context` dict
+
+        :raises ExtensionError: if ``html_context`` is not set or missing values
+        """
+        if not self.html_context:
+            raise ExtensionError(
+                "``sphinx_readme``: conf.py value "
+                "must be set for ``html_context``"
+            )
+        return get_repo_url(self.html_context)
+
+    def get_docs_url(self) -> str:
+        """Returns the base URL of the documentation source to use
+        when resolving :mod:`~.sphinx.ext.autodoc` cross-references
+
+         If :attr:`docs_url_type` is
+
+         * ``"code"``: uses the :attr:`blob_url`
+         * ``"html"``: uses the :confval:`html_baseurl`
+
+        :raises ExtensionError: if ``html_baseurl`` is missing
+        """
+        if self.docs_url_type == "html":
+            if self.html_baseurl:
+                docs_url = self.html_baseurl
+            else:
+                raise ExtensionError(
+                    "``sphinx_readme``: conf.py value "
+                    "must be set for ``html_baseurl``"
+                )
+        else:  # ``docs_url_type`` is "code"
+            docs_url = self.blob_url
+
+        return docs_url.rstrip("/")
+
+    def setup_linkcode_resolve(self, app: Sphinx) -> None:
+        linkcode_func = get_conf_val(app, "linkcode_resolve")
+
+        if not callable(linkcode_func):
+            self.logger.info(
+                "``sphinx_readme:`` using default ``linkcode_resolve``"
+            )
+            # Get the template for linking to source code
+            linkcode_url = get_linkcode_url(self.blob_url)
+            linkcode_func = get_linkcode_resolve(linkcode_url)
+
+        set_conf_val(app, 'linkcode_resolve', linkcode_func)
+        app.setup_extension("sphinx.ext.linkcode")
+
+    def get_ref_map(self) -> Dict:
+        refs = defaultdict(_map_entry)
+        refs['ref'] = []
+        refs['doc'] = []
+        return refs
+
+    def read_source_files(self) -> Dict[str, str]:
+        sources = {
+            src_file: self.read_rst(src_file)
+            for src_file in self.src_files
+        }
+        return sources
+
+    def read_rst(self, rst_file: Union[str, Path], replace_only: bool = False) -> str:
+        with open(rst_file, 'r', encoding='utf-8') as f:
+            rst = f.read()
+
+        if replace_only:
+            rst = replace_only_directives(rst)
+
+        include_pattern = r"^\.\. include:: ([./]*?[\w/-]+\.rst)"
+
+        if self.include_directive:
+            # Find all included files
+            included = re.findall(
+                pattern=include_pattern,
+                string=rst,
+                flags=re.M
+            )
+            for include in included:
+                # Determine abs path of included file
+                if include.startswith("/"):
+                    # These paths are relative to source dir
+                    file = Path(f"{self.src_dir}{include}").resolve()
+                else:
+                    # These paths are relative to rst_file dir
+                    file = (Path(rst_file).parent / Path(include)).resolve()
+
+                # Sub in the file content
+                rst = re.sub(
+                    pattern=rf".. include:: {include}",
+                    repl=self.read_rst(file, replace_only).replace(r'\n', r'\\n'),
+                    string=rst
+                )
+        else:
+            # Remove all include directives from the text
+            rst = re.sub(include_pattern, '', rst, flags=re.M)
+
+        if self.raw_directive is False:
+            rst = remove_raw_directives(rst)
+
+        return rst
+
+    @property
+    def src_files(self):
+        return self._src_files
+
+    @src_files.setter
+    def src_files(self, src_files: Union[str, List[str]]):
+        if isinstance(src_files, str):
+            src_files = [src_files]
+
+        self._src_files = [  # Absolute paths of files; files should be relative to source directory
+            str((Path(self.src_dir) / Path(src_file)).resolve())
+            for src_file in src_files
+        ]
+
+    @property
+    def out_dir(self):
+        return self._out_dir
+
+    @out_dir.setter
+    def out_dir(self, out: str):
+        out_dir = Path(out)
+
+        if not out_dir.is_absolute():
+            out_dir = (self.src_dir / out_dir).resolve()
+
+        if not out_dir.exists():
+            out_dir.mkdir()
+
+        self._out_dir = out_dir
+
+    @property
+    def docs_url_type(self):
+        return self._docs_url_type
+
+    @docs_url_type.setter
+    def docs_url_type(self, docs_url_type: str):
+        if docs_url_type not in ('html', 'code'):
+            raise ExtensionError(
+                "``sphinx_readme``: ``readme_docs_url_type`` value must be ``code`` or ``html``"
+            )
+        if docs_url_type == "code" and not self.html_baseurl:
+            raise ExtensionError(  # HTML url is needed for non-source code xrefs
+                "``sphinx_readme``: conf.py value missing for ``html_baseurl``"
+            )
+        self._docs_url_type = docs_url_type
+
+    @cached_property
+    def repo_host(self):
+        return get_repo_host(self.repo_url)
+
+    @cached_property
+    def image_baseurl(self):
+        if self.repo_host == "github":
+            # Ex. https://raw.githubusercontent.com/TDKorn/sphinx-readme/main
+            return self.blob_url.replace("github.com", "raw.githubusercontent.com").replace('blob/', '')
+
+        elif self.repo_host == "gitlab":
+            # Ex. https://gitlab.com/TDKorn/sphinx-readme/raw/main
+            return self.blob_url.replace("/blob/", "/raw/")
+
+        else:
+            # Ex. https://bitbucket.org/TDKorn/sphinx-readme/raw/main
+            return self.blob_url.replace("/src/", "/raw/")
+
+
+    @cached_property
+    def icon_map(self):
+        types = ("attention", "caution", "danger", "error", "hint", "important", "note", "tip", "warning", "default")
+        icons = ("🔔️", "⚠️", "☢️", '⛔', "🧠", "📢", "📝", "💡", "🚩", self.default_admonition_icon)
+        icon_map = dict(zip(types, icons))
+
+        # Update/add custom admonition icons from conf.py
+        if self.admonition_icons:
+            if isinstance(self.admonition_icons, Dict):
+                icon_map.update(self.admonition_icons)
+
+        return icon_map
+
+    @property
+    def admonition_template(self):
+        if self.raw_directive is True:
+            return r'''
+.. raw:: html
+
+   <table>
+       <tr align="left">
+           <th>
+
+{icon} {title}
+
+.. raw:: html
+
+   </th>
+   <tr><td>
+
+{text}
+
+.. raw:: html
+
+   </td></tr>
+   </table>
+'''
+        else:
+            return r'''
+.. csv-table::
+   :header: {icon} {title}
+
+   "\1"
+'''
+
+
+def _map_entry():
+    return copy.deepcopy(READMEConfig.REFERENCE_MAPPING)
