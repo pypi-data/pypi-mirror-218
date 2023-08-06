79e91350bd6643df365925d302a7e4c452deeaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:20 +0000

    Add package pynsm/0.9.0/pynsm-0.9.0-py3-none-any.whl

---
+++ packages/pynsm/pynsm-0.9.0-py3-none-any.whl/pynsm/arch/__init__.py
@@ -0,0 +1,12 @@
+from .base import IterationModule, IterationLossModule
+
+from .similarity import SimilarityMatching, MultiSimilarityMatching
+from .similarity import SupervisedSimilarityMatching
+
+__all__ = [
+    "IterationModule",
+    "IterationLossModule",
+    "SimilarityMatching",
+    "MultiSimilarityMatching",
+    "SupervisedSimilarityMatching",
+]
---
+++ packages/pynsm/pynsm-0.9.0-py3-none-any.whl/pynsm/util/__init__.py
@@ -0,0 +1,3 @@
+from .validation import extract_embeddings
+
+__all__ = ["extract_embeddings"]
