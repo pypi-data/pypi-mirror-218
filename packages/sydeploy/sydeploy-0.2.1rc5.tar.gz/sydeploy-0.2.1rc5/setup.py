e32e4179a5d978a9319110c92bcda689de097602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:36 +0000

    Add package sydeploy/0.2.1rc5/sydeploy-0.2.1rc5.tar.gz

---
+++ packages/sydeploy/sydeploy-0.2.1rc5.tar.gz/sydeploy-0.2.1rc5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+setup(
+    name='sydeploy',
+    version='0.2.1rc5',
+    license="LICENSE",
+    packages=find_packages(),
+    package_data={
+        "syd": ["sydcommands/*", "templates/*", "utils/*"]
+    },
+    entry_points={
+        'console_scripts': ['syd=syd.main:run']
+    },
+    install_requires=[str(ir.requirement) for ir in parse_requirements("requirements.txt", session='build')]
+)
