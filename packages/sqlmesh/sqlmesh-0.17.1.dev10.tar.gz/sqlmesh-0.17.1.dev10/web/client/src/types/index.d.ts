f63da128cedaf0b7275be8e39b79eade607e9f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:33 +0000

    Add package sqlmesh/0.17.1.dev10/sqlmesh-0.17.1.dev10.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.17.1.dev10.tar.gz/sqlmesh-0.17.1.dev10/web/client/src/types/index.d.ts
@@ -0,0 +1,5 @@
+type Subset<T, S extends T> = S
+type Path = string
+type ID = string | number
+type KeyOf<T> = T[keyof T]
+type Optional<T> = T | undefined
---
+++ packages/sqlmesh/sqlmesh-0.17.1.dev10.tar.gz/sqlmesh-0.17.1.dev10/web/client/src/workers/sqlglot/worker.ts
@@ -0,0 +1,69 @@
+declare function importScripts(...urls: string[]): void
+
+importScripts('https://cdn.jsdelivr.net/pyodide/v0.23.2/full/pyodide.js')
+
+const scope = self as any
+
+async function loadPyodideAndPackages(): Promise<any[]> {
+  scope.pyodide = await scope.loadPyodide()
+
+  await scope.pyodide.loadPackage('micropip')
+
+  const micropip = scope.pyodide.pyimport('micropip')
+
+  await micropip.install('sqlglot')
+
+  const file = await (
+    await fetch(new URL('./sqlglot.py', import.meta.url))
+  ).text()
+
+  scope.postMessage({ topic: 'init' })
+
+  return Array.from(scope.pyodide.runPython(file))
+}
+
+const pyodideReadyPromise = loadPyodideAndPackages()
+
+scope.onmessage = async (e: MessageEvent) => {
+  const [parse, get_dialect, dialects] = await pyodideReadyPromise
+
+  if (e.data.topic === 'validate') {
+    let payload
+
+    try {
+      const parsed = JSON.parse(parse(e.data.payload)).filter(Boolean)
+      payload = Boolean(parsed) && parsed.length > 0
+    } catch (error) {
+      payload = false
+    }
+
+    scope.postMessage({
+      topic: 'validate',
+      payload,
+    })
+  }
+
+  if (e.data.topic === 'dialect') {
+    const { keywords, types }: { keywords: string; types: string } = JSON.parse(
+      get_dialect(e.data.payload),
+    )
+
+    scope.postMessage({
+      topic: 'dialect',
+      payload: {
+        types: types.toLowerCase(),
+        keywords: keywords.toLowerCase(),
+      },
+    })
+  }
+
+  if (e.data.topic === 'dialects') {
+    scope.postMessage({
+      topic: 'dialects',
+      payload: {
+        dialects: JSON.parse(dialects),
+        dialect: 'mysql',
+      },
+    })
+  }
+}
