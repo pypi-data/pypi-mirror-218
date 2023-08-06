83b805ad38afb45b63248680a67fc3e34e219955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package tspng/0.0.1/tspng-0.0.1-py3-none-any.whl

---
+++ packages/tspng/tspng-0.0.1-py3-none-any.whl/tspng/extraction.py
@@ -0,0 +1,15 @@
+#import statements
+from PIL import Image
+from tspng import MIME_TYPE
+
+import json
+import os
+
+def extract(path,mime_type=MIME_TYPE):
+    #open
+    abs_path=os.path.abspath(path)
+    im=Image.open(abs_path)
+    meta=im.text
+    #load
+    dict=json.loads(meta[mime_type])
+    return dict
