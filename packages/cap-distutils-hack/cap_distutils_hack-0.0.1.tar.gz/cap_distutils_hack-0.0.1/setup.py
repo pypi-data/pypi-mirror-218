cbb22b17759842d08e25bf1dcb449d73aebb40b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:02 +0000

    Add package cap-distutils-hack/0.0.1/cap_distutils_hack-0.0.1.tar.gz

---
+++ packages/cap-distutils-hack/cap_distutils_hack-0.0.1.tar.gz/cap_distutils_hack-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="cap_distutils_hack",
+    version="0.0.1",
+    description="None",
+    # package_dir={"": "_distutils_hack"},
+    packages=['cap_distutils_hack'],
+    long_description='Long long',
+    long_description_content_type="text/markdown",
+    url="https://github.com/DevCapitalizer/cap_distutils_hack",
+    author="DDjango",
+    author_email="ddjango.786@gmail.com",
+    license="MIT",
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Operating System :: OS Independent",
+    # ],
+    python_requires=">=3.9",
+)
