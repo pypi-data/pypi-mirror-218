85e950147418fca5301d61f42b686c05eb1e94b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:51 +0000

    Add package edubot/0.7.3/edubot-0.7.3-py3-none-any.whl

---
+++ packages/edubot/edubot-0.7.3-py3-none-any.whl/edubot/__init__.py
@@ -0,0 +1,28 @@
+"""
+Read the config into the global scope when the library is imported.
+"""
+from configparser import ConfigParser
+from os import environ, path
+
+
+def _read_cfg() -> ConfigParser:
+    try:
+        config_fp = environ["EDUBOT_CONFIG"]
+    except KeyError:
+        raise EnvironmentError("EDUBOT_CONFIG environment variable not set.")
+
+    config = ConfigParser()
+
+    if not config.read(config_fp):
+        if path.exists(config_fp):
+            raise PermissionError(f"Cannot read {config_fp}.")
+        raise FileNotFoundError(f"File {config_fp} doesn't exist.")
+    return config
+
+
+CONFIG = _read_cfg()
+
+OPENAI_KEY: str | None = CONFIG.get("edubot", "openai_key", fallback=None)
+DREAMSTUDIO_KEY: str | None = CONFIG.get("edubot", "dreamstudio_key", fallback=None)
+REPLICATE_KEY: str | None = CONFIG.get("edubot", "replicate_key", fallback=None)
+DATABASE: str = CONFIG.get("edubot", "database")
---
+++ packages/edubot/edubot-0.7.3-py3-none-any.whl/edubot/types.py
@@ -0,0 +1,38 @@
+"""
+Custom types
+"""
+from datetime import datetime
+from typing import TypedDict
+
+import PIL.Image
+
+
+class MessageInfo(TypedDict):
+    """
+    Represents a message in a thread.
+    """
+
+    username: str
+    message: str
+    time: datetime
+
+
+class ImageInfo(TypedDict):
+    """
+    Represents an image in a thread.
+
+    This class is only used for processing images to text. Only the AI generated caption is stored in the database.
+    """
+
+    username: str
+    image: PIL.Image.Image
+    time: datetime
+
+
+class CompletionInfo(TypedDict):
+    """
+    Represents a bot response to a message.
+    """
+
+    message: str
+    time: datetime
