370109fc59b1f0e6499532d858c128920deac64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:19 +0000

    Add package code-to-text/0.2/code_to_text-0.2.tar.gz

---
+++ packages/code-to-text/code_to_text-0.2.tar.gz/code_to_text-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="code_to_text",
+    version="0.2",
+    packages=find_packages(include=['code_to_text', 'code_to_text.*']),
+    description="Extracts and combines all code from a project into a markdown or txt file.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="David van der Byl",
+    author_email="your.email@example.com",
+    url="https://github.com/David-vanderByl/code_to_text.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pygments',
+    ],
+    entry_points={
+        'console_scripts': [
+            'all_code=code_to_text.main:main',
+        ],
+    },
+)
