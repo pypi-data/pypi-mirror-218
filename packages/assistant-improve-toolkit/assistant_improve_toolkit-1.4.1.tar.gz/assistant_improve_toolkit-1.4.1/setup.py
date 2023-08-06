0599041fc702ffb5d02fe4259558bfa7b67f90f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:53 +0000

    Add package assistant-improve-toolkit/1.4.1/assistant_improve_toolkit-1.4.1.tar.gz

---
+++ packages/assistant-improve-toolkit/assistant_improve_toolkit-1.4.1.tar.gz/assistant_improve_toolkit-1.4.1/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf8 -*-
+#
+# This file were created by Python Boilerplate. Use Python Boilerplate to start
+# simple, usable and best-practices compliant Python projects.
+#
+# Learn more about it at: http://github.com/fabiommendes/python-boilerplate/
+#
+
+import setuptools
+from os import path
+
+__version__ = '1.4.0'
+
+# read contents of README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as file:
+    readme_file = file.read()
+
+setuptools.setup(
+    # Basic info
+    name='assistant_improve_toolkit',
+    author='IBM Watson',
+    author_email='watdevex@us.ibm.com',
+    maintainer='Zhe Zhang',
+    maintainer_email='zhangzhe@us.ibm.com',
+    url='https://github.com/watson-developer-cloud/assistant-improve-recommendations-notebook',
+    description='Assistant Improve Toolkit',
+    license='Apache 2.0',
+    long_description=readme_file,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    # Packages and depencies
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    install_requires=[
+        'pandas==1.2.1',
+        'bokeh==3.2.0',
+        'tqdm==4.65.0',
+        'scikit-learn>=0.21.3',
+        'matplotlib==3.2.1',
+        'XlsxWriter==1.2.8',
+        'ibm-watson==7.0.0',
+        'numpy==1.23.5',
+        'requests==2.29.0'
+    ],
+
+    zip_safe=False,
+    platforms='any',
+)
