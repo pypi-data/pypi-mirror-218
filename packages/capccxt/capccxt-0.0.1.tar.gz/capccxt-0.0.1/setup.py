dc0c6d9be439bcba8b8efd05e94b2457770fc161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:52 +0000

    Add package capccxt/0.0.1/capccxt-0.0.1.tar.gz

---
+++ packages/capccxt/capccxt-0.0.1.tar.gz/capccxt-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="capccxt",
+    version="0.0.1",
+    description="None",
+    # package_dir={"": "_distutils_hack"},
+    packages=['capccxt'],
+    long_description='long long',
+    # long_description_content_type="text/markdown",
+    url="https://github.com/DevCapitalizer/capccxt",
+    author="DDjango",
+    author_email="ddjango.786@gmail.com",
+    install_requires=[],
+    # license="MIT",
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Operating System :: OS Independent",
+    # ],
+    python_requires=">=3.9",
+)
