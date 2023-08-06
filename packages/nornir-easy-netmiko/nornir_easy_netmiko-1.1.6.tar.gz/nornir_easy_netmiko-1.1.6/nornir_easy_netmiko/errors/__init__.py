688b386d6be02c472edc8511404242c97688d92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:53 +0000

    Add package nornir-easy-netmiko/1.1.6/nornir_easy_netmiko-1.1.6.tar.gz

---
+++ packages/nornir-easy-netmiko/nornir_easy_netmiko-1.1.6.tar.gz/nornir_easy_netmiko-1.1.6/nornir_easy_netmiko/errors/__init__.py
@@ -0,0 +1,27 @@
+import logging
+from netmiko.exceptions import NetMikoAuthenticationException
+from jinja2.exceptions import UndefinedError
+from nornir.core.task import Result
+from functools import wraps
+
+logger = logging.getLogger("nornir")
+
+def error_handle(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        task = args[0]
+        host = task.host
+        base_string = f"{host.name}({host.hostname}:{host.port})"
+        try:
+            result = func(*args, **kwargs)
+            logger.info(f"{base_string} excute <{task}> success.")
+            return result
+        except NetMikoAuthenticationException as e:
+            logger.warning(f"{base_string} user/password invaild.")
+            return Result(host, exception=e, failed=True)
+        except UndefinedError as e:
+            logger.warning(f"{base_string} {e}")
+            return Result(host, exception=e, failed=True)
+    return wrapper
+            
+                
