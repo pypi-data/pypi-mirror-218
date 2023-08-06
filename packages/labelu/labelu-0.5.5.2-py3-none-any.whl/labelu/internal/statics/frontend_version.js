e6d22a61ff4f9ae1668882cca626901f204e0079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:55 +0000

    Add package labelu/0.5.5.2/labelu-0.5.5.2-py3-none-any.whl

---
+++ packages/labelu/labelu-0.5.5.2-py3-none-any.whl/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,10 @@
+(function () {
+  window.__frontend = {
+    version: '2.0.3',
+    deps: {
+      '@label-u/annotation': '2.0.0',
+      '@label-u/components': '2.0.1',
+      '@label-u/formatter': '1.0.1',
+    },
+  };
+})();
