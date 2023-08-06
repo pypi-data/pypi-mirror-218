889cf69a37d94ede16e169585022a1bd761d9f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:42 +0000

    Add package devopipe/0.1.5/devopipe-0.1.5.tar.gz

---
+++ packages/devopipe/devopipe-0.1.5.tar.gz/devopipe-0.1.5/devopipe/src/templates/IaC/aws/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  type    = string
+  default = {{ cluster_name }}
+}
+
+variable "state_region" {
+  type    = string
+  default = {{ state_region }}
+}
+
+# cluster_name = "demo"
