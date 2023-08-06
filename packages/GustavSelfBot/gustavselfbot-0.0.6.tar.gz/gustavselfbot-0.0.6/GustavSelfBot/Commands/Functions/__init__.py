fe76707f049e16c1d42c93b52ca70db7fb9d8ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:25 +0000

    Add package GustavSelfBot/0.0.6/gustavselfbot-0.0.6.tar.gz

---
+++ packages/GustavSelfBot/gustavselfbot-0.0.6.tar.gz/gustavselfbot-0.0.6/GustavSelfBot/Commands/Functions/__init__.py
@@ -0,0 +1,5 @@
+from GustavSelfBot import log
+from GustavSelfBot.Commands.Functions.__ping__ import func_ping
+from GustavSelfBot.Commands.Functions.__quote__ import func_quote
+
+log.info("Event-Functions module loaded!")
