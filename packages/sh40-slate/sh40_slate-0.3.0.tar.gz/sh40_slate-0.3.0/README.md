a95ff4f6b65739ecc127cbafbcca78f2cbeebf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package sh40-slate/0.3.0/sh40_slate-0.3.0.tar.gz

---
+++ packages/sh40-slate/sh40_slate-0.3.0.tar.gz/sh40_slate-0.3.0/README.md
@@ -0,0 +1,21 @@
+![slate](https://singlecolorimage.com/get/717E8D/1600x200)
+
+## slate
+
+A lightweight library to interface with the terminal.
+
+```
+pip install sh40-slate
+```
+
+This README is a work in progress.
+
+![rule](https://singlecolorimage.com/get/717E8D/1600x5)
+
+<!--
+### Purpose
+
+![rule](https://singlecolorimage.com/get/717E8D/1600x5)
+
+### Examples
+-->
---
+++ packages/sh40-slate/sh40_slate-0.3.0.tar.gz/sh40_slate-0.3.0/slate/event.py
@@ -0,0 +1,72 @@
+"""The basic Event class used by the rest of the library."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any, Callable, Union
+
+EventCallback = Union[Callable[[], Any], Callable[[Any], Any]]
+
+__all__ = [
+    "Event",
+    "EventCallback",
+    "CallbackError",
+]
+
+
+class CallbackError(Exception):
+    """Raised when something went wrong with an event's callback"""
+
+
+@dataclass
+class Event:
+    """An emittable event.
+
+    Construct an event and store it in a variable to have a reference for it. Then, you
+    can `+=` callback handlers onto it, and call the event object with data to notify
+    all of the callbacks.
+    """
+
+    name: str
+
+    _listeners: list[EventCallback] = field(default_factory=list)
+
+    def __bool__(self) -> bool:
+        """Returns whether this event has any listeners."""
+
+        return len(self._listeners) > 0
+
+    def __iadd__(self, callback: EventCallback) -> Event:
+        if not callable(callback):
+            raise ValueError(f"Invalid type for callback: {callback}")
+
+        self._listeners.append(callback)
+
+        return self
+
+    def __call__(self, data: Any | None = None) -> int:
+        """Emits the event to all listeners.
+
+        Args:
+            data: The content of the event.
+
+        Returns:
+            The amount of listeners that were notified.
+        """
+
+        for callback in self._listeners:
+            try:
+                if data is None:
+                    callback()  # type: ignore
+                else:
+                    callback(data)
+
+            except Exception as exc:
+                raise CallbackError(f"Error executing callback {callback!r}.") from exc
+
+        return len(self._listeners)
+
+    def clear(self) -> None:
+        """Removes all listeners from th event."""
+
+        self._listeners.clear()
