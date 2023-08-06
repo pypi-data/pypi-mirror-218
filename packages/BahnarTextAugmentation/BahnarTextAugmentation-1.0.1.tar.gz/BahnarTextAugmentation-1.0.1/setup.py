7cc0067fa2a8f0c6a7f5d1c90c2eeafe807c8e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:40 +0000

    Add package BahnarTextAugmentation/1.0.1/BahnarTextAugmentation-1.0.1.tar.gz

---
+++ packages/BahnarTextAugmentation/BahnarTextAugmentation-1.0.1.tar.gz/BahnarTextAugmentation-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='BahnarTextAugmentation',
+    version='1.0.1',
+    license='Apache License 2.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    description='Bahnar Text Augmentation',
+    long_description=read('README.md'),
+    install_requires=['pyvi', 
+                      'simalign', 
+                      'sentencepiece', 
+                      'transformers', 
+                      'sacremoses', 
+                      'nltk', 
+                      'numpy>=1.22.0', 
+                      'pandas',
+                      'gensim>=4.3.0',
+                      'openpyxl'
+                      ],
+)
