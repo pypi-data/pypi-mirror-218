3ba0eaa3854321d4512aa91ca1b79eb95622821a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:58 +0000

    Add package pwforecast/1.1.1/pwforecast-1.1.1.tar.gz

---
+++ packages/pwforecast/pwforecast-1.1.1.tar.gz/pwforecast-1.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+# read the contents of README file
+readme = (this_directory / 'README.md').read_text()
+
+setuptools.setup(
+    name='pwforecast',
+    version='1.1.1',
+    author='Tim Hawker',
+    description='A Python module to charge/discharge Powerwall based on solar forecast and peak/off peak tariffs.',
+    url='https://github.com/timhawker/pwforecast',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    packages=['pwforecast'],
+    install_requires=['tzlocal', 'requests', 'python-dateutil', 'TeslaPy']
+)
